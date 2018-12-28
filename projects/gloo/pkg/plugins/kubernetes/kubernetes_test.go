a281239f6f5a1431957b37e25502556d51c77657
Rick Ducott <rick.ducott@solo.io>
2018-12-28 10:41:21 -0500

    Fix test

--- projects/gloo/pkg/plugins/kubernetes/kubernetes_test.go
+++ projects/gloo/pkg/plugins/kubernetes/kubernetes_test.go
@@ -130,7 +130,7 @@ var _ = Describe("Kubernetes", func() {
 			case <-time.After(time.Second * 2):
 				Fail("no upstreams detected after 2s")
 			case upstreamList := <-upstreams:
-				Expect(upstreamList).To(HaveLen(2))
+				Expect(upstreamList).To(HaveLen(4)) // two pods, two ports per pod
 			case err, ok := <-errs:
 				if !ok {
 					return
