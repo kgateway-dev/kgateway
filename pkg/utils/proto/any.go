4d5ae82157eeb61d9417028a0bf4066fcf0e454f
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-01-02 16:22:10 -0500

    error checks

--- pkg/utils/proto/any.go
+++ pkg/utils/proto/any.go
@@ -1,20 +1,25 @@
 package proto
 
 import (
+	"fmt"
+
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/types"
 )
 
-func GetMessage(protos map[string]*types.Any, name string) proto.Message {
+func GetMessage(protos map[string]*types.Any, name string) (proto.Message, error) {
 	if any, ok := protos[name]; ok {
 		return getProto(any)
 	}
 
-	return nil
+	return nil, fmt.Errorf("message not found")
 }
 
-func getProto(p *types.Any) proto.Message {
+func getProto(p *types.Any) (proto.Message, error) {
 	var x types.DynamicAny
-	types.UnmarshalAny(p, &x)
-	return x.Message
+	err := types.UnmarshalAny(p, &x)
+	if err != nil {
+		return nil, err
+	}
+	return x.Message, nil
 }
