f506f733ca7c0eb8f0a8fe7cbb66e495dc4201f9
Scott Weiss <sdw35@cornell.edu>
2019-01-24 23:36:55 -0500

    add e2e tests for remove and sort
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/cli/pkg/cmd/remove/route_test.go
+++ projects/gloo/cli/pkg/cmd/remove/route_test.go
@@ -1,4 +1,4 @@
-package route_test
+package remove_test
 
 import (
 	"context"
@@ -23,40 +23,20 @@ var _ = Describe("Sort", func() {
 		helpers.UseMemoryClients()
 	})
 
-	It("should sort the routes on a virtualservice", func() {
-		sortedRoutes := func() []*v1.Route {
-			var routes []*v1.Route
-			for _, path := range []int{routehelpers.ExactPath, routehelpers.RegexPath, routehelpers.PrefixPath} {
-				for _, length := range []int{9, 6, 3} {
-					routes = append(routes, routehelpers.MakeRoute(path, length))
-				}
-			}
-			return routes
-		}
-
-		unsortedRoutes := func() []*v1.Route {
-			var routes []*v1.Route
-			for _, length := range []int{9, 6, 3} {
-				for _, path := range []int{routehelpers.ExactPath, routehelpers.RegexPath, routehelpers.PrefixPath} {
-					routes = append(routes, routehelpers.MakeRoute(path, length))
-				}
-			}
-			return routes
-		}
-
+	It("should remove a route from a virtualservice", func() {
 		vs, err := helpers.MustVirtualServiceClient().Write(&gatewayv1.VirtualService{
 			Metadata: core.Metadata{Namespace: "gloo-system", Name: "tacos"},
 			VirtualHost: &v1.VirtualHost{
-				Routes: unsortedRoutes(),
+				Routes: []*v1.Route{routehelpers.MakeRoute(routehelpers.RegexPath, 5)},
 			},
 		}, clients.WriteOpts{Ctx: context.TODO()})
 		Expect(err).NotTo(HaveOccurred())
 
-		err = testutils.Glooctl(fmt.Sprintf("route sort --name %v --namespace %v", vs.Metadata.Name, vs.Metadata.Namespace))
+		err = testutils.Glooctl(fmt.Sprintf("remove route --name %v --namespace %v -x 0", vs.Metadata.Name, vs.Metadata.Namespace))
 		Expect(err).NotTo(HaveOccurred())
 
 		vs, err = helpers.MustVirtualServiceClient().Read(vs.Metadata.Namespace, vs.Metadata.Name, clients.ReadOpts{})
 		Expect(err).NotTo(HaveOccurred())
-		Expect(vs.VirtualHost.Routes).To(Equal(sortedRoutes()))
+		Expect(vs.VirtualHost.Routes).To(HaveLen(0))
 	})
 })
