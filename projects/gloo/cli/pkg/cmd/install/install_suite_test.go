bced77d2f74b216f924594a2ba4a415543eae041
Scott Weiss <sdw35@cornell.edu>
2019-01-30 12:00:15 -0500

    disable cli tests in ci
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/cli/pkg/cmd/install/install_suite_test.go
+++ projects/gloo/cli/pkg/cmd/install/install_suite_test.go
@@ -1,7 +1,10 @@
 package install_test
 
 import (
+	"os"
 	"testing"
+
+	"github.com/solo-io/solo-kit/pkg/utils/log"
 
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/testutils"
 	"github.com/solo-io/gloo/test/helpers"
@@ -11,6 +14,11 @@ import (
 )
 
 func TestInstall(t *testing.T) {
+	if os.Getenv("RUN_KUBE2E_TESTS") != "1" {
+		log.Warnf("This test builds and deploys images to dockerhub and kubernetes, " +
+			"and is disabled by default. To enable, set RUN_KUBE2E_TESTS=1 in your env.")
+		return
+	}
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Install Suite")
 }
