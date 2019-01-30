0a04f019a6e7631894b7a26dbadc864901a760d4
Scott Weiss <sdw35@cornell.edu>
2019-01-30 11:34:14 -0500

    generate helm values in beforesuite in test
    Signed-off-by: Scott Weiss <sdw35@cornell.edu>

--- projects/gloo/cli/pkg/cmd/install/install_suite_test.go
+++ projects/gloo/cli/pkg/cmd/install/install_suite_test.go
@@ -3,6 +3,9 @@ package install_test
 import (
 	"testing"
 
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/testutils"
+	"github.com/solo-io/gloo/test/helpers"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +14,8 @@ func TestInstall(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Install Suite")
 }
+
+var _ = BeforeSuite(func() {
+	err := testutils.Make(helpers.GlooDir(), "prepare-helm")
+	Expect(err).NotTo(HaveOccurred())
+})
