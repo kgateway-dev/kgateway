a1b91e8f32e992487a567e09717175a1ffe0434b
Rick Ducott <rick.ducott@solo.io>
2019-02-26 21:53:11 -0500

    Changelog works

--- changelog.go
+++ changelog.go
@@ -1,38 +1,17 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"github.com/solo-io/go-utils/changelogutils"
 	"github.com/solo-io/go-utils/docsutils"
-	"github.com/solo-io/go-utils/githubutils"
-	"github.com/spf13/afero"
+	"github.com/solo-io/solo-kit/pkg/utils/log"
 )
 
 func main() {
-	client, err := githubutils.GetClient(context.TODO())
-	if err != nil {
-		panic(err)
-	}
-	fs := afero.NewOsFs()
-	latestTag, err := githubutils.FindLatestReleaseTag(context.TODO(), client, "solo-io", "gloo")
-	if err != nil {
-		panic(err)
-	}
-	proposedTag, err := changelogutils.GetProposedTag(fs, latestTag, "")
-	if err != nil {
-		panic(err)
-	}
-	changelog, err := changelogutils.ComputeChangelog(fs, latestTag, proposedTag, "")
-	if err != nil {
-		panic(err)
-	}
-	markdown := changelogutils.GenerateChangelogMarkdown(changelog)
-	fmt.Printf(markdown)
-
-	docsutils.CreateDocsPR("solo-io", "gloo", "v0.8.2", "gloo",
+	err := docsutils.CreateDocsPR("solo-io", "gloo", "gloo",
 		"docs/v1/github.com/solo-io/gloo",
 		"docs/v1/github.com/solo-io/solo-kit",
 		"docs/v1/gogoproto",
 		"docs/v1/google")
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
 }
