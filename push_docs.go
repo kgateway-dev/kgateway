e5038b047aa64d971281701be9e8abb33813c2b6
Rick Ducott <rick.ducott@solo.io>
2019-02-28 15:16:51 -0500

    Update lock

--- push_docs.go
+++ push_docs.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/solo-io/go-utils/docsutils"
 	"github.com/solo-io/solo-kit/pkg/utils/log"
+	"os"
 )
 
 func main() {
+	tag, present := os.LookupEnv("TAGGED_VERSION")
+	if !present {
+		fmt.Printf("TAGGED_VERSION not found in environment, skipping docs PR.")
+		os.Exit(0)
+	}
 	// assumes changelog goes into solo-docs/gloo/docs/changelog/gloo-changelog
-	err := docsutils.CreateDocsPRSimple("solo-io", "gloo",
+	err := docsutils.CreateDocsPRSimple("solo-io", "gloo", tag,
 		"docs/v1/github.com/solo-io/gloo",
 		"docs/v1/github.com/solo-io/solo-kit",
 		"docs/v1/gogoproto",
