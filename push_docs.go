f045ab640bd9ad9d59a5a18f6549119a3b28a862
Rick Ducott <rick.ducott@solo.io>
2019-03-01 00:45:30 -0500

    Update

--- push_docs.go
+++ push_docs.go
@@ -1,31 +1,16 @@
 package main
 
 import (
-	"fmt"
 	"github.com/solo-io/go-utils/docsutils"
-	"github.com/solo-io/go-utils/versionutils"
-	"github.com/solo-io/solo-kit/pkg/utils/log"
-	"os"
 )
 
 func main() {
-	tag, present := os.LookupEnv("TAGGED_VERSION")
-	if !present || tag == "" {
-		fmt.Printf("TAGGED_VERSION not found in environment, skipping docs PR.\n", tag)
-		os.Exit(0)
-	}
-	_, err := versionutils.ParseVersion(tag)
-	if err != nil {
-		fmt.Printf("TAGGED_VERSION %s is not a valid semver version, skipping docs PR.\n", tag)
-		os.Exit(0)
-	}
-	// assumes changelog goes into solo-docs/gloo/docs/changelog/gloo-changelog
+	// tag set by utility
 	spec := docsutils.DocsPRSpec{
 		Owner: "solo-io",
 		Repo: "gloo",
 		Product: "gloo",
 		Project: "gloo",
-		Tag: tag,
 		ApiPaths: []string {
 			"docs/v1/github.com/solo-io/gloo",
 			"docs/v1/github.com/solo-io/solo-kit",
@@ -33,9 +18,5 @@ func main() {
 			"docs/v1/google",
 		},
 	}
-
-	err = docsutils.CreateDocsPRFromSpec(&spec)
-	if err != nil {
-		log.Fatalf(err.Error())
-	}
+	docsutils.PushDocsCli(&spec)
 }
