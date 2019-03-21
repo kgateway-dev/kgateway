f36faa0c5cf6bb328cd0078051c41443763ddcaa
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-03-21 18:30:53 -0400

    allow specificying options to the http connection manager

--- projects/gloo/pkg/translator/utils.go
+++ projects/gloo/pkg/translator/utils.go
@@ -1,7 +1,51 @@
 package translator
 
-import core_solo_io1 "github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+import (
+	envoylistener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
+	"github.com/gogo/protobuf/proto"
+	"github.com/gogo/protobuf/types"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
+)
 
-func UpstreamToClusterName(upstream core_solo_io1.ResourceRef) string {
+func UpstreamToClusterName(upstream core.ResourceRef) string {
 	return upstream.Key()
+}
+
+func NewFilterWithConfig(name string, config proto.Message) (envoylistener.Filter, error) {
+
+	s := envoylistener.Filter{
+		Name: name,
+	}
+
+	if config != nil {
+		marshalledConf, err := protoutils.MarshalStruct(config)
+		if err != nil {
+			// this should NEVER HAPPEN!
+			return envoylistener.Filter{}, err
+		}
+
+		s.ConfigType = &envoylistener.Filter_Config{
+			Config: marshalledConf,
+		}
+	}
+
+	return s, nil
+}
+
+func ParseConfig(c configObject, config proto.Message) error {
+	any := c.GetTypedConfig()
+	if any != nil {
+		return types.UnmarshalAny(any, config)
+	}
+	structt := c.GetConfig()
+	if structt != nil {
+		return protoutils.UnmarshalStruct(structt, config)
+	}
+	return nil
+}
+
+type configObject interface {
+	GetConfig() *types.Struct
+	GetTypedConfig() *types.Any
 }
