baef2d7cf24f0f3904a2c245e6ced7888088c2fa
EItanya <eitan.yarmush@solo.io>
2019-03-25 13:15:38 -0400

    fixed error in consul test

--- projects/gloo/pkg/plugins/consul/eds.go
+++ projects/gloo/pkg/plugins/consul/eds.go
@@ -2,6 +2,7 @@ package consul
 
 import (
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/discovery"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/errors"
 )
@@ -9,6 +10,8 @@ import (
 type endpointController struct {
 }
 
-func (p *plugin) WatchEndpoints(writeNamespace string, upstreamsToTrack v1.UpstreamList, opts clients.WatchOpts) (<-chan v1.EndpointList, <-chan error, error) {
+var _ discovery.DiscoveryPlugin = new(plugin)
+
+func (p *plugin) WatchEndpoints(watchNamespaces []string, writeNamespace string, upstreamsToTrack v1.UpstreamList, opts clients.WatchOpts) (<-chan v1.EndpointList, <-chan error, error) {
 	return nil, nil, errors.Errorf("not implemented")
 }
