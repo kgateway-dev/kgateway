3ccc264c0ff4c40e5921cb6bf31bb5ba2bc726c5
EItanya <eitan.yarmush@solo.io>
2019-03-25 12:31:27 -0400

    fmt

--- projects/gloo/pkg/discovery/run.go
+++ projects/gloo/pkg/discovery/run.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/utils/contextutils"
 )
