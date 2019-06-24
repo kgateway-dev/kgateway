08151311cbe8c95130f945f9b6dfd55d39deb9d5
Mitch Kelley <mitch.kelley@solo.io>
2019-06-24 11:52:03 -0400

    update aws sdk

---
+++ projects/gloo/pkg/plugins/aws/ec2/plugin.go
@@ -0,0 +1,40 @@
+package ec2
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+)
+
+func tmpTest() {
+	svc := ec2.New(session.New())
+	input := &ec2.DescribeInstancesInput{
+		InstanceIds: []*string{
+			aws.String("i-1234567890abcdef0"),
+		},
+		Filters: []*ec2.Filter{
+			{
+				Name:   nil,
+				Values: nil,
+			},
+		},
+	}
+	result, err := svc.DescribeInstances(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	fmt.Println(result)
+}
