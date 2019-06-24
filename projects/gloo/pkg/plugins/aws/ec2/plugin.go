63335333356b81757d43270ba3f8a0a78865c740
Mitch Kelley <mitch.kelley@solo.io>
2019-06-24 14:47:33 -0400

    extract gloo aws utils

--- projects/gloo/pkg/plugins/aws/ec2/plugin.go
+++ projects/gloo/pkg/plugins/aws/ec2/plugin.go
@@ -1,7 +1,13 @@
-package ec2
+package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	aws2 "github.com/solo-io/gloo/projects/gloo/pkg/utils/aws"
+	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"go.uber.org/zap"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/awserr"
@@ -9,16 +15,30 @@ import (
 	"github.com/aws/aws-sdk-go/service/ec2"
 )
 
-func tmpTest() {
-	svc := ec2.New(session.New())
+// TEMP - TODO REMOVE
+func main() {
+	ctx := context.Background()
+	err := run(core.ResourceRef{}, nil)
+	if err != nil {
+		contextutils.LoggerFrom(ctx).Fatalw("failure while running", zap.Error(err))
+	}
+}
+
+// TODO
+func run(secretRef core.ResourceRef, secrets v1.SecretList) error {
+	region := "us-east-1"
+	sess, err := aws2.GetAwsSession(secretRef, secrets, &aws.Config{Region: &region})
+	if err != nil {
+		return err
+	}
+	svc := ec2.New(sess)
+	tag := tagFilterName("Name")
+	val := tagFilterValue("openshift-master")
 	input := &ec2.DescribeInstancesInput{
-		InstanceIds: []*string{
-			aws.String("i-1234567890abcdef0"),
-		},
 		Filters: []*ec2.Filter{
 			{
-				Name:   nil,
-				Values: nil,
+				Name:   tag,
+				Values: val,
 			},
 		},
 	}
@@ -34,8 +54,23 @@ func tmpTest() {
 			// Message from an error.
 			fmt.Println(err.Error())
 		}
-		return
 	}
 
 	fmt.Println(result)
+	return nil
+}
+
+func tagFilterName(tagName string) *string {
+	str := fmt.Sprintf("tag:%v", tagName)
+	return &str
+}
+
+func tagFilterValue(tagValue string) []*string {
+	return []*string{&tagValue}
+}
+
+func getLocalAwsSession(region string) (*session.Session, error) {
+	return session.NewSession(&aws.Config{
+		Region: &region,
+	})
 }
