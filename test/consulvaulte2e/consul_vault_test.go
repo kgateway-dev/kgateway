89c7f9bd4b9547258f8d777912c3e4276ad62688
Scott Weiss <sdw35@cornell.edu>
2019-07-25 08:41:23 -0400

    remove unused test

--- test/consulvaulte2e/consul_vault_test.go
+++ test/consulvaulte2e/consul_vault_test.go
@@ -3,26 +3,22 @@ package consulvaulte2e_test
 import (
 	"context"
 	"flag"
-	"fmt"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
-	v1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
-	"github.com/solo-io/gloo/test/helpers"
-
 	"github.com/gogo/protobuf/types"
 	consulapi "github.com/hashicorp/consul/api"
 	vaultapi "github.com/hashicorp/vault/api"
+	v1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	gatewaysetup "github.com/solo-io/gloo/projects/gateway/pkg/setup"
 	"github.com/solo-io/gloo/projects/gloo/pkg/bootstrap"
 	"github.com/solo-io/gloo/projects/gloo/pkg/setup"
+	"github.com/solo-io/gloo/test/helpers"
+	"github.com/solo-io/gloo/test/v1helpers"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients/factory"
 	"github.com/solo-io/solo-kit/pkg/utils/protoutils"
-
-	"github.com/solo-io/gloo/test/v1helpers"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
@@ -42,7 +38,6 @@ var _ = Describe("Consul + Vault Configuration Happy Path e2e", func() {
 		consulInstance *services.ConsulInstance
 		vaultInstance  *services.VaultInstance
 		envoyInstance  *services.EnvoyInstance
-		envoyPort      uint32
 		svc1, svc2     *v1helpers.TestUpstream
 		err            error
 		settingsDir    string
@@ -54,21 +49,6 @@ var _ = Describe("Consul + Vault Configuration Happy Path e2e", func() {
 	)
 
 	const writeNamespace = defaults.GlooSystem
-
-	queryService := func() (string, error) {
-		response, err := http.Get(fmt.Sprintf("http://localhost:%d", envoyPort))
-		if err != nil {
-			return "", err
-		}
-		//noinspection GoUnhandledErrorResult
-		defer response.Body.Close()
-
-		body, err := ioutil.ReadAll(response.Body)
-		if err != nil {
-			return "", err
-		}
-		return string(body), nil
-	}
 
 	BeforeEach(func() {
 		ctx, cancel = context.WithCancel(context.Background())
@@ -129,7 +109,6 @@ var _ = Describe("Consul + Vault Configuration Happy Path e2e", func() {
 		}()
 
 		// Start Envoy
-		envoyPort = uint32(defaults.HttpPort)
 		envoyInstance, err = envoyFactory.NewEnvoyInstance()
 		Expect(err).NotTo(HaveOccurred())
 		err = envoyInstance.RunWithRole(writeNamespace+"~"+translator.GatewayProxyName, 9977)
@@ -164,57 +143,7 @@ var _ = Describe("Consul + Vault Configuration Happy Path e2e", func() {
 		cancel()
 	})
 
-	It("can be configured using consul k-v", func() {
-
-		proxyClient, err := gloov1.NewProxyClient(consulResources)
-		Expect(err).NotTo(HaveOccurred())
-
-		_, err = proxyClient.Write(getProxyWithConsulRoute(writeNamespace, envoyPort), clients.WriteOpts{Ctx: ctx})
-		Expect(err).NotTo(HaveOccurred())
-
-		// Wait for proxy to be accepted
-		Eventually(func() bool {
-			proxy, err := proxyClient.Read(writeNamespace, "gateway-proxy", clients.ReadOpts{Ctx: ctx})
-			if err != nil {
-				return false
-			}
-			return proxy.Status.State == core.Status_Accepted
-		}, "10s", "0.2s").Should(BeTrue())
-
-		By("requests are load balanced between the two services")
-		Eventually(func() (<-chan *v1helpers.ReceivedRequest, error) {
-			_, err := queryService()
-			if err != nil {
-				return svc1.C, err
-			}
-			return svc1.C, nil
-		}, "10s", "0.2s").Should(Receive())
-
-		Eventually(func() (<-chan *v1helpers.ReceivedRequest, error) {
-			_, err := queryService()
-			if err != nil {
-				return svc2.C, err
-			}
-			return svc2.C, nil
-		}, "10s", "0.2s").Should(Receive())
-
-		By("update consul service definition")
-		err = consulInstance.RegisterService("my-svc", "my-svc-2", envoyInstance.GlooAddr, []string{"svc", "2"}, svc2.Port)
-		Expect(err).NotTo(HaveOccurred())
-
-		// Wait a bit for the new endpoint information to propagate
-		time.Sleep(3 * time.Second)
-
-		// Service 2 does not match the tags on the route anymore, so we should get only requests from service 1
-		Consistently(func() (<-chan *v1helpers.ReceivedRequest, error) {
-			_, err := queryService()
-			if err != nil {
-				return svc1.C, err
-			}
-			return svc1.C, nil
-		}, "2s", "0.2s").Should(Receive())
-	})
-	It("can read secrets using vault", func() {
+	It("can be configured using consul k-v and read secrets using vault", func() {
 		cert := helpers.Certificate()
 
 		secret := &gloov1.Secret{
@@ -256,49 +185,6 @@ var _ = Describe("Consul + Vault Configuration Happy Path e2e", func() {
 		v1helpers.TestUpstreamReachable(defaults.HttpsPort, svc1, &cert)
 	})
 })
-
-func getProxyWithConsulRoute(ns string, bindPort uint32) *gloov1.Proxy {
-	return &gloov1.Proxy{
-		Metadata: core.Metadata{
-			Name:      "gateway-proxy",
-			Namespace: ns,
-		},
-		Listeners: []*gloov1.Listener{{
-			Name:        "listener",
-			BindAddress: "::",
-			BindPort:    bindPort,
-			ListenerType: &gloov1.Listener_HttpListener{
-				HttpListener: &gloov1.HttpListener{
-					VirtualHosts: []*gloov1.VirtualHost{{
-						Name:    "vh-1",
-						Domains: []string{"*"},
-						Routes: []*gloov1.Route{{
-							Matcher: &gloov1.Matcher{
-								PathSpecifier: &gloov1.Matcher_Prefix{
-									Prefix: "/",
-								},
-							},
-							Action: &gloov1.Route_RouteAction{
-								RouteAction: &gloov1.RouteAction{
-									Destination: &gloov1.RouteAction_Single{
-										Single: &gloov1.Destination{
-											DestinationType: &gloov1.Destination_Consul{
-												Consul: &gloov1.ConsulServiceDestination{
-													ServiceName: "my-svc",
-													Tags:        []string{"svc", "1"},
-												},
-											},
-										},
-									},
-								},
-							},
-						}},
-					}},
-				},
-			},
-		}},
-	}
-}
 
 func makeSslVirtualService(secret core.ResourceRef) *v1.VirtualService {
 	return &v1.VirtualService{
