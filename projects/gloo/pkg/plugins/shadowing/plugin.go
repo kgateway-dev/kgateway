6990d5d39de80e08f1b861891a39b2d0a975dc9b
Mitch Kelley <mitch.kelley@solo.io>
2019-08-01 13:29:51 -0400

    init plugin

---
+++ projects/gloo/pkg/plugins/shadowing/plugin.go
@@ -0,0 +1,69 @@
+package shadowing
+
+import (
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	envoy_type "github.com/envoyproxy/go-control-plane/envoy/type"
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/shadowing"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"github.com/solo-io/gloo/projects/gloo/pkg/translator"
+	"github.com/solo-io/go-utils/errors"
+)
+
+func NewPlugin() *Plugin {
+	return &Plugin{}
+}
+
+var _ plugins.Plugin = new(Plugin)
+var _ plugins.RoutePlugin = new(Plugin)
+
+type Plugin struct {
+}
+
+func (p *Plugin) Init(params plugins.InitParams) error {
+	return nil
+}
+
+func (p *Plugin) ProcessRoute(params plugins.RouteParams, in *v1.Route, out *envoyroute.Route) error {
+	if in.RoutePlugins == nil || in.RoutePlugins.Shadowing == nil {
+		return nil
+	}
+	// the shadow plugin should only be used on routes that are of type envoyroute.Route_Route
+	// (this is because shadowing is not defined on redirect or direct response route actions)
+	if out.Action != nil && out.GetRoute() == nil {
+		return InvalidRouteActionError
+	}
+	shadowSpec := in.RoutePlugins.Shadowing
+	// we have already ensured that the output route action is either nil or of the proper type
+	// if it is nil, we initialize it prior to transforming it
+	outRa := out.GetRoute()
+	if outRa == nil {
+		out.Action = &envoyroute.Route_Route{
+			Route: &envoyroute.RouteAction{},
+		}
+		outRa = out.GetRoute()
+	}
+	return applyShadowSpec(outRa, shadowSpec)
+}
+
+func applyShadowSpec(out *envoyroute.RouteAction, spec *shadowing.RouteShadowing) error {
+	out.RequestMirrorPolicy = &envoyroute.RouteAction_RequestMirrorPolicy{
+		Cluster:         translator.UpstreamToClusterName(*spec.UpstreamRef),
+		RuntimeFraction: getFractionalPercent(spec.Percent),
+	}
+	return nil
+}
+
+func getFractionalPercent(numerator uint32) *envoycore.RuntimeFractionalPercent {
+	return &envoycore.RuntimeFractionalPercent{
+		DefaultValue: &envoy_type.FractionalPercent{
+			Numerator:   numerator,
+			Denominator: envoy_type.FractionalPercent_HUNDRED,
+		},
+	}
+}
+
+var (
+	InvalidRouteActionError = errors.New("cannot use shadowing plugin on non-Route_Route route actions")
+)
---
+++ projects/gloo/pkg/plugins/shadowing/plugin_test.go
@@ -0,0 +1,87 @@
+package shadowing
+
+import (
+	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/shadowing"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+)
+
+var _ = Describe("Plugin", func() {
+
+	It("should error when set on invalid routes", func() {
+		p := NewPlugin()
+
+		upRef := &core.ResourceRef{
+			Name:      "some-upstream",
+			Namespace: "default",
+		}
+		in := &v1.Route{
+			Matcher: nil,
+			Action:  nil,
+			RoutePlugins: &v1.RoutePlugins{
+				Shadowing: &shadowing.RouteShadowing{
+					UpstreamRef: upRef,
+					Percent:     100,
+				},
+			},
+		}
+		out := &envoyroute.Route{}
+		err := p.ProcessRoute(plugins.RouteParams{}, in, out)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(out.GetRoute().RequestMirrorPolicy.RuntimeFraction.DefaultValue.Numerator).To(Equal(uint32(100)))
+		Expect(out.GetRoute().RequestMirrorPolicy.Cluster).To(Equal("some-upstream_default"))
+	})
+
+	It("should handle empty configs", func() {
+		p := NewPlugin()
+		in := &v1.Route{}
+		out := &envoyroute.Route{}
+		err := p.ProcessRoute(plugins.RouteParams{}, in, out)
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	It("should error when set on invalid routes", func() {
+		p := NewPlugin()
+
+		upRef := &core.ResourceRef{
+			Name:      "some-upstream",
+			Namespace: "default",
+		}
+		in := &v1.Route{
+			Matcher: nil,
+			Action:  nil,
+			RoutePlugins: &v1.RoutePlugins{
+				Shadowing: &shadowing.RouteShadowing{
+					UpstreamRef: upRef,
+					Percent:     190,
+				},
+			},
+		}
+		// a redirect route is not a valid target for this plugin
+		out := &envoyroute.Route{
+			Action: &envoyroute.Route_Redirect{
+				Redirect: &envoyroute.RedirectAction{},
+			},
+		}
+		err := p.ProcessRoute(plugins.RouteParams{}, in, out)
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(Equal(InvalidRouteActionError))
+
+		// a direct response route is not a valid target for this plugin
+		out = &envoyroute.Route{
+			Action: &envoyroute.Route_DirectResponse{
+				DirectResponse: &envoyroute.DirectResponseAction{},
+			},
+		}
+		err = p.ProcessRoute(plugins.RouteParams{}, in, out)
+		Expect(err).To(HaveOccurred())
+		Expect(err).To(Equal(InvalidRouteActionError))
+	})
+
+})
