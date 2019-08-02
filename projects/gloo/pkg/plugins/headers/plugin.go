0f7074e842ada4d9ca8fcdb9ac856ffa425f0349
Scott Weiss <sdw35@cornell.edu>
2019-08-02 12:23:27 -0400

    add plugin for header manip

--- projects/gloo/pkg/plugins/headers/plugin.go
+++ projects/gloo/pkg/plugins/headers/plugin.go
@@ -1,123 +1,132 @@
-package basicroute
+package headers
 
 import (
-	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
-	"github.com/gogo/protobuf/types"
+	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/headers"
 
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
-	retries "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/retries"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
 	"github.com/solo-io/solo-kit/pkg/errors"
 )
 
+var (
+	MissingHeaderValueError = errors.Errorf("header section of header value option cannot be nil")
+)
+
+// Puts Header Manipulation config on Routes, VirtualHosts, and Weighted Clusters
 type Plugin struct{}
 
 var _ plugins.RoutePlugin = NewPlugin()
 var _ plugins.VirtualHostPlugin = NewPlugin()
+var _ plugins.WeightedDestinationPlugin = NewPlugin()
 
-// Handles a RoutePlugin APIs which map directly to basic Envoy config
 func NewPlugin() *Plugin {
 	return &Plugin{}
 }
 
-func (p *Plugin) Init(params plugins.InitParams) error {
+func (p *Plugin) Init(_ plugins.InitParams) error {
+	return nil
+}
+
+func (p *Plugin) ProcessWeightedDestination(_ plugins.RouteParams, in *v1.WeightedDestination, out *envoyroute.WeightedCluster_ClusterWeight) error {
+	headerManipulation := in.GetWeighedDestinationPlugins().GetHeaderManipulation()
+
+	if headerManipulation == nil {
+		return nil
+	}
+
+	envoyHeader, err := convertHeaderConfig(headerManipulation)
+	if err != nil {
+		return err
+	}
+
+	out.RequestHeadersToAdd = envoyHeader.RequestHeadersToAdd
+	out.RequestHeadersToRemove = envoyHeader.RequestHeadersToRemove
+	out.ResponseHeadersToAdd = envoyHeader.ResponseHeadersToAdd
+	out.ResponseHeadersToRemove = envoyHeader.ResponseHeadersToRemove
+
 	return nil
 }
 
 func (p *Plugin) ProcessVirtualHost(params plugins.VirtualHostParams, in *v1.VirtualHost, out *envoyroute.VirtualHost) error {
-	if in.VirtualHostPlugins == nil {
+	headerManipulation := in.GetVirtualHostPlugins().GetHeaderManipulation()
+
+	if headerManipulation == nil {
 		return nil
 	}
-	return applyRetriesVhost(in, out)
+
+	envoyHeader, err := convertHeaderConfig(headerManipulation)
+	if err != nil {
+		return err
+	}
+
+	out.RequestHeadersToAdd = envoyHeader.RequestHeadersToAdd
+	out.RequestHeadersToRemove = envoyHeader.RequestHeadersToRemove
+	out.ResponseHeadersToAdd = envoyHeader.ResponseHeadersToAdd
+	out.ResponseHeadersToRemove = envoyHeader.ResponseHeadersToRemove
+
+	return nil
 }
 
 func (p *Plugin) ProcessRoute(params plugins.RouteParams, in *v1.Route, out *envoyroute.Route) error {
-	if in.RoutePlugins == nil {
+	headerManipulation := in.GetRoutePlugins().GetHeaderManipulation()
+
+	if headerManipulation == nil {
 		return nil
 	}
-	if err := applyPrefixRewrite(in, out); err != nil {
+
+	envoyHeader, err := convertHeaderConfig(headerManipulation)
+	if err != nil {
 		return err
 	}
-	if err := applyTimeout(in, out); err != nil {
-		return err
-	}
-	if err := applyRetries(in, out); err != nil {
-		return err
-	}
+
+	out.RequestHeadersToAdd = envoyHeader.RequestHeadersToAdd
+	out.RequestHeadersToRemove = envoyHeader.RequestHeadersToRemove
+	out.ResponseHeadersToAdd = envoyHeader.ResponseHeadersToAdd
+	out.ResponseHeadersToRemove = envoyHeader.ResponseHeadersToRemove
 
 	return nil
 }
 
-func applyPrefixRewrite(in *v1.Route, out *envoyroute.Route) error {
-	if in.RoutePlugins.PrefixRewrite == nil {
-		return nil
-	}
-	routeAction, ok := out.Action.(*envoyroute.Route_Route)
-	if !ok {
-		return errors.Errorf("prefix rewrite is only available for Route Actions")
-	}
-	if routeAction.Route == nil {
-		return errors.Errorf("internal error: route %v specified a prefix, but output Envoy object "+
-			"had nil route", in.Action)
-	}
-	routeAction.Route.PrefixRewrite = in.RoutePlugins.PrefixRewrite.PrefixRewrite
-	return nil
+type envoyHeaderManipulation struct {
+	RequestHeadersToAdd     []*envoycore.HeaderValueOption
+	RequestHeadersToRemove  []string
+	ResponseHeadersToAdd    []*envoycore.HeaderValueOption
+	ResponseHeadersToRemove []string
 }
 
-func applyTimeout(in *v1.Route, out *envoyroute.Route) error {
-	if in.RoutePlugins.Timeout == nil {
-		return nil
+func convertHeaderConfig(in *headers.HeaderManipulation) (*envoyHeaderManipulation, error) {
+	requestAdd, err := convertHeaderValueOption(in.GetRequestHeadersToAdd())
+	if err != nil {
+		return nil, err
 	}
-	routeAction, ok := out.Action.(*envoyroute.Route_Route)
-	if !ok {
-		return errors.Errorf("timeout is only available for Route Actions")
-	}
-	if routeAction.Route == nil {
-		return errors.Errorf("internal error: route %v specified a prefix, but output Envoy object "+
-			"had nil route", in.Action)
+	responseAdd, err := convertHeaderValueOption(in.GetResponseHeadersToAdd())
+	if err != nil {
+		return nil, err
 	}
 
-	routeAction.Route.Timeout = in.RoutePlugins.Timeout
-	return nil
+	return &envoyHeaderManipulation{
+		RequestHeadersToAdd:     requestAdd,
+		RequestHeadersToRemove:  in.GetRequestHeadersToRemove(),
+		ResponseHeadersToAdd:    responseAdd,
+		ResponseHeadersToRemove: in.GetResponseHeadersToRemove(),
+	}, nil
 }
 
-func applyRetries(in *v1.Route, out *envoyroute.Route) error {
-	policy := in.RoutePlugins.Retries
-	if policy == nil {
-		return nil
+func convertHeaderValueOption(in []*headers.HeaderValueOption) ([]*envoycore.HeaderValueOption, error) {
+	var out []*envoycore.HeaderValueOption
+	for _, h := range in {
+		if h.Header == nil {
+			return nil, MissingHeaderValueError
+		}
+		out = append(out, &envoycore.HeaderValueOption{
+			Header: &envoycore.HeaderValue{
+				Key:   h.Header.Key,
+				Value: h.Header.Value,
+			},
+			Append: h.Append,
+		})
 	}
-	routeAction, ok := out.Action.(*envoyroute.Route_Route)
-	if !ok {
-		return errors.Errorf("retries is only available for Route Actions")
-	}
-	if routeAction.Route == nil {
-		return errors.Errorf("internal error: route %v specified a prefix, but output Envoy object "+
-			"had nil route", in.Action)
-	}
-
-	routeAction.Route.RetryPolicy = convertPolicy(policy)
-	return nil
-}
-
-func applyRetriesVhost(in *v1.VirtualHost, out *envoyroute.VirtualHost) error {
-	out.RetryPolicy = convertPolicy(in.VirtualHostPlugins.Retries)
-	return nil
-}
-
-func convertPolicy(policy *retries.RetryPolicy) *envoyroute.RetryPolicy {
-	if policy == nil {
-		return nil
-	}
-
-	numRetries := policy.NumRetries
-	if numRetries == 0 {
-		numRetries = 1
-	}
-
-	return &envoyroute.RetryPolicy{
-		RetryOn:       policy.RetryOn,
-		NumRetries:    &types.UInt32Value{Value: numRetries},
-		PerTryTimeout: policy.PerTryTimeout,
-	}
+	return out, nil
 }
