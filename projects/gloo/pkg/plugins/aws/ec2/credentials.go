af95cbddc3b3f1f3af4252f8e99de291da34da88
Mitch Kelley <mitch.kelley@solo.io>
2019-08-05 11:55:37 -0400

    add roleArns as deprecated, key creds by struct

--- projects/gloo/pkg/plugins/aws/ec2/credentials.go
+++ projects/gloo/pkg/plugins/aws/ec2/credentials.go
@@ -1,8 +1,6 @@
 package ec2
 
 import (
-	"fmt"
-
 	glooec2 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/plugins/aws/ec2"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 )
@@ -23,8 +21,11 @@ type CredentialSpec struct {
 const arnSegmentDelimiter = ":"
 
 func (cs *CredentialSpec) GetKey() CredentialKey {
-	// use a very conservative "hash" strategy to avoid having to depend on aws's arn specification
-	return CredentialKey(fmt.Sprintf("%v-%v-%v", cs.secretRef.String(), cs.region, cs.roleArn))
+	return CredentialKey{
+		secretRef: cs.SecretRef().String(),
+		region:    cs.Region(),
+		roleArn:   cs.Arn(),
+	}
 }
 
 func (cs *CredentialSpec) Region() string {
@@ -48,13 +49,20 @@ func (cs *CredentialSpec) Clone() *CredentialSpec {
 }
 
 func NewCredentialSpecFromEc2UpstreamSpec(spec *glooec2.UpstreamSpec) *CredentialSpec {
+	roleArn := spec.GetRoleArn()
+	if roleArn == "" && len(spec.GetRoleArns()) > 0 {
+		roleArn = spec.GetRoleArns()[0]
+	}
 	return &CredentialSpec{
 		secretRef: spec.SecretRef,
 		region:    spec.Region,
-		roleArn:   spec.GetRoleArn(),
+		roleArn:   roleArn,
 	}
 }
 
-// Since "==" is not defined for slices, slices (in particular, the roleArns slice) cannot be used as keys for go maps.
-// Instead, we will use a string form. We give it a name for clarity.
-type CredentialKey string
+// key by the credential itself since all
+type CredentialKey struct {
+	secretRef string
+	region    string
+	roleArn   string
+}
