275854802a9e8b556cf95237aedbc3fde70f204d
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-08-07 07:28:39 -0400

    fix nil context in test

--- projects/gloo/pkg/plugins/kubernetes/kubernetes_test.go
+++ projects/gloo/pkg/plugins/kubernetes/kubernetes_test.go
@@ -182,7 +182,7 @@ var _ = Describe("Kubernetes", func() {
 			eds, errs, err := plug.WatchEndpoints(
 				"",
 				v1.UpstreamList{makeUpstream("a"), makeUpstream("b"), makeUpstream("c")},
-				clients.WatchOpts{})
+				clients.WatchOpts{Ctx: context.TODO()})
 			Expect(err).NotTo(HaveOccurred())
 
 			Eventually(eds, time.Second).Should(Receive(HaveLen(6)))
