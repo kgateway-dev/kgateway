009557a97e2f616922479d36a2a642472e9a047a
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-08-14 08:25:15 -0400

    add hooks and fix tests

--- projects/gloo/cli/pkg/cmd/install/util.go
+++ projects/gloo/cli/pkg/cmd/install/util.go
@@ -45,8 +45,9 @@ func init() {
 		"ClusterRole",
 		"ClusterRoleBinding",
 	}
-
-	GlooInstallKinds = append(GlooSystemKinds, GlooRbacKinds...)
+	GlooPreInstallKinds = append(GlooPreInstallKinds, "ServiceAccount")
+	GlooPreInstallKinds = append(GlooPreInstallKinds, GlooRbacKinds...)
+	GlooInstallKinds = GlooSystemKinds
 
 	GlooGatewayUpgradeKinds = append(GlooInstallKinds, "Job")
 
