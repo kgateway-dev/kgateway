9ef27126c5bc02dc036a329d8980bb11dbd1a9a9
Mitch Kelley <mitch.kelley@solo.io>
2019-09-03 11:56:24 -0400

    regen clients

--- projects/gloo/pkg/mocks/mock_artifact_client.go
+++ projects/gloo/pkg/mocks/mock_artifact_client.go
@@ -5,11 +5,10 @@
 package mocks
 
 import (
-	reflect "reflect"
-
 	gomock "github.com/golang/mock/gomock"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	clients "github.com/solo-io/solo-kit/pkg/api/v1/clients"
+	reflect "reflect"
 )
 
 // MockArtifactClient is a mock of ArtifactClient interface
@@ -37,6 +36,7 @@ func (m *MockArtifactClient) EXPECT() *MockArtifactClientMockRecorder {
 
 // BaseClient mocks base method
 func (m *MockArtifactClient) BaseClient() clients.ResourceClient {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "BaseClient")
 	ret0, _ := ret[0].(clients.ResourceClient)
 	return ret0
@@ -44,11 +44,13 @@ func (m *MockArtifactClient) BaseClient() clients.ResourceClient {
 
 // BaseClient indicates an expected call of BaseClient
 func (mr *MockArtifactClientMockRecorder) BaseClient() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "BaseClient", reflect.TypeOf((*MockArtifactClient)(nil).BaseClient))
 }
 
 // Delete mocks base method
 func (m *MockArtifactClient) Delete(arg0, arg1 string, arg2 clients.DeleteOpts) error {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Delete", arg0, arg1, arg2)
 	ret0, _ := ret[0].(error)
 	return ret0
@@ -56,11 +58,13 @@ func (m *MockArtifactClient) Delete(arg0, arg1 string, arg2 clients.DeleteOpts) 
 
 // Delete indicates an expected call of Delete
 func (mr *MockArtifactClientMockRecorder) Delete(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Delete", reflect.TypeOf((*MockArtifactClient)(nil).Delete), arg0, arg1, arg2)
 }
 
 // List mocks base method
 func (m *MockArtifactClient) List(arg0 string, arg1 clients.ListOpts) (v1.ArtifactList, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "List", arg0, arg1)
 	ret0, _ := ret[0].(v1.ArtifactList)
 	ret1, _ := ret[1].(error)
@@ -69,11 +73,13 @@ func (m *MockArtifactClient) List(arg0 string, arg1 clients.ListOpts) (v1.Artifa
 
 // List indicates an expected call of List
 func (mr *MockArtifactClientMockRecorder) List(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "List", reflect.TypeOf((*MockArtifactClient)(nil).List), arg0, arg1)
 }
 
 // Read mocks base method
 func (m *MockArtifactClient) Read(arg0, arg1 string, arg2 clients.ReadOpts) (*v1.Artifact, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Read", arg0, arg1, arg2)
 	ret0, _ := ret[0].(*v1.Artifact)
 	ret1, _ := ret[1].(error)
@@ -82,11 +88,13 @@ func (m *MockArtifactClient) Read(arg0, arg1 string, arg2 clients.ReadOpts) (*v1
 
 // Read indicates an expected call of Read
 func (mr *MockArtifactClientMockRecorder) Read(arg0, arg1, arg2 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Read", reflect.TypeOf((*MockArtifactClient)(nil).Read), arg0, arg1, arg2)
 }
 
 // Register mocks base method
 func (m *MockArtifactClient) Register() error {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Register")
 	ret0, _ := ret[0].(error)
 	return ret0
@@ -94,11 +102,13 @@ func (m *MockArtifactClient) Register() error {
 
 // Register indicates an expected call of Register
 func (mr *MockArtifactClientMockRecorder) Register() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Register", reflect.TypeOf((*MockArtifactClient)(nil).Register))
 }
 
 // Watch mocks base method
 func (m *MockArtifactClient) Watch(arg0 string, arg1 clients.WatchOpts) (<-chan v1.ArtifactList, <-chan error, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Watch", arg0, arg1)
 	ret0, _ := ret[0].(<-chan v1.ArtifactList)
 	ret1, _ := ret[1].(<-chan error)
@@ -108,11 +118,13 @@ func (m *MockArtifactClient) Watch(arg0 string, arg1 clients.WatchOpts) (<-chan 
 
 // Watch indicates an expected call of Watch
 func (mr *MockArtifactClientMockRecorder) Watch(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Watch", reflect.TypeOf((*MockArtifactClient)(nil).Watch), arg0, arg1)
 }
 
 // Write mocks base method
 func (m *MockArtifactClient) Write(arg0 *v1.Artifact, arg1 clients.WriteOpts) (*v1.Artifact, error) {
+	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Write", arg0, arg1)
 	ret0, _ := ret[0].(*v1.Artifact)
 	ret1, _ := ret[1].(error)
@@ -121,5 +133,6 @@ func (m *MockArtifactClient) Write(arg0 *v1.Artifact, arg1 clients.WriteOpts) (*
 
 // Write indicates an expected call of Write
 func (mr *MockArtifactClientMockRecorder) Write(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Write", reflect.TypeOf((*MockArtifactClient)(nil).Write), arg0, arg1)
 }
