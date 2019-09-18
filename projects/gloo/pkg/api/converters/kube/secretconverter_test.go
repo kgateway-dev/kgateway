9a92ca8f2aab33fdeb4f809ce21ad10b200c47c1
EItanya <eitan.yarmush@solo.io>
2019-09-18 14:00:28 -0400

    generated code

--- projects/gloo/pkg/api/converters/kube/secretconverter_test.go
+++ projects/gloo/pkg/api/converters/kube/secretconverter_test.go
@@ -25,8 +25,8 @@ var _ = Describe("SecretConverter", func() {
 				kubev1.TLSPrivateKeyKey: []byte("key"),
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "s1",
-				Namespace: "ns",
+				Name:            "s1",
+				Namespace:       "ns",
 				OwnerReferences: []metav1.OwnerReference{},
 			},
 		}
@@ -70,9 +70,9 @@ var _ = Describe("SecretConverter", func() {
 				kubev1.TLSPrivateKeyKey: []byte("key"),
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "s1",
-				Namespace: "ns",
-				Labels:    map[string]string{},
+				Name:            "s1",
+				Namespace:       "ns",
+				Labels:          map[string]string{},
 				OwnerReferences: []metav1.OwnerReference{},
 			},
 		}
@@ -94,9 +94,9 @@ var _ = Describe("SecretConverter", func() {
 		kubeSecret := &kubev1.Secret{
 			Type: kubev1.SecretTypeOpaque,
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "s1",
-				Namespace: "ns",
-				Labels:    map[string]string{},
+				Name:            "s1",
+				Namespace:       "ns",
+				Labels:          map[string]string{},
 				OwnerReferences: []metav1.OwnerReference{},
 			},
 			Data: map[string][]byte{
@@ -122,9 +122,9 @@ var _ = Describe("SecretConverter", func() {
 				AwsSecretKeyName: []byte("secret"),
 			},
 			ObjectMeta: metav1.ObjectMeta{
-				Name:      "s1",
-				Namespace: "ns",
-				Labels:    map[string]string{},
+				Name:            "s1",
+				Namespace:       "ns",
+				Labels:          map[string]string{},
 				OwnerReferences: []metav1.OwnerReference{},
 			},
 		}
