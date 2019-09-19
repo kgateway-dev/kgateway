1f82c47a703a830aa8c23410f455a45c8588f20e
EItanya <eitan.yarmush@solo.io>
2019-09-19 16:40:14 -0400

    shelving

--- projects/metrics/pkg/metricsservice/service.go
+++ projects/metrics/pkg/metricsservice/service.go
@@ -2,20 +2,25 @@ package metricsservice
 
 import (
 	"context"
+	"fmt"
 	"strings"
 	"time"
 
 	envoymet "github.com/envoyproxy/go-control-plane/envoy/service/metrics/v2"
 	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/go-utils/errors"
 	"go.uber.org/zap"
+	prometheus "istio.io/gogo-genproto/prometheus"
 )
 
 const (
 	ReadConfigStatPrefix = "read_config"
 	PrometheusStatPrefix = "prometheus"
 
-	TcpStatPrefix = "tcp"
-	HttpStatPrefix = "http"
+	ServerUptime = "server.uptime"
+
+	TcpStatPrefix      = "tcp"
+	HttpStatPrefix     = "http"
 	ListenerStatPrefix = "listener"
 )
 
@@ -31,7 +36,6 @@ type GlooUsageMetrics map[string]EnvoyUsageMetrics
 func AddMetric(id *envoymet.StreamMetricsMessage_Identifier) {
 
 }
-
 
 func GetNodeCount() int {
 	return 0
@@ -60,6 +64,8 @@ func (s *Server) StreamMetrics(envoyMetrics envoymet.MetricsService_StreamMetric
 		zap.Int("number of metrics", len(met.EnvoyMetrics)),
 	)
 
+	tempMetricsMap := make(map[string])
+
 	for _, v := range met.EnvoyMetrics {
 		switch {
 		case strings.HasPrefix(v.GetName(), ListenerStatPrefix) && strings.HasSuffix(v.GetName(), "downstream_rq_completed"):
@@ -68,6 +74,21 @@ func (s *Server) StreamMetrics(envoyMetrics envoymet.MetricsService_StreamMetric
 		}
 	}
 	return nil
+}
+
+func getUptime(metrics []*prometheus.MetricFamily) (time.Duration, error) {
+	for _, metricFamily := range metrics {
+		if metricFamily.GetName() == ServerUptime {
+			for _, metric := range metricFamily.GetMetric() {
+				gauge := metric.GetGauge()
+				if gauge == nil {
+					continue
+				}
+				return time.ParseDuration(fmt.Sprintf("%fs", gauge.GetValue()))
+			}
+		}
+	}
+	return 0, errors.New("could not get server uptime")
 }
 
 type Options struct {
