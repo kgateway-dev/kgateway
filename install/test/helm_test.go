e3b749ad143536bc6dd411a7caba74cbf12c4153
Scott Weiss <sdw35@cornell.edu>
2019-10-14 19:17:05 -0400

    fix unit test

--- install/test/helm_test.go
+++ install/test/helm_test.go
@@ -633,7 +633,7 @@ spec:
   gloo:
     xdsBindAddr: 0.0.0.0:9977
     invalidConfigPolicy:
-      fallbackResponseBody: Gateway has invalid configuration. Administrators should run
+      fallbackResponseBody: Gloo Gateway has invalid configuration. Administrators should run
         ` + "`" + `glooctl check` + "`" + ` to find and fix config errors.
       fallbackResponseCode: 404
 
