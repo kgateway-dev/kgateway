53c1a6fb441527ceee95fd9a4030152cfd4f5025
Yuval Kohavi <yuval.kohavi@gmail.com>
2019-12-10 15:30:21 -0500

    add test

---
+++ test/checks/checks_test.go
@@ -0,0 +1,38 @@
+package checks
+
+import (
+	"golang.org/x/mod/modfile"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Checks", func() {
+
+	It("should used forked klog instead of klog", func() {
+		// regular klog writes to disk, so make sure we used a forked version that doesn't write to
+		// disk
+
+		gomod, err := exec.Command("go", "env", "GOMOD").CombinedOutput()
+		Expect(err).NotTo(HaveOccurred())
+		gomodfile := strings.TrimSpace(string(gomod))
+		data, err := ioutil.ReadFile(gomodfile)
+		Expect(err).NotTo(HaveOccurred())
+		modFile, err := modfile.Parse(gomodfile, data, nil)
+
+		var klogReplace *modfile.Replace
+		for _, replace := range modFile.Replace {
+			if replace.Old.Path == "k8s.io/klog" {
+				klogReplace = replace
+				break
+			}
+		}
+		Expect(klogReplace).NotTo(BeNil())
+		Expect(klogReplace.New.Path).To(Equal("github.com/stefanprodan/klog"))
+		Expect(klogReplace.New.Version).To(Equal("v0.0.0-20190418165334-9cbb78b20423"))
+	})
+
+})
