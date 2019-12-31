256a50e6fd6d1009498067d59e2d9b682f91012d
Kevin Dorosh <kcdorosh@gmail.com>
2019-12-31 15:31:40 -0500

    Hack script needs more permissions; improve err logging

--- hack/find_latest_enterprise_version.go
+++ hack/find_latest_enterprise_version.go
@@ -1,21 +1,22 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	"github.com/solo-io/gloo/pkg/version"
 )
 
 func main() {
-	os.Mkdir("./_output", 0644)
+	os.Mkdir("./_output", 0755)
 	f, err := os.Create("./_output/gloo-enterprise-version")
 	if err != nil {
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	defer f.Close()
 	enterpriseVersion, err := version.GetLatestEnterpriseVersion(false)
 	if err != nil {
-		os.Exit(1)
+		log.Fatal(err)
 	}
 	f.WriteString(enterpriseVersion)
 	f.Sync()
