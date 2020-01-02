70caa5148e78d108adcae0ffff414772ef0c413c
Yuval Kohavi <yuval.kohavi@gmail.com>
2020-01-02 17:48:32 -0500

    format

--- test/e2e/util_test.go
+++ test/e2e/util_test.go
@@ -2,6 +2,7 @@ package e2e_test
 
 import (
 	"context"
+
 	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	gwdefaults "github.com/solo-io/gloo/projects/gateway/pkg/defaults"
 	"github.com/solo-io/gloo/test/helpers"
