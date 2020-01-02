230460135b59e0b40c4b57b4a0019e717359e1d4
Kevin Dorosh <kcdorosh@gmail.com>
2020-01-02 00:21:14 -0500

    Update version logic to find latest enterprise release with max constraint

--- pkg/version/enterprise.go
+++ pkg/version/enterprise.go
@@ -1,8 +1,12 @@
 package version
 
 import (
+	"strings"
+
+	"github.com/Masterminds/semver/v3"
 	"github.com/solo-io/go-utils/errors"
 	"github.com/solo-io/go-utils/githubutils"
+	"github.com/solo-io/go-utils/versionutils"
 	"github.com/spf13/afero"
 	"helm.sh/helm/v3/pkg/repo"
 )
@@ -11,8 +15,13 @@ const EnterpriseHelmRepoIndex = "https://storage.googleapis.com/gloo-ee-helm/ind
 const GlooEE = "gloo-ee"
 
 // The version of GlooE installed by the CLI.
-// Calculated from the latest gloo-ee version in the helm repo index
+// Calculated from the largest semver gloo-ee version in the helm repo index
 func GetLatestEnterpriseVersion(stableOnly bool) (string, error) {
+	return GetLatestEnterpriseVersionWithMaxVersion(stableOnly, &versionutils.Zero)
+}
+
+// Calculated from the largest gloo-ee version in the helm repo index with version constraints
+func GetLatestEnterpriseVersionWithMaxVersion(stableOnly bool, maxVersion *versionutils.Version) (string, error) {
 	fs := afero.NewOsFs()
 	tmpFile, err := afero.TempFile(fs, "", "")
 	if err != nil {
@@ -22,27 +31,43 @@ func GetLatestEnterpriseVersion(stableOnly bool) (string, error) {
 		return "", err
 	}
 	defer fs.Remove(tmpFile.Name())
-	return LatestVersionFromRepo(tmpFile.Name(), stableOnly)
+	return LatestVersionFromRepoWithMaxVersion(tmpFile.Name(), stableOnly, maxVersion)
 }
 
 func LatestVersionFromRepo(file string, stableOnly bool) (string, error) {
+	return LatestVersionFromRepoWithMaxVersion(file, stableOnly, &versionutils.Zero)
+}
+
+func LatestVersionFromRepoWithMaxVersion(file string, stableOnly bool, maxVersion *versionutils.Version) (string, error) {
 	ind, err := repo.LoadIndexFile(file)
 	if err != nil {
 		return "", err
 	}
-	if stableOnly {
-		version, err := ind.Get(GlooEE, "")
-		if err != nil {
-			return "", err
-		}
-		return version.Version, nil
-	} else {
-		ind.SortEntries()
-		if vs, ok := ind.Entries[GlooEE]; ok {
-			if len(vs) > 0 {
-				return vs[0].Version, nil
+
+	ind.SortEntries()
+	if chartVersions, ok := ind.Entries[GlooEE]; ok && len(chartVersions) > 0 {
+		for _, chartVersion := range chartVersions {
+
+			if stableOnly {
+				stableOnlyConstraint, _ := semver.NewConstraint("*")
+				test, err := semver.NewVersion(chartVersion.Version)
+				if err != nil || !stableOnlyConstraint.Check(test) {
+					continue
+				}
 			}
+
+			tag := "v" + strings.TrimPrefix(chartVersion.Version, "v")
+			version, err := versionutils.ParseVersion(tag)
+			if err != nil {
+				continue
+			}
+			versionConstraintSatisfied, err := maxVersion.IsGreaterThanOrEqualTo(version)
+			if err == nil && versionConstraintSatisfied {
+				return chartVersion.Version, nil
+			}
+
 		}
 	}
+
 	return "", errors.Errorf("Couldn't find any %s versions in index file %s", GlooEE, file)
 }
