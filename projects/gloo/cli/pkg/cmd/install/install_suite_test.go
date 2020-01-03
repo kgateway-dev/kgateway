5c2478f20be312279c8e21f52eff9c242f95c751
Mitch Kelley <mitch.kelley@solo.io>
2020-01-03 14:14:48 -0500

    cleanup helm docs after helm tests

--- projects/gloo/cli/pkg/cmd/install/install_suite_test.go
+++ projects/gloo/cli/pkg/cmd/install/install_suite_test.go
@@ -76,4 +76,7 @@ var _ = AfterSuite(func() {
 	Expect(err).NotTo(HaveOccurred())
 	err = os.RemoveAll(dir)
 	Expect(err).NotTo(HaveOccurred())
+	// Restore the helm docs to their make generated-code format
+	err = testutils.Make(RootDir, "generate-helm-files TEST_ASSET_DIR=\""+dir+"\" BUILD_ID=unit-testing -B")
+	Expect(err).NotTo(HaveOccurred())
 })
