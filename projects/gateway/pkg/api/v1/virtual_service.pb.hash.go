b9afa392923f3516d8797362b1669fbdc3bb5e98
Marco <marcog.schmidt@gmail.com>
2020-01-03 10:43:15 -0500

    Generate code before docs

--- projects/gateway/pkg/api/v1/virtual_service.pb.hash.go
+++ projects/gateway/pkg/api/v1/virtual_service.pb.hash.go
@@ -264,6 +264,9 @@ func (m *DelegateAction) Hash(hasher hash.Hash64) (uint64, error) {
 		hasher = fnv.New64()
 	}
 	var err error
+	if _, err = hasher.Write([]byte("gateway.solo.io.github.com/solo-io/gloo/projects/gateway/pkg/api/v1.DelegateAction")); err != nil {
+		return 0, err
+	}
 
 	if _, err = hasher.Write([]byte(m.GetName())); err != nil {
 		return 0, err
@@ -321,6 +324,9 @@ func (m *RouteTableSelector) Hash(hasher hash.Hash64) (uint64, error) {
 		hasher = fnv.New64()
 	}
 	var err error
+	if _, err = hasher.Write([]byte("gateway.solo.io.github.com/solo-io/gloo/projects/gateway/pkg/api/v1.RouteTableSelector")); err != nil {
+		return 0, err
+	}
 
 	for _, v := range m.GetNamespaces() {
 
