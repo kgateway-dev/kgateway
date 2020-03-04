solo-io/gloo

update envoy (#2379)
* update envoy
* don't use deprecated tls context
* fix test
* fix test
* fix test
* fix test
* Merge remote-tracking branch 'origin/master' into update-envoy-02-2020
* fix more deprecated fields
* fix test
* mod tidy
* fix test
* more deprecated fields
* more deprecation fixes
* fix test
* pr comments
* un focus
* chng

projects/gloo/pkg/plugins/tcp/plugin.go
@@ -2,6 +2,7 @@ package tcp
 
 import (
 	envoyauth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	envoylistener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
 	envoytcp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/tcp_proxy/v2"
 	"github.com/gogo/protobuf/types"
@@ -186,8 +187,11 @@ func newSslFilterChain(downstreamConfig *envoyauth.DownstreamTlsContext, sniDoma
 		FilterChainMatch: &envoylistener.FilterChainMatch{
 			ServerNames: sniDomains,
 		},
-		Filters:       listenerFilters,
-		TlsContext:    downstreamConfig,
+		Filters: listenerFilters,
+		TransportSocket: &envoycore.TransportSocket{
+			Name:       pluginutils.TlsTransportSocket,
+			ConfigType: &envoycore.TransportSocket_TypedConfig{TypedConfig: pluginutils.MustMessageToAny(downstreamConfig)},
+		},
 		UseProxyProto: gogoutils.BoolGogoToProto(useProxyProto),
 	}
 }

projects/gloo/pkg/translator/listener.go
@@ -14,6 +14,7 @@ import (
 	validationapi "github.com/solo-io/gloo/projects/gloo/pkg/api/grpc/validation"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins/pluginutils"
 	"github.com/solo-io/gloo/projects/gloo/pkg/utils/validation"
 	"github.com/solo-io/go-utils/contextutils"
 )
@@ -225,8 +226,13 @@ func newSslFilterChain(downstreamConfig *envoyauth.DownstreamTlsContext, sniDoma
 		FilterChainMatch: &envoylistener.FilterChainMatch{
 			ServerNames: sniDomains,
 		},
-		Filters:       listenerFilters,
-		TlsContext:    downstreamConfig,
+		Filters: listenerFilters,
+
+		TransportSocket: &envoycore.TransportSocket{
+			Name:       pluginutils.TlsTransportSocket,
+			ConfigType: &envoycore.TransportSocket_TypedConfig{TypedConfig: pluginutils.MustMessageToAny(downstreamConfig)},
+		},
+
 		UseProxyProto: gogoutils.BoolGogoToProto(useProxyProto),
 	}
 }

kgateway-dev/kgateway

(cherry picked from commit a15ebb34ca1273489fa30853454b18e7c0a4741e)

projects/gloo/pkg/plugins/tcp/plugin.go
@@ -2,6 +2,7 @@ package tcp
 
 import (
 	envoyauth "github.com/envoyproxy/go-control-plane/envoy/api/v2/auth"
+	envoycore "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	envoylistener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
 	envoytcp "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/tcp_proxy/v2"
 	"github.com/gogo/protobuf/proto"
@@ -193,8 +194,11 @@ func newSslFilterChain(downstreamConfig *envoyauth.DownstreamTlsContext, sniDoma
 		FilterChainMatch: &envoylistener.FilterChainMatch{
 			ServerNames: sniDomains,
 		},
-		Filters:       listenerFiltersCopy,
-		TlsContext:    downstreamConfig,
+		Filters: listenerFiltersCopy,
+		TransportSocket: &envoycore.TransportSocket{
+			Name:       pluginutils.TlsTransportSocket,
+			ConfigType: &envoycore.TransportSocket_TypedConfig{TypedConfig: pluginutils.MustMessageToAny(downstreamConfig)},
+		},
 		UseProxyProto: gogoutils.BoolGogoToProto(useProxyProto),
 	}
 }

projects/gloo/pkg/translator/listener.go
@@ -16,6 +16,7 @@ import (
 	validationapi "github.com/solo-io/gloo/projects/gloo/pkg/api/grpc/validation"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
+	"github.com/solo-io/gloo/projects/gloo/pkg/plugins/pluginutils"
 	"github.com/solo-io/gloo/projects/gloo/pkg/utils/validation"
 	"github.com/solo-io/go-utils/contextutils"
 )
@@ -233,8 +234,13 @@ func newSslFilterChain(downstreamConfig *envoyauth.DownstreamTlsContext, sniDoma
 		FilterChainMatch: &envoylistener.FilterChainMatch{
 			ServerNames: sniDomains,
 		},
-		Filters:       listenerFiltersCopy,
-		TlsContext:    downstreamConfig,
+		Filters: listenerFiltersCopy,
+
+		TransportSocket: &envoycore.TransportSocket{
+			Name:       pluginutils.TlsTransportSocket,
+			ConfigType: &envoycore.TransportSocket_TypedConfig{TypedConfig: pluginutils.MustMessageToAny(downstreamConfig)},
+		},
+
 		UseProxyProto: gogoutils.BoolGogoToProto(useProxyProto),
 	}
 }