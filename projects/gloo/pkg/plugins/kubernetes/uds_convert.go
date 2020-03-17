ca9524faa03f39c025541590bc23fdc183418f94
Kevin Dorosh <kcdorosh@gmail.com>
2020-03-17 12:49:20 -0400

    Delete more unused code

--- projects/gloo/pkg/plugins/kubernetes/uds_convert.go
+++ projects/gloo/pkg/plugins/kubernetes/uds_convert.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"reflect"
-	"sort"
 	"strings"
 
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins/kubernetes/serviceconverter"
@@ -37,11 +36,6 @@ type KubeUpstreamConverter struct {
 }
 
 func (uc *KubeUpstreamConverter) UpstreamsForService(ctx context.Context, svc *kubev1.Service) v1.UpstreamList {
-
-	return uc.createUpstreamForLabels(ctx, svc)
-}
-
-func (uc *KubeUpstreamConverter) createUpstreamForLabels(ctx context.Context, svc *kubev1.Service) v1.UpstreamList {
 	var upstreams v1.UpstreamList
 	for _, port := range svc.Spec.Ports {
 		upstreams = append(upstreams, uc.CreateUpstream(ctx, svc, port))
@@ -90,28 +84,6 @@ func containsString(s string, slice []string) bool {
 		}
 	}
 	return false
-}
-
-func containsMap(maps []map[string]string, item map[string]string) bool {
-	for _, m := range maps {
-		if reflect.DeepEqual(m, item) {
-			return true
-		}
-	}
-	return false
-}
-
-func keysAndValues(m map[string]string) ([]string, []string) {
-	var keys []string
-	for k := range m {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-	var values []string
-	for _, k := range keys {
-		values = append(values, m[k])
-	}
-	return keys, values
 }
 
 func skip(svc *kubev1.Service, opts discovery.Opts) bool {
