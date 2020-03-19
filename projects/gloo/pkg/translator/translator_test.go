356d580cb3df684456c93a39abba77ceba000a36
Ashley <ashley.wang@solo.io>
2020-03-19 10:00:17 -0400

    test error message

--- projects/gloo/pkg/translator/translator_test.go
+++ projects/gloo/pkg/translator/translator_test.go
@@ -294,6 +294,7 @@ var _ = Describe("Translator", func() {
 
 			Expect(err).To(BeNil())
 			Expect(errs.Validate()).To(HaveOccurred())
+			Expect(errs.Validate().Error()).To(ContainSubstring("HttpListener Error: ProcessingError. Reason: auth config not found:"))
 		})
 	})
 
