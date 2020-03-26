2367792460a19ee22463bab9e9046e67c130680a
Kevin Dorosh <kcdorosh@gmail.com>
2020-03-26 13:16:55 -0400

    Add test for zero setting

--- projects/gloo/pkg/syncer/setup_syncer_test.go
+++ projects/gloo/pkg/syncer/setup_syncer_test.go
@@ -210,6 +210,12 @@ var _ = Describe("SetupSyncer", func() {
 				Expect(func() { setup(ctx, kubeCoreCache, memcache, settings) }).To(Panic())
 			})
 
+			It("doesn't panic when endpoints don't arrive in a timely manner if set to zero", func() {
+				settings.Gloo.EndpointsWarmingTimeout = types.DurationProto(0)
+				setup := NewSetupFunc()
+				Expect(func() { setup(ctx, kubeCoreCache, memcache, settings) }).NotTo(Panic())
+			})
+
 		})
 	})
 })
