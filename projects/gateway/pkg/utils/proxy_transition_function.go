415348429de0aac670c1b9517951329399834b8c
Kevin Dorosh <kcdorosh@gmail.com>
2020-04-13 15:24:05 -0400

    Revert "Update transition function so gateway and gloo resync match"
    This reverts commit a8f7660d4e3339974f002f0adaa4af2d1485b584.

--- projects/gateway/pkg/utils/proxy_transition_function.go
+++ projects/gateway/pkg/utils/proxy_transition_function.go
@@ -2,17 +2,16 @@ package utils
 
 import (
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
-	"github.com/solo-io/go-utils/hashutils"
 )
 
 func TransitionFunction(original, desired *v1.Proxy) (bool, error) {
-	// this logic needs to match the resync equality for gloo, or else
-	// we can get into a scenario where gateway reconciles a new proxy that
-	// has the same hash as the previous, but with a pending status, that never
-	// gets cleared because gloo is waiting for a "significant" change before resyncing.
-	equal, ok := hashutils.HashableEqual(original, desired)
-	if ok {
-		return !equal, nil
+	if len(original.Listeners) != len(desired.Listeners) {
+		return true, nil
+	}
+	for i := range original.Listeners {
+		if !original.Listeners[i].Equal(desired.Listeners[i]) {
+			return true, nil
+		}
 	}
 	// default behavior: perform the update if one if the objects are not hashable
 	return true, nil
