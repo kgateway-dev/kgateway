510ade0b246a730fb32051150c8eb1eef3dd6cf0
Rick Ducott <rick.ducott@solo.io>
2020-04-20 14:59:45 -0400

    Fix test description

--- projects/gloo/pkg/plugins/listener/plugin_test.go
+++ projects/gloo/pkg/plugins/listener/plugin_test.go
@@ -21,7 +21,7 @@ var _ = Describe("Plugin", func() {
 		plugin = NewPlugin()
 	})
 
-	It("should set HealthyPanicThreshold", func() {
+	It("should set perConnectionBufferLimitBytes", func() {
 
 		in := &v1.Listener{
 			Options: &v1.ListenerOptions{
