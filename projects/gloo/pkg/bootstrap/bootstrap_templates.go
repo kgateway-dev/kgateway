a693ae902a220ae971ef9ec589e0fffc1fbd442f
Kevin Dorosh <kcdorosh@gmail.com>
2020-04-21 11:42:55 -0400

    Moving to go structs

--- projects/gloo/pkg/bootstrap/bootstrap_templates.go
+++ projects/gloo/pkg/bootstrap/bootstrap_templates.go
@@ -1,25 +1,14 @@
 package bootstrap
 
 import (
-	"bytes"
 	"context"
 	"os"
 	"os/exec"
-	"strings"
-	"text/template"
 
 	"github.com/solo-io/go-utils/contextutils"
 
-	"github.com/golang/protobuf/proto"
-	"github.com/solo-io/go-utils/protoutils"
-	"sigs.k8s.io/yaml"
-
 	"github.com/rotisserie/eris"
 )
-
-type EnvoyInstance struct {
-	Transformations string
-}
 
 const defaultEnvoyPath = "/usr/local/bin/envoy"
 
@@ -31,25 +20,9 @@ func getEnvoyPath() string {
 	return ep
 }
 
-func ToYaml(m proto.Message) ([]byte, error) {
-	jsn, err := protoutils.MarshalBytes(m)
-	if err != nil {
-		return nil, err
-	}
-	return yaml.JSONToYAML(jsn)
-}
-
-func IndentYaml(yml string, numSpaces int) string {
-	lines := strings.Split(yml, "\n")
-	indent := strings.Repeat(" ", numSpaces)
-	indentedYaml := strings.Join(lines, "\n"+indent)
-	return indentedYaml
-}
-
-func (ei *EnvoyInstance) ValidateBootstrap(ctx context.Context, bootstrapTemplate string) error {
-	configYaml := ei.buildBootstrap(bootstrapTemplate)
+func ValidateBootstrap(ctx context.Context, bootstrapYaml string) error {
 	envoyPath := getEnvoyPath()
-	validateCmd := exec.Command(envoyPath, "--mode", "validate", "--config-yaml", configYaml)
+	validateCmd := exec.Command(envoyPath, "--mode", "validate", "--config-yaml", bootstrapYaml)
 	if output, err := validateCmd.CombinedOutput(); err != nil {
 		if os.IsNotExist(err) {
 			// log a warning and return nil; will allow users to continue to run Gloo locally without
@@ -62,52 +35,3 @@ func (ei *EnvoyInstance) ValidateBootstrap(ctx context.Context, bootstrapTemplat
 	}
 	return nil
 }
-
-func (ei *EnvoyInstance) buildBootstrap(bootstrapTemplate string) string {
-	var b bytes.Buffer
-	parsedTemplate := template.Must(template.New("bootstrap").Parse(bootstrapTemplate))
-	if err := parsedTemplate.Execute(&b, ei); err != nil {
-		panic(err)
-	}
-	return b.String()
-}
-
-const TransformationBootstrapTemplate = `
-node:
-  cluster: doesntmatter
-  id: imspecial
-  metadata:
-    role: "gloo-system~gateway-proxy"
-static_resources:
-  clusters:
-  - name: placeholder_cluster
-    connect_timeout: 5.000s
-  listeners:
-  - address:
-      socket_address:
-        address: 0.0.0.0
-        port_value: 8081
-    filter_chains:
-    - filters:
-      - config:
-          route_config:
-            name: placeholder_route
-            virtual_hosts:
-            - domains:
-              - '*'
-              name: placeholder_host
-              routes:
-              - match:
-                  headers:
-                  - exact_match: GET
-                    name: :method
-                  path: /
-                route:
-                  cluster: placeholder_cluster
-              per_filter_config:
-                io.solo.transformation:
-                  {{.Transformations}}
-          stat_prefix: placeholder
-        name: envoy.http_connection_manager
-    name: placeholder_listener
-`
--- projects/gloo/pkg/plugins/transformation/plugin.go
+++ projects/gloo/pkg/plugins/transformation/plugin.go
@@ -3,8 +3,18 @@ package transformation
 import (
 	"context"
 
+	udpa_type_v1 "github.com/cncf/udpa/go/udpa/type/v1"
 	envoyroute "github.com/envoyproxy/go-control-plane/envoy/api/v2/route"
-	"github.com/rotisserie/eris"
+	envoy_config_bootstrap_v3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
+	v35 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
+	v34 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	envoy_extensions_filters_network_http_connection_manager_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
+	"github.com/envoyproxy/go-control-plane/pkg/conversion"
+	"github.com/golang/protobuf/ptypes/any"
+	"github.com/golang/protobuf/ptypes/duration"
+	"github.com/solo-io/gloo/pkg/utils/protoutils"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/extensions/transformation"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/bootstrap"
@@ -84,18 +94,94 @@ func (p *Plugin) HttpFilters(params plugins.Params, listener *v1.HttpListener) (
 }
 
 func validateTransformation(ctx context.Context, transformations *transformation.RouteTransformations) error {
-	yml, err := bootstrap.ToYaml(transformations)
-	if err != nil {
-		// should never happen
-		return eris.Errorf("Unable to convert transformation to yaml, error: %v", err)
-	}
-	// indentation needs to match go template indentation so multiline yaml renders properly
-	indentedYml := bootstrap.IndentYaml(string(yml), 18)
-
-	envoyInstance := bootstrap.EnvoyInstance{Transformations: indentedYml}
-	err = envoyInstance.ValidateBootstrap(ctx, bootstrap.TransformationBootstrapTemplate)
+	err := bootstrap.ValidateBootstrap(ctx, buildBootstrap(transformations))
 	if err != nil {
 		return err
 	}
 	return nil
+}
+
+func buildBootstrap(transformations *transformation.RouteTransformations) string {
+
+	configStruct, err := conversion.MessageToStruct(transformations)
+	if err != nil {
+		panic(err)
+	}
+	tAny := pluginutils.MustMessageToAny(transformations) //is gogoproto, no idea how to marshal with goproto
+
+	// create a typed struct so goproto any can handle
+	ts := &udpa_type_v1.TypedStruct{Value: configStruct, TypeUrl: tAny.TypeUrl}
+
+	tAny2 := pluginutils.MustMessageToAny(ts)
+	goAny := &any.Any{Value: tAny2.Value, TypeUrl: tAny2.TypeUrl}
+
+	vhosts := []*envoy_config_route_v3.VirtualHost{
+		{
+			Name:    "placeholder_host",
+			Domains: []string{"*"},
+			Routes: []*envoy_config_route_v3.Route{
+				{
+					Action: &envoy_config_route_v3.Route_Route{Route: &envoy_config_route_v3.RouteAction{ClusterSpecifier: &envoy_config_route_v3.RouteAction_Cluster{Cluster: "placeholder_cluster"}}},
+					Match: &envoy_config_route_v3.RouteMatch{
+						PathSpecifier: &envoy_config_route_v3.RouteMatch_Prefix{Prefix: "/"},
+					},
+					TypedPerFilterConfig: map[string]*any.Any{
+						FilterName: goAny,
+					},
+				},
+			},
+		},
+	}
+
+	rc3 := &envoy_config_route_v3.RouteConfiguration{VirtualHosts: vhosts}
+
+	hcm := &envoy_extensions_filters_network_http_connection_manager_v3.HttpConnectionManager{
+		StatPrefix:     "placeholder",
+		RouteSpecifier: &envoy_extensions_filters_network_http_connection_manager_v3.HttpConnectionManager_RouteConfig{RouteConfig: rc3},
+	}
+
+	hcmAny := pluginutils.MustMessageToAny(hcm)
+	bootstrap := &envoy_config_bootstrap_v3.Bootstrap{
+		Node: &v3.Node{
+			Id:      "imspecial",
+			Cluster: "doesntmatter",
+		},
+		StaticResources: &envoy_config_bootstrap_v3.Bootstrap_StaticResources{
+			Listeners: []*v34.Listener{
+				{
+					Name: "placeholder_listener",
+					Address: &v3.Address{
+						Address: &v3.Address_SocketAddress{SocketAddress: &v3.SocketAddress{
+							Address:       "0.0.0.0",
+							PortSpecifier: &v3.SocketAddress_PortValue{PortValue: 8081},
+						}},
+					},
+					FilterChains: []*v34.FilterChain{
+						{
+							Name: "placeholder_filter_chain",
+							Filters: []*v34.Filter{
+								{
+									ConfigType: &v34.Filter_TypedConfig{
+										TypedConfig: hcmAny,
+									},
+									Name: "envoy.http_connection_manager",
+								},
+							},
+						},
+					},
+				},
+			},
+			Clusters: []*v35.Cluster{
+				{
+					Name:           "placeholder_cluster",
+					ConnectTimeout: &duration.Duration{Seconds: 5},
+				},
+			},
+		},
+	}
+
+	b, _ := protoutils.MarshalBytes(bootstrap)
+	re := string(b)
+
+	return re
 }
