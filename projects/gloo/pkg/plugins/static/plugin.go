9449a2e581272dea5c341e863d10efdd0f0b3754
Ashley <ashley.wang@solo.io>
2020-05-05 15:09:58 -0400

    small fixes

--- projects/gloo/pkg/plugins/static/plugin.go
+++ projects/gloo/pkg/plugins/static/plugin.go
@@ -82,7 +82,7 @@ func (p *plugin) ProcessUpstream(params plugins.Params, in *v1.Upstream, out *en
 			&envoyendpoint.LbEndpoint{
 				HostIdentifier: &envoyendpoint.LbEndpoint_Endpoint{
 					Endpoint: &envoyendpoint.Endpoint{
-						Hostname:    host.Addr,
+						Hostname: host.Addr,
 						Address: &envoycore.Address{
 							Address: &envoycore.Address_SocketAddress{
 								SocketAddress: &envoycore.SocketAddress{
