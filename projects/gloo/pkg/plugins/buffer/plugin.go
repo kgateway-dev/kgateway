935d12d7d5a5f2115a9ff9c983414cb46bbe3df5
Shane O'Donnell <sodman74@gmail.com>
2020-06-02 10:10:18 -0400

    Update filter name and changelog description

--- projects/gloo/pkg/plugins/buffer/plugin.go
+++ projects/gloo/pkg/plugins/buffer/plugin.go
@@ -11,13 +11,12 @@ import (
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins"
 	"github.com/solo-io/gloo/projects/gloo/pkg/plugins/pluginutils"
-	"github.com/solo-io/solo-kit/pkg/api/v1/control-plane/util"
 )
 
 // filter should be called after routing decision has been made
 var pluginStage = plugins.DuringStage(plugins.RouteStage)
 
-const FilterName = util.Buffer
+const FilterName = "envoy.filters.http.buffer"
 
 func NewPlugin() *Plugin {
 	return &Plugin{}
