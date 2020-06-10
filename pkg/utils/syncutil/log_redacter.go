83f43595e905a32393ae3b81bd51b45aad5e2a54
GrahamGoudeau <grahamgoudeau@gmail.com>
2020-06-10 10:12:03 -0400

    Implement redacter in Gloo

--- pkg/utils/syncutil/log_redacter.go
+++ pkg/utils/syncutil/log_redacter.go
@@ -1,14 +1,22 @@
 package syncutil
 
 import (
+	"encoding/json"
 	"fmt"
 	"reflect"
 	"strings"
 
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	xdsproto "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/enterprise/options/extauth/v1"
 )
 
-const Redacted = "[REDACTED]"
+const (
+	Redacted = "[REDACTED]"
+
+	TagName                  = "logging"
+	TagValue                 = "redact"
+	SerializationFieldPrefix = "XXX"
+)
 
 // stringify the contents of the snapshot
 //
@@ -52,4 +60,57 @@ func StringifySnapshot(snapshot interface{}) string {
 	}
 
 	return stringBuilder.String()
+}
+
+type Logger interface {
+	Infof(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+// Convert all the configs into JSON, but without XXX_ serialization fields and without struct fields tagged with "logging: redact", then log them in that form
+func LogRedactedExtAuthConfigs(logger Logger, configs []*xdsproto.ExtAuthConfig) {
+	logger.Infof("Processing %d new configs", len(configs))
+	for _, authConfig := range configs {
+		for _, authConfigSpec := range authConfig.Configs {
+			redactedJson := redactObject(authConfigSpec)
+			jsonBytes, err := json.Marshal(redactedJson)
+			if err == nil {
+				logger.Infof("New config: %s", string(jsonBytes))
+			} else {
+				logger.Errorf("Failed to convert auth config into redacted JSON for logging: %+v", err)
+			}
+		}
+	}
+}
+
+func redactObject(o interface{}) map[string]interface{} {
+	if reflect.ValueOf(o).IsNil() {
+		return nil
+	}
+	jsonRepresentation := map[string]interface{}{}
+	if o == struct{}{} {
+		return jsonRepresentation
+	}
+	elem := reflect.ValueOf(o).Elem()
+	for i := 0; i < elem.NumField(); i++ {
+		fieldName := elem.Type().Field(i).Name
+
+		if strings.HasPrefix(fieldName, SerializationFieldPrefix) {
+			continue
+		}
+
+		fieldValue := elem.Field(i).Interface()
+		kind := elem.Field(i).Kind()
+
+		tagValue := elem.Type().Field(i).Tag.Get(TagName)
+		if kind == reflect.Struct || kind == reflect.Interface || kind == reflect.Ptr {
+			jsonRepresentation[fieldName] = redactObject(fieldValue)
+		} else if tagValue == TagValue {
+			jsonRepresentation[fieldName] = Redacted
+		} else {
+			jsonRepresentation[fieldName] = fieldValue
+		}
+	}
+
+	return jsonRepresentation
 }
