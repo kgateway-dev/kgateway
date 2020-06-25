770abea7ef564732f9152931001b5d3da8db5c87
Shane O'Donnell <shane.odonnell@solo.io>
2020-06-25 14:29:51 -0400

    Move expectNonRoot helper func into the describe context

--- install/test/helm_test.go
+++ install/test/helm_test.go
@@ -2251,6 +2251,45 @@ metadata:
 			})
 
 			Describe("Deployment Privileges Test", func() {
+
+				// Helper func for testing pod & container root privileges logic
+				expectNonRoot := func(testManifest manifesttestutils.TestManifest) {
+					deployments := testManifest.SelectResources(func(resource *unstructured.Unstructured) bool {
+						return resource.GetKind() == "Deployment"
+					})
+
+					Expect(deployments.NumResources()).NotTo(BeZero())
+
+					deployments.ExpectAll(func(resource *unstructured.Unstructured) {
+						rawDeploy, err := resource.MarshalJSON()
+						Expect(err).NotTo(HaveOccurred())
+
+						deploy := appsv1.Deployment{}
+						err = json.Unmarshal(rawDeploy, &deploy)
+						Expect(err).NotTo(HaveOccurred())
+
+						Expect(deploy.Spec.Template).NotTo(BeNil())
+
+						podLevelSecurity := false
+						// Check for root at the pod level
+						if deploy.Spec.Template.Spec.SecurityContext != nil {
+							Expect(deploy.Spec.Template.Spec.SecurityContext.RunAsUser).NotTo(Equal(0))
+							podLevelSecurity = true
+						}
+
+						// Check for root at the container level
+						for _, container := range deploy.Spec.Template.Spec.Containers {
+							if !podLevelSecurity {
+								// If pod level security is not set, containers need to explicitly not be run as root
+								Expect(container.SecurityContext).NotTo(BeNil())
+								Expect(container.SecurityContext.RunAsUser).NotTo(Equal(0))
+							} else if container.SecurityContext != nil {
+								// If podLevel security is set to non-root, make sure containers don't override it:
+								Expect(container.SecurityContext.RunAsUser).NotTo(Equal(0))
+							}
+						}
+					})
+				}
 				Context("Gloo", func() {
 					Context("all cluster-scoped deployments", func() {
 						It("is running all deployments with non root user permissions by default", func() {
@@ -2292,42 +2331,4 @@ func cloneMap(input map[string]string) map[string]string {
 	}
 
 	return ret
-}
-
-func expectNonRoot(testManifest manifesttestutils.TestManifest) {
-	deployments := testManifest.SelectResources(func(resource *unstructured.Unstructured) bool {
-		return resource.GetKind() == "Deployment"
-	})
-
-	Expect(deployments.NumResources()).NotTo(BeZero())
-
-	deployments.ExpectAll(func(resource *unstructured.Unstructured) {
-		rawDeploy, err := resource.MarshalJSON()
-		Expect(err).NotTo(HaveOccurred())
-
-		deploy := appsv1.Deployment{}
-		err = json.Unmarshal(rawDeploy, &deploy)
-		Expect(err).NotTo(HaveOccurred())
-
-		Expect(deploy.Spec.Template).NotTo(BeNil())
-
-		podLevelSecurity := false
-		// Check for root at the pod level
-		if deploy.Spec.Template.Spec.SecurityContext != nil {
-			Expect(deploy.Spec.Template.Spec.SecurityContext.RunAsUser).NotTo(Equal(0))
-			podLevelSecurity = true
-		}
-
-		// Check for root at the container level
-		for _, container := range deploy.Spec.Template.Spec.Containers {
-			if !podLevelSecurity {
-				// If pod level security is not set, containers need to explicitly not be run as root
-				Expect(container.SecurityContext).NotTo(BeNil())
-				Expect(container.SecurityContext.RunAsUser).NotTo(Equal(0))
-			} else if container.SecurityContext != nil {
-				// If podLevel security is set to non-root, make sure containers don't override it:
-				Expect(container.SecurityContext.RunAsUser).NotTo(Equal(0))
-			}
-		}
-	})
 }
