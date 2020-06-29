2d7eb059f64f6028637c8f8b51ac35df219a400b
arianaw66 <ariana.weinstock@solo.io>
2020-06-29 18:01:26 -0400

    unmarshal to hcm v3

--- projects/gloo/pkg/xds/envoy_resource.go
+++ projects/gloo/pkg/xds/envoy_resource.go
@@ -17,7 +17,11 @@ package xds
 import (
 	v2 "github.com/envoyproxy/go-control-plane/envoy/api/v2"
 	listener "github.com/envoyproxy/go-control-plane/envoy/api/v2/listener"
-	hcm "github.com/envoyproxy/go-control-plane/envoy/config/filter/network/http_connection_manager/v2"
+	cluster "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
+	endpoint "github.com/envoyproxy/go-control-plane/envoy/config/endpoint/v3"
+	listenerv3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
+	route "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
+	hcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/conversion"
 	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"github.com/golang/protobuf/ptypes"
@@ -63,6 +67,15 @@ func (e *EnvoyResource) Self() cache.XdsResourceReference {
 // GetResourceName returns the resource name for a valid xDS response type.
 func (e *EnvoyResource) Name() string {
 	switch v := e.ProtoMessage.(type) {
+	case *endpoint.ClusterLoadAssignment:
+		return v.GetClusterName()
+	case *cluster.Cluster:
+		return v.GetName()
+	case *route.RouteConfiguration:
+		return v.GetName()
+	case *listenerv3.Listener:
+		return v.GetName()
+	// keeping cases below as temporary solution to enable incremental changes
 	case *v2.ClusterLoadAssignment:
 		return v.GetClusterName()
 	case *v2.Cluster:
@@ -82,6 +95,15 @@ func (e *EnvoyResource) ResourceProto() cache.ResourceProto {
 
 func (e *EnvoyResource) Type() string {
 	switch e.ProtoMessage.(type) {
+	case *endpoint.ClusterLoadAssignment:
+		return EndpointType
+	case *cluster.Cluster:
+		return ClusterType
+	case *route.RouteConfiguration:
+		return RouteType
+	case *listenerv3.Listener:
+		return ListenerType
+	// keeping cases below in case- as temporary solution to enable incremental changes
 	case *v2.ClusterLoadAssignment:
 		return EndpointType
 	case *v2.Cluster:
