aa5a5039c31fa23171f27f9fe3f587e76d6070aa
John Hawley <john.hawley@solo.io>
2020-07-14 15:34:56 +0000

    use getters

--- projects/discovery/pkg/fds/syncer/discovery_syncer.go
+++ projects/discovery/pkg/fds/syncer/discovery_syncer.go
@@ -75,14 +75,12 @@ func selectUpstreamsForDiscovery(fdsMode v1.Settings_DiscoveryOptions_FdsMode, u
 
 func isBlacklistedUpstream(us *v1.Upstream) bool {
 	// Fall back to Metadata labels to support legacy Upstreams if needed
-	return (us.DiscoveryMetadata != nil && isBlacklisted(us.DiscoveryMetadata.Labels)) ||
-		isBlacklisted(us.Metadata.Labels)
+	return isBlacklisted(us.GetDiscoveryMetadata().GetLabels()) || isBlacklisted(us.Metadata.Labels)
 }
 
 func isWhitelistedUpstream(us *v1.Upstream) bool {
 	// Fall back to Metadata labels to support legacy Upstreams if needed
-	return (us.DiscoveryMetadata != nil && isWhitelisted(us.DiscoveryMetadata.Labels)) ||
-		isWhitelisted(us.Metadata.Labels)
+	return isWhitelisted(us.GetDiscoveryMetadata().GetLabels()) || isWhitelisted(us.Metadata.Labels)
 }
 
 func isBlacklisted(labels map[string]string) bool {
