f72673b7f8d74bb5494eea66eb278164aa75eba7
Miles Holland <milesholland@Miless-MacBook-Pro.local>
2020-07-23 15:43:47 -0400

    saving stuff

--- projects/gloo/cli/pkg/cmd/install/installer.go
+++ projects/gloo/cli/pkg/cmd/install/installer.go
@@ -170,6 +170,9 @@ func (i *installer) createNamespace(namespace string) {
 		if _, err := i.kubeNsClient.Create(&corev1.Namespace{
 			ObjectMeta: metav1.ObjectMeta{
 				Name: namespace,
+				Labels: map[string]string{
+					"namespace": namespace,
+				},
 			},
 		}); err != nil {
 			fmt.Printf("\nUnable to create namespace %s. Continuing...\n", namespace)
