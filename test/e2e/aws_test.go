6f2b80bcdb40ff27a523278ced409e85aaf9abe7
EItanya <eitan.yarmush@solo.io>
2020-07-24 16:36:32 -0400

    code-gen

--- test/e2e/aws_test.go
+++ test/e2e/aws_test.go
@@ -8,6 +8,8 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/aws/aws-sdk-go/aws/credentials/stscreds"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/solo-io/gloo/test/helpers"
 
 	gwdefaults "github.com/solo-io/gloo/projects/gateway/pkg/defaults"
@@ -43,33 +45,48 @@ var _ = Describe("AWS Lambda", func() {
 		upstream      *gloov1.Upstream
 	)
 
-	addCredentials := func() {
+	setupEnvoy := func() {
+		ctx, cancel = context.WithCancel(context.Background())
+		defaults.HttpPort = services.NextBindPort()
+		defaults.HttpsPort = services.NextBindPort()
 
-		localAwsCredentials := credentials.NewSharedCredentials("", "")
-		v, err := localAwsCredentials.Get()
-		if err != nil {
-			Skip("no AWS creds available")
-		}
-		var opts clients.WriteOpts
+		testClients = services.RunGateway(ctx, false)
 
-		accesskey := v.AccessKeyID
-		secretkey := v.SecretAccessKey
+		err := helpers.WriteDefaultGateways(defaults.GlooSystem, testClients.GatewayClient)
+		Expect(err).NotTo(HaveOccurred(), "Should be able to write default gateways")
 
-		secret = &gloov1.Secret{
-			Metadata: core.Metadata{
-				Namespace: "default",
-				Name:      region,
-			},
-			Kind: &gloov1.Secret_Aws{
-				Aws: &gloov1.AwsSecret{
-					AccessKey: accesskey,
-					SecretKey: secretkey,
-				},
-			},
-		}
-
-		_, err = testClients.SecretClient.Write(secret, opts)
+		envoyInstance, err = envoyFactory.NewEnvoyInstance()
 		Expect(err).NotTo(HaveOccurred())
+	}
+
+	validateLambda := func(offset int, envoyPort uint32, substring string) {
+
+		body := []byte("\"solo.io\"")
+
+		EventuallyWithOffset(offset, func() (string, error) {
+			// send a request with a body
+			var buf bytes.Buffer
+			buf.Write(body)
+
+			res, err := http.Post(fmt.Sprintf("http://%s:%d/1", "localhost", envoyPort), "application/octet-stream", &buf)
+			if err != nil {
+				return "", err
+			}
+			if res.StatusCode != http.StatusOK {
+				return "", errors.New(fmt.Sprintf("%v is not OK", res.StatusCode))
+			}
+
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			if err != nil {
+				return "", err
+			}
+
+			return string(body), nil
+		}, "20s", "1s").Should(ContainSubstring(substring))
+	}
+	validateLambdaUppercase := func(envoyPort uint32) {
+		validateLambda(2, envoyPort, "SOLO.IO")
 	}
 
 	addUpstream := func() {
@@ -100,55 +117,7 @@ var _ = Describe("AWS Lambda", func() {
 		var opts clients.WriteOpts
 		_, err := testClients.UpstreamClient.Write(upstream, opts)
 		Expect(err).NotTo(HaveOccurred())
-
 	}
-
-	validateLambda := func(envoyPort uint32, substring string) {
-
-		body := []byte("\"solo.io\"")
-
-		Eventually(func() (string, error) {
-			// send a request with a body
-			var buf bytes.Buffer
-			buf.Write(body)
-
-			res, err := http.Post(fmt.Sprintf("http://%s:%d/1", "localhost", envoyPort), "application/octet-stream", &buf)
-			if err != nil {
-				return "", err
-			}
-			if res.StatusCode != http.StatusOK {
-				return "", errors.New(fmt.Sprintf("%v is not OK", res.StatusCode))
-			}
-
-			defer res.Body.Close()
-			body, err := ioutil.ReadAll(res.Body)
-			if err != nil {
-				return "", err
-			}
-
-			return string(body), nil
-		}, "20s", "1s").Should(ContainSubstring(substring))
-	}
-	validateLambdaUppercase := func(envoyPort uint32) {
-		validateLambda(envoyPort, "SOLO.IO")
-	}
-
-	BeforeEach(func() {
-		ctx, cancel = context.WithCancel(context.Background())
-		defaults.HttpPort = services.NextBindPort()
-		defaults.HttpsPort = services.NextBindPort()
-
-		testClients = services.RunGateway(ctx, false)
-
-		err := helpers.WriteDefaultGateways(defaults.GlooSystem, testClients.GatewayClient)
-		Expect(err).NotTo(HaveOccurred(), "Should be able to write default gateways")
-
-		envoyInstance, err = envoyFactory.NewEnvoyInstance()
-		Expect(err).NotTo(HaveOccurred())
-
-		addCredentials()
-		addUpstream()
-	})
 
 	AfterEach(func() {
 		if envoyInstance != nil {
@@ -157,147 +126,275 @@ var _ = Describe("AWS Lambda", func() {
 		cancel()
 	})
 
-	It("be able to call lambda", func() {
-		err := envoyInstance.Run(testClients.GlooPort)
-		Expect(err).NotTo(HaveOccurred())
+	Context("Basic Auth", func() {
 
-		proxy := &gloov1.Proxy{
-			Metadata: core.Metadata{
-				Name:      "proxy",
-				Namespace: "default",
-			},
-			Listeners: []*gloov1.Listener{{
-				Name:        "listener",
-				BindAddress: "::",
-				BindPort:    defaults.HttpPort,
-				ListenerType: &gloov1.Listener_HttpListener{
-					HttpListener: &gloov1.HttpListener{
-						VirtualHosts: []*gloov1.VirtualHost{{
-							Name:    "virt1",
-							Domains: []string{"*"},
-							Routes: []*gloov1.Route{{
-								Action: &gloov1.Route_RouteAction{
-									RouteAction: &gloov1.RouteAction{
-										Destination: &gloov1.RouteAction_Single{
-											Single: &gloov1.Destination{
-												DestinationType: &gloov1.Destination_Upstream{
-													Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
-												},
-												DestinationSpec: &gloov1.DestinationSpec{
-													DestinationType: &gloov1.DestinationSpec_Aws{
-														Aws: &aws_plugin.DestinationSpec{
-															LogicalName: "uppercase",
+		addCredentials := func() {
+
+			localAwsCredentials := credentials.NewSharedCredentials("", "")
+			v, err := localAwsCredentials.Get()
+			if err != nil {
+				Skip("no AWS creds available")
+			}
+			var opts clients.WriteOpts
+
+			accesskey := v.AccessKeyID
+			secretkey := v.SecretAccessKey
+
+			secret = &gloov1.Secret{
+				Metadata: core.Metadata{
+					Namespace: "default",
+					Name:      region,
+				},
+				Kind: &gloov1.Secret_Aws{
+					Aws: &gloov1.AwsSecret{
+						AccessKey: accesskey,
+						SecretKey: secretkey,
+					},
+				},
+			}
+
+			_, err = testClients.SecretClient.Write(secret, opts)
+			Expect(err).NotTo(HaveOccurred())
+		}
+
+		BeforeEach(func() {
+			setupEnvoy()
+			addCredentials()
+			addUpstream()
+		})
+
+		It("be able to call lambda", func() {
+			err := envoyInstance.Run(testClients.GlooPort)
+			Expect(err).NotTo(HaveOccurred())
+
+			proxy := &gloov1.Proxy{
+				Metadata: core.Metadata{
+					Name:      "proxy",
+					Namespace: "default",
+				},
+				Listeners: []*gloov1.Listener{{
+					Name:        "listener",
+					BindAddress: "::",
+					BindPort:    defaults.HttpPort,
+					ListenerType: &gloov1.Listener_HttpListener{
+						HttpListener: &gloov1.HttpListener{
+							VirtualHosts: []*gloov1.VirtualHost{{
+								Name:    "virt1",
+								Domains: []string{"*"},
+								Routes: []*gloov1.Route{{
+									Action: &gloov1.Route_RouteAction{
+										RouteAction: &gloov1.RouteAction{
+											Destination: &gloov1.RouteAction_Single{
+												Single: &gloov1.Destination{
+													DestinationType: &gloov1.Destination_Upstream{
+														Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
+													},
+													DestinationSpec: &gloov1.DestinationSpec{
+														DestinationType: &gloov1.DestinationSpec_Aws{
+															Aws: &aws_plugin.DestinationSpec{
+																LogicalName: "uppercase",
+															},
 														},
 													},
 												},
 											},
 										},
 									},
-								},
+								}},
 							}},
-						}},
+						},
 					},
+				}},
+			}
+
+			var opts clients.WriteOpts
+			_, err = testClients.ProxyClient.Write(proxy, opts)
+			Expect(err).NotTo(HaveOccurred())
+
+			validateLambdaUppercase(defaults.HttpPort)
+		})
+
+		It("be able lambda with response transform", func() {
+			err := envoyInstance.Run(testClients.GlooPort)
+			Expect(err).NotTo(HaveOccurred())
+
+			proxy := &gloov1.Proxy{
+				Metadata: core.Metadata{
+					Name:      "proxy",
+					Namespace: "default",
 				},
-			}},
-		}
-
-		var opts clients.WriteOpts
-		_, err = testClients.ProxyClient.Write(proxy, opts)
-		Expect(err).NotTo(HaveOccurred())
-
-		validateLambdaUppercase(defaults.HttpPort)
-	})
-
-	It("be able lambda with response transform", func() {
-		err := envoyInstance.Run(testClients.GlooPort)
-		Expect(err).NotTo(HaveOccurred())
-
-		proxy := &gloov1.Proxy{
-			Metadata: core.Metadata{
-				Name:      "proxy",
-				Namespace: "default",
-			},
-			Listeners: []*gloov1.Listener{{
-				Name:        "listener",
-				BindAddress: "::",
-				BindPort:    defaults.HttpPort,
-				ListenerType: &gloov1.Listener_HttpListener{
-					HttpListener: &gloov1.HttpListener{
-						VirtualHosts: []*gloov1.VirtualHost{{
-							Name:    "virt1",
-							Domains: []string{"*"},
-							Routes: []*gloov1.Route{{
-								Action: &gloov1.Route_RouteAction{
-									RouteAction: &gloov1.RouteAction{
-										Destination: &gloov1.RouteAction_Single{
-											Single: &gloov1.Destination{
-												DestinationType: &gloov1.Destination_Upstream{
-													Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
-												},
-												DestinationSpec: &gloov1.DestinationSpec{
-													DestinationType: &gloov1.DestinationSpec_Aws{
-														Aws: &aws_plugin.DestinationSpec{
-															LogicalName:            "contact-form",
-															ResponseTransformation: true,
+				Listeners: []*gloov1.Listener{{
+					Name:        "listener",
+					BindAddress: "::",
+					BindPort:    defaults.HttpPort,
+					ListenerType: &gloov1.Listener_HttpListener{
+						HttpListener: &gloov1.HttpListener{
+							VirtualHosts: []*gloov1.VirtualHost{{
+								Name:    "virt1",
+								Domains: []string{"*"},
+								Routes: []*gloov1.Route{{
+									Action: &gloov1.Route_RouteAction{
+										RouteAction: &gloov1.RouteAction{
+											Destination: &gloov1.RouteAction_Single{
+												Single: &gloov1.Destination{
+													DestinationType: &gloov1.Destination_Upstream{
+														Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
+													},
+													DestinationSpec: &gloov1.DestinationSpec{
+														DestinationType: &gloov1.DestinationSpec_Aws{
+															Aws: &aws_plugin.DestinationSpec{
+																LogicalName:            "contact-form",
+																ResponseTransformation: true,
+															},
 														},
 													},
 												},
 											},
 										},
 									},
-								},
+								}},
 							}},
-						}},
+						},
 					},
+				}},
+			}
+
+			var opts clients.WriteOpts
+			_, err = testClients.ProxyClient.Write(proxy, opts)
+			Expect(err).NotTo(HaveOccurred())
+
+			validateLambda(1, defaults.HttpPort, `<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>`)
+		})
+
+		It("be able to call lambda via gateway", func() {
+			err := envoyInstance.RunWithRole("gloo-system~"+gwdefaults.GatewayProxyName, testClients.GlooPort)
+			Expect(err).NotTo(HaveOccurred())
+
+			vs := &gw1.VirtualService{
+				Metadata: core.Metadata{
+					Name:      "app",
+					Namespace: "gloo-system",
 				},
-			}},
-		}
-
-		var opts clients.WriteOpts
-		_, err = testClients.ProxyClient.Write(proxy, opts)
-		Expect(err).NotTo(HaveOccurred())
-
-		validateLambda(defaults.HttpPort, `<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>`)
-	})
-
-	It("be able to call lambda via gateway", func() {
-		err := envoyInstance.RunWithRole("gloo-system~"+gwdefaults.GatewayProxyName, testClients.GlooPort)
-		Expect(err).NotTo(HaveOccurred())
-
-		vs := &gw1.VirtualService{
-			Metadata: core.Metadata{
-				Name:      "app",
-				Namespace: "gloo-system",
-			},
-			VirtualHost: &gw1.VirtualHost{
-				Domains: []string{"*"},
-				Routes: []*gw1.Route{{
-					Action: &gw1.Route_RouteAction{
-						RouteAction: &gloov1.RouteAction{
-							Destination: &gloov1.RouteAction_Single{
-								Single: &gloov1.Destination{
-									DestinationType: &gloov1.Destination_Upstream{
-										Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
-									},
-									DestinationSpec: &gloov1.DestinationSpec{
-										DestinationType: &gloov1.DestinationSpec_Aws{
-											Aws: &aws_plugin.DestinationSpec{
-												LogicalName: "uppercase",
+				VirtualHost: &gw1.VirtualHost{
+					Domains: []string{"*"},
+					Routes: []*gw1.Route{{
+						Action: &gw1.Route_RouteAction{
+							RouteAction: &gloov1.RouteAction{
+								Destination: &gloov1.RouteAction_Single{
+									Single: &gloov1.Destination{
+										DestinationType: &gloov1.Destination_Upstream{
+											Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
+										},
+										DestinationSpec: &gloov1.DestinationSpec{
+											DestinationType: &gloov1.DestinationSpec_Aws{
+												Aws: &aws_plugin.DestinationSpec{
+													LogicalName: "uppercase",
+												},
 											},
 										},
 									},
 								},
 							},
 						},
+					}},
+				},
+			}
+
+			var opts clients.WriteOpts
+			_, err = testClients.VirtualServiceClient.Write(vs, opts)
+			Expect(err).NotTo(HaveOccurred())
+
+			validateLambdaUppercase(defaults.HttpPort)
+		})
+	})
+
+	FContext("Temporary Credentials", func() {
+
+		addCredentials := func() {
+
+			sess := session.Must(session.NewSession())
+
+			localAwsCredentials := stscreds.NewCredentials(sess, "myRoleArn")
+
+			v, err := localAwsCredentials.Get()
+			if err != nil {
+				Skip("no AWS creds available")
+			}
+			var opts clients.WriteOpts
+			secret = &gloov1.Secret{
+				Metadata: core.Metadata{
+					Namespace: "default",
+					Name:      region,
+				},
+				Kind: &gloov1.Secret_Aws{
+					Aws: &gloov1.AwsSecret{
+						AccessKey:    v.AccessKeyID,
+						SecretKey:    v.SecretAccessKey,
+						SessionToken: v.SessionToken,
 					},
-				}},
-			},
+				},
+			}
+
+			_, err = testClients.SecretClient.Write(secret, opts)
+			Expect(err).NotTo(HaveOccurred())
 		}
 
-		var opts clients.WriteOpts
-		_, err = testClients.VirtualServiceClient.Write(vs, opts)
-		Expect(err).NotTo(HaveOccurred())
+		BeforeEach(func() {
+			setupEnvoy()
+			addCredentials()
+			addUpstream()
+		})
 
-		validateLambdaUppercase(defaults.HttpPort)
+		It("be able to call lambda", func() {
+			err := envoyInstance.Run(testClients.GlooPort)
+			Expect(err).NotTo(HaveOccurred())
+
+			proxy := &gloov1.Proxy{
+				Metadata: core.Metadata{
+					Name:      "proxy",
+					Namespace: "default",
+				},
+				Listeners: []*gloov1.Listener{{
+					Name:        "listener",
+					BindAddress: "::",
+					BindPort:    defaults.HttpPort,
+					ListenerType: &gloov1.Listener_HttpListener{
+						HttpListener: &gloov1.HttpListener{
+							VirtualHosts: []*gloov1.VirtualHost{{
+								Name:    "virt1",
+								Domains: []string{"*"},
+								Routes: []*gloov1.Route{{
+									Action: &gloov1.Route_RouteAction{
+										RouteAction: &gloov1.RouteAction{
+											Destination: &gloov1.RouteAction_Single{
+												Single: &gloov1.Destination{
+													DestinationType: &gloov1.Destination_Upstream{
+														Upstream: utils.ResourceRefPtr(upstream.Metadata.Ref()),
+													},
+													DestinationSpec: &gloov1.DestinationSpec{
+														DestinationType: &gloov1.DestinationSpec_Aws{
+															Aws: &aws_plugin.DestinationSpec{
+																LogicalName: "uppercase",
+															},
+														},
+													},
+												},
+											},
+										},
+									},
+								}},
+							}},
+						},
+					},
+				}},
+			}
+
+			var opts clients.WriteOpts
+			_, err = testClients.ProxyClient.Write(proxy, opts)
+			Expect(err).NotTo(HaveOccurred())
+
+			validateLambdaUppercase(defaults.HttpPort)
+		})
 	})
+
 })
