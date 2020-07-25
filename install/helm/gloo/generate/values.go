1336309de95f61c6a04a21f0f85baf2961931170
EItanya <eitan.yarmush@solo.io>
2020-07-25 12:45:53 -0400

    added SA

--- install/helm/gloo/generate/values.go
+++ install/helm/gloo/generate/values.go
@@ -125,7 +125,8 @@ type InvalidConfigPolicy struct {
 }
 
 type Gloo struct {
-	Deployment *GlooDeployment `json:"deployment,omitempty"`
+	Deployment     *GlooDeployment `json:"deployment,omitempty"`
+	ServiceAccount `json:"serviceAccount" `
 }
 
 type GlooDeployment struct {
@@ -142,9 +143,10 @@ type GlooDeployment struct {
 }
 
 type Discovery struct {
-	Deployment *DiscoveryDeployment `json:"deployment,omitempty"`
-	FdsMode    string               `json:"fdsMode" desc:"mode for function discovery (blacklist or whitelist). See more info in the settings docs"`
-	Enabled    *bool                `json:"enabled" desc:"enable Discovery features"`
+	Deployment     *DiscoveryDeployment `json:"deployment,omitempty"`
+	FdsMode        string               `json:"fdsMode" desc:"mode for function discovery (blacklist or whitelist). See more info in the settings docs"`
+	Enabled        *bool                `json:"enabled" desc:"enable Discovery features"`
+	ServiceAccount `json:"serviceAccount" `
 }
 
 type DiscoveryDeployment struct {
@@ -156,17 +158,22 @@ type DiscoveryDeployment struct {
 }
 
 type Gateway struct {
-	Enabled                       *bool              `json:"enabled" desc:"enable Gloo API Gateway features"`
-	Validation                    *GatewayValidation `json:"validation" desc:"enable Validation Webhook on the Gateway. This will cause requests to modify Gateway-related Custom Resources to be validated by the Gateway."`
-	Deployment                    *GatewayDeployment `json:"deployment,omitempty"`
-	CertGenJob                    *CertGenJob        `json:"certGenJob,omitempty" desc:"generate self-signed certs with this job to be used with the gateway validation webhook. this job will only run if validation is enabled for the gateway"`
-	UpdateValues                  bool               `json:"updateValues" desc:"if true, will use a provided helm helper 'gloo.updatevalues' to update values during template render - useful for plugins/extensions"`
-	ProxyServiceAccount           ServiceAccount     `json:"proxyServiceAccount" `
-	ReadGatewaysFromAllNamespaces bool               `json:"readGatewaysFromAllNamespaces" desc:"if true, read Gateway custom resources from all watched namespaces rather than just the namespace of the Gateway controller"`
+	Enabled                       *bool               `json:"enabled" desc:"enable Gloo API Gateway features"`
+	Validation                    *GatewayValidation  `json:"validation" desc:"enable Validation Webhook on the Gateway. This will cause requests to modify Gateway-related Custom Resources to be validated by the Gateway."`
+	Deployment                    *GatewayDeployment  `json:"deployment,omitempty"`
+	CertGenJob                    *CertGenJob         `json:"certGenJob,omitempty" desc:"generate self-signed certs with this job to be used with the gateway validation webhook. this job will only run if validation is enabled for the gateway"`
+	UpdateValues                  bool                `json:"updateValues" desc:"if true, will use a provided helm helper 'gloo.updatevalues' to update values during template render - useful for plugins/extensions"`
+	ProxyServiceAccount           ProxyServiceAccount `json:"proxyServiceAccount" `
+	ReadGatewaysFromAllNamespaces bool                `json:"readGatewaysFromAllNamespaces" desc:"if true, read Gateway custom resources from all watched namespaces rather than just the namespace of the Gateway controller"`
+}
+
+type ProxyServiceAccount struct {
+	DisableAutomount bool `json:"disableAutomount" desc:"disable automunting the service account to the gateway proxy. not mounting the token hardens the proxy container, but may interfere with service mesh integrations"`
+	ServiceAccount   `json:"serviceAccount" `
 }
 
 type ServiceAccount struct {
-	DisableAutomount bool `json:"disableAutomount" desc:"disable automunting the service account to the gateway proxy. not mounting the token hardens the proxy container, but may interfere with service mesh integrations"`
+	ExtraAnnotations map[string]string `json:"extraAnnotations,omitempty" desc:"extra annotations to add to the service account"`
 }
 
 type GatewayValidation struct {
