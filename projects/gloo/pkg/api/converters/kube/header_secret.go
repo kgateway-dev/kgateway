02f8d24fcd6f4d25872c071fdb9e81ff7080b321
John Hawley <john.hawley@solo.io>
2020-08-12 16:31:16 -0400

    tests

---
+++ projects/gloo/pkg/api/converters/kube/header_secret.go
@@ -0,0 +1,71 @@
+package kubeconverters
+
+import (
+	"context"
+	skcore "github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
+	"github.com/solo-io/solo-kit/pkg/api/v1/clients/kubesecret"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
+	"github.com/solo-io/solo-kit/pkg/utils/kubeutils"
+	kubev1 "k8s.io/api/core/v1"
+)
+
+type HeaderSecretConverter struct{}
+
+var _ kubesecret.SecretConverter = &HeaderSecretConverter{}
+
+const (
+	HeaderName = "header-name"
+	Value      = "value"
+)
+
+func (t *HeaderSecretConverter) FromKubeSecret(_ context.Context, _ *kubesecret.ResourceClient, secret *kubev1.Secret) (resources.Resource, error) {
+	headerName, hasHeaderName := secret.Data[HeaderName]
+	value, hasValue := secret.Data[Value]
+	if hasHeaderName && hasValue {
+		skSecret := &v1.Secret{
+			Metadata: skcore.Metadata{
+				Name:        secret.Name,
+				Namespace:   secret.Namespace,
+				Cluster:     secret.ClusterName,
+				Labels:      secret.Labels,
+				Annotations: secret.Annotations,
+			},
+			Kind: &v1.Secret_Header{
+				Header: &v1.HeaderSecret{
+					HeaderName: string(headerName),
+					Value:      string(value),
+				},
+			},
+		}
+
+		return skSecret, nil
+	}
+	// any unmatched secrets will be handled by subsequent converters
+	return nil, nil
+}
+
+func (t *HeaderSecretConverter) ToKubeSecret(_ context.Context, _ *kubesecret.ResourceClient, resource resources.Resource) (*kubev1.Secret, error) {
+	glooSecret, ok := resource.(*v1.Secret)
+	if !ok {
+		return nil, nil
+	}
+	headerGlooSecret, ok := glooSecret.Kind.(*v1.Secret_Header)
+	if !ok {
+		return nil, nil
+	}
+
+	kubeMeta := kubeutils.ToKubeMeta(glooSecret.Metadata)
+
+	kubeSecret := &kubev1.Secret{
+		ObjectMeta: kubeMeta,
+		Type:       kubev1.SecretTypeOpaque,
+		Data: map[string][]byte{
+			HeaderName: []byte(headerGlooSecret.Header.HeaderName),
+			Value:      []byte(headerGlooSecret.Header.Value),
+		},
+	}
+
+	return kubeSecret, nil
+}
