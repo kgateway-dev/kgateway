f35a811997ad497c9c2ccdf59e5a015868eaa941
Miles Holland <milesholland@Miless-MacBook-Pro.local>
2020-08-17 14:17:31 -0400

    CI nits

--- projects/gloo/pkg/translator/listener.go
+++ projects/gloo/pkg/translator/listener.go
@@ -7,9 +7,6 @@ import (
 
 	errors "github.com/rotisserie/eris"
 
-	"github.com/solo-io/gloo/projects/gloo/pkg/utils"
-
-	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
 	"github.com/gogo/protobuf/proto"
 
 	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
