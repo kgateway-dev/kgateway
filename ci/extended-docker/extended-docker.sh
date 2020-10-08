c609c91e9912ae9e96656c2b47af0251b2708b4a
Kevin Dorosh <kcdorosh@gmail.com>
2020-10-08 13:46:03 -0400

    Harden script

--- ci/extended-docker/extended-docker.sh
+++ ci/extended-docker/extended-docker.sh
@@ -1,10 +1,27 @@
-#!/bin/bash -ex
+#!/bin/bash
 
-make docker-push | tee docker-logs.txt
+# make all the docker images
+# write the output to a temp file so that we can grab the image names out of it
+# also ensure we clean up the file once we're done
+TEMP_FILE=$(mktemp)
+make docker-push | tee ${TEMP_FILE}
 
-sed -n '/^Successfully tagged /p' docker-logs.txt | sed 's/^Successfully tagged //' > docker-containers.txt
+cleanup() {
+    echo ">> Removing ${TEMP_FILE}"
+    rm ${TEMP_FILE}
+}
+trap "cleanup" EXIT SIGINT
 
-xargs -I % docker build ci/extended-docker --build-arg BASE_IMAGE=% -t %-extended < docker-containers.txt
-xargs -I % docker push %-extended < docker-containers.txt
+echo ">> Temporary output file ${TEMP_FILE}"
 
-rm docker-logs.txt docker-containers.txt
+# grab the image names out of the `make docker` output
+sed -nE 's|Successfully tagged (.*$)|\1|p' ${TEMP_FILE} | while read f;
+do
+  docker build ci/extended-docker --build-arg BASE_IMAGE=$f -t $f-extended;
+  docker push $f-extended;
+done
+
+#xargs -I % docker build ci/extended-docker --build-arg BASE_IMAGE=% -t %-extended < docker-containers.txt
+#xargs -I % docker push %-extended < docker-containers.txt
+
+#rm docker-logs.txt docker-containers.txt
