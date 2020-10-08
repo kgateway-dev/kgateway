9c127d731545136610fb4abe75ca01c476501bda
Kevin Dorosh <kcdorosh@gmail.com>
2020-10-08 13:30:32 -0400

    Small tweaks

--- ci/extended-docker/extended-docker.sh
+++ ci/extended-docker/extended-docker.sh
@@ -4,7 +4,7 @@ make docker-push | tee docker-logs.txt
 
 sed -n '/^Successfully tagged /p' docker-logs.txt | sed 's/^Successfully tagged //' > docker-containers.txt
 
-xargs -I % docker build ci --build-arg BASE_IMAGE=% -t %-extended < docker-containers.txt
+xargs -I % docker build ci/extended-docker --build-arg BASE_IMAGE=% -t %-extended < docker-containers.txt
 xargs -I % docker push %-extended < docker-containers.txt
 
 rm docker-logs.txt docker-containers.txt
