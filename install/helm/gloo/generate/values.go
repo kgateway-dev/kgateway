f4929b36c4effa436aea4ed69711e8e8fc183b86
Andy Librian <andylibrian@gmail.com>
2020-10-19 12:20:35 +0700

    Run make generated-code

--- install/helm/gloo/generate/values.go
+++ install/helm/gloo/generate/values.go
@@ -437,7 +437,7 @@ type K8s struct {
 }
 
 type Stats struct {
-	Enabled bool `json:"enabled,omitempty" desc:"Controls whether or not envoy stats are enabled"`
+	Enabled            bool    `json:"enabled,omitempty" desc:"Controls whether or not envoy stats are enabled"`
 	RoutePrefixRewrite *string `json:"routePrefixRewrite,omitempty" desc:"The envoy stats endpoint to which the metrics endpoint is rewritten"`
 }
 
