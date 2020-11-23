86074ec829a0af66fdf019a6a91dd58b8865dc0e
Sam Heilbron <samheilbron@gmail.com>
2020-11-23 15:56:55 -0500

    Support optional rootCa if provided

--- projects/gloo/pkg/api/converters/kube/secretconverter.go
+++ projects/gloo/pkg/api/converters/kube/secretconverter.go
@@ -74,6 +74,7 @@ func (t *TLSSecretConverter) FromKubeSecret(_ context.Context, _ *kubesecret.Res
 				Tls: &v1.TlsSecret{
 					PrivateKey: string(secret.Data[kubev1.TLSPrivateKeyKey]),
 					CertChain:  string(secret.Data[kubev1.TLSCertKey]),
+					RootCa: 	string(secret.Data[kubev1.ServiceAccountRootCAKey]),
 				},
 			},
 			Metadata: kubeutils.FromKubeMeta(secret.ObjectMeta),
@@ -102,10 +103,12 @@ func (t *TLSSecretConverter) ToKubeSecret(_ context.Context, _ *kubesecret.Resou
 						ObjectMeta: objectMeta,
 						Type:       kubev1.SecretTypeTLS,
 						Data: map[string][]byte{
-							kubev1.TLSPrivateKeyKey: []byte(tlsGlooSecret.Tls.PrivateKey),
-							kubev1.TLSCertKey:       []byte(tlsGlooSecret.Tls.CertChain),
+							kubev1.TLSPrivateKeyKey:		[]byte(tlsGlooSecret.Tls.PrivateKey),
+							kubev1.TLSCertKey:     			[]byte(tlsGlooSecret.Tls.CertChain),
+							kubev1.ServiceAccountRootCAKey: []byte(tlsGlooSecret.Tls.RootCa),
 						},
 					}
+
 					return kubeSecret, nil
 				}
 			}
