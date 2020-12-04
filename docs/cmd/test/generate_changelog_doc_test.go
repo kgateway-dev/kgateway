6e312bdba03bf4d8cdfcdc65172545d4cbff0ce7
Keerthan Ekbote <saiskee@gmail.com>
2020-12-04 11:23:18 -0500

    fixed tests and added changelog

--- docs/cmd/test/generate_changelog_doc_test.go
+++ docs/cmd/test/generate_changelog_doc_test.go
@@ -80,7 +80,7 @@ var _ = Describe("Generate Changelog Test", func() {
 			osVersion := Version{Major: 1, Minor: 5, Patch: 9, Label: "beta", LabelVersion: 3}
 			useHeaderPrefix := false
 			bulletPointPrefix := changelogutils.GetOSDependencyPrefix(osVersion, useHeaderPrefix)
-			expectedBulletPrefix := "\n- (From OSS v1.5.9-beta3(../../../reference/changelog/open_source/#v159-beta3)) "
+			expectedBulletPrefix := "\n- (From [OSS v1.5.9-beta3](../../../reference/changelog/open_source/#v159-beta3)) "
 			Expect(bulletPointPrefix).To(Equal(expectedBulletPrefix))
 
 			useHeaderPrefix = true
