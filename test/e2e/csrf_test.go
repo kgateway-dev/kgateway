4c0d561d1a6122dab1cfad095e4607f6af1ebe32
Sam Heilbron <samheilbron@gmail.com>
2020-12-17 16:39:06 -0500

    e2e tests

--- test/e2e/csrf_test.go
+++ test/e2e/csrf_test.go
@@ -1,16 +1,17 @@
 package e2e_test
 
 import (
-	"bytes"
 	"context"
-	"errors"
 	"fmt"
-	"github.com/onsi/gomega"
+	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
+	gatewaydefaults "github.com/solo-io/gloo/projects/gateway/pkg/defaults"
 	gloo_config_core "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/config/core/v3"
 	gloo_type_matcher "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/type/matcher/v3"
 	glootype "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/type/v3"
-	"io"
+	gloohelpers "github.com/solo-io/gloo/test/helpers"
+	"io/ioutil"
 	"net/http"
+	"time"
 
 	csrf "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/extensions/filters/http/csrf/v3"
 	"github.com/solo-io/gloo/projects/gloo/pkg/defaults"
@@ -21,271 +22,172 @@ import (
 	"github.com/solo-io/gloo/test/services"
 	"github.com/solo-io/gloo/test/v1helpers"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
-	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 )
 
-type perCsrfTestData struct {
-	up            *gloov1.Upstream
-	envoyInstance *services.EnvoyInstance
-	envoyPort     uint32
-	envoyAdminUrl string
-}
-type csrfTestData struct {
-	testClients services.TestClients
-	ctx         context.Context
-	cancel      context.CancelFunc
-	per         perCsrfTestData
-}
 
-var _ = FDescribe("CSRF", func() {
+const (
+	allowedOriginRegex = "allowThisOne.solo.io"
+	unAllowedOriginRegex = "doNot.allowThisOne.solo.io"
+)
 
-	var td csrfTestData
+var _ = Describe("CSRF", func() {
 
-	apiFilter := &gloo_config_core.RuntimeFractionalPercent{
-		DefaultValue: &glootype.FractionalPercent{
-			Numerator:   uint32(100),
-			Denominator: glootype.FractionalPercent_HUNDRED,
-		},
+	var (
+		err           error
+		ctx           context.Context
+		cancel        context.CancelFunc
+		testClients   services.TestClients
+		envoyInstance *services.EnvoyInstance
+		testUs        *v1helpers.TestUpstream
+
+		writeNamespace = defaults.GlooSystem
+	)
+
+	BeforeEach(func() {
+		ctx, cancel = context.WithCancel(context.Background())
+		defaults.HttpPort = services.NextBindPort()
+		defaults.HttpsPort = services.NextBindPort()
+
+		// run gloo
+		writeNamespace = defaults.GlooSystem
+		ro := &services.RunOptions{
+			NsToWrite: writeNamespace,
+			NsToWatch: []string{"default", writeNamespace},
+			WhatToRun: services.What{
+				DisableFds: true,
+				DisableUds: true,
+			},
+		}
+		testClients = services.RunGlooGatewayUdsFds(ctx, ro)
+
+		// write gateways and wait for them to be created
+		err = gloohelpers.WriteDefaultGateways(writeNamespace, testClients.GatewayClient)
+		Expect(err).NotTo(HaveOccurred(), "Should be able to write default gateways")
+		Eventually(func() (gatewayv1.GatewayList, error) {
+			return testClients.GatewayClient.List(writeNamespace, clients.ListOpts{})
+		}, "10s", "0.1s").Should(HaveLen(2), "Gateways should be present")
+
+		// run envoy
+		envoyInstance, err = envoyFactory.NewEnvoyInstance()
+		Expect(err).NotTo(HaveOccurred())
+		err = envoyInstance.RunWithRole(writeNamespace+"~"+gatewaydefaults.GatewayProxyName, testClients.GlooPort)
+		Expect(err).NotTo(HaveOccurred())
+
+		// write a test upstream
+		// this is the upstream that will handle requests
+		testUs = v1helpers.NewTestHttpUpstream(ctx, envoyInstance.LocalAddr())
+		_, err = testClients.UpstreamClient.Write(testUs.Upstream, clients.WriteOpts{OverwriteExisting: true})
+		Expect(err).NotTo(HaveOccurred())
+	})
+
+	AfterEach(func() {
+		if envoyInstance != nil {
+			_ = envoyInstance.Clean()
+		}
+		cancel()
+	})
+
+	// A safe http method is one that doesn't alter the state of the server (ie read only)
+	// A CSRF attack targets state changing requests, so the filter only acts on unsafe methods (ones that change state)
+	// This is used to spoof requests from various origins
+	buildRequestFromOrigin := func(origin string, safeRequest bool) func() (string, error) {
+		return func() (string, error) {
+			method := "POST"
+			if safeRequest {
+				method = "GET"
+			}
+			req, err := http.NewRequest(method, fmt.Sprintf("http://%s:%d/test", "localhost", defaults.HttpPort), nil)
+			if err != nil {
+				return "", err
+			}
+			req.Header.Set("Origin", origin)
+
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return "", err
+			}
+			defer res.Body.Close()
+			body, err := ioutil.ReadAll(res.Body)
+			return string(body), err
+		}
 	}
 
-	apiAdditionalOrigins := []*gloo_type_matcher.StringMatcher{
-		{
+	Context("defined on listener", func() {
+
+		JustBeforeEach(func() {
+			gatewayClient := testClients.GatewayClient
+			gw, err := gatewayClient.Read(writeNamespace, gatewaydefaults.GatewayProxyName, clients.ReadOpts{})
+			Expect(err).NotTo(HaveOccurred())
+
+			// build a csrf policy
+			csrfPolicy := getCsrfPolicyWithAllowedRegex()
+
+			// update the listener to include the csrf policy
+			httpGateway := gw.GetHttpGateway()
+			httpGateway.Options = &gloov1.HttpListenerOptions{
+				Csrf: csrfPolicy,
+			}
+			_, err = gatewayClient.Write(gw, clients.WriteOpts{Ctx: ctx, OverwriteExisting: true})
+			Expect(err).NotTo(HaveOccurred())
+
+			// write a virtual service so we have a proxy to our test upstream
+			testVs := getTrivialVirtualServiceForUpstream(writeNamespace, testUs.Upstream.Metadata.Ref())
+			_, err = testClients.VirtualServiceClient.Write(testVs, clients.WriteOpts{})
+			Expect(err).NotTo(HaveOccurred())
+
+			// ensure the proxy and virtual service are created
+			Eventually(func() (*gloov1.Proxy, error) {
+				return testClients.ProxyClient.Read(writeNamespace, gatewaydefaults.GatewayProxyName, clients.ReadOpts{})
+			}, "5s", "0.1s").ShouldNot(BeNil())
+			Eventually(func() (*gatewayv1.VirtualService, error) {
+				return testClients.VirtualServiceClient.Read(testVs.Metadata.GetNamespace(), testVs.Metadata.GetName(), clients.ReadOpts{})
+			}, "5s", "0.1s").ShouldNot(BeNil())
+		})
+
+		It("should succeed with allowed origin, safe request", func() {
+			spoofedRequest := buildRequestFromOrigin(allowedOriginRegex, true)
+			Eventually(spoofedRequest, 10*time.Second, 1*time.Second).Should(BeEmpty())
+		})
+
+		It("should succeed with allowed origin, unsafe request", func() {
+			spoofedRequest := buildRequestFromOrigin(allowedOriginRegex, false)
+			Eventually(spoofedRequest, 10*time.Second, 1*time.Second).Should(BeEmpty())
+		})
+
+		It("should succeed with un-allowed origin, safe request", func() {
+			// confirm that a safe (read only) request is not affected by filter
+			spoofedRequest := buildRequestFromOrigin(unAllowedOriginRegex, true)
+			Eventually(spoofedRequest, 10*time.Second, 1*time.Second).Should(BeEmpty())
+		})
+
+		It("should fail with un-allowed origin", func() {
+			spoofedRequest := buildRequestFromOrigin(unAllowedOriginRegex, false)
+			Eventually(spoofedRequest, 10*time.Second, 1*time.Second).Should(Equal("Invalid origin"))
+		})
+
+	})
+
+	// TODO - add support for applying csrf filter to routes and virtual hosts...etc
+
+})
+
+func getCsrfPolicyWithAllowedRegex() *csrf.CsrfPolicy {
+	return &csrf.CsrfPolicy{
+		FilterEnabled: &gloo_config_core.RuntimeFractionalPercent{
+			DefaultValue: &glootype.FractionalPercent{
+				Numerator:   uint32(100),
+				Denominator: glootype.FractionalPercent_HUNDRED,
+			},
+		},
+		AdditionalOrigins: []*gloo_type_matcher.StringMatcher{{
 			MatchPattern: &gloo_type_matcher.StringMatcher_SafeRegex{
 				SafeRegex: &gloo_type_matcher.RegexMatcher{
 					EngineType: &gloo_type_matcher.RegexMatcher_GoogleRe2{
 						GoogleRe2: &gloo_type_matcher.RegexMatcher_GoogleRE2{},
 					},
-					Regex: "allowThisOne.solo.io",
-				},
-			},
-		},
-	}
-
-	BeforeEach(func() {
-		td.ctx, td.cancel = context.WithCancel(context.Background())
-		td.testClients = services.RunGateway(td.ctx, true)
-		td.per = perCsrfTestData{}
-	})
-
-	AfterEach(func() {
-		td.cancel()
-	})
-
-	Context("with envoy", func() {
-
-		BeforeEach(func() {
-			var err error
-			td.per.envoyInstance, err = envoyFactory.NewEnvoyInstance()
-			Expect(err).NotTo(HaveOccurred())
-			td.per.envoyAdminUrl = fmt.Sprintf("http://%s:%d/config_dump",
-				td.per.envoyInstance.LocalAddr(),
-				td.per.envoyInstance.AdminPort)
-
-			err = td.per.envoyInstance.Run(td.testClients.GlooPort)
-			Expect(err).NotTo(HaveOccurred())
-
-			td.per.up = td.setupUpstream()
-		})
-
-		AfterEach(func() {
-			if td.per.envoyInstance != nil {
-				td.per.envoyInstance.Clean()
-			}
-		})
-
-		FIt("should run with csrf", func() {
-			allowedOrigins := []string{"allowThisOne.solo.io"}
-
-			csrf := &csrf.CsrfPolicy{
-				FilterEnabled:     apiFilter,
-				AdditionalOrigins: apiAdditionalOrigins,
-			}
-
-			td.setupInitialProxy(csrf)
-
-			envoyConfig := td.per.getEnvoyConfig()
-			By("Check config")
-			Expect(envoyConfig).To(MatchRegexp("\"numerator\": 100"))
-			Expect(envoyConfig).To(MatchRegexp(allowedOrigins[0]))
-
-			By("Request with allowed origin")
-			mockOrigin := allowedOrigins[0]
-			td.per.testRequestService(mockOrigin, "GET").Should(BeNil())
-
-			envoyStats := td.per.getEnvoyStats()
-			Expect(envoyStats).To(MatchRegexp("http.http.csrf.request_invalid: 0"))
-
-			By("Request with allowed origin")
-			mockOriginUnallowed := "no"
-			td.per.testRequestService(mockOriginUnallowed, "GET").Should(BeNil())
-
-			envoyStatsUpdate := td.per.getEnvoyStats()
-			Expect(envoyStatsUpdate).To(MatchRegexp("http.http.csrf.request_invalid: 0"))
-		})
-
-		It("should run without csrf", func() {
-			allowedOrigins := []string{"allowThisOne.solo.io"}
-			td.setupInitialProxy(nil)
-
-			By("Request with allowed origin")
-			mockOrigin := allowedOrigins[0]
-			td.per.testRequestService(mockOrigin, "GET").Should(BeNil())
-		})
-	})
-})
-
-func (td *csrfTestData) getGlooCsrfProxy(csrf *csrf.CsrfPolicy) (*gloov1.Proxy, error) {
-	readProxy, err := td.testClients.ProxyClient.Read("default", "proxy", clients.ReadOpts{})
-	if err != nil {
-		return nil, err
-	}
-	return td.per.getGlooCsrfProxyWithVersion(readProxy.Metadata.ResourceVersion, csrf), nil
-}
-
-func (ptd *perCsrfTestData) getGlooCsrfProxyWithVersion(resourceVersion string, csrf *csrf.CsrfPolicy) *gloov1.Proxy {
-	return &gloov1.Proxy{
-		Metadata: &core.Metadata{
-			Name:            "proxy",
-			Namespace:       "default",
-			ResourceVersion: resourceVersion,
-		},
-		Listeners: []*gloov1.Listener{{
-			Name:        "listener",
-			BindAddress: "0.0.0.0",
-			BindPort:    ptd.envoyPort,
-			ListenerType: &gloov1.Listener_HttpListener{
-				HttpListener: &gloov1.HttpListener{
-					VirtualHosts: []*gloov1.VirtualHost{{
-						Name:    "virt1",
-						Domains: []string{"*"},
-						Routes: []*gloov1.Route{{
-							Action: &gloov1.Route_RouteAction{
-								RouteAction: &gloov1.RouteAction{
-									Destination: &gloov1.RouteAction_Single{
-										Single: &gloov1.Destination{
-											DestinationType: &gloov1.Destination_Upstream{
-												Upstream: ptd.up.Metadata.Ref(),
-											},
-										},
-									},
-								},
-							},
-						}},
-						Options: &gloov1.VirtualHostOptions{
-							Csrf: csrf,
-						},
-					}},
+					Regex: allowedOriginRegex,
 				},
 			},
 		}},
 	}
-}
-
-func (td *csrfTestData) setupProxy(proxy *gloov1.Proxy) error {
-	proxyCli := td.testClients.ProxyClient
-	_, err := proxyCli.Write(proxy, clients.WriteOpts{OverwriteExisting: true})
-	return err
-}
-
-func (td *csrfTestData) setupInitialProxy(csrf *csrf.CsrfPolicy) {
-	By("Setup proxy")
-	td.per.envoyPort = defaults.HttpPort
-	proxy := td.per.getGlooCsrfProxyWithVersion("", csrf)
-	err := td.setupProxy(proxy)
-	// Call with retries to ensure proxy is available
-	Eventually(func() error {
-		proxy, err := td.getGlooCsrfProxy(csrf)
-		if err != nil {
-			return err
-		}
-		return td.setupProxy(proxy)
-	}, "10s", ".1s").Should(BeNil())
-	Expect(err).NotTo(HaveOccurred())
-	Eventually(func() error {
-		_, err := http.Get(fmt.Sprintf("http://%s:%d/status/200", "localhost", td.per.envoyPort))
-		if err != nil {
-			return err
-		}
-		return nil
-	}, "1m", ".1s").Should(BeNil())
-}
-
-func (td *csrfTestData) setupUpstream() *gloov1.Upstream {
-	tu := v1helpers.NewTestHttpUpstream(td.ctx, td.per.envoyInstance.LocalAddr())
-	// drain channel as we don't care about it
-	go func() {
-		for range tu.C {
-		}
-	}()
-	up := tu.Upstream
-	_, err := td.testClients.UpstreamClient.Write(up, clients.WriteOpts{OverwriteExisting: true})
-	Expect(err).NotTo(HaveOccurred())
-	return up
-}
-
-func (ptd *perCsrfTestData) testRequestService(origin, method string) gomega.AsyncAssertion {
-	return Eventually(func() error {
-
-		req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:%d/status/200", "localhost", ptd.envoyPort), nil)
-		if err != nil {
-			return err
-		}
-		req.Header.Set("Origin", origin)
-		req.Header.Set("Access-Control-Request-Method", method)
-		req.Header.Set("Access-Control-Request-Headers", "X-Requested-With")
-
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			return err
-		}
-		defer resp.Body.Close()
-		if resp.StatusCode == 200 {
-			return nil
-		}
-		print(resp.StatusCode)
-		return errors.New(fmt.Sprintf("status code: %d", resp.StatusCode))
-	}, "20s", ".1s")
-}
-
-func (ptd *perCsrfTestData) getEnvoyConfig() string {
-	By("Get config")
-	envoyConfig := ""
-	Eventually(func() error {
-		r, err := http.Get(ptd.envoyAdminUrl)
-		if err != nil {
-			return err
-		}
-		p := new(bytes.Buffer)
-		if _, err := io.Copy(p, r.Body); err != nil {
-			return err
-		}
-		defer r.Body.Close()
-		envoyConfig = p.String()
-		return nil
-	}, "10s", ".1s").Should(BeNil())
-	return envoyConfig
-}
-
-func (ptd *perCsrfTestData) getEnvoyStats() string {
-	By("Get config")
-	envoyStats := ""
-	Eventually(func() error {
-
-		envoyStatsUrl := fmt.Sprintf("http://%s:%d/stats",
-			ptd.envoyInstance.LocalAddr(),
-			ptd.envoyInstance.AdminPort)
-
-		r, err := http.Get(envoyStatsUrl)
-		if err != nil {
-			return err
-		}
-		p := new(bytes.Buffer)
-		if _, err := io.Copy(p, r.Body); err != nil {
-			return err
-		}
-		defer r.Body.Close()
-		envoyStats = p.String()
-		return nil
-	}, "10s", ".1s").Should(BeNil())
-	return envoyStats
 }
