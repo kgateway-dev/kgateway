919b4374850143546792cfe9d91868f87a0657e4
Kevin Dorosh <kcdorosh@gmail.com>
2020-12-28 10:29:11 -0500

    Default rest eds to false

--- projects/gloo/pkg/xds/utils.go
+++ projects/gloo/pkg/xds/utils.go
@@ -15,17 +15,8 @@ func SetEdsOnCluster(out *envoy_config_cluster_v3.Cluster, settings *v1.Settings
 		Type: envoy_config_cluster_v3.Cluster_EDS,
 	}
 	// The default value for enableRestEds should be set to true via helm.
-	// If nil will default to rest eds.
-	if !settings.GetGloo().GetEnableRestEds().GetValue() {
-		out.EdsClusterConfig = &envoy_config_cluster_v3.Cluster_EdsClusterConfig{
-			EdsConfig: &envoy_config_core_v3.ConfigSource{
-				ResourceApiVersion: envoy_config_core_v3.ApiVersion_V3,
-				ConfigSourceSpecifier: &envoy_config_core_v3.ConfigSource_Ads{
-					Ads: &envoy_config_core_v3.AggregatedConfigSource{},
-				},
-			},
-		}
-	} else {
+	// If nil will not enable rest eds.
+	if settings.GetGloo().GetEnableRestEds().GetValue() {
 		out.EdsClusterConfig = &envoy_config_cluster_v3.Cluster_EdsClusterConfig{
 			EdsConfig: &envoy_config_core_v3.ConfigSource{
 				ResourceApiVersion: envoy_config_core_v3.ApiVersion_V3,
@@ -37,6 +28,15 @@ func SetEdsOnCluster(out *envoy_config_cluster_v3.Cluster, settings *v1.Settings
 						RefreshDelay:        ptypes.DurationProto(time.Second * 5),
 						RequestTimeout:      ptypes.DurationProto(time.Second * 5),
 					},
+				},
+			},
+		}
+	} else {
+		out.EdsClusterConfig = &envoy_config_cluster_v3.Cluster_EdsClusterConfig{
+			EdsConfig: &envoy_config_core_v3.ConfigSource{
+				ResourceApiVersion: envoy_config_core_v3.ApiVersion_V3,
+				ConfigSourceSpecifier: &envoy_config_core_v3.ConfigSource_Ads{
+					Ads: &envoy_config_core_v3.AggregatedConfigSource{},
 				},
 			},
 		}
