daafff0b423a50a327fe29368046f39f5767ce11
Sam Heilbron <samheilbron@gmail.com>
2021-01-27 12:29:33 -0500

    fixup test. bad backport

--- projects/gloo/pkg/plugins/listener/plugin_test.go
+++ projects/gloo/pkg/plugins/listener/plugin_test.go
@@ -1,9 +1,9 @@
 package listener_test
 
 import (
-	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	envoy_config_listener_v3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
-	"github.com/golang/protobuf/ptypes/wrappers"
+	envoyapi "github.com/envoyproxy/go-control-plane/envoy/api/v2"
+	envoy_api_v2_core "github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
+	"github.com/gogo/protobuf/types"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
@@ -16,10 +16,10 @@ var _ = Describe("Plugin", func() {
 
 	var (
 		plugin *Plugin
-		out    *envoy_config_listener_v3.Listener
+		out    *envoyapi.Listener
 	)
 	BeforeEach(func() {
-		out = new(envoy_config_listener_v3.Listener)
+		out = new(envoyapi.Listener)
 		plugin = NewPlugin()
 	})
 
@@ -27,7 +27,7 @@ var _ = Describe("Plugin", func() {
 
 		in := &v1.Listener{
 			Options: &v1.ListenerOptions{
-				PerConnectionBufferLimitBytes: &wrappers.UInt32Value{
+				PerConnectionBufferLimitBytes: &types.UInt32Value{
 					Value: uint32(4096),
 				},
 			},
@@ -53,13 +53,13 @@ var _ = Describe("Plugin", func() {
 		}
 		err := plugin.ProcessListener(plugins.Params{}, in, out)
 		Expect(err).NotTo(HaveOccurred())
-		Expect(out.SocketOptions).To(BeEquivalentTo([]*envoy_config_core_v3.SocketOption{
+		Expect(out.SocketOptions).To(BeEquivalentTo([]*envoy_api_v2_core.SocketOption{
 			{
 				Description: "desc",
 				Level:       1,
 				Name:        2,
-				Value:       &envoy_config_core_v3.SocketOption_IntValue{IntValue: 123},
-				State:       envoy_config_core_v3.SocketOption_STATE_LISTENING,
+				Value:       &envoy_api_v2_core.SocketOption_IntValue{IntValue: 123},
+				State:       envoy_api_v2_core.SocketOption_STATE_LISTENING,
 			},
 		}))
 	})
