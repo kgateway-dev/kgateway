e95f21a93d36fb2ba0a25e63aab4d06453f6b3c6
kevin-shelaga <kevin.shelaga@canada.ca>
2021-02-23 10:33:49 -0500

    add istioMetaMeshId and istioMetaClusterId to glooctl istio options

--- projects/gloo/cli/pkg/cmd/options/options.go
+++ projects/gloo/cli/pkg/cmd/options/options.go
@@ -159,6 +159,8 @@ type Add struct {
 type Istio struct {
 	Upstream  string // upstream for which we are changing the istio mTLS settings
 	Namespace string // namespace in which istio is installed
+	IstioMetaMeshId string // IstioMetaMeshId sets ISTIO_META_MESH_ID env var
+	IstioMetaClusterId string // IstioMetaMeshId sets ISTIO_META_CLUSTER_ID env var
 }
 
 type InputRoute struct {
