13bd52f2b2d653baed0516c06b4e869f2cc1cf92
Sam Heilbron <samheilbron@gmail.com>
2021-02-26 14:24:26 -0700

    Trigger Build

---
+++ docs/generate-active-versions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+###################################################################################
+# This script modifies the active_versions.json file which controls which versions
+# of our docs are published
+###################################################################################
