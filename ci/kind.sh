8d0eeb498a0c9fce9e03abdec75b734722e044d5
EItanya <eitan.yarmush@solo.io>
2021-03-31 13:39:26 -0400

    revert kind script

--- ci/kind.sh
+++ ci/kind.sh
@@ -33,7 +33,7 @@ EOF
 # write the output to a temp file so that we can grab the image names out of it
 # also ensure we clean up the file once we're done
 TEMP_FILE=$(mktemp)
-make docker | tee ${TEMP_FILE}
+VERSION=kind make docker | tee ${TEMP_FILE}
 
 cleanup() {
     echo ">> Removing ${TEMP_FILE}"
@@ -46,7 +46,7 @@ echo ">> Temporary output file ${TEMP_FILE}"
 # grab the image names out of the `make docker` output
 sed -nE 's|(\\x1b\[0m)?Successfully tagged (.*$)|\2|p' ${TEMP_FILE} | while read f; do kind load docker-image --name kind $f; done
 
-make build-test-chart
+VERSION=kind make build-test-chart
 make glooctl-linux-amd64
 
 if [ "$KUBE2E_TESTS" = "eds" ]; then
