2a7c070f6a9e2de92ebaaa03f02d9427e3819144
Nina Polshakova <ninapolshakova@gmail.com>
2021-04-29 17:12:04 -0400

    regen

--- install/test/helm_suite_test.go
+++ install/test/helm_suite_test.go
@@ -61,7 +61,6 @@ type renderTestCase struct {
 
 var renderers = []renderTestCase{
 	{"Helm 3", helm3Renderer{chartDir}},
-	//{"Helm 2", helm2Renderer{chartDir}},
 }
 
 func runTests(callback func(testCase renderTestCase)) {
@@ -164,7 +163,6 @@ func (h2 helm2Renderer) RenderManifest(namespace string, values helmValues) (Tes
 		return nil, err
 	}
 
-	values.valuesArgs = append(values.valuesArgs, "settings.helm2=true")
 	helmValues, err := buildHelmValues(h2.chartDir, values)
 	if err != nil {
 		return nil, err
