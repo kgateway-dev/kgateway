a1438dd014837fae464cf68e7460ebdc47b36311
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-06-09 10:40:50 -0400

    more boilerplate for CRDs

--- projects/gloo/cli/pkg/cmd/install/util.go
+++ projects/gloo/cli/pkg/cmd/install/util.go
@@ -52,8 +52,8 @@ func init() {
 		"routetables.gateway.solo.io",
 		"authconfigs.enterprise.gloo.solo.io",
 		"ratelimitconfigs.ratelimit.solo.io",
-		"virtualhostoption.gateway.solo.io",
-		"routeoption.gateway.solo.io",
+		"virtualhostoptions.gateway.solo.io",
+		"routeoptions.gateway.solo.io",
 	}
 
 	GlooFedCrdNames = []string{
