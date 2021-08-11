28113fe44d8b69c4901f0048dd64c0e5b50d45f9
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-08-11 12:23:03 -0400

    add getter check linter

--- projects/gloo/pkg/utils/ssl.go
+++ projects/gloo/pkg/utils/ssl.go
@@ -245,7 +245,7 @@ func (s *sslConfigTranslator) ResolveCommonSslConfig(cs CertSource, secrets v1.S
 			return nil, err
 		}
 	} else if sslSecrets := cs.GetSslFiles(); sslSecrets != nil {
-		certChain, privateKey, rootCa = sslSecrets.TlsCert, sslSecrets.TlsKey, sslSecrets.RootCa
+		certChain, privateKey, rootCa = sslSecrets.GetTlsCert(), sslSecrets.GetTlsKey(), sslSecrets.GetRootCa()
 	} else if sslSecrets := cs.GetSds(); sslSecrets != nil {
 		tlsContext, err := s.handleSds(sslSecrets, verifySanListToMatchSanList(cs.GetVerifySubjectAltName()))
 		if err != nil {
@@ -331,9 +331,9 @@ func getSslSecrets(ref core.ResourceRef, secrets v1.SecretList) (string, string,
 		return "", "", "", NotTlsSecretError(secret.GetMetadata().Ref())
 	}
 
-	certChain := sslSecret.Tls.CertChain
-	privateKey := sslSecret.Tls.PrivateKey
-	rootCa := sslSecret.Tls.RootCa
+	certChain := sslSecret.Tls.GetCertChain()
+	privateKey := sslSecret.Tls.GetPrivateKey()
+	rootCa := sslSecret.Tls.GetRootCa()
 	return certChain, privateKey, rootCa, nil
 }
 
