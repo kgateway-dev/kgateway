90961efc0e66182582ac488d0b13464606b4d653
Mitch Aman <mitch.aman@solo.io>
2021-08-13 12:50:14 -0400

    int32 -> google.protobuf.Int32Value

--- projects/gloo/pkg/api/v1/settings.pb.equal.go
+++ projects/gloo/pkg/api/v1/settings.pb.equal.go
@@ -1430,8 +1430,14 @@ func (m *GatewayOptions_ValidationOptions) Equal(that interface{}) bool {
 		}
 	}
 
-	if m.GetValidationServerGrpcMaxSize() != target.GetValidationServerGrpcMaxSize() {
-		return false
+	if h, ok := interface{}(m.GetValidationServerGrpcMaxSize()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetValidationServerGrpcMaxSize()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetValidationServerGrpcMaxSize(), target.GetValidationServerGrpcMaxSize()) {
+			return false
+		}
 	}
 
 	return true
