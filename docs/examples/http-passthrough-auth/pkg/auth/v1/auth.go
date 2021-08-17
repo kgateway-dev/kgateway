091e8d907a5ef639bb2c337e9aae7a604d4b2bae
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-08-17 16:12:41 -0400

    Add http passthrough guide

--- docs/examples/http-passthrough-auth/pkg/auth/v1/auth.go
+++ docs/examples/http-passthrough-auth/pkg/auth/v1/auth.go
@@ -1,14 +1,35 @@
 package v1
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
 type HttpPassthroughService struct{}
 
+const ServerPort = 9001
+
 func (h *HttpPassthroughService) StartServer() {
+	fmt.Printf("Listening on port %d for auth requests\n", ServerPort)
 	handler := func(rw http.ResponseWriter, r *http.Request) {
-		rw.WriteHeader(200)
+		fmt.Printf("received request with url: %s, with headers %+v\n", r.URL.String(), r.Header)
+		switch r.URL.Path {
+		case "/auth":
+			if r.Header.Get("authorization") == "authorize me" {
+				rw.WriteHeader(200)
+			} else {
+				rw.WriteHeader(403)
+				rw.Write([]byte("Please also set the `authorization: authorize me` header."))
+			}
+		default:
+			fmt.Println("Did not reach the right path, use /auth to authenticate requests! Make sure to include the header 'authorization: authorize me' too.")
+			rw.WriteHeader(403)
+		}
 	}
-	http.ListenAndServe("127.0.0.1:9001", http.HandlerFunc(handler))
+	address := fmt.Sprintf(":%d", ServerPort)
+	err := http.ListenAndServe(address, http.HandlerFunc(handler))
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
