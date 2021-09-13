2a4bc45082dd3beed70fd08f1a4881363740a2e5
Bernie Birnbaum <bewebi@earthlink.net>
2021-09-13 18:37:53 -0400

    propagate IgnoreHealthOnHostRemoval

--- projects/gloo/pkg/translator/clusters.go
+++ projects/gloo/pkg/translator/clusters.go
@@ -106,6 +106,7 @@ func (t *translatorInstance) initializeCluster(
 		// this field can be overridden by plugins
 		ConnectTimeout:       ptypes.DurationProto(ClusterConnectionTimeout),
 		Http2ProtocolOptions: getHttp2options(upstream),
+		IgnoreHealthOnHostRemoval: upstream.GetIgnoreHealthOnHostRemoval(),
 	}
 
 	if sslConfig := upstream.GetSslConfig(); sslConfig != nil {
