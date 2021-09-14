a1184f8f83285b57524960c19134247f6d2f68bc
Sam Heilbron <samheilbron@gmail.com>
2021-09-14 17:52:04 -0400

    proxy_transition_function, bubble error up

--- projects/gateway/pkg/utils/proxy_transition_function.go
+++ projects/gateway/pkg/utils/proxy_transition_function.go
@@ -8,19 +8,23 @@ import (
 
 func TransitionFunction(original, desired *v1.Proxy) (bool, error) {
 	if len(original.GetListeners()) != len(desired.GetListeners()) {
-		updateDesiredStatus(original, desired)
+		if err := updateDesiredStatus(original, desired); err != nil {
+			return false, err
+		}
 		return true, nil
 	}
 	for i := range original.GetListeners() {
 		if !original.GetListeners()[i].Equal(desired.GetListeners()[i]) {
-			updateDesiredStatus(original, desired)
+			if err := updateDesiredStatus(original, desired); err != nil {
+				return false, err
+			}
 			return true, nil
 		}
 	}
 	return false, nil
 }
 
-func updateDesiredStatus(original, desired *v1.Proxy) {
+func updateDesiredStatus(original, desired *v1.Proxy) error {
 	// we made an update to the proxy from the gateway's point of view.
 	// let's make sure we update the status for gloo if the hash hasn't changed.
 	// the proxy can change from the gateway's point of view but not from gloo's if,
@@ -30,6 +34,7 @@ func updateDesiredStatus(original, desired *v1.Proxy) {
 	// until we make the full move.
 	equal, ok := hashutils.HashableEqual(original, desired)
 	if ok && equal {
-		_ = statusutils.CopyStatusForPodNamespace(original, desired)
+		return statusutils.CopyStatusForPodNamespace(original, desired)
 	}
+	return nil
 }
