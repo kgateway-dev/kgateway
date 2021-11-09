d1cfa6e87b7daac8472c383cef1f94c72e6958d7
nfuden <nathan.fudenberg@solo.io>
2021-11-09 15:06:48 -0500

    docs:  Run make target for doc gen

--- projects/gloo/constants/values.go
+++ projects/gloo/constants/values.go
@@ -1,5 +1,5 @@
 package constants
 
-// AllowEmpty is a reusable term to avoid setting defaults. 
+// AllowEmpty is a reusable term to avoid setting defaults.
 // This should be used where gloo decides to set common sense defaults but needs a way to specify a empty set.
 const AllowEnabled = "allow_empty"
