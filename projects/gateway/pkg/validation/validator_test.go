solo-io/gloo

validate route tables delegated via selector (#5607)
* validate route tables delegated via selector

* Adding changelog file to new location

* Deleting changelog file from old location

Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
Co-authored-by: changelog-bot <changelog-bot>

projects/gateway/pkg/validation/validator_test.go
@@ -416,6 +416,33 @@ var _ = Describe("Validator", func() {
 				Expect(*(reports.ProxyReports)).To(HaveLen(0))
 			})
 		})
+
+		Context("route table delegation with selectors", func() {
+			It("accepts route table with valid prefix", func() {
+				vc.validate = acceptProxy
+				us := samples.SimpleUpstream()
+				snap := samples.GatewaySnapshotWithDelegateSelector(us.Metadata.Ref(), ns)
+				err := v.Sync(context.TODO(), snap)
+				Expect(err).NotTo(HaveOccurred())
+
+				rt := samples.RouteTableWithLabelsAndPrefix("route2", ns, "/foo/2", map[string]string{"pick": "me"})
+				_, err = v.ValidateRouteTable(context.TODO(), rt, false)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("rejects route table with invalid prefix", func() {
+				vc.validate = acceptProxy
+				us := samples.SimpleUpstream()
+				snap := samples.GatewaySnapshotWithDelegateSelector(us.Metadata.Ref(), ns)
+				err := v.Sync(context.TODO(), snap)
+				Expect(err).NotTo(HaveOccurred())
+
+				// the prefix doesn't start with the parent's prefix so validation will fail
+				rt := samples.RouteTableWithLabelsAndPrefix("route2", ns, "/not", map[string]string{"pick": "me"})
+				_, err = v.ValidateRouteTable(context.TODO(), rt, false)
+				Expect(err).To(HaveOccurred())
+			})
+		})
 	})
 
 	Context("delete a route table", func() {

kgateway-dev/kgateway

projects/gateway/pkg/validation/validator_test.go
@@ -212,6 +212,33 @@ var _ = Describe("Validator", func() {
 				Expect(proxyReports).To(HaveLen(0))
 			})
 		})
+
+		Context("route table delegation with selectors", func() {
+			It("accepts route table with valid prefix", func() {
+				vc.validateProxy = acceptProxy
+				us := samples.SimpleUpstream()
+				snap := samples.GatewaySnapshotWithDelegateSelector(us.Metadata.Ref(), ns)
+				err := v.Sync(context.TODO(), snap)
+				Expect(err).NotTo(HaveOccurred())
+
+				rt := samples.RouteTableWithLabelsAndPrefix("route2", ns, "/foo/2", map[string]string{"pick": "me"})
+				_, err = v.ValidateRouteTable(context.TODO(), rt, false)
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("rejects route table with invalid prefix", func() {
+				vc.validateProxy = acceptProxy
+				us := samples.SimpleUpstream()
+				snap := samples.GatewaySnapshotWithDelegateSelector(us.Metadata.Ref(), ns)
+				err := v.Sync(context.TODO(), snap)
+				Expect(err).NotTo(HaveOccurred())
+
+				// the prefix doesn't start with the parent's prefix so validation will fail
+				rt := samples.RouteTableWithLabelsAndPrefix("route2", ns, "/not", map[string]string{"pick": "me"})
+				_, err = v.ValidateRouteTable(context.TODO(), rt, false)
+				Expect(err).To(HaveOccurred())
+			})
+		})
 	})
 
 	Context("delete a route table", func() {