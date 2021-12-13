940de0589b69fce0ef2072bd7fff48e1215dfbf0
Jenny Shu <jenny.shu@solo.io>
2021-12-13 17:14:14 -0500

    update ingress apis from v1beta1 to v1

--- projects/ingress/pkg/status/status_syncer_test.go
+++ projects/ingress/pkg/status/status_syncer_test.go
@@ -18,9 +18,8 @@ import (
 	"github.com/solo-io/solo-kit/test/helpers"
 	"github.com/solo-io/solo-kit/test/setup"
 	kubev1 "k8s.io/api/core/v1"
-	"k8s.io/api/extensions/v1beta1"
+	networkingv1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/intstr"
 	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/rest"
@@ -85,14 +84,17 @@ var _ = Describe("StatusSyncer", func() {
 			Expect(err).NotTo(HaveOccurred())
 		}()
 
-		kubeIngressClient := kubeClientset.ExtensionsV1beta1().Ingresses(namespace)
-		backend := &v1beta1.IngressBackend{
-			ServiceName: "foo",
-			ServicePort: intstr.IntOrString{
-				IntVal: 8080,
+		kubeIngressClient := kubeClientset.NetworkingV1().Ingresses(namespace)
+		backend := &networkingv1.IngressBackend{
+			Service: &networkingv1.IngressServiceBackend{
+				Name: "foo",
+				Port: networkingv1.ServiceBackendPort{
+					Number: 8080,
+				},
 			},
 		}
-		kubeIng, err := kubeIngressClient.Create(ctx, &v1beta1.Ingress{
+		pathType := networkingv1.PathTypeImplementationSpecific
+		kubeIng, err := kubeIngressClient.Create(ctx, &networkingv1.Ingress{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "rusty",
 				Namespace: namespace,
@@ -100,22 +102,23 @@ var _ = Describe("StatusSyncer", func() {
 					translator.IngressClassKey: "gloo",
 				},
 			},
-			Spec: v1beta1.IngressSpec{
-				Backend: backend,
-				TLS: []v1beta1.IngressTLS{
+			Spec: networkingv1.IngressSpec{
+				DefaultBackend: backend,
+				TLS: []networkingv1.IngressTLS{
 					{
 						Hosts:      []string{"some.host"},
 						SecretName: "doesntexistanyway",
 					},
 				},
-				Rules: []v1beta1.IngressRule{
+				Rules: []networkingv1.IngressRule{
 					{
 						Host: "some.host",
-						IngressRuleValue: v1beta1.IngressRuleValue{
-							HTTP: &v1beta1.HTTPIngressRuleValue{
-								Paths: []v1beta1.HTTPIngressPath{
+						IngressRuleValue: networkingv1.IngressRuleValue{
+							HTTP: &networkingv1.HTTPIngressRuleValue{
+								Paths: []networkingv1.HTTPIngressPath{
 									{
-										Backend: *backend,
+										PathType: &pathType,
+										Backend:  *backend,
 									},
 								},
 							},
@@ -218,15 +221,18 @@ var _ = Describe("StatusSyncer", func() {
 			Expect(err).Should(MatchError(ContainSubstring("Invalid attempt to use localhost name")))
 		}()
 
-		backend := &v1beta1.IngressBackend{
-			ServiceName: "foo",
-			ServicePort: intstr.IntOrString{
-				IntVal: 8080,
+		backend := &networkingv1.IngressBackend{
+			Service: &networkingv1.IngressServiceBackend{
+				Name: "foo",
+				Port: networkingv1.ServiceBackendPort{
+					Number: 8080,
+				},
 			},
 		}
 
-		kubeIngressClient := kubeClientset.ExtensionsV1beta1().Ingresses(namespace)
-		kubeIngress, err := kubeIngressClient.Create(ctx, &v1beta1.Ingress{
+		kubeIngressClient := kubeClientset.NetworkingV1().Ingresses(namespace)
+		pathType := networkingv1.PathTypeImplementationSpecific
+		kubeIngress, err := kubeIngressClient.Create(ctx, &networkingv1.Ingress{
 			ObjectMeta: metav1.ObjectMeta{
 				Name:      "rusty",
 				Namespace: namespace,
@@ -234,22 +240,23 @@ var _ = Describe("StatusSyncer", func() {
 					translator.IngressClassKey: "gloo",
 				},
 			},
-			Spec: v1beta1.IngressSpec{
-				Backend: backend,
-				TLS: []v1beta1.IngressTLS{
+			Spec: networkingv1.IngressSpec{
+				DefaultBackend: backend,
+				TLS: []networkingv1.IngressTLS{
 					{
 						Hosts:      []string{"some.host"},
 						SecretName: "doesntexistanyway",
 					},
 				},
-				Rules: []v1beta1.IngressRule{
+				Rules: []networkingv1.IngressRule{
 					{
 						Host: "some.host",
-						IngressRuleValue: v1beta1.IngressRuleValue{
-							HTTP: &v1beta1.HTTPIngressRuleValue{
-								Paths: []v1beta1.HTTPIngressPath{
+						IngressRuleValue: networkingv1.IngressRuleValue{
+							HTTP: &networkingv1.HTTPIngressRuleValue{
+								Paths: []networkingv1.HTTPIngressPath{
 									{
-										Backend: *backend,
+										PathType: &pathType,
+										Backend:  *backend,
 									},
 								},
 							},
