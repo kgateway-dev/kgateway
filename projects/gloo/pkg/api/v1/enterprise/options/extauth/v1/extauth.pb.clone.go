6ab190782f3f6bf4916ba5c808002ad6e9a1efe2
Ben Taussig <ben.taussig@solo.io>
2021-12-16 10:08:09 -0500

    Codegen

--- projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.clone.go
+++ projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.clone.go
@@ -1653,6 +1653,8 @@ func (m *ExtAuthConfig_OidcAuthorizationCodeConfig) Clone() proto.Message {
 
 	target.SessionIdHeaderName = m.GetSessionIdHeaderName()
 
+	target.ParseCallbackPathAsRegex = m.GetParseCallbackPathAsRegex()
+
 	return target
 }
 
