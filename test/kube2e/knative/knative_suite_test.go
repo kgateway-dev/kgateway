d14494f248b4deb0861a82c32c7823f1114aa248
Sam Heilbron <samheilbron@gmail.com>
2021-12-20 17:00:29 -0500

    skip knative tests

--- test/kube2e/knative/knative_suite_test.go
+++ test/kube2e/knative/knative_suite_test.go
@@ -22,7 +22,15 @@ import (
 )
 
 func TestKnative(t *testing.T) {
-	if os.Getenv("KUBE2E_TESTS") != "knative" {
+	// Knative support has been deprecated in Gloo Edge 1.10 (https://github.com/solo-io/gloo/issues/5707)
+	// and will be removed in Gloo Edge 1.11.
+	// These tests are not run during CI.
+	if true {
+		log.Warnf("Knative is deprecated and this test is disabled.")
+		return
+	}
+
+	if os.Getenv("KUBE2E_TESTS") != "knative"  {
 		log.Warnf("This test is disabled. " +
 			"To enable, set KUBE2E_TESTS to 'knative' in your env.")
 		return
