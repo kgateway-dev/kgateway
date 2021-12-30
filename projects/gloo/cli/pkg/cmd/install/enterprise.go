ad9ea39d08328190df4ca54317271618d58abb14
Keerthan Ekbote <keerthan.ekbote@solo.io>
2021-12-30 16:45:54 -0500

    update glooctl

--- projects/gloo/cli/pkg/cmd/install/enterprise.go
+++ projects/gloo/cli/pkg/cmd/install/enterprise.go
@@ -1,49 +1,49 @@
 package install
 
 import (
-	"github.com/rotisserie/eris"
-	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
-	"github.com/solo-io/gloo/projects/gloo/cli/pkg/flagutils"
-	"github.com/spf13/cobra"
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
+  "github.com/rotisserie/eris"
+  "github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
+  "github.com/solo-io/gloo/projects/gloo/cli/pkg/flagutils"
+  "github.com/spf13/cobra"
+  _ "k8s.io/client-go/plugin/pkg/client/auth"
 )
 
 const (
-	GlooEHelmRepoTemplate = "https://storage.googleapis.com/gloo-ee-helm/charts/gloo-ee-%s.tgz"
+  GlooEHelmRepoTemplate = "https://storage.googleapis.com/gloo-ee-helm/charts/gloo-ee-%s.tgz"
 )
 
 func enterpriseCmd(opts *options.Options) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:    "enterprise",
-		Short:  "install the Gloo Enterprise Gateway on Kubernetes",
-		Long:   "requires kubectl to be installed",
-		PreRun: setVerboseMode(opts),
-		RunE: func(cmd *cobra.Command, args []string) error {
+  cmd := &cobra.Command{
+    Use:    "enterprise",
+    Short:  "install the Gloo Enterprise Gateway on Kubernetes",
+    Long:   "requires kubectl to be installed",
+    PreRun: setVerboseMode(opts),
+    RunE: func(cmd *cobra.Command, args []string) error {
 
-			extraValues := map[string]interface{}{
-				"license_key":      opts.Install.LicenseKey,
-				"gloo-fed.enabled": opts.Install.WithGlooFed,
-			}
+      extraValues := map[string]interface{}{
+        "gloo.license_key": opts.Install.LicenseKey,
+        "gloo-fed.enabled": opts.Install.WithGlooFed,
+      }
 
-			mode := Enterprise
-			if err := NewInstaller(DefaultHelmClient()).Install(&InstallerConfig{
-				InstallCliArgs: &opts.Install,
-				ExtraValues:    extraValues,
-				Mode:           mode, // mode will be overwritten in Install to Gloo if the helm chart doesn't have gloo subchart
-				Verbose:        opts.Top.Verbose,
-				Ctx:            opts.Top.Ctx,
-			}); err != nil {
-				return eris.Wrapf(err, "installing Gloo Edge Enterprise in gateway mode")
-			}
+      mode := Enterprise
+      if err := NewInstaller(DefaultHelmClient()).Install(&InstallerConfig{
+        InstallCliArgs: &opts.Install,
+        ExtraValues:    extraValues,
+        Mode:           mode, // mode will be overwritten in Install to Gloo if the helm chart doesn't have gloo subchart
+        Verbose:        opts.Top.Verbose,
+        Ctx:            opts.Top.Ctx,
+      }); err != nil {
+        return eris.Wrapf(err, "installing Gloo Edge Enterprise in gateway mode")
+      }
 
-			return nil
-		},
-	}
+      return nil
+    },
+  }
 
-	pFlags := cmd.PersistentFlags()
-	flagutils.AddGlooInstallFlags(cmd.Flags(), &opts.Install)
-	flagutils.AddEnterpriseInstallFlags(pFlags, &opts.Install)
+  pFlags := cmd.PersistentFlags()
+  flagutils.AddGlooInstallFlags(cmd.Flags(), &opts.Install)
+  flagutils.AddEnterpriseInstallFlags(pFlags, &opts.Install)
 
-	pFlags.Lookup("gloo-fed-values").Hidden = true
-	return cmd
+  pFlags.Lookup("gloo-fed-values").Hidden = true
+  return cmd
 }
