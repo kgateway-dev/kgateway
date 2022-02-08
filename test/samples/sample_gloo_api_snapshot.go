c6529bedeaf71a286efcc21931eebda03ec73c09
Ben Taussig <ben.taussig@solo.io>
2022-02-08 09:10:11 -0500

    Revert merge changes to sample_gloo_api_snapshot.go

--- test/samples/sample_gloo_api_snapshot.go
+++ test/samples/sample_gloo_api_snapshot.go
@@ -6,9 +6,7 @@ import (
 	"github.com/solo-io/gloo/projects/gateway/pkg/defaults"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/core/matchers"
-	v1snap "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/gloosnapshot"
 	"github.com/solo-io/gloo/projects/gloo/pkg/api/v1/options/static"
-	gloohelpers "github.com/solo-io/gloo/test/helpers"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 )
 
@@ -31,52 +29,8 @@ func SimpleUpstream() *v1.Upstream {
 	}
 }
 
-func UpstreamWithSecret(secret *v1.Secret) *v1.Upstream {
-	return &v1.Upstream{
-		Metadata: &core.Metadata{
-			Name:      "test",
-			Namespace: "gloo-system",
-		},
-		UpstreamType: &v1.Upstream_Static{
-			Static: &static.UpstreamSpec{
-				Hosts: []*static.Host{
-					{
-						Addr: "Test",
-						Port: 124,
-					},
-				},
-			},
-		},
-		SslConfig: &v1.UpstreamSslConfig{
-			SslSecrets: &v1.UpstreamSslConfig_SecretRef{
-				SecretRef: &core.ResourceRef{
-					Name:      secret.GetMetadata().GetName(),
-					Namespace: secret.GetMetadata().GetNamespace(),
-				},
-			},
-		},
-	}
-}
-
-func SimpleSecret() *v1.Secret {
-	return &v1.Secret{
-		Metadata: &core.Metadata{
-			Name:      "secret",
-			Namespace: "gloo-system",
-		},
-		Kind: &v1.Secret_Tls{
-			Tls: &v1.TlsSecret{
-				CertChain:  gloohelpers.Certificate(),
-				PrivateKey: gloohelpers.PrivateKey(),
-				RootCa:     gloohelpers.Certificate(),
-			},
-		},
-	}
-}
-
-func SimpleGlooSnapshot() *v1snap.ApiSnapshot {
-	secret := SimpleSecret()
-	us := UpstreamWithSecret(secret)
+func SimpleGlooSnapshot() *v1.ApiSnapshot {
+	us := SimpleUpstream()
 	routes := []*v1.Route{{
 		Action: &v1.Route_RouteAction{
 			RouteAction: &v1.RouteAction{
