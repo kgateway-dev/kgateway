57b1ede8b194ce1e0d530dd74a00e16e18be4e62
Sam Heilbron <samheilbron@gmail.com>
2022-03-24 17:36:15 -0400

    include app=gloo label on gateway proxy services

--- install/test/helm_test.go
+++ install/test/helm_test.go
@@ -466,6 +466,7 @@ var _ = Describe("Helm Test", func() {
 						ExpectWithOffset(1, err).NotTo(HaveOccurred(), fmt.Sprintf("Service %+v should be able to convert from unstructured", service))
 						structuredService, ok := serviceObject.(*v1.Service)
 						ExpectWithOffset(1, ok).To(BeTrue(), fmt.Sprintf("Service %+v should be able to cast to a structured service", service))
+						ExpectWithOffset(1, structuredService.Labels["app"]).To(Equal("gloo"), "Service has app=gloo label")
 
 						if serviceContainsMonitoringPort(structuredService) {
 							actualServicesWithHttpMonitoring = append(actualServicesWithHttpMonitoring, structuredService.GetName())
@@ -504,6 +505,7 @@ var _ = Describe("Helm Test", func() {
 						ExpectWithOffset(1, err).NotTo(HaveOccurred(), fmt.Sprintf("Deployment %+v should be able to convert from unstructured", deployment))
 						structuredDeployment, ok := deploymentObject.(*appsv1.Deployment)
 						ExpectWithOffset(1, ok).To(BeTrue(), fmt.Sprintf("Deployment %+v should be able to cast to a structured deployment", deployment))
+						ExpectWithOffset(1, structuredDeployment.Labels["app"]).To(Equal("gloo"), "Deployment has app=gloo label")
 
 						if deploymentContainsMonitoringPort(structuredDeployment) {
 							actualDeploymentsWithHttpMonitoring = append(actualDeploymentsWithHttpMonitoring, structuredDeployment.GetName())
