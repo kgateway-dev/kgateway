f0a0e3009bcefa481e217a178ccb62ffca021112
Fabian Gonzalez <fabiangonz98@gmail.com>
2022-03-31 14:33:09 -0400

    Add precenseFilter to ldap config

--- projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.equal.go
+++ projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.equal.go
@@ -1638,6 +1638,10 @@ func (m *Ldap) Equal(that interface{}) bool {
 		}
 	}
 
+	if strings.Compare(m.GetPresenceFilter(), target.GetPresenceFilter()) != 0 {
+		return false
+	}
+
 	return true
 }
 
