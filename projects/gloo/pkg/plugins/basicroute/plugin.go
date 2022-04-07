2e32ff9441570f59f43d35629d66dbe1adc92149
Ben Taussig <ben.taussig@solo.io>
2022-04-07 09:43:57 -0400

    remove unnecessary check in basicroute ProcessRoute method

--- projects/gloo/pkg/plugins/basicroute/plugin.go
+++ projects/gloo/pkg/plugins/basicroute/plugin.go
@@ -48,11 +48,6 @@ func (p *plugin) ProcessVirtualHost(
 }
 
 func (p *plugin) ProcessRoute(params plugins.RouteParams, in *v1.Route, out *envoy_config_route_v3.Route) error {
-	// This plugin is only available for routeActions. return early if a different action is specified.
-	if _, ok := in.GetAction().(*v1.Route_RouteAction); !ok {
-		return nil
-	}
-
 	if in.GetOptions() == nil {
 		return nil
 	}
