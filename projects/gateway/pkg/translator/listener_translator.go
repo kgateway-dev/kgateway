4f5506378a9c44dbac1a7a0ea4b71e02967bde41
Sam Heilbron <samheilbron@gmail.com>
2022-04-20 19:24:56 -0400

    Add error when gatewya has missing or invalid gateway type

--- projects/gateway/pkg/translator/listener_translator.go
+++ projects/gateway/pkg/translator/listener_translator.go
@@ -3,12 +3,14 @@ package translator
 import (
 	"context"
 
+	"github.com/solo-io/solo-kit/pkg/errors"
+
 	v1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	gloov1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	"github.com/solo-io/solo-kit/pkg/api/v2/reporter"
 )
 
-var _ ListenerTranslator = new(NoOpTranslator)
+var _ ListenerTranslator = new(InvalidGatewayTypeTranslator)
 
 type ListenerTranslator interface {
 	Name() string
@@ -29,12 +31,13 @@ func NewTranslatorParams(ctx context.Context, snapshot *v1.ApiSnapshot, reports 
 	}
 }
 
-type NoOpTranslator struct{}
+type InvalidGatewayTypeTranslator struct{}
 
-func (n NoOpTranslator) Name() string {
-	return "no-op"
+func (n InvalidGatewayTypeTranslator) Name() string {
+	return "invalid-gateway-type"
 }
 
-func (n NoOpTranslator) ComputeListener(params Params, proxyName string, gateway *v1.Gateway) *gloov1.Listener {
+func (n InvalidGatewayTypeTranslator) ComputeListener(params Params, proxyName string, gateway *v1.Gateway) *gloov1.Listener {
+	params.reports.AddError(gateway, errors.Errorf("Gateway contained invalid GatewayType: %v", gateway.GetGatewayType()))
 	return nil
 }
