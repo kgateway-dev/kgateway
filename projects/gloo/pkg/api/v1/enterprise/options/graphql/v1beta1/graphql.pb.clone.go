8d9b33871edd94a22565b5d46e81486111953e29
brendan slabe <brendan.slabe@solo.io>
2022-05-13 11:05:44 -0400

    update dependencies

--- projects/gloo/pkg/api/v1/enterprise/options/graphql/v1beta1/graphql.pb.clone.go
+++ projects/gloo/pkg/api/v1/enterprise/options/graphql/v1beta1/graphql.pb.clone.go
@@ -682,12 +682,6 @@ func (m *Executor_Remote) Clone() proto.Message {
 
 	target.SpanName = m.GetSpanName()
 
-	if h, ok := interface{}(m.GetOptions()).(clone.Cloner); ok {
-		target.Options = h.Clone().(*Executor_Remote_RemoteExecutorOptions)
-	} else {
-		target.Options = proto.Clone(m.GetOptions()).(*Executor_Remote_RemoteExecutorOptions)
-	}
-
 	return target
 }
 
@@ -703,23 +697,6 @@ func (m *Executor_Local_LocalExecutorOptions) Clone() proto.Message {
 		target.MaxDepth = h.Clone().(*github_com_golang_protobuf_ptypes_wrappers.UInt32Value)
 	} else {
 		target.MaxDepth = proto.Clone(m.GetMaxDepth()).(*github_com_golang_protobuf_ptypes_wrappers.UInt32Value)
-	}
-
-	return target
-}
-
-// Clone function
-func (m *Executor_Remote_RemoteExecutorOptions) Clone() proto.Message {
-	var target *Executor_Remote_RemoteExecutorOptions
-	if m == nil {
-		return target
-	}
-	target = &Executor_Remote_RemoteExecutorOptions{}
-
-	if h, ok := interface{}(m.GetTimeout()).(clone.Cloner); ok {
-		target.Timeout = h.Clone().(*github_com_golang_protobuf_ptypes_duration.Duration)
-	} else {
-		target.Timeout = proto.Clone(m.GetTimeout()).(*github_com_golang_protobuf_ptypes_duration.Duration)
 	}
 
 	return target
--- projects/gloo/pkg/api/v1/enterprise/options/graphql/v1beta1/graphql.pb.equal.go
+++ projects/gloo/pkg/api/v1/enterprise/options/graphql/v1beta1/graphql.pb.equal.go
@@ -1126,16 +1126,6 @@ func (m *Executor_Remote) Equal(that interface{}) bool {
 		return false
 	}
 
-	if h, ok := interface{}(m.GetOptions()).(equality.Equalizer); ok {
-		if !h.Equal(target.GetOptions()) {
-			return false
-		}
-	} else {
-		if !proto.Equal(m.GetOptions(), target.GetOptions()) {
-			return false
-		}
-	}
-
 	return true
 }
 
@@ -1166,40 +1156,6 @@ func (m *Executor_Local_LocalExecutorOptions) Equal(that interface{}) bool {
 		}
 	} else {
 		if !proto.Equal(m.GetMaxDepth(), target.GetMaxDepth()) {
-			return false
-		}
-	}
-
-	return true
-}
-
-// Equal function
-func (m *Executor_Remote_RemoteExecutorOptions) Equal(that interface{}) bool {
-	if that == nil {
-		return m == nil
-	}
-
-	target, ok := that.(*Executor_Remote_RemoteExecutorOptions)
-	if !ok {
-		that2, ok := that.(Executor_Remote_RemoteExecutorOptions)
-		if ok {
-			target = &that2
-		} else {
-			return false
-		}
-	}
-	if target == nil {
-		return m == nil
-	} else if m == nil {
-		return false
-	}
-
-	if h, ok := interface{}(m.GetTimeout()).(equality.Equalizer); ok {
-		if !h.Equal(target.GetTimeout()) {
-			return false
-		}
-	} else {
-		if !proto.Equal(m.GetTimeout(), target.GetTimeout()) {
 			return false
 		}
 	}
