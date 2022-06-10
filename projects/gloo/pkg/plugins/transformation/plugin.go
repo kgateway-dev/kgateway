f0229e34723460d0ae5d3902447a3a7f965f96db
nfuden <nathan.fudenberg@solo.io>
2022-06-10 08:41:19 -0400

    plugins: Migrate to the new method name now that review is completed

--- projects/gloo/pkg/plugins/transformation/plugin.go
+++ projects/gloo/pkg/plugins/transformation/plugin.go
@@ -192,14 +192,14 @@ func (p *Plugin) HttpFilters(params plugins.Params, listener *v1.HttpListener) (
 		earlyStageConfig := &envoytransformation.FilterTransformations{
 			Stage: EarlyStageNumber,
 		}
-		earlyFilter, err := plugins.NewStagedFilterWithConfig(FilterName, earlyStageConfig, earlyPluginStage)
+		earlyFilter, err := plugins.NewStagedFilter(FilterName, earlyStageConfig, earlyPluginStage)
 		if err != nil {
 			return nil, err
 		}
 		filters = append(filters, earlyFilter)
 	}
 
-	emptyFilter, _ := plugins.NewStagedFilterWithConfig(FilterName, &envoytransformation.FilterTransformations{}, pluginStage)
+	emptyFilter, _ := plugins.NewStagedFilter(FilterName, &envoytransformation.FilterTransformations{}, pluginStage)
 	filters = append(filters, emptyFilter)
 
 	return filters, nil
