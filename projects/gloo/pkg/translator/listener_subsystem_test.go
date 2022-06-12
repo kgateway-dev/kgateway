f91bcd813a339351f92250c4463fa4b3bc17bde1
Sam Heilbron <samheilbron@gmail.com>
2022-06-12 16:58:55 -0400

    add listener report, initial handilng of aggregate listener

---
+++ projects/gloo/pkg/translator/listener_subsystem_test.go
@@ -0,0 +1,13 @@
+package translator_test
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Listener Subsystem", func() {
+
+	// TODO (sam-heilbron)
+	// Currently our translator_test holds all of the logic. It would be great to split off some of those tests
+	// into more focused files
+
+})
