628f732db2aec5f66d0962a6f164f01384e25d22
Ian MacClancy <ianmacclancy@gmail.com>
2022-06-14 17:08:55 -0400

    Add route error when no weights passed on multiaction route weighted destination

--- projects/gloo/pkg/translator/route_config.go
+++ projects/gloo/pkg/translator/route_config.go
@@ -435,7 +435,7 @@ func (h *httpRouteConfigurationTranslator) setWeightedClusters(params plugins.Ro
 
 			validation.AppendRouteError(routeReport,
 				validationapi.RouteReport_Error_ProcessingError,
-				fmt.Sprintf("Incorrect configuration for Weighted Destination for upstream: %s - Weighted Destinations require a weight that is greater than 1", weightedDestUpstream.Name),
+				fmt.Sprintf("Incorrect configuration for Weighted Destination for upstream: %s - Weighted Destinations require a weight that is greater than 0", weightedDestUpstream.GetName()),
 				routeName,
 			)
 		}
@@ -466,8 +466,10 @@ func (h *httpRouteConfigurationTranslator) setWeightedClusters(params plugins.Ro
 		}
 	}
 
-	//Envoy has a default total weight of 100 and requires all weights to equal 100
-	//This sets the total weight to the sum of all passed weights making all passed weights
+	//Enovy has a default total weight of 100 and requires all weights to equal the current value of total weight
+	//This overrides the default of 100 to the sum of all passed weights both satisfying the requirements
+	// - that all weights equal total weight
+	// - the passed weights are weighted proportional to each other
 	clusterSpecifier.WeightedClusters.TotalWeight = &wrappers.UInt32Value{Value: totalWeight}
 
 	return nil
