95cfcd20903387da84a0f9330ade83876fba2ec1
Gunnar <gunnar.horve@solo.io>
2022-06-16 13:28:54 -0400

    fix(glooctl): apply CRDs to cluster prior to register

--- projects/gloo/cli/pkg/cmd/federation/register/register.go
+++ projects/gloo/cli/pkg/cmd/federation/register/register.go
@@ -2,13 +2,41 @@ package register
 
 import (
 	"context"
+	"fmt"
+	"os"
 
+	"github.com/solo-io/gloo/pkg/cliutil"
+	linkedversion "github.com/solo-io/gloo/pkg/version"
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/install"
 	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
 	"github.com/solo-io/skv2/pkg/multicluster/kubeconfig"
 	"github.com/solo-io/skv2/pkg/multicluster/register"
 	v1 "k8s.io/api/rbac/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+func installCrdsToRemote(context string) error {
+	helmClient := install.DefaultHelmClient()
+
+	chartObj, err := helmClient.DownloadChart("https://storage.googleapis.com/solo-public-helm/charts/gloo-" + linkedversion.Version + ".tgz")
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "\nGloo failed to install CRDs! Detailed logs available at %s.\n", cliutil.GetLogsPath())
+		return err
+	}
+	chartObj.Templates = nil
+
+	helmInstall, _, err := helmClient.NewInstall("default", "gloo-automatic-crd-application", false, context)
+	if err != nil {
+		return err
+	}
+
+	_, err = helmInstall.Run(chartObj, nil)
+	if err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "\nGloo failed to install CRDs! Detailed logs available at %s.\n", cliutil.GetLogsPath())
+		return err
+	}
+	return nil
+}
 
 func Register(opts *options.Options) error {
 	ctx := context.Background()
@@ -21,6 +49,10 @@ func Register(opts *options.Options) error {
 	if err != nil {
 		return err
 	}
+
+	// always apply CRDs when registering a cluster to prevent gloo-fed crash loops when registering empty cluster:
+	// https://github.com/solo-io/gloo/issues/5832
+	installCrdsToRemote(registerOpts.RemoteContext)
 
 	clusterRegisterOpts := register.RegistrationOptions{
 		KubeCfg:          mgmtKubeCfg,
