7fe5b2dc3107f363162cd6c6edb21b30b1228622
Bernie Birnbaum <bewebi@earthlink.net>
2022-06-23 16:10:06 -0400

    fix test

--- projects/gateway/pkg/syncer/translator_syncer_integration_test.go
+++ projects/gateway/pkg/syncer/translator_syncer_integration_test.go
@@ -71,7 +71,7 @@ var _ = Describe("TranslatorSyncer integration test", func() {
 
 		proxyClient, err = gloov1.NewProxyClient(ctx, memFactory)
 		Expect(err).NotTo(HaveOccurred())
-		proxyReconciler := reconciler.NewProxyReconciler(nil, proxyClient, statusClient)
+		proxyReconciler := reconciler.NewProxyReconciler(nil, proxyClient, statusClient, 0)
 		rpt := reporter.NewReporter("gateway", statusClient, gatewayClient.BaseClient(), virtualServiceClient.BaseClient(), routeTableClient.BaseClient())
 		xlator := translator.NewDefaultTranslator(translator.Opts{})
 		ts = NewTranslatorSyncer(ctx, "gloo-system", proxyClient, proxyReconciler, rpt, xlator, statusClient)
