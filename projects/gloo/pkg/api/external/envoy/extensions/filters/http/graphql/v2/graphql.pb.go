4700f3cc318a5c4dd0ffc95a5c433d0a3419542f
Jake Cukjati <jakecukjati@gmail.com>
2022-06-23 08:53:04 -0500

    pushing changes

--- projects/gloo/pkg/api/external/envoy/extensions/filters/http/graphql/v2/graphql.pb.go
+++ projects/gloo/pkg/api/external/envoy/extensions/filters/http/graphql/v2/graphql.pb.go
@@ -7,13 +7,14 @@
 package v2
 
 import (
+	reflect "reflect"
+	sync "sync"
+
 	_ "github.com/envoyproxy/protoc-gen-validate/validate"
 	any "github.com/golang/protobuf/ptypes/any"
 	v3 "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/config/core/v3"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
-	reflect "reflect"
-	sync "sync"
 )
 
 const (
