f40cc64f87ec863b78d25165b2d8d4e9ab8a0ab4
Sam Heilbron <samheilbron@gmail.com>
2022-07-15 15:18:04 -0400

    undo validation webhook changes

--- projects/gateway/pkg/services/k8sadmisssion/validating_admission_webhook.go
+++ projects/gateway/pkg/services/k8sadmisssion/validating_admission_webhook.go
@@ -11,8 +11,6 @@ import (
 	"net/http/httputil"
 	"time"
 
-	"github.com/solo-io/solo-kit/pkg/api/v2/reporter"
-
 	"github.com/hashicorp/go-multierror"
 
 	"github.com/ghodss/yaml"
@@ -165,8 +163,6 @@ type gatewayValidationWebhook struct {
 	alwaysAccept                  bool
 	readGatewaysFromAllNamespaces bool
 	webhookNamespace              string
-
-	ignoredFieldManagers map[string]struct{}
 }
 
 type AdmissionReviewWithProxies struct {
@@ -191,13 +187,7 @@ func NewGatewayValidationHandler(ctx context.Context, validator validation.Valid
 		watchNamespaces:               watchNamespaces,
 		alwaysAccept:                  alwaysAccept,
 		readGatewaysFromAllNamespaces: readGatewaysFromAllNamespaces,
-		webhookNamespace:              webhookNamespace,
-		ignoredFieldManagers: map[string]struct{}{
-			// The StatusReporter is responsible for updating the status of resources in Gloo Edge
-			// Therefore, we can ignore requests from this FieldManager
-			reporter.KubeFieldManager: {},
-		},
-	}
+		webhookNamespace:              webhookNamespace}
 }
 
 func (wh *gatewayValidationWebhook) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -287,15 +277,6 @@ func (wh *gatewayValidationWebhook) makeAdmissionResponse(ctx context.Context, r
 
 	logger.Debugf("AdmissionReview for Kind=%v, Namespace=%v Name=%v UID=%v patchOperation=%v UserInfo=%v",
 		req.Kind, req.Namespace, req.Name, req.UID, req.Operation, req.UserInfo)
-
-	// if the request originated from a FieldManager that the webhook is configured to ignore, do not validate
-	if wh.isIgnoredFieldManager(req) {
-		return &AdmissionResponseWithProxies{
-			AdmissionResponse: &v1beta1.AdmissionResponse{
-				Allowed: true,
-			},
-		}
-	}
 
 	gvk := schema.GroupVersionKind{
 		Group:   req.Kind.Group,
@@ -393,29 +374,6 @@ func (wh *gatewayValidationWebhook) makeAdmissionResponse(ctx context.Context, r
 		},
 		Proxies: proxies,
 	}
-}
-
-// isIgnoredFieldManager returns true if the AdmissionRequest was initiated by a FieldManager
-// that can be ignored by the webhook.
-func (wh *gatewayValidationWebhook) isIgnoredFieldManager(request *v1beta1.AdmissionRequest) bool {
-	var fieldManager string
-
-	switch request.Operation {
-	case v1beta1.Create:
-		var createOpts metav1.CreateOptions
-		if err := json.Unmarshal(request.Options.Raw, &createOpts); err == nil {
-			fieldManager = createOpts.FieldManager
-		}
-
-	case v1beta1.Update:
-		var updateOpts metav1.UpdateOptions
-		if err := json.Unmarshal(request.Options.Raw, &updateOpts); err == nil {
-			fieldManager = updateOpts.FieldManager
-		}
-	}
-
-	_, ok := wh.ignoredFieldManagers[fieldManager]
-	return ok
 }
 
 func getFailureCauses(validationErr *multierror.Error) []metav1.StatusCause {
