684ee64e3b5833128e11a41b82eede420b5ffe2f
changran <changran@megvii.com>
2022-08-08 16:28:26 +0800

    feat(plugins/kubernetes): use get instead of list to improve performance

--- projects/gloo/pkg/plugins/kubernetes/plugin.go
+++ projects/gloo/pkg/plugins/kubernetes/plugin.go
@@ -7,6 +7,7 @@ import (
 	envoy_config_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
 	errors "github.com/rotisserie/eris"
 	"k8s.io/apimachinery/pkg/labels"
+	kubelisters "k8s.io/client-go/listers/core/v1"
 
 	"github.com/solo-io/gloo/projects/gloo/pkg/discovery"
 
@@ -79,13 +80,26 @@ func (p *plugin) ProcessUpstream(params plugins.Params, in *v1.Upstream, out *en
 			upstreamRef.String(), kube.Kube.GetServiceName(), kube.Kube.GetServiceNamespace())
 	}
 
-	svcs, err := lister.List(labels.NewSelector())
-	if err != nil {
-		return err
+	var sn kubelisters.ServiceNamespaceLister
+	if sn, ok = lister.(kubelisters.ServiceNamespaceLister); !ok {
+		if sl, ok := lister.(kubelisters.ServiceLister); ok {
+			sn = sl.Services(kube.Kube.GetServiceNamespace())
+		}
 	}
-	for _, s := range svcs {
-		if s.Name == kube.Kube.GetServiceName() {
+	if sn != nil {
+		if _, err := sn.Get(kube.Kube.GetServiceName()); err == nil {
 			return nil
+		}
+	} else {
+		svcs, err := lister.List(labels.NewSelector())
+		if err != nil {
+			return err
+		}
+
+		for _, s := range svcs {
+			if s.Name == kube.Kube.GetServiceName() {
+				return nil
+			}
 		}
 	}
 
