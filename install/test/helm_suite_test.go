be2d37a119fd5c0f1f7cb6b5b3973319ef090391
Bernie Birnbaum <bewebi@earthlink.net>
2022-09-06 13:43:27 -0400

    helm test CI switch

--- install/test/helm_suite_test.go
+++ install/test/helm_suite_test.go
@@ -9,6 +9,7 @@ import (
 	"os/exec"
 	"path"
 	"path/filepath"
+	"strings"
 	"testing"
 	"text/template"
 
@@ -111,9 +112,11 @@ func MustGetVersion() string {
 	output := MustMakeReturnStdout(".", "-C", "../../", "print-VERSION") // use print-VERSION so version matches on forks
 	// sample output
 	// make: Entering directory '/var/home/kdorosh/git/forks/gloo'\n0.0.0-fork\nmake: Leaving directory '/var/home/kdorosh/git/forks/gloo'\n"
-	//lines := strings.Split(output, "\n")
-	//Expect(len(lines)).To(BeNumerically(">", 2))
-	//return lines[1]
+	if os.Getenv("CI") == "true" {
+		lines := strings.Split(output, "\n")
+		Expect(len(lines)).To(BeNumerically(">", 2))
+		return lines[1]
+	}
 	return output[:len(output)-1]
 }
 
