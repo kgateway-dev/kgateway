db623c5ce69a882e64de23ce611a9f8fb0910e3c
brendan slabe <brendan.slabe@solo.io>
2022-09-21 12:40:47 -0400

    revert to panic

--- docs/content/guides/dev/example-proxy-controller.go
+++ docs/content/guides/dev/example-proxy-controller.go
@@ -14,7 +14,6 @@ import (
 
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 	matchers "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/core/matchers"
-	"github.com/solo-io/go-utils/contextutils"
 	"github.com/solo-io/k8s-utils/kubeutils"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients"
 	"github.com/solo-io/solo-kit/pkg/api/v1/clients/factory"
@@ -212,6 +211,6 @@ func makeDesiredProxy(upstreams v1.UpstreamList) *v1.Proxy {
 // make our lives easy
 func must(err error) {
 	if err != nil {
-		contextutils.LoggerFrom(nil).DPanic(err)
+		panic(err)
 	}
 }
