8018aa477d3d77c32fab72db416b33a100f6e57c
brendan slabe <brendan.slabe@solo.io>
2022-12-27 09:36:45 -0500

    test name nit

--- test/kube2e/glooctl/glooctl_test.go
+++ test/kube2e/glooctl/glooctl_test.go
@@ -508,7 +508,7 @@ var _ = Describe("Kube2e: glooctl", func() {
 		})
 	})
 	Context("check-crds", func() {
-		It("validate correct CRDs", func() {
+		It("validates correct CRDs", func() {
 			_, err := runGlooctlCommand("check-crds")
 			Expect(err).ToNot(HaveOccurred())
 		})
