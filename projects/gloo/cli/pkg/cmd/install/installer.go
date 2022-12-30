83efbae09766eb1f87c841e69732021dd5972460
Jake Cukjati <jakecukjati@gmail.com>
2022-12-30 13:08:21 -0600

    get rid of TODO

--- projects/gloo/cli/pkg/cmd/install/installer.go
+++ projects/gloo/cli/pkg/cmd/install/installer.go
@@ -146,7 +146,6 @@ func (i *installer) installFromConfig(installerConfig *InstallerConfig) error {
 
 	// Merge the CLI flag values into the extra values, giving the latter higher precedence.
 	// (The first argument to CoalesceTables has higher priority)
-	// TODO-JAKE installing from the config can cause issue.
 	completeValues := chartutil.CoalesceTables(installerConfig.ExtraValues, cliValues)
 	if installerConfig.Verbose {
 		b, err := json.Marshal(completeValues)
