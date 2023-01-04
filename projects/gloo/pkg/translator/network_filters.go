243752963a06b04f6ca53d4b1f93548807b405a6
bryan_english <bryan.english@solo.io>
2023-01-04 10:55:55 -0600

    added internal logic to fetch and parse the router object

--- projects/gloo/pkg/translator/network_filters.go
+++ projects/gloo/pkg/translator/network_filters.go
@@ -4,10 +4,8 @@ import (
 	"sort"
 
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
-	envoyhcm "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	envoyhttp "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	"github.com/envoyproxy/go-control-plane/pkg/wellknown"
-	"github.com/golang/protobuf/ptypes/any"
 	"github.com/golang/protobuf/ptypes/wrappers"
 	errors "github.com/rotisserie/eris"
 	"github.com/solo-io/go-utils/contextutils"
@@ -209,30 +207,16 @@ func (h *hcmNetworkFilterTranslator) computeHttpFilters(params plugins.Params) [
 	// As outlined by the Envoy docs, the last configured filter has to be a terminal filter.
 	// We set the Router filter (https://www.envoyproxy.io/docs/envoy/latest/configuration/http/http_filters/router_filter#config-http-filters-router)
 	// as the terminal filter in Gloo Edge.
-
-	//options := h.listener.GetOptions().Gzip.
-
 	if router := h.listener.Options.GetRouter(); router != nil {
-		// TODO
-		envoyHttpFilters = append(envoyHttpFilters, &envoyhttp.HttpFilter{
-			Name: wellknown.Router,
-			ConfigType: &envoyhcm.HttpFilter_TypedConfig{
-				TypedConfig: &any.Any{
-					TypeUrl: "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router",
-				},
-			},
-		})
+		routerFilter := plugins.MustNewStagedFilter(
+			wellknown.Router,
+			router,
+			plugins.AfterStage(plugins.RouteStage),
+		)
+
+		envoyHttpFilters = append(envoyHttpFilters, routerFilter.HttpFilter)
 		return envoyHttpFilters
 	}
-
-	envoyHttpFilters = append(envoyHttpFilters, &envoyhttp.HttpFilter{
-		Name: wellknown.Router,
-		ConfigType: &envoyhcm.HttpFilter_TypedConfig{
-			TypedConfig: &any.Any{
-				TypeUrl: "type.googleapis.com/envoy.extensions.filters.http.router.v3.Router",
-			},
-		},
-	})
 
 	return envoyHttpFilters
 }
