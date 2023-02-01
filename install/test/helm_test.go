019ca89162e3c5211ad6445eb07975d11c495e53
sheidkamp <seth.heidkamp@gmail.com>
2023-02-01 11:39:42 -0500

    debugging

--- install/test/helm_test.go
+++ install/test/helm_test.go
@@ -6153,7 +6153,7 @@ spec:
 		})
 
 		// Lines ending with whitespace causes malformatted config map (https://github.com/solo-io/gloo/issues/4645)
-		FIt("should not contain trailing whitespace", func() {
+		It("should not contain trailing whitespace", func() {
 			out, err := exec.Command("helm", "template", "../helm/gloo").Output()
 			if true {
 
