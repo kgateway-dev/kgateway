f515b0bf2ae8db33550798c7a3d0f23b7b3415df
Sam Heilbron <samheilbron@gmail.com>
2023-02-13 17:04:38 -0500

    replace other instances of ginkgo

--- test/services/envoy.go
+++ test/services/envoy.go
@@ -21,8 +21,8 @@ import (
 
 	"github.com/solo-io/gloo/projects/gloo/pkg/defaults"
 
-	"github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
+	"github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	errors "github.com/rotisserie/eris"
 
