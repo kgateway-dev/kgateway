2961251dcf919b9038b23de44d586fef776ec23a
Sam Heilbron <samheilbron@gmail.com>
2023-02-14 09:55:17 -0500

    cleanup cli create tests

--- projects/gloo/cli/pkg/cmd/create/authconfig/authconfig_suite_test.go
+++ projects/gloo/cli/pkg/cmd/create/authconfig/authconfig_suite_test.go
@@ -3,6 +3,8 @@ package authconfig_test
 import (
 	"testing"
 
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/helpers"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
@@ -11,3 +13,7 @@ func TestAuthConfig(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "AuthConfig Suite")
 }
+
+var _ = BeforeSuite(func() {
+	helpers.UseMemoryClients()
+})
