475dc2e3b16b801477bd114d81bc98d7ff69b5e9
Jacob Bohanon <jacobbohanon@gmail.com>
2023-02-23 08:52:29 -0500

    remove log

--- projects/gloo/pkg/plugins/aws/plugin_test.go
+++ projects/gloo/pkg/plugins/aws/plugin_test.go
@@ -2,9 +2,9 @@ package aws_test
 
 import (
 	"context"
-	"log"
 	"net/url"
 
+	. "github.com/onsi/ginkgo/extensions/table"
 	"github.com/onsi/gomega/types"
 
 	envoy_config_cluster_v3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
@@ -13,7 +13,7 @@ import (
 	gogoproto "github.com/golang/protobuf/proto"
 	"github.com/golang/protobuf/ptypes"
 	"github.com/golang/protobuf/ptypes/duration"
-	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	. "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/extensions/aws"
 	envoytransform "github.com/solo-io/gloo/projects/gloo/pkg/api/external/envoy/extensions/transformation"
@@ -500,7 +500,6 @@ var _ = Describe("Plugin", func() {
 
 			perRouteCfg := getPerRouteConfig(outroute)
 			Expect(perRouteCfg.GetTransformerConfig()).NotTo(BeNil())
-			log.Println(perRouteCfg.GetTransformerConfig().GetTypedConfig().GetTypeUrl())
 			Expect(perRouteCfg.GetTransformerConfig().GetTypedConfig().GetTypeUrl()).To(Equal(ResponseTransformationTypeUrl))
 		})
 
