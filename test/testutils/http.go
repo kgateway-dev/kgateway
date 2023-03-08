3645a37a5286bf664f341911a758a677923a0eb3
Sam Heilbron <samheilbron@gmail.com>
2023-03-08 10:27:49 -0500

    Add http testutils

---
+++ test/testutils/http.go
@@ -0,0 +1,107 @@
+package testutils
+
+import (
+    "context"
+    "errors"
+    "fmt"
+    "github.com/onsi/ginkgo/v2"
+    "github.com/onsi/gomega"
+    "io"
+    "net/http"
+)
+
+type HttpRequestBuilder struct {
+    ctx context.Context
+
+    method string
+
+    scheme   string
+    hostname string
+    port     int
+    path     string
+
+    body io.Reader
+
+    host    string
+    headers map[string]string
+}
+
+// DefaultRequestBuilder returns an HttpRequestBuilder with some default values
+func DefaultRequestBuilder() *HttpRequestBuilder {
+    return &HttpRequestBuilder{
+        ctx:      context.Background(),
+        method:   http.MethodGet,
+        scheme:   "http", // https://github.com/golang/go/issues/40587
+        hostname: "localhost",
+        port:     0,
+        path:     "",
+        body:     nil,
+        host:     "",
+        headers:  nil,
+    }
+}
+
+func NewHttpRequestBuilder() *HttpRequestBuilder {
+    return DefaultRequestBuilder().WithScheme("http")
+}
+
+func NewHttpsRequestBuilder() *HttpRequestBuilder {
+    return DefaultRequestBuilder().WithScheme("https")
+}
+
+func (h *HttpRequestBuilder) WithContext(ctx context.Context) *HttpRequestBuilder {
+    h.ctx = ctx
+    return h
+}
+
+func (h *HttpRequestBuilder) WithScheme(scheme string) *HttpRequestBuilder {
+    h.scheme = scheme
+    return h
+}
+
+func (h *HttpRequestBuilder) WithPostBody(body io.Reader) *HttpRequestBuilder {
+    h.method = http.MethodPost
+    h.body = body
+    return h
+}
+
+func (h *HttpRequestBuilder) WithHeaders(headers map[string]string) *HttpRequestBuilder {
+    h.headers = headers
+    return h
+}
+
+func (h *HttpRequestBuilder) errorIfInvalid() error {
+    if h.scheme == "" {
+        return errors.New("scheme is empty, but required")
+    }
+    if h.hostname == "" {
+        return errors.New("hostname is empty, but required")
+    }
+    if h.port == 0 {
+        return errors.New("port is empty, but required")
+    }
+    return nil
+}
+
+func (h *HttpRequestBuilder) Build() *http.Request {
+    if err := h.errorIfInvalid(); err != nil {
+        // We error loudly here
+        // These types of errors are intended to prevent developers from creating resources
+        // which are semantically correct, but lead to test flakes/confusion
+        ginkgo.Fail(err.Error())
+    }
+
+    request, err := http.NewRequestWithContext(
+        h.ctx,
+        h.method,
+        fmt.Sprintf("%s://%s:%d/%s", h.scheme, h.hostname, h.port, h.path),
+        h.body)
+    gomega.Expect(err).NotTo(gomega.HaveOccurred(), "generating http request")
+
+    request.Host = h.host
+    for headerName, headerValue := range h.headers {
+        request.Header.Set(headerName, headerValue)
+    }
+
+    return request
+}
