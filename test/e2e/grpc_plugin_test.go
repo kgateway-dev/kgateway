ed1fe5c8ea1b33bfc93f22486f12158c8e520115
Ethan <ethan.casteel@solo.io>
2023-03-10 08:50:03 -0500

    forgot to remove focus

--- test/e2e/grpc_plugin_test.go
+++ test/e2e/grpc_plugin_test.go
@@ -29,7 +29,7 @@ import (
 	"github.com/solo-io/gloo/projects/gloo/pkg/defaults"
 )
 
-var _ = FDescribe("GRPC to JSON Transcoding Plugin - Gloo API", func() {
+var _ = Describe("GRPC to JSON Transcoding Plugin - Gloo API", func() {
 
 	var (
 		ctx           context.Context
