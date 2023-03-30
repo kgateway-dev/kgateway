eed86794a17d0c56ef1c4270dee85819dba004e2
Jenny Shu <jenny.shu@solo.io>
2023-03-30 16:12:32 -0400

    update validation error msg

--- test/kube2e/gateway/gateway_test.go
+++ test/kube2e/gateway/gateway_test.go
@@ -2008,7 +2008,7 @@ spec:
   virtualHoost: {}
 `,
 							// This is handled by validation schemas now
-							expectedErr: `ValidationError(VirtualService.spec): unknown field "virtualHoost" in io.solo.gateway.v1.VirtualService.spec`,
+							expectedErr: `Error from server (BadRequest): error when creating "STDIN": VirtualService in version "v1" cannot be handled as a VirtualService: strict decoding error: unknown field "spec.virtualHoost"`,
 						},
 						{
 							resourceYaml: `
