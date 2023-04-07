a43c65f7dd57d85680547e003a5d334c88ca9217
nfuden <nathan.fudenberg@solo.io>
2023-04-07 10:35:32 -0400

    test/upgrade: Update unclean test

--- test/kube2e/upgrade/upgrade_utils_test.go
+++ test/kube2e/upgrade/upgrade_utils_test.go
@@ -5,6 +5,7 @@ import (
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	"github.com/solo-io/go-utils/githubutils"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 )
 
@@ -43,6 +44,17 @@ var _ = Describe("upgrade utils unit tests", func() {
 
 			belief := err != nil || lastminor == nil
 			Expect(belief).To(BeTrue())
+		})
+	})
+
+	Context("knows how to handle certain github cases", func() {
+
+		It("should return latest patch", func() {
+			ctx := context.Background()
+			client, _ := githubutils.GetClient(ctx)
+			minor, err := getLatestReleasedPatchVersion(ctx, client, "gloo", 1, 8)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(minor.String()).To(Equal("v1.8.37"))
 		})
 	})
 
