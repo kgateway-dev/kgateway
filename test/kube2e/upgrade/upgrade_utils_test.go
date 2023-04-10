caf29f93b74119eb6c0c91883fc07d66982bd6d2
nfuden <nathan.fudenberg@solo.io>
2023-04-10 12:32:04 -0400

    test/upgrade: Update to account for upgrade nixing its own folder

--- test/kube2e/upgrade/upgrade_utils_test.go
+++ test/kube2e/upgrade/upgrade_utils_test.go
@@ -2,6 +2,7 @@ package upgrade
 
 import (
 	"context"
+	"fmt"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
@@ -19,21 +20,21 @@ func (m mockDirEntry) Name() string {
 
 var _ = Describe("upgrade utils unit tests", func() {
 	baseEntries := []mockDirEntry{
-		{"v1.7.0"}, {"v1.8.0-beta1"}, {"v1.7.1"},
+		{"v1.8.0-beta2"}, {"v1.7.0"}, {"v1.8.0-beta1"}, {"v1.7.1"},
 	}
 	Context("versions are returned as expected", func() {
 		It("should return the last minor version", func() {
-			entries := []mockDirEntry{{"v1.8.0-beta2"}}
+			entries := []mockDirEntry{{"v1.8.0-beta3"}}
 			entries = append(entries, baseEntries...) // dont pollute baseEntries
 			ver, err := filterFilesForLatestRelease(entries...)
 			Expect(err).NotTo(HaveOccurred())
-			Expect(ver.String()).To(Equal("v1.8.0-beta2"), entries)
+			Expect(ver.String()).To(Equal("v1.8.0-beta2"), fmt.Sprintf("%v", entries))
 		})
 		It("should note that we are missing the last minor version", func() {
 
 			ver, err := filterFilesForLatestRelease(baseEntries...)
 			Expect(err).To(HaveOccurred())
-			Expect(ver.String()).To(Equal("v1.8.0-beta1"), baseEntries)
+			Expect(ver.String()).To(Equal("v1.8.0-beta1"), fmt.Sprintf("%v", baseEntries))
 			Expect(err).To(Equal(FirstReleaseError))
 		})
 	})
