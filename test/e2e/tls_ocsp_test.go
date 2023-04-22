1522f927ed79871caee14f9e32333df10172bafe
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-04-22 15:26:34 -0400

    update tls ocsp e2e tests - refactoring

--- test/e2e/tls_ocsp_test.go
+++ test/e2e/tls_ocsp_test.go
@@ -20,28 +20,26 @@ import (
 )
 
 var _ = Describe("TLS OCSP e2e", func() {
+	const (
+		defaultVirtualServiceDomain = "client.com"
+		defaultVirtualServiceName   = "client-vs"
+	)
+
 	var (
-		testContext           *e2e.TestContext
-		rootCa, rootKey       string
-		clientCert, clientKey string
-		fakeOcspResponder     *helpers.FakeOcspResponder
-		tlsSecretWithNoOcsp   = &core.Metadata{
-			Name:      "tls-no-ocsp",
-			Namespace: writeNamespace,
-		}
-		tlsSecretWithOcsp = &core.Metadata{
-			Name:      "tls-with-ocsp",
-			Namespace: writeNamespace,
-		}
-		tlsSecretWithExpiredOcsp = &core.Metadata{
-			Name:      "tls-with-expired-ocsp",
-			Namespace: writeNamespace,
-		}
+		testContext              *e2e.TestContext
+		rootCa, rootKey          string
+		clientCert, clientKey    string
+		fakeOcspResponder        *helpers.FakeOcspResponder
+		tlsSecretWithNoOcsp      = &core.Metadata{Name: "tls-no-ocsp", Namespace: writeNamespace}
+		tlsSecretWithOcsp        = &core.Metadata{Name: "tls-with-ocsp", Namespace: writeNamespace}
+		tlsSecretWithExpiredOcsp = &core.Metadata{Name: "tls-with-expired-ocsp", Namespace: writeNamespace}
+		defaultUpstream          *gloov1.Upstream
 	)
 
 	BeforeEach(func() {
 		testContext = testContextFactory.NewTestContext()
 		testContext.BeforeEach()
+		defaultUpstream = testContext.TestUpstream().Upstream
 
 		// Create SSL Gateway
 		testContext.ResourcesToCreate().Gateways = v1.GatewayList{
@@ -116,53 +114,37 @@ var _ = Describe("TLS OCSP e2e", func() {
 		testContext.JustAfterEach()
 	})
 
-	buildVirtualService := func(sslRef *core.ResourceRef, ocspStaplePolicy ssl.SslConfig_OcspStaplePolicy) {
-		customVS := helpers.NewVirtualServiceBuilder().
-			WithName("client-vs").
-			WithNamespace(writeNamespace).
-			WithDomain("client.com").
-			WithRoutePrefixMatcher(e2e.DefaultRouteName, "/").
-			WithRouteActionToUpstream(e2e.DefaultRouteName, testContext.TestUpstream().Upstream).
-			WithSslConfig(&ssl.SslConfig{
-				OcspStaplePolicy: ocspStaplePolicy,
-				SniDomains:       []string{"client.com"},
-				SslSecrets: &ssl.SslConfig_SecretRef{
-					SecretRef: sslRef,
-				},
-			}).
-			Build()
-
-		// For e2e.TestContext, Snapshots are written in its `JustBeforeEach`, but since we're creating the virtual service
-		// during the test's run, we need to manually write the snapshot.
-		err := testContext.TestClients().WriteSnapshot(testContext.Ctx(), &gloosnapshot.ApiSnapshot{
-			VirtualServices: v1.VirtualServiceList{customVS},
-		})
-		ExpectWithOffset(1, err).NotTo(HaveOccurred())
+	eventualConsistent := func(fn func(g Gomega)) {
+		// Setting an offset of 2 as this function is called from within a helper function
+		EventuallyWithOffset(2, fn, "10s", "1s").Should(Succeed())
+		ConsistentlyWithOffset(2, fn, "2s", ".5s").Should(Succeed())
 	}
 
-	buildHttpsRequestClient := func(tlsRootCa, host string) (*http.Client, *testutils.HttpRequestBuilder) {
-		httpClient := testutils.DefaultClientBuilder().
-			WithTLSRootCa(tlsRootCa).
-			WithTLSServerName(host).
-			Build()
+	expectConsistentResponseStatus := func(httpClient *http.Client, httpRequestBuilder *testutils.HttpRequestBuilder, expectedStatusCode int) {
+		eventualConsistent(func(g Gomega) {
+			resp, err := httpClient.Do(httpRequestBuilder.Build())
+			g.ExpectWithOffset(1, err).NotTo(HaveOccurred())
+			g.ExpectWithOffset(1, resp).To(matchers.HaveStatusCode(expectedStatusCode))
+		})
+	}
 
-		httpRequestBuilder := testContext.GetHttpsRequestBuilder().
-			WithHost(host)
-
-		return httpClient, httpRequestBuilder
+	expectConsistentError := func(httpClient *http.Client, httpRequestBuilder *testutils.HttpRequestBuilder, expectedError string) {
+		eventualConsistent(func(g Gomega) {
+			_, err := httpClient.Do(httpRequestBuilder.Build())
+			g.ExpectWithOffset(1, err).To(HaveOccurred())
+			if expectedError != "" {
+				g.ExpectWithOffset(1, err).To(MatchError(ContainSubstring(expectedError)))
+			}
+		})
 	}
 
 	Context("with OCSP Staple Policy set to LENIENT_STAPLING", func() {
 		DescribeTable("should successfully contact upstream", func(sslRef *core.ResourceRef, expectedStatusCode int) {
-			buildVirtualService(sslRef, ssl.SslConfig_LENIENT_STAPLING)
-			httpClient, httpRequestBuilder := buildHttpsRequestClient(rootCa, "client.com")
+			err := createVirtualService(testContext, defaultVirtualServiceName, defaultVirtualServiceDomain, writeNamespace, defaultUpstream, sslRef, ssl.SslConfig_LENIENT_STAPLING)
+			Expect(err).NotTo(HaveOccurred())
+			httpClient, httpRequestBuilder := buildHttpsRequestClient(testContext, defaultVirtualServiceDomain, rootCa)
 
-			Eventually(func(g Gomega) {
-				g.Expect(httpClient.Do(httpRequestBuilder.Build())).To(matchers.HaveStatusCode(expectedStatusCode))
-			}, "10s", "1s").Should(Succeed())
-			Consistently(func(g Gomega) {
-				g.Expect(httpClient.Do(httpRequestBuilder.Build())).To(matchers.HaveStatusCode(expectedStatusCode))
-			}, "2s", ".5s").Should(Succeed())
+			expectConsistentResponseStatus(httpClient, httpRequestBuilder, expectedStatusCode)
 		},
 			Entry("OK with no ocsp staple", tlsSecretWithNoOcsp.Ref(), http.StatusOK),
 			Entry("OK with valid ocsp staple", tlsSecretWithOcsp.Ref(), http.StatusOK),
@@ -172,89 +154,95 @@ var _ = Describe("TLS OCSP e2e", func() {
 
 	Context("with  OCSP Staple Policy set to STRICT_STAPLING", func() {
 		DescribeTable("should successfully contact upstream", func(sslRef *core.ResourceRef, expectedStatusCode int) {
-			buildVirtualService(sslRef, ssl.SslConfig_STRICT_STAPLING)
-			httpClient, httpRequestBuilder := buildHttpsRequestClient(rootCa, "client.com")
+			err := createVirtualService(testContext, defaultVirtualServiceName, defaultVirtualServiceDomain, writeNamespace, defaultUpstream, sslRef, ssl.SslConfig_STRICT_STAPLING)
+			Expect(err).NotTo(HaveOccurred())
+			httpClient, httpRequestBuilder := buildHttpsRequestClient(testContext, defaultVirtualServiceDomain, rootCa)
 
-			Eventually(func(g Gomega) {
-				g.Expect(httpClient.Do(httpRequestBuilder.Build())).To(matchers.HaveStatusCode(expectedStatusCode))
-			}, "10s", "1s").Should(Succeed())
-			Consistently(func(g Gomega) {
-				g.Expect(httpClient.Do(httpRequestBuilder.Build())).To(matchers.HaveStatusCode(expectedStatusCode))
-			}, "2s", ".5s").Should(Succeed())
+			expectConsistentResponseStatus(httpClient, httpRequestBuilder, expectedStatusCode)
 		},
 			Entry("with no ocsp staple", tlsSecretWithNoOcsp.Ref(), http.StatusOK),
 			Entry("with valid ocsp staple", tlsSecretWithOcsp.Ref(), http.StatusOK),
 		)
 
 		It("fails handshake with expired ocsp staple", func() {
-			buildVirtualService(tlsSecretWithExpiredOcsp.Ref(), ssl.SslConfig_STRICT_STAPLING)
-			httpClient, httpRequestBuilder := buildHttpsRequestClient(rootCa, "client.com")
+			err := createVirtualService(testContext, defaultVirtualServiceName, defaultVirtualServiceDomain, writeNamespace, defaultUpstream, tlsSecretWithExpiredOcsp.Ref(), ssl.SslConfig_STRICT_STAPLING)
+			Expect(err).NotTo(HaveOccurred())
+			httpClient, httpRequestBuilder := buildHttpsRequestClient(testContext, defaultVirtualServiceDomain, rootCa)
 
-			Eventually(func(g Gomega) {
-				resp, err := httpClient.Do(httpRequestBuilder.Build())
-				g.Expect(err).To(HaveOccurred())
-				g.Expect(resp).To(BeNil())
-				g.Expect(err).To(MatchError(ContainSubstring("handshake failure")))
-			}, "10s", "1s").Should(Succeed())
-			Consistently(func(g Gomega) {
-				resp, err := httpClient.Do(httpRequestBuilder.Build())
-				g.Expect(err).To(HaveOccurred())
-				g.Expect(resp).To(BeNil())
-				g.Expect(err).To(MatchError(ContainSubstring("handshake failure")))
-			}, "2s", ".5s").Should(Succeed())
+			expectConsistentError(httpClient, httpRequestBuilder, "handshake failure")
 		})
 
 	})
 
 	Context("with  OCSP Staple Policy set to MUST_STAPLE", func() {
 		It("fails with no ocsp staple", func() {
-			buildVirtualService(tlsSecretWithNoOcsp.Ref(), ssl.SslConfig_MUST_STAPLE)
-			httpClient, httpRequestBuilder := buildHttpsRequestClient(rootCa, "client.com")
+			err := createVirtualService(testContext, defaultVirtualServiceName, defaultVirtualServiceDomain, writeNamespace, defaultUpstream, tlsSecretWithNoOcsp.Ref(), ssl.SslConfig_MUST_STAPLE)
+			Expect(err).NotTo(HaveOccurred())
+			httpClient, httpRequestBuilder := buildHttpsRequestClient(testContext, defaultVirtualServiceDomain, rootCa)
 
 			// TODO (fabian): figure out the proper way to test this test exactly.
-			// When doing this through an Envoy bootstrap Envoy fails to start, or at least resulted in many errors, since the `MUST_STAPLE` requires an ocsp staple.
+			// When doing this through an Envoy bootstrap Envoy fails to start, or at least resulted in many errors logs in Envoy, since the `MUST_STAPLE` requires an ocsp staple.
 			// Getting an error makes sense, as the downstream/resource would, at the very least, not be created.
-			// The specific error is unknown. Locally, it was `EOF`, but in CI there was a SyscallError.
-			Eventually(func(g Gomega) {
-				resp, err := httpClient.Do(httpRequestBuilder.Build())
-				g.Expect(err).To(HaveOccurred())
-				g.Expect(resp).To(BeNil())
-			}, "10s", "1s").Should(Succeed())
-			Consistently(func(g Gomega) {
-				resp, err := httpClient.Do(httpRequestBuilder.Build())
-				g.Expect(err).To(HaveOccurred())
-				g.Expect(resp).To(BeNil())
-			}, "2s", ".5s").Should(Succeed())
+			// The specific error is nondeterministic. Locally, it was `EOF`, but in CI there was a `SyscallError`.
+			expectConsistentError(httpClient, httpRequestBuilder, "")
 		})
 
 		It("successfully contacts upstream with valid ocsp staple", func() {
-			buildVirtualService(tlsSecretWithOcsp.Ref(), ssl.SslConfig_MUST_STAPLE)
-			httpClient, httpRequestBuilder := buildHttpsRequestClient(rootCa, "client.com")
+			err := createVirtualService(testContext, defaultVirtualServiceName, defaultVirtualServiceDomain, writeNamespace, defaultUpstream, tlsSecretWithOcsp.Ref(), ssl.SslConfig_MUST_STAPLE)
+			Expect(err).NotTo(HaveOccurred())
+			httpClient, httpRequestBuilder := buildHttpsRequestClient(testContext, defaultVirtualServiceDomain, rootCa)
 
-			Eventually(func(g Gomega) {
-				g.Expect(httpClient.Do(httpRequestBuilder.Build())).To(matchers.HaveStatusCode(http.StatusOK))
-			}, "10s", "1s").Should(Succeed())
-			Consistently(func(g Gomega) {
-				g.Expect(httpClient.Do(httpRequestBuilder.Build())).To(matchers.HaveStatusCode(http.StatusOK))
-			}, "2s", ".5s").Should(Succeed())
+			expectConsistentResponseStatus(httpClient, httpRequestBuilder, http.StatusOK)
 		})
 
 		It("fails handshake with expired ocsp staple", func() {
-			buildVirtualService(tlsSecretWithExpiredOcsp.Ref(), ssl.SslConfig_MUST_STAPLE)
-			httpClient, httpRequestBuilder := buildHttpsRequestClient(rootCa, "client.com")
+			err := createVirtualService(testContext, defaultVirtualServiceName, defaultVirtualServiceDomain, writeNamespace, defaultUpstream, tlsSecretWithExpiredOcsp.Ref(), ssl.SslConfig_MUST_STAPLE)
+			Expect(err).NotTo(HaveOccurred())
+			httpClient, httpRequestBuilder := buildHttpsRequestClient(testContext, defaultVirtualServiceDomain, rootCa)
 
-			Eventually(func(g Gomega) {
-				resp, err := httpClient.Do(httpRequestBuilder.Build())
-				g.Expect(err).To(HaveOccurred())
-				g.Expect(resp).To(BeNil())
-				g.Expect(err).To(MatchError(ContainSubstring("handshake failure")))
-			}, "10s", "1s").Should(Succeed())
-			Consistently(func(g Gomega) {
-				resp, err := httpClient.Do(httpRequestBuilder.Build())
-				g.Expect(err).To(HaveOccurred())
-				g.Expect(resp).To(BeNil())
-				g.Expect(err).To(MatchError(ContainSubstring("handshake failure")))
-			}, "2s", ".5s").Should(Succeed())
+			expectConsistentError(httpClient, httpRequestBuilder, "handshake failure")
 		})
 	})
 })
+
+// createVirtualService creates a virtual service with the given information and writes it.
+func createVirtualService(
+	testContext *e2e.TestContext,
+	name, domain, namespace string,
+	upstream *gloov1.Upstream,
+	sslRef *core.ResourceRef,
+	ocspStaplePolicy ssl.SslConfig_OcspStaplePolicy) error {
+	vs := helpers.NewVirtualServiceBuilder().
+		WithName(name).
+		WithNamespace(namespace).
+		WithDomain(domain).
+		WithRoutePrefixMatcher(e2e.DefaultRouteName, "/").
+		WithRouteActionToUpstream(e2e.DefaultRouteName, upstream).
+		WithSslConfig(&ssl.SslConfig{
+			OcspStaplePolicy: ocspStaplePolicy,
+			SniDomains:       []string{domain},
+			SslSecrets: &ssl.SslConfig_SecretRef{
+				SecretRef: sslRef,
+			},
+		}).
+		Build()
+
+	// For e2e.TestContext, Snapshots are written in its `JustBeforeEach`, but since we're creating the virtual service
+	// during the test's run, we need to manually write the snapshot.
+	return testContext.TestClients().WriteSnapshot(testContext.Ctx(), &gloosnapshot.ApiSnapshot{
+		VirtualServices: v1.VirtualServiceList{vs},
+	})
+}
+
+// buildHttpsRequestClient builds an http client and request builder.
+func buildHttpsRequestClient(testContext *e2e.TestContext, vsName, tlsRootCa string) (*http.Client, *testutils.HttpRequestBuilder) {
+	httpClient := testutils.DefaultClientBuilder().
+		WithTLSRootCa(tlsRootCa).
+		WithTLSServerName(vsName).
+		Build()
+
+	httpRequestBuilder := testContext.GetHttpsRequestBuilder().
+		WithHost(vsName)
+
+	return httpClient, httpRequestBuilder
+}
