a156f87db170bef37cfc6858b553cc6867f870e9
Bernie Birnbaum <bewebi@earthlink.net>
2023-05-18 18:32:01 -0400

    remove kube client

--- test/e2e/aws_ec2_test.go
+++ test/e2e/aws_ec2_test.go
@@ -10,8 +10,6 @@ import (
 
 	"github.com/solo-io/gloo/test/testutils"
 
-	//"github.com/solo-io/gloo/test/kube2e"
-
 	"github.com/solo-io/gloo/test/helpers"
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources"
 
@@ -61,7 +59,7 @@ curl http://<instance-public-ip>/
 ```
 */
 
-var _ = FDescribe("AWS EC2 Plugin utils test", func() {
+var _ = Describe("AWS EC2 Plugin utils test", func() {
 	if testutils.ShouldSkipTempDisabledTests() {
 		return
 	}
@@ -244,9 +242,6 @@ var _ = FDescribe("AWS EC2 Plugin utils test", func() {
 	})
 
 	BeforeEach(func() {
-		//testutils.ValidateRequirementsAndNotifyGinkgo(
-		//	testutils.Kubernetes("Uses a Kubernetes client"),
-		//)
 
 		ctx, cancel = context.WithCancel(context.Background())
 		defaults.HttpPort = services.NextBindPort()
@@ -258,7 +253,6 @@ var _ = FDescribe("AWS EC2 Plugin utils test", func() {
 			WhatToRun: services.What{
 				DisableGateway: true,
 			},
-			//KubeClient: kube2e.MustKubeClient(),
 		}
 		testClients = services.RunGlooGatewayUdsFds(ctx, runOptions)
 
