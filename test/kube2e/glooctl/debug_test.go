8b26716cee293dd5e9d2c1fcfe71e3f16a2dadcb
Sam Heilbron <samheilbron@gmail.com>
2023-05-22 17:32:05 -0500

    gofmt

--- test/kube2e/glooctl/debug_test.go
+++ test/kube2e/glooctl/debug_test.go
@@ -1,97 +1,97 @@
 package glooctl_test
 
 import (
-    "bufio"
-    "bytes"
+	"bufio"
+	"bytes"
 
-    "github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/debug"
-    "github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
-    gloodefaults "github.com/solo-io/gloo/projects/gloo/pkg/defaults"
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/debug"
+	"github.com/solo-io/gloo/projects/gloo/cli/pkg/cmd/options"
+	gloodefaults "github.com/solo-io/gloo/projects/gloo/pkg/defaults"
 
-    "io"
-    "os"
-    "path/filepath"
+	"io"
+	"os"
+	"path/filepath"
 
-    "github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 
-    . "github.com/onsi/ginkgo/v2"
-    . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("Debug", func() {
 
-    // These tests formerly lived at: https://github.com/solo-io/gloo/blob/063dbf3ba7b7666d0111741c083b197364b14716/projects/gloo/cli/pkg/cmd/debug/debug_test.go
-    // They were migrated to this package since they depend on a k8s cluster
+	// These tests formerly lived at: https://github.com/solo-io/gloo/blob/063dbf3ba7b7666d0111741c083b197364b14716/projects/gloo/cli/pkg/cmd/debug/debug_test.go
+	// They were migrated to this package since they depend on a k8s cluster
 
-    var (
-        tmpDir string
-    )
-    BeforeEach(func() {
-        var err error
-        tmpDir, err = os.MkdirTemp("", "testDir")
-        Expect(err).NotTo(HaveOccurred())
-    })
+	var (
+		tmpDir string
+	)
+	BeforeEach(func() {
+		var err error
+		tmpDir, err = os.MkdirTemp("", "testDir")
+		Expect(err).NotTo(HaveOccurred())
+	})
 
-    AfterEach(func() {
-        _ = os.RemoveAll(tmpDir)
-    })
+	AfterEach(func() {
+		_ = os.RemoveAll(tmpDir)
+	})
 
-    It("should output logs by default", func() {
-        opts := options.Options{
-            Metadata: core.Metadata{
-                Namespace: gloodefaults.GlooSystem,
-            },
-        }
+	It("should output logs by default", func() {
+		opts := options.Options{
+			Metadata: core.Metadata{
+				Namespace: gloodefaults.GlooSystem,
+			},
+		}
 
-        var b bytes.Buffer
-        w := bufio.NewWriter(&b)
+		var b bytes.Buffer
+		w := bufio.NewWriter(&b)
 
-        err := debug.DebugLogs(&opts, w)
-        Expect(err).NotTo(HaveOccurred())
+		err := debug.DebugLogs(&opts, w)
+		Expect(err).NotTo(HaveOccurred())
 
-        err = w.Flush()
-        Expect(err).NotTo(HaveOccurred())
-    })
+		err = w.Flush()
+		Expect(err).NotTo(HaveOccurred())
+	})
 
-    It("should create a tar file at location specified in --file when --zip is enabled", func() {
-        opts := options.Options{
-            Metadata: core.Metadata{
-                Namespace: gloodefaults.GlooSystem,
-            },
-            Top: options.Top{
-                Zip: true,
-            },
-        }
-        opts.Top.File = filepath.Join(tmpDir, "log.tgz")
+	It("should create a tar file at location specified in --file when --zip is enabled", func() {
+		opts := options.Options{
+			Metadata: core.Metadata{
+				Namespace: gloodefaults.GlooSystem,
+			},
+			Top: options.Top{
+				Zip: true,
+			},
+		}
+		opts.Top.File = filepath.Join(tmpDir, "log.tgz")
 
-        err := debug.DebugLogs(&opts, io.Discard)
-        Expect(err).NotTo(HaveOccurred())
+		err := debug.DebugLogs(&opts, io.Discard)
+		Expect(err).NotTo(HaveOccurred())
 
-        _, err = os.Stat(opts.Top.File)
-        Expect(err).NotTo(HaveOccurred())
+		_, err = os.Stat(opts.Top.File)
+		Expect(err).NotTo(HaveOccurred())
 
-        err = os.RemoveAll(opts.Top.File)
-        Expect(err).NotTo(HaveOccurred())
-    })
+		err = os.RemoveAll(opts.Top.File)
+		Expect(err).NotTo(HaveOccurred())
+	})
 
-    It("should create a text file at location specified in --file when --zip is not enabled", func() {
-        opts := options.Options{
-            Metadata: core.Metadata{
-                Namespace: gloodefaults.GlooSystem,
-            },
-            Top: options.Top{
-                Zip: false,
-            },
-        }
-        opts.Top.File = filepath.Join(tmpDir, "log.txt")
+	It("should create a text file at location specified in --file when --zip is not enabled", func() {
+		opts := options.Options{
+			Metadata: core.Metadata{
+				Namespace: gloodefaults.GlooSystem,
+			},
+			Top: options.Top{
+				Zip: false,
+			},
+		}
+		opts.Top.File = filepath.Join(tmpDir, "log.txt")
 
-        err := debug.DebugLogs(&opts, io.Discard)
-        Expect(err).NotTo(HaveOccurred())
+		err := debug.DebugLogs(&opts, io.Discard)
+		Expect(err).NotTo(HaveOccurred())
 
-        _, err = os.Stat(opts.Top.File)
-        Expect(err).NotTo(HaveOccurred())
+		_, err = os.Stat(opts.Top.File)
+		Expect(err).NotTo(HaveOccurred())
 
-        err = os.RemoveAll(opts.Top.File)
-        Expect(err).NotTo(HaveOccurred())
-    })
+		err = os.RemoveAll(opts.Top.File)
+		Expect(err).NotTo(HaveOccurred())
+	})
 })
