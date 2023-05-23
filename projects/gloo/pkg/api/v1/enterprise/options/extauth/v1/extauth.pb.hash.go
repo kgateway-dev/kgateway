8521b129f500ed96075b1114eecc37e49a79999a
JM Huibonhoa <jm.huibonhoa@solo.io>
2023-05-23 17:08:55 -0400

    PR comments

--- projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.hash.go
+++ projects/gloo/pkg/api/v1/enterprise/options/extauth/v1/extauth.pb.hash.go
@@ -1519,7 +1519,7 @@ func (m *OidcAuthorizationCode) Hash(hasher hash.Hash64) (uint64, error) {
 	{
 		var result uint64
 		innerHash := fnv.New64()
-		for k, v := range m.GetClaimsToMetadata() {
+		for k, v := range m.GetMetadataKeyToClaim() {
 			innerHash.Reset()
 
 			if _, err = innerHash.Write([]byte(v)); err != nil {
@@ -1840,7 +1840,7 @@ func (m *AccessTokenValidation) Hash(hasher hash.Hash64) (uint64, error) {
 	{
 		var result uint64
 		innerHash := fnv.New64()
-		for k, v := range m.GetClaimsToMetadata() {
+		for k, v := range m.GetMetadataKeyToClaim() {
 			innerHash.Reset()
 
 			if _, err = innerHash.Write([]byte(v)); err != nil {
