6e6bbd2b50cc4e549206508b2e64b8e06d850dd9
Sam Heilbron <samheilbron@gmail.com>
2023-05-23 15:01:51 -0500

    add time for delay of adjusting accept all

--- test/kube2e/util.go
+++ test/kube2e/util.go
@@ -101,12 +101,20 @@ func UpdateDisableTransformationValidationSetting(ctx context.Context, shouldDis
 	}, installNamespace)
 }
 
-// enable/disable strict validation
+// UpdateAlwaysAcceptSetting enables/disables strict validation
 func UpdateAlwaysAcceptSetting(ctx context.Context, alwaysAccept bool, installNamespace string) {
-	UpdateSettings(ctx, func(settings *v1.Settings) {
-		Expect(settings.GetGateway().GetValidation()).NotTo(BeNil())
-		settings.GetGateway().GetValidation().AlwaysAccept = &wrappers.BoolValue{Value: alwaysAccept}
-	}, installNamespace)
+	UpdateSettingsWithPropagationDelay(
+		func(settings *v1.Settings) {
+			settings.GetGateway().GetValidation().AlwaysAccept = &wrappers.BoolValue{
+				Value: alwaysAccept,
+			}
+		},
+		func() {
+			// Not ideal, but just to be safe, that when we change AcceptAll config, it has time to propagate
+			time.Sleep(5 * time.Second)
+		},
+		ctx,
+		installNamespace)
 }
 
 func UpdateRestEdsSetting(ctx context.Context, enableRestEds bool, installNamespace string) {
