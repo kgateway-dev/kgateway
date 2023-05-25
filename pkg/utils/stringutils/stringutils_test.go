26780ddc5c7d105605cf012a64e3254ff1f2e12f
sheidkamp <seth.heidkamp@gmail.com>
2023-05-25 15:33:30 -0400

    Update stringutils_test.go

--- pkg/utils/stringutils/stringutils_test.go
+++ pkg/utils/stringutils/stringutils_test.go
@@ -2,6 +2,7 @@ package stringutils_test
 
 import (
 	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
 
 	. "github.com/solo-io/gloo/pkg/utils/stringutils"
