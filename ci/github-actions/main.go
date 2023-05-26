d1460fb9fa8060f693a92252452a2113ad8194ef
Sam Heilbron <samheilbron@gmail.com>
2023-05-26 15:37:47 -0500

    add skip ci code, placehlder

---
+++ ci/github-actions/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    process_skip_directives "github.com/solo-io/gloo/ci/github-actions/process-skip-directives"
+    "os"
+)
+
+func main() {
+    argsWithoutProgram := os.Args[1:]
+    action := argsWithoutProgram[0]
+
+    switch action {
+    case "process-skip-directives":
+        process_skip_directives.ProcessChangelogDirectives()
+    default:
+        fmt.Printf("%s is not a supported action", action)
+
+    }
+}
---
+++ ci/github-actions/process-skip-directives/skip_ci_tests.go
@@ -0,0 +1,25 @@
+package process_skip_directives
+
+import "fmt"
+
+const (
+	// skipCIFile is the name of the artifact that will be published if tests should be skipped
+	skipCIFile = "skip-ci.txt"
+)
+
+// ProcessChangelogDirectives checks to see if a changelog file has been added
+// with any of the following fields:
+//   - "skip-ci-tests"
+//     (this is the only one currently, but we may add more)
+//
+// field set to true.
+//
+// Based on the directive in the changelog, it will write to a file with the set of directives that
+// should be skipped. For example:
+//
+//	SKIP_CI_TESTS=true
+//
+// This file can then be pulled down by jobs in the same workflow
+func ProcessChangelogDirectives() {
+	fmt.Print("RUNNING PROCESS CHANGELOG DIRECTIVES")
+}
