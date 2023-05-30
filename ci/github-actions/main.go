6095f2bd5de28eba7790a6c292e6685d97a17a30
Sam Heilbron <samheilbron@gmail.com>
2023-05-30 13:54:01 -0400

    gofmt

--- ci/github-actions/main.go
+++ ci/github-actions/main.go
@@ -1,20 +1,20 @@
 package main
 
 import (
-    "fmt"
-    process_skip_directives "github.com/solo-io/gloo/ci/github-actions/process-skip-directives"
-    "os"
+	"fmt"
+	process_skip_directives "github.com/solo-io/gloo/ci/github-actions/process-skip-directives"
+	"os"
 )
 
 func main() {
-    argsWithoutProgram := os.Args[1:]
-    action := argsWithoutProgram[0]
+	argsWithoutProgram := os.Args[1:]
+	action := argsWithoutProgram[0]
 
-    switch action {
-    case "process-skip-directives":
-        process_skip_directives.ProcessChangelogDirectives()
-    default:
-        fmt.Printf("%s is not a supported action", action)
+	switch action {
+	case "process-skip-directives":
+		process_skip_directives.ProcessChangelogDirectives()
+	default:
+		fmt.Printf("%s is not a supported action", action)
 
-    }
+	}
 }
--- ci/github-actions/process-skip-directives/skip_ci_tests.go
+++ ci/github-actions/process-skip-directives/skip_ci_tests.go
@@ -3,5 +3,5 @@ package process_skip_directives
 // ProcessChangelogDirectives checks to see if a changelog file has been added
 // with any of the special directives:
 func ProcessChangelogDirectives() {
-    // TODO - In the future, we should migrate the bash script (./script.sh) to Go code
+	// TODO - In the future, we should migrate the bash script (./script.sh) to Go code
 }
