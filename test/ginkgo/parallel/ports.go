450df7a536f66cfb4bee0076f29dfe6c723ba4e3
Sam Heilbron <samheilbron@gmail.com>
2023-06-28 08:37:20 -0600

    use 1000 not 100 for offset, try to avoid dupes

--- test/ginkgo/parallel/ports.go
+++ test/ginkgo/parallel/ports.go
@@ -11,11 +11,11 @@ func GetParallelProcessCount() int {
 	return ginkgo.GinkgoParallelProcess()
 }
 
-// GetPortOffset returns the number of parallel Ginkgo processes * 100
+// GetPortOffset returns the number of parallel Ginkgo processes * 1000
 // This is intended to be used by tests which need to produce unique ports so that they can be run
 // in parallel without port conflict
 func GetPortOffset() int {
-	return GetParallelProcessCount() * 100
+	return GetParallelProcessCount() * 1000
 }
 
 func AdvancePort(p *uint32) uint32 {
