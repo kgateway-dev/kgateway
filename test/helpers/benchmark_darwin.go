58a272968194e1bdea82571abcafd8d3b03d1c19
Bernie Birnbaum <bewebi@earthlink.net>
2023-07-11 13:23:10 -0400

    add Measure(), comments

--- test/helpers/benchmark_darwin.go
+++ test/helpers/benchmark_darwin.go
@@ -7,6 +7,7 @@ import (
 // MeasureIgnore0ns as implemented here for Mac/Darwin is meant to be used in performance tests when developing locally
 // It is a less-precise method for measuring than the Linux implementation, and targets should be derived based on
 // performance when running on the Linux GHA runner we use for Nightly tests
+// Ignore will always be false
 func MeasureIgnore0ns(f func()) (Result, bool, error) {
 	before := time.Now()
 	f()
@@ -14,4 +15,16 @@ func MeasureIgnore0ns(f func()) (Result, bool, error) {
 	return Result{
 		Total: elapsed,
 	}, false, nil
+}
+
+// Measure as implemented here for Mac/Darwin is meant to be used in performance tests when developing locally
+// It is a less-precise method for measuring than the Linux implementation, and targets should be derived based on
+// performance when running on the Linux GHA runner we use for Nightly tests
+func Measure(f func()) (Result, error) {
+	before := time.Now()
+	f()
+	elapsed := time.Since(before)
+	return Result{
+		Total: elapsed,
+	}, nil
 }
