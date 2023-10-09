e70ddb9d42ae8b4603f51ba37b0307ebfc5003d8
Fabian Gonzalez <fabian.gonzalez@solo.io>
2023-10-09 07:20:27 -0400

    Update test/helpers/secrets.go
    Co-authored-by: Bernie Birnbaum <bewebi@earthlink.net>

--- test/helpers/secrets.go
+++ test/helpers/secrets.go
@@ -2,8 +2,8 @@ package helpers
 
 import v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1"
 
-// SecretBuilder contains options for building Endpoints to be included in scaled Snapshots
-// there are no options currently configurable for the endpointBuilder
+// SecretBuilder contains options for building Secrets to be included in scaled Snapshots
+// there are no options currently configurable for the SecretBuilder
 type SecretBuilder struct{}
 
 func NewSecretBuilder() *SecretBuilder {
