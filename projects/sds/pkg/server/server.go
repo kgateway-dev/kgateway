778ebae187cfaf3e792c17b0ace23f19c1c20607
Jenny Shu <jenny.shu@solo.io>
2023-10-19 15:34:51 -0400

    remove unused func

--- projects/sds/pkg/server/server.go
+++ projects/sds/pkg/server/server.go
@@ -12,15 +12,13 @@ import (
 	envoy_config_core_v3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoy_extensions_transport_sockets_tls_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
 	envoy_service_secret_v3 "github.com/envoyproxy/go-control-plane/envoy/service/secret/v3"
-	"github.com/solo-io/go-utils/contextutils"
-	"github.com/solo-io/go-utils/hashutils"
-
-	"go.uber.org/zap"
-	"google.golang.org/grpc"
-
 	cache_types "github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	cache "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	server "github.com/envoyproxy/go-control-plane/pkg/server/v3"
+	"github.com/solo-io/go-utils/contextutils"
+	"github.com/solo-io/go-utils/hashutils"
+	"go.uber.org/zap"
+	"google.golang.org/grpc"
 )
 
 var (
@@ -175,33 +173,6 @@ func readAndVerifyCert(ctx context.Context, certFilePath string) ([]byte, error)
 
 	return fileBytes, nil
 }
-
-// checkCert uses pem.Decode to verify that the given
-// bytes are not malformed, as could be caused by a
-// write-in-progress.
-// func checkCert(ctx context.Context, certs []byte) bool {
-// 	var rootDecoded []byte
-// 	rest := certs
-// 	for {
-// 		var pemBlock *pem.Block
-// 		pemBlock, rest = pem.Decode(rest)
-// 		if pemBlock == nil {
-// 			break
-// 		}
-// 		rootDecoded = append(rootDecoded, pemBlock.Bytes...)
-// 	}
-
-// 	parsedCerts, err := x509.ParseCertificates(rootDecoded)
-// 	if err != nil {
-// 		contextutils.LoggerFrom(ctx).Warnw("error parsing certs", zap.Error(err))
-// 		return false
-// 	}
-// 	if len(parsedCerts) == 0 {
-// 		contextutils.LoggerFrom(ctx).Warn("did not find any PEM-encoded certs")
-// 		return false
-// 	}
-// 	return true
-// }
 
 // checkCert uses pem.Decode to verify that the given
 // bytes are not malformed, as could be caused by a
