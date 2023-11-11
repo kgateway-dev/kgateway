14eea346bceb7244e311cd049f6ebf58fb4418bb
Eitan Yarmush <eitan.yarmush@solo.io>
2023-11-11 14:43:01 +0000

    golang/mock is deprecated, it's now supported by uber

--- projects/gateway2/translator/httproute/gateway_http_route_translator.go
+++ projects/gateway2/translator/httproute/gateway_http_route_translator.go
@@ -2,7 +2,6 @@ package httproute
 
 import (
 	"context"
-	"errors"
 	"net/http"
 	"regexp"
 
@@ -15,10 +14,7 @@ import (
 	"github.com/solo-io/gloo/projects/gateway2/translator/httproute/filterplugins"
 	"github.com/solo-io/gloo/projects/gateway2/translator/httproute/filterplugins/registry"
 	"github.com/solo-io/gloo/projects/gateway2/translator/routeutils"
-	"github.com/solo-io/gloo/projects/gloo/pkg/plugins/kubernetes"
 	"google.golang.org/protobuf/proto"
-	corev1 "k8s.io/api/core/v1"
-	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
@@ -241,57 +237,9 @@ func translateRouteAction(
 	for _, backendRef := range backendRefs {
 		clusterName := "blackhole_cluster"
 		cli, err := queries.GetBackendForRef(context.TODO(), queries.ObjToFrom(gwroute), &backendRef.BackendObjectReference)
-		if err != nil {
-			switch {
-			case errors.Is(err, query.ErrUnknownKind):
-				reporter.SetCondition(reports.HTTPRouteCondition{
-					Type:   gwv1.RouteConditionResolvedRefs,
-					Status: metav1.ConditionFalse,
-					Reason: gwv1.RouteReasonInvalidKind,
-				})
-			case errors.Is(err, query.ErrMissingReferenceGrant):
-				reporter.SetCondition(reports.HTTPRouteCondition{
-					Type:   gwv1.RouteConditionResolvedRefs,
-					Status: metav1.ConditionFalse,
-					Reason: gwv1.RouteReasonRefNotPermitted,
-				})
-			case apierrors.IsNotFound(err):
-				reporter.SetCondition(reports.HTTPRouteCondition{
-					Type:   gwv1.RouteConditionResolvedRefs,
-					Status: metav1.ConditionFalse,
-					Reason: gwv1.RouteReasonBackendNotFound,
-				})
-			default:
-				// setting other errors to not found. not sure if there's a better option.
-				reporter.SetCondition(reports.HTTPRouteCondition{
-					Type:   gwv1.RouteConditionResolvedRefs,
-					Status: metav1.ConditionFalse,
-					Reason: gwv1.RouteReasonBackendNotFound,
-				})
-			}
-		} else {
-			var port uint32
-			if backendRef.Port != nil {
-				port = uint32(*backendRef.Port)
-			}
-			switch cli := cli.(type) {
-			case *corev1.Service:
-				if port == 0 {
-					reporter.SetCondition(reports.HTTPRouteCondition{
-						Type:   gwv1.RouteConditionResolvedRefs,
-						Status: metav1.ConditionFalse,
-						Reason: gwv1.RouteReasonUnsupportedValue,
-					})
-				} else {
-					clusterName = kubernetes.UpstreamName(cli.Namespace, cli.Name, int32(port))
-				}
-			default:
-				reporter.SetCondition(reports.HTTPRouteCondition{
-					Type:   gwv1.RouteConditionResolvedRefs,
-					Status: metav1.ConditionFalse,
-					Reason: gwv1.RouteReasonInvalidKind,
-				})
-			}
+		ptrClusterName := query.ResolveBackendRef(cli, err, reporter, backendRef.BackendObjectReference)
+		if ptrClusterName != nil {
+			clusterName = *ptrClusterName
 		}
 
 		var weight *wrappers.UInt32Value
