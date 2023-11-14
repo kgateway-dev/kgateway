5573b611ea3836953344de08f4e7ec00240de020
Yuval Kohavi <yuval.kohavi@gmail.com>
2023-11-14 10:12:44 -0500

    Self-contain gloo/v1 deps Move all the deps from gloo/v1 to be self-contained in the gateway2 which will make restructuring easier.

--- projects/gateway2/discovery/controller.go
+++ projects/gateway2/discovery/controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/solo-io/gloo/projects/gateway2/xds"
 	corev1 "k8s.io/api/core/v1"
 	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -44,13 +45,9 @@ func (c *controllerBuilder) watchEndpoints(ctx context.Context) error {
 }
 
 func (c *controllerBuilder) watchPods(ctx context.Context) error {
-	err := ctrl.NewControllerManagedBy(c.mgr).
-		For(&corev1.Pod{}).
+	return ctrl.NewControllerManagedBy(c.mgr).
+		Watches(&corev1.Pod{}, handler.Funcs{}). // Empty funcs means that pod will be in the cache but reconcile will never be called.
 		Complete(reconcile.Func(c.translator.ReconcilePod))
-	if err != nil {
-		return err
-	}
-	return nil
 }
 
 func (c *controllerBuilder) watchServices(ctx context.Context) error {
@@ -62,3 +59,9 @@ func (c *controllerBuilder) watchServices(ctx context.Context) error {
 	}
 	return nil
 }
+
+type EnvoyState struct {
+}
+
+func (e *EnvoyState) updateClusterAndEndpoints() {}
+func (e *EnvoyState) removeService()             {}
