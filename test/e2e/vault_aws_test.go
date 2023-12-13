7a10a6d89547dd13531ce4564283e5625c729348
sheidkamp <seth.heidkamp@gmail.com>
2023-12-13 13:16:42 -0500

    Update vault_aws_test.go

--- test/e2e/vault_aws_test.go
+++ test/e2e/vault_aws_test.go
@@ -19,8 +19,8 @@ const (
 	// If you want to run these tests locally, ensure that your local AWS credentials match,
 	// or use another role
 	// https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-profiles.html
-	// vaultAwsRole   = "arn:aws:iam::802411188784:user/gloo-edge-e2e-user"
-	vaultAwsRole   = "arn:aws:iam::802411188784:user/sheidkamp"
+	vaultAwsRole = "arn:aws:iam::802411188784:user/gloo-edge-e2e-user"
+	// vaultAwsRole   = "arn:aws:iam::802411188784:user/sheidkamp"
 	vaultAwsRegion = "us-east-1"
 
 	vaultRole = "vault-role"
