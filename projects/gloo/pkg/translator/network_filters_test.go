3e784ede8f5e57afc26e227568b6230cf21e3423
Ashish Banerjee <ashish.banerjee@solo.io>
2024-03-25 15:09:55 -0400

    Remove more redundant code in the table tests

--- projects/gloo/pkg/translator/network_filters_test.go
+++ projects/gloo/pkg/translator/network_filters_test.go
@@ -4,7 +4,6 @@ import (
 	"context"
 
 	envoy_config_listener_v3 "github.com/envoyproxy/go-control-plane/envoy/config/listener/v3"
-	envoy_config_route_v3 "github.com/envoyproxy/go-control-plane/envoy/config/route/v3"
 	routerv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/http/router/v3"
 	envoy_http_connection_manager_v3 "github.com/envoyproxy/go-control-plane/envoy/extensions/filters/network/http_connection_manager/v3"
 	"github.com/golang/protobuf/ptypes/wrappers"
@@ -65,13 +64,32 @@ var _ = Describe("Router filter test", func() {
 
 	// FIXME remove focus
 	FDescribeTable("GetAggregateListenerTranslators (success)",
-		func(aggregateListener *v1.AggregateListener, assertionHandler ResourceAssertionHandler) {
+		func(router *routerV1.Router, assertionHandler func(*routerv3.Router)) {
 			listener := &v1.Listener{
 				Name:        "aggregate-listener",
 				BindAddress: gatewaydefaults.GatewayBindAddress,
 				BindPort:    defaults.HttpPort,
 				ListenerType: &v1.Listener_AggregateListener{
-					AggregateListener: aggregateListener,
+					AggregateListener: &v1.AggregateListener{
+						HttpResources: &v1.AggregateListener_HttpResources{
+							HttpOptions: map[string]*v1.HttpListenerOptions{
+								"http-options-ref": {
+									HttpConnectionManagerSettings: &hcm.HttpConnectionManagerSettings{},
+									Router:                        router,
+								},
+							},
+							VirtualHosts: map[string]*v1.VirtualHost{
+								"vhost-ref": {
+									Name: "virtual-host",
+								},
+							},
+						},
+						HttpFilterChains: []*v1.AggregateListener_HttpFilterChain{{
+							Matcher:         nil,
+							HttpOptionsRef:  "http-options-ref",
+							VirtualHostRefs: []string{"vhost-ref"},
+						}},
+					},
 				},
 			}
 			proxy := &v1.Proxy{
@@ -99,102 +117,57 @@ var _ = Describe("Router filter test", func() {
 				},
 			}
 			envoyListener := listenerTranslator.ComputeListener(params)
-			envoyRouteConfigs := routeConfigurationTranslator.ComputeRouteConfiguration(params)
+			_ = routeConfigurationTranslator.ComputeRouteConfiguration(params)
 
 			// Validate that no Errors were encountered during translation
 			Expect(gloovalidation.GetProxyError(proxyReport)).NotTo(HaveOccurred())
 
-			// Validate the ResourceAssertionHandler defined by each test
-			assertionHandler(envoyListener, envoyRouteConfigs)
+			By("configuring the envoy router from gloo settings")
+			filterChain := envoyListener.GetFilterChains()[0]
+			hcmFilter := filterChain.GetFilters()[0]
+			_, err := sslutils.AnyToMessage(hcmFilter.GetConfigType().(*envoy_config_listener_v3.Filter_TypedConfig).TypedConfig)
+			Expect(err).NotTo(HaveOccurred())
+
+			hcm := &envoy_http_connection_manager_v3.HttpConnectionManager{}
+			err = translator.ParseTypedConfig(hcmFilter, hcm)
+			Expect(err).NotTo(HaveOccurred())
+			Expect(hcm.HttpFilters).To(HaveLen(1))
+
+			routeFilter := hcm.GetHttpFilters()[0]
+			typedRouterFilter := routerv3.Router{}
+			routeFilter.GetTypedConfig().UnmarshalTo(&typedRouterFilter)
+			// Perform assertions on generated Envoy router filter
+			assertionHandler(&typedRouterFilter)
 		},
 
 		Entry(
-			"Set dynamic_stats to false and suppress_envoy_headers to true",
-			&v1.AggregateListener{
-				HttpResources: &v1.AggregateListener_HttpResources{
-					HttpOptions: map[string]*v1.HttpListenerOptions{
-						"http-options-ref": {
-							HttpConnectionManagerSettings: &hcm.HttpConnectionManagerSettings{},
-							Router: &routerV1.Router{
-								DynamicStats: &wrappers.BoolValue{
-									Value: false,
-								},
-								SuppressEnvoyHeaders: &wrappers.BoolValue{
-									Value: true,
-								},
-							},
-						},
-					},
-					VirtualHosts: map[string]*v1.VirtualHost{
-						"vhost-ref": {
-							Name: "virtual-host",
-						},
-					},
+			"Set suppress_envoy_headers to true",
+			&routerV1.Router{
+				SuppressEnvoyHeaders: &wrappers.BoolValue{
+					Value: true,
 				},
-				HttpFilterChains: []*v1.AggregateListener_HttpFilterChain{{
-					Matcher:         nil,
-					HttpOptionsRef:  "http-options-ref",
-					VirtualHostRefs: []string{"vhost-ref"},
-				}},
 			},
-			func(listener *envoy_config_listener_v3.Listener, routeConfigs []*envoy_config_route_v3.RouteConfiguration) {
-				By("configuring the envoy router from gloo settings")
-				filterChain := listener.GetFilterChains()[0]
-				hcmFilter := filterChain.GetFilters()[0]
-				_, err := sslutils.AnyToMessage(hcmFilter.GetConfigType().(*envoy_config_listener_v3.Filter_TypedConfig).TypedConfig)
-				Expect(err).NotTo(HaveOccurred())
-
-				hcm := &envoy_http_connection_manager_v3.HttpConnectionManager{}
-				err = translator.ParseTypedConfig(hcmFilter, hcm)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(hcm.HttpFilters).To(HaveLen(1))
-
-				routeFilter := hcm.GetHttpFilters()[0]
-				typedRouterFilter := routerv3.Router{}
-				routeFilter.GetTypedConfig().UnmarshalTo(&typedRouterFilter)
-				Expect(typedRouterFilter.GetDynamicStats().GetValue()).To(BeFalse())
+			func(typedRouterFilter *routerv3.Router) {
 				Expect(typedRouterFilter.GetSuppressEnvoyHeaders()).To(BeTrue())
+			},
+		),
+		Entry(
+			"Set dynamic_stats to false",
+			&routerV1.Router{
+				DynamicStats: &wrappers.BoolValue{
+					Value: false,
+				},
+			},
+			func(typedRouterFilter *routerv3.Router) {
+				Expect(typedRouterFilter.GetDynamicStats().GetValue()).To(BeFalse())
 			},
 		),
 
 		Entry(
 			"Leave envoy's dynamic_stats as nil if not specified in gloo",
-			&v1.AggregateListener{
-				HttpResources: &v1.AggregateListener_HttpResources{
-					HttpOptions: map[string]*v1.HttpListenerOptions{
-						"http-options-ref": {
-							HttpConnectionManagerSettings: &hcm.HttpConnectionManagerSettings{},
-							Router:                        &routerV1.Router{},
-						},
-					},
-					VirtualHosts: map[string]*v1.VirtualHost{
-						"vhost-ref": {
-							Name: "virtual-host",
-						},
-					},
-				},
-				HttpFilterChains: []*v1.AggregateListener_HttpFilterChain{{
-					Matcher:         nil,
-					HttpOptionsRef:  "http-options-ref",
-					VirtualHostRefs: []string{"vhost-ref"},
-				}},
-			},
-			func(listener *envoy_config_listener_v3.Listener, routeConfigs []*envoy_config_route_v3.RouteConfiguration) {
-				By("configuring the envoy router from gloo settings")
-				filterChain := listener.GetFilterChains()[0]
-				hcmFilter := filterChain.GetFilters()[0]
-				_, err := sslutils.AnyToMessage(hcmFilter.GetConfigType().(*envoy_config_listener_v3.Filter_TypedConfig).TypedConfig)
-				Expect(err).NotTo(HaveOccurred())
-
-				hcm := &envoy_http_connection_manager_v3.HttpConnectionManager{}
-				err = translator.ParseTypedConfig(hcmFilter, hcm)
-				Expect(err).NotTo(HaveOccurred())
-				Expect(hcm.HttpFilters).To(HaveLen(1))
-
-				typedRouterFiler := routerv3.Router{}
-				routeFilter := hcm.GetHttpFilters()[0]
-				routeFilter.GetTypedConfig().UnmarshalTo(&typedRouterFiler)
-				Expect(typedRouterFiler.GetDynamicStats()).To(BeNil())
+			&routerV1.Router{},
+			func(typedRouterFilter *routerv3.Router) {
+				Expect(typedRouterFilter.GetDynamicStats()).To(BeNil())
 			},
 		),
 	)
