f3b649d872f69d72e1ace191d7e1f9122172bc73
Sam Heilbron <samheilbron@gmail.com>
2024-04-11 15:34:47 -0600

    add failing test

--- test/kube2e/example/k8s_gw_test.go
+++ test/kube2e/example/k8s_gw_test.go
@@ -6,6 +6,7 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/solo-io/gloo/test/kube2e/testutils/spec"
 	"github.com/solo-io/gloo/test/kube2e/testutils/specassertions"
+	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -63,6 +64,34 @@ var _ = Describe("Example Test", Ordered, func() {
 
 			err = scenarioRunner.RunScenario(ctx, spec)
 			Expect(err).NotTo(HaveOccurred())
+		})
+
+		It("fails to produce running replicas", func() {
+			// These are the resources that we expect to be dynamically provisioned when we run the test
+			// The name and namespace of these objects is determined from the manifest file
+			proxyDeployment := &appsv1.Deployment{ObjectMeta: metav1.ObjectMeta{Name: "gloo-proxy-gw", Namespace: "default"}}
+			proxyService := &corev1.Service{ObjectMeta: metav1.ObjectMeta{Name: "gloo-proxy-gw", Namespace: "default"}}
+
+			spec, err := scenarioProvider.NewScenario(
+				spec.WithName("running-replicas-test"),
+				spec.WithManifestFile(filepath.Join(cwd, "manifests", "basic-test.yaml")),
+				spec.WithInitializedAssertion(
+					assertionProvider.ObjectsExist(proxyDeployment, proxyService),
+				),
+				spec.WithAssertion(
+					assertionProvider.RunningReplicas(&core.ResourceRef{
+						Name:      "gloo-proxy-gw",
+						Namespace: "default",
+					}, 1),
+				),
+				spec.WithFinalizedAssertion(
+					assertionProvider.ObjectsNotExist(proxyDeployment, proxyService),
+				),
+			)
+			Expect(err).NotTo(HaveOccurred())
+
+			err = scenarioRunner.RunScenario(ctx, spec)
+			Expect(err).To(HaveOccurred(), "The gloo-proxy-gw that is deployed has the tag 1.0.0-ci which will fail to start")
 		})
 
 	})
