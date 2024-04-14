e12105d4c05468d2048ea557e026255d3c4b6ee4
Sam Heilbron <samheilbron@gmail.com>
2024-04-14 02:07:14 -0600

    fixup run_source

--- test/kubernetes/testutils/runtime/run_source.go
+++ test/kubernetes/testutils/runtime/run_source.go
@@ -3,14 +3,14 @@ package runtime
 // RunSource identifies who/what triggered the test
 type RunSource int
 
-// Declare related constants for each direction starting with index 1
+// Declare related constants for each RunSource
 const (
 	// LocalDevelopment signifies that the test is invoked locally
-	LocalDevelopment RunSource = iota + 1 // EnumIndex = 1
+	LocalDevelopment RunSource = iota + 1
 
 	// PullRequest means that the test was invoked while running CI against a Pull Request
-	PullRequest // EnumIndex = 2
+	PullRequest
 
 	// NightlyTest means that the test was invoked while running CI as part of a Nightly operation
-	NightlyTest // EnumIndex = 3
+	NightlyTest
 )
