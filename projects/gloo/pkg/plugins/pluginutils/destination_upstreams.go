290a9434eb3cfec33ee1e44a9125f95230859b18
David Jumani <dj.davidjumani1994@gmail.com>
2024-04-30 09:54:04 -0400

    revert unrequired changes

--- projects/gloo/pkg/plugins/pluginutils/destination_upstreams.go
+++ projects/gloo/pkg/plugins/pluginutils/destination_upstreams.go
@@ -12,8 +12,6 @@ import (
 	"github.com/solo-io/solo-kit/pkg/api/v1/resources/core"
 )
 
-const InvalidRouteErrMsg = "invalid route"
-
 func DestinationUpstreams(snap *v1snap.ApiSnapshot, in *v1.RouteAction) ([]core.ResourceRef, error) {
 	switch dest := in.GetDestination().(type) {
 	case *v1.RouteAction_Single:
@@ -34,7 +32,7 @@ func DestinationUpstreams(snap *v1snap.ApiSnapshot, in *v1.RouteAction) ([]core.
 		}
 		return destinationsToRefs(upstreamGroup.GetDestinations())
 	}
-	return nil, errors.New(InvalidRouteErrMsg)
+	return nil, errors.New("invalid route")
 }
 
 func destinationsToRefs(destinations []*v1.WeightedDestination) ([]core.ResourceRef, error) {
