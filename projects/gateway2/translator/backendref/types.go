34a7cf488ec7871be4cd7a57116a033d5330ad18
Scott Weiss <sdw35@cornell.edu>
2024-05-02 13:47:18 -0400

    fix test, fix gvk

--- projects/gateway2/translator/backendref/types.go
+++ projects/gateway2/translator/backendref/types.go
@@ -2,6 +2,7 @@ package backendref
 
 import (
 	"github.com/solo-io/gloo/projects/gateway2/wellknown"
+	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/kube/apis/gloo.solo.io/v1"
 	corev1 "k8s.io/api/core/v1"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
@@ -14,7 +15,7 @@ func RefIsService(ref gwv1.BackendObjectReference) bool {
 
 // RefIsUpstream checks if the BackendObjectReference is an Upstream.
 func RefIsUpstream(ref gwv1.BackendObjectReference) bool {
-	return (ref.Kind != nil && *ref.Kind == "Upstream") && (ref.Group != nil && *ref.Group == "v1")
+	return (ref.Kind != nil && *ref.Kind == "Upstream") && (ref.Group != nil && *ref.Group == v1.GroupName)
 }
 
 // RefIsHTTPRoute checks if the BackendObjectReference is an HTTPRoute
