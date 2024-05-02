38dd09a83cef618981bd2696e004e8b46af05ea5
npolshakova <nina.polshakova@solo.io>
2024-05-02 13:58:33 -0700

    classic edge automtls tests

---
+++ test/kubernetes/e2e/classicedge/automtls_istio_classic_api_test.go
@@ -0,0 +1,60 @@
+package classicedge_test
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/solo-io/gloo/test/kube2e/helper"
+	"github.com/solo-io/gloo/test/kubernetes/e2e/features/headless_svc"
+	"github.com/solo-io/gloo/test/kubernetes/e2e/features/istio"
+
+	"github.com/solo-io/skv2/codegen/util"
+	"github.com/stretchr/testify/suite"
+
+	"github.com/solo-io/gloo/test/kubernetes/e2e"
+	"github.com/solo-io/gloo/test/kubernetes/testutils/gloogateway"
+)
+
+// TestClassicEdgeGateway is the function which executes a series of tests against a given installation where
+// the k8s Gateway controller is disabled
+func TestAutomtlsIstioClassicEdgeGateway(t *testing.T) {
+	ctx := context.Background()
+	testCluster := e2e.MustTestCluster()
+	testInstallation := testCluster.RegisterTestInstallation(
+		t,
+		&gloogateway.Context{
+			InstallNamespace:   "automtls-istio-classic-edge-test",
+			ValuesManifestFile: filepath.Join(util.MustGetThisDir(), "manifests", "istio-automtls-classic-gateway-test-helm.yaml"),
+		},
+	)
+
+	testHelper := e2e.MustTestHelper(ctx, testInstallation)
+
+	// We register the cleanup function _before_ we actually perform the installation.
+	// This allows us to uninstall Gloo Gateway, in case the original installation only completed partially
+	t.Cleanup(func() {
+		if t.Failed() {
+			testInstallation.PreFailHandler(ctx)
+		}
+
+		testInstallation.UninstallGlooGateway(ctx, func(ctx context.Context) error {
+			return testHelper.UninstallGlooAll()
+		})
+		testCluster.UnregisterTestInstallation(testInstallation)
+	})
+
+	// Install Gloo Gateway with only classic APIs enabled
+	testInstallation.InstallGlooGateway(ctx, func(ctx context.Context) error {
+		return testHelper.InstallGloo(ctx, helper.GATEWAY, 5*time.Minute, helper.ExtraArgs("--values", testInstallation.Metadata.ValuesManifestFile))
+	})
+
+	t.Run("HeadlessSvc", func(t *testing.T) {
+		suite.Run(t, headless_svc.NewClassicHeadlessSvcSuite(ctx, testInstallation))
+	})
+
+	t.Run("IstioIntegration", func(t *testing.T) {
+		suite.Run(t, istio.NewIstioAutoMtlsSuite(ctx, testInstallation))
+	})
+}
