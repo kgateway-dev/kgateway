ec3625ecbe45f9d3b13b368838a456733d792c09
JM Huibonhoa <jm.huibonhoa@solo.io>
2024-05-03 17:48:55 -0400

    imports

--- test/kubernetes/testutils/assertions/objects.go
+++ test/kubernetes/testutils/assertions/objects.go
@@ -3,6 +3,7 @@ package assertions
 import (
 	"context"
 	"fmt"
+	"github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -11,31 +12,31 @@ import (
 
 func (p *Provider) EventuallyObjectsExist(ctx context.Context, objects ...client.Object) {
 	for _, o := range objects {
-		p.Gomega.Eventually(ctx, func(innerG Gomega) {
+		p.Gomega.Eventually(ctx, func(innerG gomega.Gomega) {
 			err := p.clusterContext.Client.Get(ctx, client.ObjectKeyFromObject(o), o)
-			innerG.Expect(err).NotTo(HaveOccurred(), "object %s %s should be available in cluster", o.GetObjectKind().GroupVersionKind().String(), client.ObjectKeyFromObject(o).String())
+			innerG.Expect(err).NotTo(gomega.HaveOccurred(), "object %s %s should be available in cluster", o.GetObjectKind().GroupVersionKind().String(), client.ObjectKeyFromObject(o).String())
 		}).
 			WithContext(ctx).
 			WithTimeout(p.assertionOptions.Timeout).
 			WithPolling(p.assertionOptions.Polling).
-			Should(Succeed())
+			Should(gomega.Succeed())
 	}
 }
 
 func (p *Provider) EventuallyObjectsNotExist(ctx context.Context, objects ...client.Object) {
 	for _, o := range objects {
-		p.Gomega.Eventually(ctx, func(innerG Gomega) {
+		p.Gomega.Eventually(ctx, func(innerG gomega.Gomega) {
 			err := p.clusterContext.Client.Get(ctx, client.ObjectKeyFromObject(o), o)
-			innerG.Expect(apierrors.IsNotFound(err)).To(BeTrue(), "object %s %s should not be found in cluster", o.GetObjectKind().GroupVersionKind().String(), client.ObjectKeyFromObject(o).String())
+			innerG.Expect(apierrors.IsNotFound(err)).To(gomega.BeTrue(), "object %s %s should not be found in cluster", o.GetObjectKind().GroupVersionKind().String(), client.ObjectKeyFromObject(o).String())
 		}).
 			WithContext(ctx).
 			WithTimeout(p.assertionOptions.Timeout).
 			WithPolling(p.assertionOptions.Polling).
-			Should(Succeed())
+			Should(gomega.Succeed())
 	}
 }
 
 func (p *Provider) ExpectNamespaceNotExist(ctx context.Context, ns string) {
 	_, err := p.clusterContext.Clientset.CoreV1().Namespaces().Get(ctx, ns, metav1.GetOptions{})
-	p.Gomega.Expect(apierrors.IsNotFound(err)).To(BeTrue(), fmt.Sprintf("namespace %s should not be found in cluster", ns))
+	p.Gomega.Expect(apierrors.IsNotFound(err)).To(gomega.BeTrue(), fmt.Sprintf("namespace %s should not be found in cluster", ns))
 }
