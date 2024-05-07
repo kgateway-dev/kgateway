ac0c63980ebbb2a6672f60803742b5a0665ca24a
Lawrence Gadban <lawrence.gadban@solo.io>
2024-05-07 17:14:49 -0500

    cleanup install script

--- projects/gateway2/install.sh
+++ projects/gateway2/install.sh
@@ -3,10 +3,6 @@ set -eux
 
 ./ci/kind/setup-kind.sh
 
-./projects/gateway2/kind.sh
-
-kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml
-
 helm upgrade --install --create-namespace \
   --namespace gloo-system gloo \
   ./_test/gloo-1.0.0-ci1.tgz \
