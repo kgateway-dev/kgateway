c2bfe0ef2b5f05e82f35bbcd9eb1059a0f3cb6f2
Bernie Birnbaum <bewebi@earthlink.net>
2024-05-07 13:09:45 -0400

    fix/cleanup tests

--- test/e2e/aws_test.go
+++ test/e2e/aws_test.go
@@ -157,14 +157,6 @@ var _ = Describe("AWS Lambda", func() {
 		})
 	}
 
-	validateLambda302 := func(envoyPort uint32) {
-		validateLambda(lambdaValidationParams{
-			offset:             2,
-			envoyPort:          envoyPort,
-			expectedSubstrings: []string{`"statusCode": 302`, `\"302 test body\"`, `"multiValueHeaders": {"foo": [null, "bar"]}`},
-		})
-	}
-
 	addUpstream := func() {
 		upstream = &gloov1.Upstream{
 			Metadata: &core.Metadata{
@@ -286,7 +278,7 @@ var _ = Describe("AWS Lambda", func() {
 		helpers.EventuallyResourceAccepted(func() (resources.InputResource, error) {
 			proxy, err := testClients.ProxyClient.Read(proxy.Metadata.Namespace, proxy.Metadata.Name, clients.ReadOpts{})
 			return proxy, err
-		}, "300s", "1s")
+		}, "30s", "1s")
 
 	}
 
@@ -354,15 +346,16 @@ var _ = Describe("AWS Lambda", func() {
 		})
 	}
 
-	testProxyWith302Response := func() {
+	testProxyWithUnwrapAsApiGateway302Response := func() {
 		err := envoyInstance.RunWithRole(envoy.DefaultProxyName, testClients.GlooPort)
 		Expect(err).NotTo(HaveOccurred())
 
-		createProxy(false, false, false, "302-test")
+		createProxy(true, false, false, "302-test")
 		validateLambda(lambdaValidationParams{
 			offset:             1,
 			envoyPort:          envoyInstance.HttpPort,
-			expectedSubstrings: []string{`"statusCode": 302`, `"\"302 test body\""`, `"multiValueHeaders": {"foo": [null, "bar"]}`},
+			expectedSubstrings: []string{`"302 test body"`},
+			expectedHeaders:    http.Header{"Foo": []string{"null,bar,123"}},
 		})
 	}
 
@@ -417,47 +410,6 @@ var _ = Describe("AWS Lambda", func() {
 		Expect(err).NotTo(HaveOccurred())
 
 		validateLambdaUppercase(envoyInstance.HttpPort)
-	}
-
-	testLambdaWithVirtualService302 := func() {
-		err := envoyInstance.RunWithRoleAndRestXds("gloo-system~"+gwdefaults.GatewayProxyName, testClients.GlooPort, testClients.RestXdsPort)
-		Expect(err).NotTo(HaveOccurred())
-
-		vs := &gw1.VirtualService{
-			Metadata: &core.Metadata{
-				Name:      "app",
-				Namespace: "gloo-system",
-			},
-			VirtualHost: &gw1.VirtualHost{
-				Domains: []string{"*"},
-				Routes: []*gw1.Route{{
-					Action: &gw1.Route_RouteAction{
-						RouteAction: &gloov1.RouteAction{
-							Destination: &gloov1.RouteAction_Single{
-								Single: &gloov1.Destination{
-									DestinationType: &gloov1.Destination_Upstream{
-										Upstream: upstream.Metadata.Ref(),
-									},
-									DestinationSpec: &gloov1.DestinationSpec{
-										DestinationType: &gloov1.DestinationSpec_Aws{
-											Aws: &aws_plugin.DestinationSpec{
-												LogicalName: "302-test",
-											},
-										},
-									},
-								},
-							},
-						},
-					},
-				}},
-			},
-		}
-
-		var opts clients.WriteOpts
-		_, err = testClients.VirtualServiceClient.Write(vs, opts)
-		Expect(err).NotTo(HaveOccurred())
-
-		validateLambda302(envoyInstance.HttpPort)
 	}
 
 	testLambdaTransformations := func() {
@@ -633,11 +585,11 @@ var _ = Describe("AWS Lambda", func() {
 
 			It("should be able to call lambda with unwrapAsApiGateway", testProxyWithUnwrapAsApiGateway)
 
+			FIt("should be able to call lambda with unwrapAsApiGateway with 302 response", testProxyWithUnwrapAsApiGateway302Response)
+
 			It("should be able to call lambda with request transform", testProxyWithRequestTransform)
 
 			It("should be able to call lambda with request and response transforms", testProxyWithRequestAndResponseTransforms)
-
-			It("should be able to call lambda with 302 response", testProxyWith302Response)
 		})
 		Context("With gateway translation", func() {
 			BeforeEach(func() {
@@ -648,8 +600,6 @@ var _ = Describe("AWS Lambda", func() {
 			It("should be able to call lambda via gateway", testLambdaWithVirtualService)
 
 			It("should be able to call lambda transformation and regular transformation", testLambdaTransformations)
-
-			It("should be able to call lambda with 302 response", testLambdaWithVirtualService302)
 		})
 		Context("Resource-based cross-account lambda", func() {
 			BeforeEach(func() {
