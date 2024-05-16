09641c65c82da44a8189fa2db853df2c02e70e82
sheidkamp <seth.heidkamp@gmail.com>
2024-05-16 10:52:18 -0400

    Update helm_test.go

--- install/test/helm_test.go
+++ install/test/helm_test.go
@@ -6370,7 +6370,7 @@ metadata:
 					return defaultRestrictedContainerSecurityContext
 				}
 
-				FDescribeTable("applies default retricted container security contexts", func(resourceName string, containerName string, resourceType string, securityRoot string, applyDefaults applyContainerSecurityDefaults, extraArgs ...string) {
+				DescribeTable("applies default retricted container security contexts", func(resourceName string, containerName string, resourceType string, securityRoot string, applyDefaults applyContainerSecurityDefaults, extraArgs ...string) {
 
 					for _, seccompTypeValue := range []string{"RuntimeDefault", "Localhost", ""} {
 						helmArgs := extraArgs
