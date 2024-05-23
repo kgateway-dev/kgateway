264728f0d550327f943487486eadcbc9f6b9c46e
sheidkamp <seth.heidkamp@gmail.com>
2024-05-23 17:58:11 -0400

    updates

--- projects/gloo/cli/pkg/cmd/istio/sidecars/matcher.go
+++ projects/gloo/cli/pkg/cmd/istio/sidecars/matcher.go
@@ -2,8 +2,6 @@ package sidecars
 
 import (
 	"errors"
-	"regexp"
-	"strings"
 
 	corev1 "k8s.io/api/core/v1"
 )
@@ -15,13 +13,5 @@ var ErrNoSupportedSidecar = errors.New("no valid istio sidecar found for this is
 // version of Istio, with the given jwtPolicy, to run
 // in the gateway-proxy pod
 func GetIstioSidecar(istioVersion, jwtPolicy string, istioMetaMeshID string, istioMetaClusterID string, istioDiscoveryAddress string) (*corev1.Container, error) {
-
-	istio1dot7to1dotX, _ := regexp.MatchString("1.([7-9]|[1-9][0-9]+).[0-9]*", istioVersion)
-
-	if istio1dot7to1dotX {
-		return generateIstio17to1xSidecar(istioVersion, jwtPolicy, istioMetaMeshID, istioMetaClusterID, istioDiscoveryAddress), nil
-	} else if strings.HasPrefix(istioVersion, "1.6.") {
-		return generateIstio16Sidecar(istioVersion, jwtPolicy, istioMetaMeshID, istioMetaClusterID, istioDiscoveryAddress), nil
-	}
-	return nil, ErrNoSupportedSidecar
+	return generateIstioSidecar(istioVersion, jwtPolicy, istioMetaMeshID, istioMetaClusterID, istioDiscoveryAddress), nil
 }
