92caa1bf76b45cc0f48c8d1c7e548cda3e2c2b42
sheidkamp <seth.heidkamp@gmail.com>
2024-05-23 22:57:54 -0400

    remove error

--- projects/gloo/cli/pkg/cmd/istio/sidecars/matcher.go
+++ projects/gloo/cli/pkg/cmd/istio/sidecars/matcher.go
@@ -1,13 +1,8 @@
 package sidecars
 
 import (
-	"errors"
-
 	corev1 "k8s.io/api/core/v1"
 )
-
-// ErrNoSupportedSidecar occurs when we can't find any premade sidecar for the given Istio version
-var ErrNoSupportedSidecar = errors.New("no valid istio sidecar found for this istio version")
 
 // GetIstioSidecar will return an Istio sidecar for the given
 // version of Istio, with the given jwtPolicy, to run
