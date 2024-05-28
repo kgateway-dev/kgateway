e981db663e109eb9d578e0e3426bbaaee6bc5b29
Lawrence Gadban <lawrence.gadban@solo.io>
2024-05-28 17:14:36 -0500

    codegen & update-codegen.sh

--- projects/gateway/pkg/api/v1/external_options.pb.equal.go
+++ projects/gateway/pkg/api/v1/external_options.pb.equal.go
@@ -152,3 +152,118 @@ func (m *RouteOption) Equal(that interface{}) bool {
 
 	return true
 }
+
+// Equal function
+func (m *ListenerOption) Equal(that interface{}) bool {
+	if that == nil {
+		return m == nil
+	}
+
+	target, ok := that.(*ListenerOption)
+	if !ok {
+		that2, ok := that.(ListenerOption)
+		if ok {
+			target = &that2
+		} else {
+			return false
+		}
+	}
+	if target == nil {
+		return m == nil
+	} else if m == nil {
+		return false
+	}
+
+	if h, ok := interface{}(m.GetMetadata()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetMetadata()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetMetadata(), target.GetMetadata()) {
+			return false
+		}
+	}
+
+	if h, ok := interface{}(m.GetOptions()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetOptions()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetOptions(), target.GetOptions()) {
+			return false
+		}
+	}
+
+	if len(m.GetTargetRef()) != len(target.GetTargetRef()) {
+		return false
+	}
+	for idx, v := range m.GetTargetRef() {
+
+		if h, ok := interface{}(v).(equality.Equalizer); ok {
+			if !h.Equal(target.GetTargetRef()[idx]) {
+				return false
+			}
+		} else {
+			if !proto.Equal(v, target.GetTargetRef()[idx]) {
+				return false
+			}
+		}
+
+	}
+
+	return true
+}
+
+// Equal function
+func (m *HttpListenerOption) Equal(that interface{}) bool {
+	if that == nil {
+		return m == nil
+	}
+
+	target, ok := that.(*HttpListenerOption)
+	if !ok {
+		that2, ok := that.(HttpListenerOption)
+		if ok {
+			target = &that2
+		} else {
+			return false
+		}
+	}
+	if target == nil {
+		return m == nil
+	} else if m == nil {
+		return false
+	}
+
+	if h, ok := interface{}(m.GetMetadata()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetMetadata()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetMetadata(), target.GetMetadata()) {
+			return false
+		}
+	}
+
+	if h, ok := interface{}(m.GetOptions()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetOptions()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetOptions(), target.GetOptions()) {
+			return false
+		}
+	}
+
+	if h, ok := interface{}(m.GetTargetRef()).(equality.Equalizer); ok {
+		if !h.Equal(target.GetTargetRef()) {
+			return false
+		}
+	} else {
+		if !proto.Equal(m.GetTargetRef(), target.GetTargetRef()) {
+			return false
+		}
+	}
+
+	return true
+}
