a9793bebed38b22c25211f79a400b66ba6c861ac
Sam Heilbron <samheilbron@gmail.com>
2024-06-05 13:59:45 -0600

    support weighted cluster

--- projects/gloo/pkg/translator/route_config.go
+++ projects/gloo/pkg/translator/route_config.go
@@ -594,12 +594,16 @@ func (h *httpRouteConfigurationTranslator) setWeightedClusters(params plugins.Ro
 		// run the plugins for Weighted Destinations
 		for _, plugin := range h.pluginRegistry.GetWeightedDestinationPlugins() {
 			if err := plugin.ProcessWeightedDestination(params, weightedDest, weightedCluster); err != nil {
-				// TODO(npolshak): Add check for ConfigurationError type: https://github.com/solo-io/gloo/issues/7357
-				validation.AppendRouteError(routeReport,
-					validationapi.RouteReport_Error_ProcessingError,
-					err.Error(),
-					routeName,
-				)
+				message := fmt.Sprintf("invalid weighted cluster [%s] while processing plugin %s: %s", weightedCluster.GetName(), plugin.Name(), err.Error())
+				doReportErr := func() {
+					validation.AppendRouteError(routeReport,
+						validationapi.RouteReport_Error_ProcessingError,
+						err.Error(),
+						routeName,
+					)
+				}
+
+				reportPluginProcessingError(params.Params, err, message, doReportErr)
 			}
 		}
 
