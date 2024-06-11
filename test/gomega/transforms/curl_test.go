364adbcd983c9221650a89078765b06b1d648bed
sheidkamp <seth.heidkamp@gmail.com>
2024-06-11 15:56:12 -0400

    Update curl_test.go

--- test/gomega/transforms/curl_test.go
+++ test/gomega/transforms/curl_test.go
@@ -3,7 +3,6 @@ package transforms_test
 import (
 	"net/http"
 
-	"github.com/solo-io/gloo/pkg/utils/kubeutils/kubectl"
 	"github.com/solo-io/gloo/test/gomega/transforms"
 
 	"github.com/onsi/gomega/gstruct"
@@ -17,9 +16,6 @@ var _ = Describe("Curl", func() {
 
 	var (
 		validStringResponse = "*   Trying 10.96.92.168...\n* TCP_NODELAY set\n* Connected to gateway-proxy (10.96.92.168) port 80 (#0)\n> GET /test HTTP/1.1\n> Host: test-domain\n> User-Agent: curl/7.58.0\n> Accept: */*\n> header1: value1\n> header2: value2\n> instructions: invalid json value\n> \n< HTTP/1.1 200 OK\n< x-powered-by: Express\n< content-type: application/json; charset=utf-8\n< content-length: 444\n< etag: W/\"1bc-u/C5Wu/6BvNtW0jEh2E+mCP4gUg\"\n< date: Thu, 28 Mar 2024 19:40:18 GMT\n< x-envoy-upstream-service-time: 374\n< server: envoy\n< \n{ [444 bytes data]\n* Connection #0 to host gateway-proxy left intact"
-		validCurlResponse   = kubectl.CurlResponse{
-			StdErr: validStringResponse,
-		}
 	)
 
 	Describe("WithCurlHttpResponse", func() {
@@ -33,17 +29,4 @@ var _ = Describe("Curl", func() {
 					})))
 		})
 	})
-
-	Describe("WithCurlResponse", func() {
-
-		It("matches valid response", func() {
-			Expect(&validCurlResponse).To(
-				WithTransform(transforms.WithCurlResponse,
-					matchers.HaveHttpResponse(&matchers.HttpResponse{
-						StatusCode: http.StatusOK,
-						Body:       gstruct.Ignore(),
-					})))
-		})
-	})
-
 })
