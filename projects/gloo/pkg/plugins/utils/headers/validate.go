7f5008bc65b51b4d9e546df7d35e758495dc0fd6
David Jumani <dj.davidjumani1994@gmail.com>
2024-06-14 11:17:06 -0400

    address comments

--- projects/gloo/pkg/plugins/utils/headers/validate.go
+++ projects/gloo/pkg/plugins/utils/headers/validate.go
@@ -5,15 +5,18 @@ import (
 	"regexp"
 )
 
+var (
+	// Regex to check that header names consists of only valid ASCII characters
+	// https://github.com/envoyproxy/envoy/blob/b0f4332867267913d9aa80c5c0befda14a00d826/source/common/http/character_set_validation.h#L24-L35
+	validHeaderNameRegex = regexp.MustCompile("^([a-zA-Z0-9!#$%&'*+.^_`|~-])+$")
+)
+
 // ValidateHeaderKey checks whether a header is valid based on the RFC and envoy's regex to accept a header key
 func ValidateHeaderKey(key string) error {
 	if len(key) == 0 {
 		return fmt.Errorf("empty HTTP header names are not allowed")
 	}
-	// check that header name is only valid ASCII characters
-	// https://github.com/envoyproxy/envoy/blob/b0f4332867267913d9aa80c5c0befda14a00d826/source/common/http/character_set_validation.h#L24-L35
-	validHeaderRegex := regexp.MustCompile("^([a-zA-Z0-9!#$%&'*+.^_`|~-])+$")
-	if !validHeaderRegex.MatchString(key) {
+	if !validHeaderNameRegex.MatchString(key) {
 		return fmt.Errorf("'%s' is an invalid HTTP header key", key)
 	}
 	return nil
