1f76a43b806207cb1c64a52302124748db9cf20b
Ashish Banerjee <ashish.banerjee@solo.io>
2024-06-17 13:39:05 -0400

    Move configuration to a new plugin

--- projects/gloo/pkg/plugins/hcm/plugin.go
+++ projects/gloo/pkg/plugins/hcm/plugin.go
@@ -77,7 +77,6 @@ func (p *plugin) ProcessHcmNetworkFilter(params plugins.Params, _ *v1.Listener, 
 		out.HttpProtocolOptions = &envoycore.Http1ProtocolOptions{
 			AcceptHttp_10:         true,
 			DefaultHostForHttp_10: in.GetDefaultHostForHttp_10().GetValue(),
-			AllowCustomMethods:    listener.GetOptions().GetHeaderValidationSettings().GetAllowCustomHeaderMethods(),
 		}
 	}
 
@@ -138,16 +137,16 @@ func (p *plugin) ProcessHcmNetworkFilter(params plugins.Params, _ *v1.Listener, 
 		out.GetHttpProtocolOptions().EnableTrailers = in.GetEnableTrailers().GetValue()
 	}
 
-	if listener.GetOptions().GetHeaderValidationSettings().GetAllowCustomHeaderMethods() {
-		if out.GetHttpProtocolOptions() == nil {
-			out.HttpProtocolOptions = &envoycore.Http1ProtocolOptions{}
-		}
-		// ALERT: AllowCustomMethods is deprecated from upstream Envoy and
-		// scheduled for removal. When that setting is removed, we must use
-		// Universal Header Validation to support this functionality. See
-		// https://soloio.slab.com/posts/extended-http-methods-design-doc-40j7pjeu
-		out.HttpProtocolOptions.AllowCustomMethods = listener.GetOptions().GetHeaderValidationSettings().GetAllowCustomHeaderMethods()
-	}
+// 	if listener.GetOptions().GetHeaderValidationSettings().GetAllowCustomHeaderMethods() {
+// 		if out.GetHttpProtocolOptions() == nil {
+// 			out.HttpProtocolOptions = &envoycore.Http1ProtocolOptions{}
+// 		}
+// 		// ALERT: AllowCustomMethods is deprecated from upstream Envoy and
+// 		// scheduled for removal. When that setting is removed, we must use
+// 		// Universal Header Validation to support this functionality. See
+// 		// https://soloio.slab.com/posts/extended-http-methods-design-doc-40j7pjeu
+// 		out.HttpProtocolOptions.AllowCustomMethods = listener.GetOptions().GetHeaderValidationSettings().GetAllowCustomHeaderMethods()
+// 	}
 
 	if in.GetIdleTimeout() != nil {
 		if out.GetCommonHttpProtocolOptions() == nil {
