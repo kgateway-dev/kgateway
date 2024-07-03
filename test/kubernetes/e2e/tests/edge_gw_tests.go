da1597e4f6409b51d893e891880c5572ba75270b
npolshakova <nina.polshakova@solo.io>
2024-07-03 16:47:12 -0400

    combine glooctl helm manifest and edge gw manifest

--- test/kubernetes/e2e/tests/edge_gw_tests.go
+++ test/kubernetes/e2e/tests/edge_gw_tests.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"github.com/solo-io/gloo/test/kubernetes/e2e"
+	"github.com/solo-io/gloo/test/kubernetes/e2e/features/glooctl"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/headless_svc"
 	"github.com/solo-io/gloo/test/kubernetes/e2e/features/port_routing"
 )
@@ -11,6 +12,9 @@ func EdgeGwSuiteRunner() e2e.SuiteRunner {
 
 	edgeGwSuiteRunner.Register("HeadlessSvc", headless_svc.NewEdgeGatewayHeadlessSvcSuite)
 	edgeGwSuiteRunner.Register("PortRouting", port_routing.NewEdgeGatewayApiTestingSuite)
+	edgeGwSuiteRunner.Register("GlooctlCheck", glooctl.NewCheckSuite)
+	edgeGwSuiteRunner.Register("GlooctlCheckCrds", glooctl.NewCheckCrdsSuite)
+	edgeGwSuiteRunner.Register("GlooctlCheckCrds", glooctl.NewDebugSuite)
 
 	return edgeGwSuiteRunner
 }
