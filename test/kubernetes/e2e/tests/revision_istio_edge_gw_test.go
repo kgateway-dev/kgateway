034a8168a20fbe624c06d9cf03f23e0944831a40
Nina Polshakova <nina.polshakova@solo.io>
2024-07-11 10:41:01 -0400

    Remove Istio trust domain (#9713)
    * add trust domain
    
    * changelog
    
    * tests
    
    * fix test name
    
    * add to workflow
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * rebalance tests, fix helm
    
    * update workflow with numbers for loadbalancing between e2e test clusters
    
    * pr feedback
    
    * update gateway proxies to be disabled for k8s gateway e2e tests
    
    * cleanup istio install, remove old trust domain tests
    
    * fix merge
    
    * minimal ci change
    
    * fix test name
    
    * fix glooctl test helm chart
    
    * fix istio edge gw manifest
    
    * fix configmap template
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    * missing gatewayProxy
    
    * t.Error
    
    * Adding changelog file to new location
    
    * Deleting changelog file from old location
    
    ---------
    
    Co-authored-by: changelog-bot <changelog-bot>
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>

--- test/kubernetes/e2e/tests/revision_istio_edge_gw_test.go
+++ test/kubernetes/e2e/tests/revision_istio_edge_gw_test.go
@@ -7,23 +7,23 @@ import (
 	"testing"
 	"time"
 
-	"github.com/solo-io/gloo/test/kube2e/helper"
 	"github.com/solo-io/gloo/test/kubernetes/e2e"
 	. "github.com/solo-io/gloo/test/kubernetes/e2e/tests"
 	"github.com/solo-io/gloo/test/kubernetes/testutils/gloogateway"
+	"github.com/solo-io/gloo/test/kubernetes/testutils/helper"
 
 	"github.com/solo-io/skv2/codegen/util"
 )
 
-// TestIstioEdgeApiGateway is the function which executes a series of tests against a given installation where
-// the k8s Gateway controller is disabled
-func TestIstioEdgeApiGateway(t *testing.T) {
+// TestRevisionIstioRegression is the function which executes a series of tests against a given installation where
+// the k8s Gateway controller is disabled and the deprecated Istio integration values are used to check for regressions
+func TestRevisionIstioRegression(t *testing.T) {
 	ctx := context.Background()
 	testInstallation := e2e.CreateTestInstallation(
 		t,
 		&gloogateway.Context{
-			InstallNamespace:   "istio-edge-api-gateway-test",
-			ValuesManifestFile: filepath.Join(util.MustGetThisDir(), "manifests", "istio-edge-gateway-test-helm.yaml"),
+			InstallNamespace:   "istio-rev-regression-test",
+			ValuesManifestFile: filepath.Join(util.MustGetThisDir(), "manifests", "istio-revision-helm.yaml"),
 		},
 	)
 
@@ -60,7 +60,7 @@ func TestIstioEdgeApiGateway(t *testing.T) {
 	})
 
 	// Install Istio before Gloo Gateway to make sure istiod is present before istio-proxy
-	err = testInstallation.InstallMinimalIstio(ctx)
+	err = testInstallation.InstallRevisionedIstio(ctx)
 	if err != nil {
 		log.Printf("failed to install: %v\n", err)
 		// immediately stop if Istio installation fails
@@ -69,8 +69,8 @@ func TestIstioEdgeApiGateway(t *testing.T) {
 
 	// Install Gloo Gateway with only Edge APIs enabled
 	testInstallation.InstallGlooGateway(ctx, func(ctx context.Context) error {
-		return testHelper.InstallGloo(ctx, helper.GATEWAY, 5*time.Minute, helper.ExtraArgs("--values", testInstallation.Metadata.ValuesManifestFile))
+		return testHelper.InstallGloo(ctx, 5*time.Minute, helper.WithExtraArgs("--values", testInstallation.Metadata.ValuesManifestFile))
 	})
 
-	IstioEdgeApiSuiteRunner().Run(ctx, t, testInstallation)
+	RevisionIstioEdgeGatewaySuiteRunner().Run(ctx, t, testInstallation)
 }
