8cfd3bba0aca214a0e1559991610937b2ef99693
Sam Heilbron <samheilbron@gmail.com>
2024-07-19 13:55:19 -0600

    add comments to exported functions

--- test/gomega/matchers/custom_resource.go
+++ test/gomega/matchers/custom_resource.go
@@ -6,10 +6,12 @@ import (
 	"github.com/onsi/gomega/types"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 
-	k8s_types "k8s.io/apimachinery/pkg/types"
+	k8stypes "k8s.io/apimachinery/pkg/types"
 )
 
-func HaveObjectMeta(namespacedName k8s_types.NamespacedName) types.GomegaMatcher {
+// HaveObjectMeta returns a GomegaMatcher which matches a struct that has the provided name/namespace
+// This should be used when asserting that a CustomResource has a provided name/namespace
+func HaveObjectMeta(namespacedName k8stypes.NamespacedName) types.GomegaMatcher {
 	return gstruct.MatchFields(gstruct.IgnoreExtras, gstruct.Fields{
 		"Name":          Equal(namespacedName.Name),
 		"Namespace":     Equal(namespacedName.Namespace),
@@ -17,6 +19,7 @@ func HaveObjectMeta(namespacedName k8s_types.NamespacedName) types.GomegaMatcher
 	})
 }
 
+// HaveTypeMeta returns a GomegaMatcher which matches a struct that has the provide Group/Version/Kind
 func HaveTypeMeta(gvk schema.GroupVersionKind) types.GomegaMatcher {
 	return gstruct.MatchFields(gstruct.IgnoreExtras, gstruct.Fields{
 		"APIVersion": Equal(gvk.GroupVersion().String()),
@@ -24,6 +27,8 @@ func HaveTypeMeta(gvk schema.GroupVersionKind) types.GomegaMatcher {
 	})
 }
 
+// ContainCustomResource returns a GomegaMatcher which matches resource in a list if the provided
+// typeMeta, objectMeta and spec matchers match
 func ContainCustomResource(typeMetaMatcher, objectMetaMatcher, specMatcher types.GomegaMatcher) types.GomegaMatcher {
 	return ContainElement(gstruct.MatchFields(gstruct.IgnoreExtras, gstruct.Fields{
 		"TypeMeta":   typeMetaMatcher,
