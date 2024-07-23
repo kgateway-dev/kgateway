solo-io/gloo

k8s gateway extensions: allow customizing translator

projects/gateway2/extensions/extensions.go
@@ -8,18 +8,25 @@ import (
 	"github.com/solo-io/gloo/pkg/version"
 	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gateway2/query"
+	"github.com/solo-io/gloo/projects/gateway2/translator"
 	"github.com/solo-io/gloo/projects/gateway2/translator/plugins/registry"
 	"github.com/solo-io/solo-kit/pkg/api/v2/reporter"
 	controllerruntime "sigs.k8s.io/controller-runtime"
+	apiv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 // K8sGatewayExtensions is responsible for providing implementations for translation utilities
 // which have Enterprise variants.
 type K8sGatewayExtensions interface {
-	// CreatePluginRegistry returns the PluginRegistry
-	CreatePluginRegistry(ctx context.Context) registry.PluginRegistry
+	// GetPluginRegistry exposes the plugins supported by this implementation.
+	GetPluginRegistry() registry.PluginRegistry
 
-	// GetEnvoyImage returns the envoy image and tag used by the proxy deployment.
+	// GetTranslator allows an extnsion to provide custom translation for
+	// different gateway classes.
+	GetTranslator(*apiv1.Gateway) translator.K8sGwTranslator
+
+	// GetEnvoyImage returns the image repo and tag to use for the envoy container image
+	// in the proxy deployment.
 	GetEnvoyImage() Image
 }
 
@@ -44,11 +51,26 @@ func NewK8sGatewayExtensions(
 	_ context.Context,
 	params K8sGatewayExtensionsFactoryParameters,
 ) (K8sGatewayExtensions, error) {
+	queries := query.NewData(
+		params.Mgr.GetClient(),
+		params.Mgr.GetScheme(),
+	)
+	plugins := registry.BuildPlugins(
+		queries,
+		params.Mgr.GetClient(),
+		params.RouteOptionClient,
+		params.VirtualHostOptionClient,
+		params.StatusReporter,
+	)
+	pluginRegistry := registry.NewPluginRegistry(plugins)
+
 	return &k8sGatewayExtensions{
 		mgr:                     params.Mgr,
 		routeOptionClient:       params.RouteOptionClient,
 		virtualHostOptionClient: params.VirtualHostOptionClient,
 		statusReporter:          params.StatusReporter,
+		translator:              translator.NewTranslator(queries, pluginRegistry),
+		pluginRegistry:          pluginRegistry,
 	}, nil
 }
 
@@ -57,26 +79,18 @@ type k8sGatewayExtensions struct {
 	routeOptionClient       gatewayv1.RouteOptionClient
 	virtualHostOptionClient gatewayv1.VirtualHostOptionClient
 	statusReporter          reporter.StatusReporter
+	translator              translator.K8sGwTranslator
+	pluginRegistry          registry.PluginRegistry
 }
 
-// CreatePluginRegistry returns the PluginRegistry
-func (e *k8sGatewayExtensions) CreatePluginRegistry(_ context.Context) registry.PluginRegistry {
-	queries := query.NewData(
-		e.mgr.GetClient(),
-		e.mgr.GetScheme(),
-	)
-	plugins := registry.BuildPlugins(
-		queries,
-		e.mgr.GetClient(),
-		e.routeOptionClient,
-		e.virtualHostOptionClient,
-		e.statusReporter,
-	)
-	return registry.NewPluginRegistry(plugins)
+func (e *k8sGatewayExtensions) GetTranslator(_ *apiv1.Gateway) translator.K8sGwTranslator {
+	return e.translator
 }
 
-// GetEnvoyImage returns the image repo and tag to use for the envoy container image
-// in the proxy deployment.
+func (e *k8sGatewayExtensions) GetPluginRegistry() registry.PluginRegistry {
+	return e.pluginRegistry
+}
+
 func (e *k8sGatewayExtensions) GetEnvoyImage() Image {
 	return Image{
 		Repository: "gloo-envoy-wrapper",

kgateway-dev/kgateway

projects/gateway2/extensions/extensions.go
@@ -5,17 +5,23 @@ import (
 
 	gatewayv1 "github.com/solo-io/gloo/projects/gateway/pkg/api/v1"
 	"github.com/solo-io/gloo/projects/gateway2/query"
+	"github.com/solo-io/gloo/projects/gateway2/translator"
 	"github.com/solo-io/gloo/projects/gateway2/translator/plugins/registry"
 	v1 "github.com/solo-io/gloo/projects/gloo/pkg/api/v1/enterprise/options/extauth/v1"
 	"github.com/solo-io/solo-kit/pkg/api/v2/reporter"
 	controllerruntime "sigs.k8s.io/controller-runtime"
+	apiv1 "sigs.k8s.io/gateway-api/apis/v1"
 )
 
 // K8sGatewayExtensions is responsible for providing implementations for translation utilities
 // which have Enterprise variants.
 type K8sGatewayExtensions interface {
-	// CreatePluginRegistry returns the PluginRegistry
-	CreatePluginRegistry(ctx context.Context) registry.PluginRegistry
+	// CreatePluginRegistry exposes the plugins supported by this implementation.
+	CreatePluginRegistry() registry.PluginRegistry
+
+	// GetTranslator allows an extension to provide custom translation for
+	// different gateway classes.
+	GetTranslator(*apiv1.Gateway, registry.PluginRegistry) translator.K8sGwTranslator
 }
 
 // K8sGatewayExtensionsFactoryParameters contains the parameters required to start Gloo K8s Gateway Extensions (including Translator Plugins)
@@ -39,11 +45,17 @@ func NewK8sGatewayExtensions(
 	_ context.Context,
 	params K8sGatewayExtensionsFactoryParameters,
 ) (K8sGatewayExtensions, error) {
+	queries := query.NewData(
+		params.Mgr.GetClient(),
+		params.Mgr.GetScheme(),
+	)
+
 	return &k8sGatewayExtensions{
 		mgr:                     params.Mgr,
 		routeOptionClient:       params.RouteOptionClient,
 		virtualHostOptionClient: params.VirtualHostOptionClient,
 		statusReporter:          params.StatusReporter,
+		queries:                 queries,
 	}, nil
 }
 
@@ -52,16 +64,18 @@ type k8sGatewayExtensions struct {
 	routeOptionClient       gatewayv1.RouteOptionClient
 	virtualHostOptionClient gatewayv1.VirtualHostOptionClient
 	statusReporter          reporter.StatusReporter
+	translator              translator.K8sGwTranslator
+	pluginRegistry          registry.PluginRegistry
+	queries                 query.GatewayQueries
 }
 
-// CreatePluginRegistry returns the PluginRegistry
-func (e *k8sGatewayExtensions) CreatePluginRegistry(_ context.Context) registry.PluginRegistry {
-	queries := query.NewData(
-		e.mgr.GetClient(),
-		e.mgr.GetScheme(),
-	)
+func (e *k8sGatewayExtensions) GetTranslator(_ *apiv1.Gateway, pluginRegistry registry.PluginRegistry) translator.K8sGwTranslator {
+	return translator.NewTranslator(e.queries, pluginRegistry)
+}
+
+func (e *k8sGatewayExtensions) CreatePluginRegistry() registry.PluginRegistry {
 	plugins := registry.BuildPlugins(
-		queries,
+		e.queries,
 		e.mgr.GetClient(),
 		e.routeOptionClient,
 		e.virtualHostOptionClient,