06bba1f998a6b01e5797705882ee7df0b6d72299
David Jumani <dj.davidjumani1994@gmail.com>
2024-08-02 10:20:36 -0400

    update the comment again

--- test/ginkgo/parallel/ports.go
+++ test/ginkgo/parallel/ports.go
@@ -25,7 +25,7 @@ func GetPortOffset() int {
 	return GetParallelProcessCount() * 1000
 }
 
-// RetryWithLoggedError retries if there is an error and prints that error that occurs to stdout
+// RetryWithLoggedError retries if there is an error and prints that error to stdout
 func RetryWithLoggedError() retry.Option {
 	return retry.RetryIf(func(err error) bool {
 		if err != nil {
