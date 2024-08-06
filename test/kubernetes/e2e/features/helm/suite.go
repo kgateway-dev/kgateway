4d8a167592666fe9e848b0935351f331eee6e62e
David Jumani <dj.davidjumani1994@gmail.com>
2024-08-05 22:09:39 -0400

    increase timeout

--- test/kubernetes/e2e/features/helm/suite.go
+++ test/kubernetes/e2e/features/helm/suite.go
@@ -18,7 +18,7 @@ import (
 
 var _ e2e.NewSuiteFunc = NewTestingSuite
 
-// testingSuite is the entire Suite of tests for the Upgrade Tests
+// testingSuite is the entire Suite of tests for the Helm Tests
 type testingSuite struct {
 	*base.BaseTestingSuite
 }
