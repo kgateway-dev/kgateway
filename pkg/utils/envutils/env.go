c18544f4e39c3499108f797889560c48a1d11ca6
Jacob Bohanon <jacob.bohanon@solo.io>
2024-08-15 09:19:16 -0400

    make IsEnvTruthy use IsTruthyValue

--- pkg/utils/envutils/env.go
+++ pkg/utils/envutils/env.go
@@ -8,8 +8,7 @@ import (
 // IsEnvTruthy returns true if a given environment variable has a truthy value
 // Examples of truthy values are: "1", "t", "T", "true", "TRUE", "True". Anything else is considered false.
 func IsEnvTruthy(envVarName string) bool {
-	envValue, _ := strconv.ParseBool(os.Getenv(envVarName))
-	return envValue
+	return IsTruthyValue(os.Getenv(envVarName))
 }
 
 // IsEnvDefined returns true if a given environment variable has any value
