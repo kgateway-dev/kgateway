d4fa2aad3fef789934954193534c979c35c4d46d
Jenny Shu <jenny.shu@solo.io>
2024-09-26 16:39:43 -0400

    merge test

--- projects/gateway2/deployer/merge_test.go
+++ projects/gateway2/deployer/merge_test.go
@@ -47,4 +47,72 @@ var _ = Describe("deepMergeGatewayParameters", func() {
 		Expect(out).To(Equal(dst))
 		Expect(out.Spec.Kube.Deployment.Replicas).To(Equal(src.Spec.Kube.Deployment.Replicas))
 	})
+
+	It("merges maps", func() {
+		dst := &gw2_v1alpha1.GatewayParameters{
+			Spec: gw2_v1alpha1.GatewayParametersSpec{
+				Kube: &gw2_v1alpha1.KubernetesProxyConfig{
+					PodTemplate: &gw2_v1alpha1.Pod{
+						ExtraAnnotations: map[string]string{
+							"a": "b",
+						},
+					},
+					Service: &gw2_v1alpha1.Service{
+						ExtraAnnotations: map[string]string{
+							"c": "d",
+						},
+					},
+					ServiceAccount: &gw2_v1alpha1.ServiceAccount{
+						ExtraLabels: map[string]string{
+							"e": "f",
+						},
+						ExtraAnnotations: map[string]string{
+							"g": "h",
+						},
+					},
+				},
+			},
+		}
+		src := &gw2_v1alpha1.GatewayParameters{
+			Spec: gw2_v1alpha1.GatewayParametersSpec{
+				Kube: &gw2_v1alpha1.KubernetesProxyConfig{
+					PodTemplate: &gw2_v1alpha1.Pod{
+						ExtraAnnotations: map[string]string{
+							"i": "j",
+						},
+					},
+					Service: &gw2_v1alpha1.Service{
+						ExtraAnnotations: map[string]string{
+							"k": "l",
+						},
+					},
+					ServiceAccount: &gw2_v1alpha1.ServiceAccount{
+						ExtraLabels: map[string]string{
+							"m": "n",
+						},
+						ExtraAnnotations: map[string]string{
+							"o": "p",
+						},
+					},
+				},
+			},
+		}
+		out := deepMergeGatewayParameters(dst, src)
+		Expect(out.Spec.Kube.PodTemplate.ExtraAnnotations).To(Equal(map[string]string{
+			"a": "b",
+			"i": "j",
+		}))
+		Expect(out.Spec.Kube.Service.ExtraAnnotations).To(Equal(map[string]string{
+			"c": "d",
+			"k": "l",
+		}))
+		Expect(out.Spec.Kube.ServiceAccount.ExtraLabels).To(Equal(map[string]string{
+			"e": "f",
+			"m": "n",
+		}))
+		Expect(out.Spec.Kube.ServiceAccount.ExtraAnnotations).To(Equal(map[string]string{
+			"g": "h",
+			"o": "p",
+		}))
+	})
 })
