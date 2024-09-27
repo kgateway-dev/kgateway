741fa3071d22723a584a86bf7c3cb1e5eac3aed8
Jenny Shu <28537278+jenshu@users.noreply.github.com>
2024-09-27 13:35:11 -0400

    k8s gateway: make ServiceAccount labels/annotations configurable (#10094)
    Co-authored-by: soloio-bulldozer[bot] <48420018+soloio-bulldozer[bot]@users.noreply.github.com>
    Co-authored-by: changelog-bot <changelog-bot>

--- install/test/k8sgateway_test.go
+++ install/test/k8sgateway_test.go
@@ -111,6 +111,8 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 
 				Expect(*gwpKube.GetService().GetType()).To(Equal(corev1.ServiceTypeLoadBalancer))
 
+				Expect(gwpKube.GetServiceAccount()).To(BeNil())
+
 				Expect(*gwpKube.GetStats().GetEnabled()).To(BeTrue())
 				Expect(*gwpKube.GetStats().GetRoutePrefixRewrite()).To(Equal("/stats/prometheus"))
 				Expect(*gwpKube.GetStats().GetEnableStatsRoute()).To(BeTrue())
@@ -146,6 +148,8 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 						fmt.Sprintf("kubeGateway.gatewayParameters.glooGateway.envoyContainer.resources.limits.cpu=%s", envoyLimits["cpu"].ToUnstructured()),
 						"kubeGateway.gatewayParameters.glooGateway.proxyDeployment.replicas=5",
 						"kubeGateway.gatewayParameters.glooGateway.service.type=ClusterIP",
+						"kubeGateway.gatewayParameters.glooGateway.serviceAccount.extraLabels.label1=a",
+						"kubeGateway.gatewayParameters.glooGateway.serviceAccount.extraAnnotations.anno1=b",
 						"kubeGateway.gatewayParameters.glooGateway.sdsContainer.image.tag=sds-override-tag",
 						"kubeGateway.gatewayParameters.glooGateway.sdsContainer.image.registry=sds-override-registry",
 						"kubeGateway.gatewayParameters.glooGateway.sdsContainer.image.repository=sds-override-repository",
@@ -238,6 +242,9 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 
 					Expect(*gwpKube.GetService().GetType()).To(Equal(corev1.ServiceTypeClusterIP))
 
+					Expect(gwpKube.GetServiceAccount().GetExtraLabels()).To(matchers.ContainMapElements(map[string]string{"label1": "a"}))
+					Expect(gwpKube.GetServiceAccount().GetExtraAnnotations()).To(matchers.ContainMapElements(map[string]string{"anno1": "b"}))
+
 					Expect(*gwpKube.GetStats().GetEnabled()).To(BeFalse())
 					Expect(*gwpKube.GetStats().GetRoutePrefixRewrite()).To(Equal("/foo/bar"))
 					Expect(*gwpKube.GetStats().GetEnableStatsRoute()).To(BeFalse())
@@ -260,6 +267,8 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 						"kubeGateway.gatewayParameters.glooGateway.envoyContainer.image.pullPolicy=Always",
 						"kubeGateway.gatewayParameters.glooGateway.proxyDeployment.replicas=5",
 						"kubeGateway.gatewayParameters.glooGateway.service.type=ClusterIP",
+						"kubeGateway.gatewayParameters.glooGateway.serviceAccount.extraLabels.label1=a",
+						"kubeGateway.gatewayParameters.glooGateway.serviceAccount.extraAnnotations.anno1=b",
 						"kubeGateway.gatewayParameters.glooGateway.sdsContainer.image.tag=sds-override-tag",
 						"kubeGateway.gatewayParameters.glooGateway.sdsContainer.image.registry=sds-override-registry",
 						"kubeGateway.gatewayParameters.glooGateway.sdsContainer.image.repository=sds-override-repository",
@@ -306,6 +315,9 @@ var _ = Describe("Kubernetes Gateway API integration", func() {
 					Expect(*gwpKube.GetSdsContainer().GetBootstrap().GetLogLevel()).To(Equal("debug"))
 
 					Expect(*gwpKube.GetService().GetType()).To(Equal(corev1.ServiceTypeClusterIP))
+
+					Expect(gwpKube.GetServiceAccount().GetExtraLabels()).To(matchers.ContainMapElements(map[string]string{"label1": "a"}))
+					Expect(gwpKube.GetServiceAccount().GetExtraAnnotations()).To(matchers.ContainMapElements(map[string]string{"anno1": "b"}))
 				})
 			})
 
