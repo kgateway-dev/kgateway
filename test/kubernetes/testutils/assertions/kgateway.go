a6b4f37de769332e2f4345a6c0cc1ce0abb04c39
Jenny Shu <jenny.shu@solo.io>
2025-02-19 14:33:45 -0500

    revert the kgateway check
    Signed-off-by: Jenny Shu <jenny.shu@solo.io>

--- test/kubernetes/testutils/assertions/kgateway.go
+++ test/kubernetes/testutils/assertions/kgateway.go
@@ -3,19 +3,16 @@ package assertions
 import (
 	"context"
 
-	"github.com/onsi/gomega"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/kgateway-dev/kgateway/v2/pkg/utils/kubeutils"
 )
 
 func (p *Provider) EventuallyKgatewayInstallSucceeded(ctx context.Context) {
 	p.expectInstallContextDefined()
 
-	p.EventuallyReadyReplicas(ctx, metav1.ObjectMeta{
-		Name:      kubeutils.GlooDeploymentName,
-		Namespace: p.installContext.InstallNamespace,
-	}, gomega.Equal(1))
+	p.EventuallyPodsRunning(ctx, p.installContext.InstallNamespace,
+		metav1.ListOptions{
+			LabelSelector: "app.kubernetes.io/name=kgateway",
+		})
 }
 
 func (p *Provider) EventuallyKgatewayUninstallSucceeded(ctx context.Context) {
@@ -30,8 +27,8 @@ func (p *Provider) EventuallyKgatewayUninstallSucceeded(ctx context.Context) {
 func (p *Provider) EventuallyKgatewayUpgradeSucceeded(ctx context.Context, version string) {
 	p.expectInstallContextDefined()
 
-	p.EventuallyReadyReplicas(ctx, metav1.ObjectMeta{
-		Name:      kubeutils.GlooDeploymentName,
-		Namespace: p.installContext.InstallNamespace,
-	}, gomega.Equal(1))
+	p.EventuallyPodsRunning(ctx, p.installContext.InstallNamespace,
+		metav1.ListOptions{
+			LabelSelector: "app.kubernetes.io/name=kgateway",
+		})
 }
