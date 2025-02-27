26b05a280c492f45914f790bb39a61decc4a8b05
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-02-27 13:06:37 -0500

    adding bind addresses as fields in setupOpts

--- internal/kgateway/controller/start.go
+++ internal/kgateway/controller/start.go
@@ -52,9 +52,9 @@ type SetupOpts struct {
 	// static set of global Settings
 	GlobalSettings *settings.Settings
 
-	// If running from env tests, set to true. so we won't bind to any ports
-	// and tests can run in parallel.
-	Test bool
+	PprofBindAddress       string
+	HealthProbeBindAddress string
+	MetricsBindAddress     string
 }
 
 var setupLog = ctrl.Log.WithName("setup")
@@ -104,24 +104,14 @@ func NewControllerBuilder(ctx context.Context, cfg StartConfig) (*ControllerBuil
 		return nil, err
 	}
 
-	pprofBindAddress := "127.0.0.1:9099"
-	healthProbeBindAddress := ":9093"
-	metricsBindAddress := ":9092"
-
-	if cfg.SetupOpts.Test {
-		pprofBindAddress = ""
-		healthProbeBindAddress = ""
-		metricsBindAddress = ""
-	}
-
 	mgrOpts := ctrl.Options{
 		BaseContext:      func() context.Context { return ctx },
 		Scheme:           scheme,
-		PprofBindAddress: pprofBindAddress,
+		PprofBindAddress: cfg.SetupOpts.PprofBindAddress,
 		// if you change the port here, also change the port "health" in the helmchart.
-		HealthProbeBindAddress: healthProbeBindAddress,
+		HealthProbeBindAddress: cfg.SetupOpts.HealthProbeBindAddress,
 		Metrics: metricsserver.Options{
-			BindAddress: metricsBindAddress,
+			BindAddress: cfg.SetupOpts.MetricsBindAddress,
 		},
 		Controller: config.Controller{
 			// see https://github.com/kubernetes-sigs/controller-runtime/issues/2937
