415383bb6fe60fbe50a576dd649bef1d5f22c53c
omar <omar.hammami@solo.io>
2025-02-26 22:20:28 -0600

    reenable httproute tests

--- test/kubernetes/e2e/tests/kgateway_tests.go
+++ test/kubernetes/e2e/tests/kgateway_tests.go
@@ -15,7 +15,7 @@ import (
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/port_routing"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/route_delegation"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/route_options"
-	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/services/httproute"
+	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/services/httproute"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/services/tcproute"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/upstreams"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/virtualhost_options"
@@ -32,7 +32,7 @@ func KubeGatewaySuiteRunner() e2e.SuiteRunner {
 	// kubeGatewaySuiteRunner.Register("RouteOptions", route_options.NewTestingSuite)
 	// kubeGatewaySuiteRunner.Register("VirtualHostOptions", virtualhost_options.NewTestingSuite)
 	// kubeGatewaySuiteRunner.Register("Upstreams", upstreams.NewTestingSuite)
-	// kubeGatewaySuiteRunner.Register("HTTPRouteServices", httproute.NewTestingSuite)
+	kubeGatewaySuiteRunner.Register("HTTPRouteServices", httproute.NewTestingSuite)
 	kubeGatewaySuiteRunner.Register("TCPRouteServices", tcproute.NewTestingSuite)
 	// kubeGatewaySuiteRunner.Register("HeadlessSvc", headless_svc.NewK8sGatewayHeadlessSvcSuite)
 	// kubeGatewaySuiteRunner.Register("PortRouting", port_routing.NewK8sGatewayTestingSuite)
