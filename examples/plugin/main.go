0d3253620167a440a16bc48ec92d85e8689ec092
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-03-10 19:15:24 -0400

    StartKgateway

--- examples/plugin/main.go
+++ examples/plugin/main.go
@@ -242,5 +242,5 @@ func main() {
 	// This demonstrates how to start Kgateway with a custom plugin.
 	// This binary is the control plane. normally it would be packaged in a docker image and run
 	// in a k8s cluster.
-	setup.StartGGv2(context.Background(), pluginFactory, nil)
+	setup.StartKgateway(context.Background(), pluginFactory, nil)
 }
