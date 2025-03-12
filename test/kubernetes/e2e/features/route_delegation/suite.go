cf3b93dbcf701e43c8c91bb7d3533c26ee6d2d30
Jenny Shu <jenny.shu@solo.io>
2025-03-12 15:43:59 -0400

    route delegation flakes
    Signed-off-by: Jenny Shu <jenny.shu@solo.io>

--- test/kubernetes/e2e/features/route_delegation/suite.go
+++ test/kubernetes/e2e/features/route_delegation/suite.go
@@ -2,11 +2,13 @@ package route_delegation
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 	"time"
 
 	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/suite"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 
@@ -54,7 +56,7 @@ func (s *tsuite) SetupSuite() {
 	// Not every resource that is applied needs to be verified. We are not testing `kubectl apply`,
 	// but the below code demonstrates how it can be done if necessary
 	s.manifestObjects = map[string][]client.Object{
-		commonManifest:                      {proxyService, proxyDeployment, defaults.CurlPod, httpbinTeam1, httpbinTeam2, gateway},
+		commonManifest:                      {proxyService, proxyDeployment, httpbinTeam1, httpbinTeam2, gateway},
 		basicRoutesManifest:                 {routeRoot, routeTeam1, routeTeam2},
 		cyclicRoutesManifest:                {routeRoot, routeTeam1, routeTeam2},
 		recursiveRoutesManifest:             {routeRoot, routeTeam1, routeTeam2},
@@ -65,10 +67,21 @@ func (s *tsuite) SetupSuite() {
 		unresolvedChildManifest:             {routeRoot},
 		matcherInheritanceManifest:          {routeParent1, routeParent2, routeTeam1},
 	}
+
+	// set up curl once
+	err := s.ti.Actions.Kubectl().ApplyFile(s.ctx, defaults.CurlPodManifest)
+	s.Require().NoError(err, "can apply curl pod manifest")
+	s.ti.Assertions.EventuallyObjectsExist(s.ctx, defaults.CurlPod)
+	s.ti.Assertions.EventuallyPodsRunning(s.ctx, defaults.CurlPod.GetNamespace(), metav1.ListOptions{
+		LabelSelector: defaults.CurlPodLabelSelector,
+	})
 }
 
 func (s *tsuite) TearDownSuite() {
-	// nothing at the moment
+	// clean up curl
+	err := s.ti.Actions.Kubectl().DeleteFileSafe(s.ctx, defaults.CurlPodManifest)
+	s.Require().NoError(err, "can delete curl pod manifest")
+	s.ti.Assertions.EventuallyObjectsNotExist(s.ctx, defaults.CurlPod)
 }
 
 func (s *tsuite) BeforeTest(suiteName, testName string) {
@@ -78,6 +91,11 @@ func (s *tsuite) BeforeTest(suiteName, testName string) {
 		s.Require().NoError(err)
 		s.ti.Assertions.EventuallyObjectsExist(s.ctx, s.manifestObjects[manifest]...)
 	}
+
+	// make sure the proxy pods are running
+	s.ti.Assertions.EventuallyPodsRunning(s.ctx, proxyMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s", proxyMeta.GetName()),
+	})
 }
 
 func (s *tsuite) AfterTest(suiteName, testName string) {
@@ -87,6 +105,11 @@ func (s *tsuite) AfterTest(suiteName, testName string) {
 		s.Require().NoError(err)
 		s.ti.Assertions.EventuallyObjectsNotExist(s.ctx, s.manifestObjects[manifest]...)
 	}
+
+	// make sure the proxy pods are gone before the next test starts
+	s.ti.Assertions.EventuallyPodsNotExist(s.ctx, proxyMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app.kubernetes.io/name=%s", proxyMeta.GetName()),
+	})
 }
 
 func (s *tsuite) TestBasic() {
