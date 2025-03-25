c636943002d100d7d97a076ebfa415981e435bb0
Steven Landow <steven@landow.dev>
2025-03-24 17:59:17 -0700

    ServiceEntry support (#10870)
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/translator/gateway/testutils/test_translator.go
+++ internal/kgateway/translator/gateway/testutils/test_translator.go
@@ -217,6 +217,8 @@ func (tc TestCase) Run(t test.Failer, ctx context.Context) (map[types.Namespaced
 		gvr.Pod,
 		gvr.TCPRoute,
 		gvr.TLSRoute,
+		gvr.ServiceEntry,
+		gvr.WorkloadEntry,
 	} {
 		clienttest.MakeCRD(t, cli, crd)
 	}
