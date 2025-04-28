0ffc5038f3a394a74354a2ce20d93d553209cc8f
Shashank Ram <shashank.ram@solo.io>
2025-04-28 09:15:25 -0700

    status: correctly merge parentRefs for routes
    When a route has multiple parentRefs, the exitsing status
    merging code does not correctly merge parentRefs belonging
    to different proxies. As a result, a route with multiple
    parents does not have both parents in their status.
    
    This change fixes it.
    
    Signed-off-by: Shashank Ram <shashank.ram@solo.io>

--- internal/kgateway/proxy_syncer/proxy_syncer.go
+++ internal/kgateway/proxy_syncer/proxy_syncer.go
@@ -265,9 +265,9 @@ func mergeProxyReports(
 				merged.HTTPRoutes[rnn] = rr
 				continue
 			}
-			// else, let's merge our parentRefs into the existing map
-			// obsGen will stay as-is...
-			maps.Copy(p.reports.HTTPRoutes[rnn].Parents, rr.Parents)
+			// else, this route has already been seen for a proxy, merge this proxy's parents
+			// into the merged report
+			maps.Copy(merged.HTTPRoutes[rnn].Parents, rr.Parents)
 		}
 
 		// 3. merge tcproute parentRefs into RouteReports
@@ -278,9 +278,9 @@ func mergeProxyReports(
 				merged.TCPRoutes[rnn] = rr
 				continue
 			}
-			// else, let's merge our parentRefs into the existing map
-			// obsGen will stay as-is...
-			maps.Copy(p.reports.TCPRoutes[rnn].Parents, rr.Parents)
+			// else, this route has already been seen for a proxy, merge this proxy's parents
+			// into the merged report
+			maps.Copy(merged.TCPRoutes[rnn].Parents, rr.Parents)
 		}
 
 		for rnn, rr := range p.reports.TLSRoutes {
@@ -290,9 +290,9 @@ func mergeProxyReports(
 				merged.TLSRoutes[rnn] = rr
 				continue
 			}
-			// else, let's merge our parentRefs into the existing map
-			// obsGen will stay as-is...
-			maps.Copy(p.reports.TLSRoutes[rnn].Parents, rr.Parents)
+			// else, this route has already been seen for a proxy, merge this proxy's parents
+			// into the merged report
+			maps.Copy(merged.TLSRoutes[rnn].Parents, rr.Parents)
 		}
 	}
 
