04c98ba37e434a2f53bf75893b82a4abae93d3dc
omar <omar.hammami@solo.io>
2025-05-07 10:24:25 -0500

    remove unused types
    Signed-off-by: omar <omar.hammami@solo.io>

--- test/kubernetes/e2e/features/services/grpcroute/types.go
+++ test/kubernetes/e2e/features/services/grpcroute/types.go
@@ -7,8 +7,6 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	v1 "sigs.k8s.io/gateway-api/apis/v1"
-	v1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
 
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/fsutils"
 )
@@ -18,18 +16,15 @@ var (
 	setupManifest = filepath.Join(fsutils.MustGetThisDir(), "testdata", "setup.yaml")
 
 	// Resource names
-	gatewayName       = "gw"
-	grpcRouteName     = "grpc-route"
-	grpcSvcName       = "grpc-echo-svc"
-	grpcDeployName    = "grpc-echo"
-	grpcContainerName = "grpc-echo"
+	gatewayName    = "gw"
+	grpcRouteName  = "grpc-route"
+	grpcSvcName    = "grpc-echo-svc"
+	grpcDeployName = "grpc-echo"
 
 	// Timeouts
 	timeout    = 1 * time.Minute
 	ctxTimeout = 5 * time.Minute
 
-	// Ports
-	grpcPort    = 9000
 	gatewayPort = 8080
 
 	// Test namespace
@@ -42,24 +37,11 @@ var (
 
 // Gateway resources
 var (
-	gateway = &v1.Gateway{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      gatewayName,
-			Namespace: testNamespace,
-		},
-	}
 	gatewayObjectMeta = metav1.ObjectMeta{
 		Name:      "gw",
 		Namespace: "default",
 	}
 	gatewayService = &corev1.Service{ObjectMeta: gatewayObjectMeta}
-
-	grpcRoute = &v1alpha2.GRPCRoute{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      grpcRouteName,
-			Namespace: testNamespace,
-		},
-	}
 
 	grpcEchoDeployment = &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
