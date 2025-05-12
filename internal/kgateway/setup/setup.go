b5500af5eb456eb49a47f3d6d7cc9c283408c62e
omar <omar.hammami@solo.io>
2025-05-12 11:48:14 -0500

    nvm, still need to reset log level
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/setup/setup.go
+++ internal/kgateway/setup/setup.go
@@ -160,6 +160,8 @@ func setupLogging(levelStr string) {
 	controllerLogger := logging.New("controllerruntime")
 	logrSink := logr.FromSlogHandler(controllerLogger.Handler())
 	ctrl.SetLogger(logrSink)
+	// set all loggers to the specified level
+	logging.Reset(level)
 }
 
 func createKubeClient(restConfig *rest.Config) (istiokube.Client, error) {
