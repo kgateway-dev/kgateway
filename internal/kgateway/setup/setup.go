e7235a6d12d92b8d1d4c4e599c669c26a4a105a7
omar <omar.hammami@solo.io>
2025-05-12 00:39:09 -0500

    add comment to clarify logger level setup in setup.go
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/setup/setup.go
+++ internal/kgateway/setup/setup.go
@@ -160,6 +160,7 @@ func setupLogging(levelStr string) {
 	controllerLogger := logging.New("controllerruntime")
 	logrSink := logr.FromSlogHandler(controllerLogger.Handler())
 	ctrl.SetLogger(logrSink)
+	// set all loggers to the specified level
 	logging.Reset(level)
 }
 
