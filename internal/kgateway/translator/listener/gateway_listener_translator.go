6d524a8532d88ce3adf0412d6d54066199c9276e
Omar Hammami <58956785+puertomontt@users.noreply.github.com>
2025-05-15 21:58:04 -0500

    rename logger
    Co-authored-by: Shashank Ram <21697719+shashankram@users.noreply.github.com>
    Signed-off-by: Omar Hammami <58956785+puertomontt@users.noreply.github.com>

--- internal/kgateway/translator/listener/gateway_listener_translator.go
+++ internal/kgateway/translator/listener/gateway_listener_translator.go
@@ -28,7 +28,7 @@ import (
 	reports "github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/reporter"
 )
 
-var logger = logging.New("listener")
+var logger = logging.New("translator/listener")
 
 // TranslateListeners translates the set of gloo listeners required to produce a full output proxy (either form one Gateway or multiple merged Gateways)
 func TranslateListeners(
