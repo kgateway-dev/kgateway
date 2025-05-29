kgateway-dev/kgateway

commit 65aa25533f0e8962217d512343a31cf4db4ee44d

feat: support section name in policies, and add tests for all attachment types. (#11272)
Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

internal/kgateway/krtcollections/policy.go
@@ -305,12 +305,12 @@ type targetRefIndexKey struct {
 	Group       string
 	Kind        string
 	Name        string
-	SectionName string
 	Namespace   string
+	SectionName string
 }
 
 func (k targetRefIndexKey) String() string {
-	return fmt.Sprintf("%s/%s/%s/%s", k.Group, k.Kind, k.Name, k.Namespace)
+	return fmt.Sprintf("%s/%s/%s/%s/%s", k.Group, k.Kind, k.Name, k.Namespace, k.SectionName)
 }
 
 // HTTPRouteSelector is used to lookup HttpRouteIR using one of the following ways:
@@ -529,32 +529,24 @@ func (p *PolicyIndex) getTargetingPoliciesMaybeForBackends(
 
 	// no need for ref grants here as target refs are namespace local
 	refIndexKey := targetRefIndexKey{
-		Group:     targetRef.Group,
-		Kind:      targetRef.Kind,
-		Name:      targetRef.Name,
-		Namespace: targetRef.Namespace,
+		Group:       targetRef.Group,
+		Kind:        targetRef.Kind,
+		Name:        targetRef.Name,
+		Namespace:   targetRef.Namespace,
+		SectionName: sectionName,
 	}
+
 	policies := p.fetchByTargetRef(kctx, refIndexKey, onlyBackends)
-	var sectionNamePolicies []ir.PolicyWrapper
-	if sectionName != "" {
-		refIndexKey.SectionName = sectionName
-		sectionNamePolicies = p.fetchByTargetRef(kctx, refIndexKey, onlyBackends)
-	}
 	// Lookup policies that select targetLabels
 	if len(targetLabels) > 0 {
 		refIndexKeyByNamespace := targetRefIndexKey{
-			Group:     targetRef.Group,
-			Kind:      targetRef.Kind,
-			Namespace: targetRef.Namespace,
+			Group:       targetRef.Group,
+			Kind:        targetRef.Kind,
+			Namespace:   targetRef.Namespace,
+			SectionName: sectionName,
 		}
 		policiesByLabel := p.fetchByTargetRefLabels(kctx, refIndexKeyByNamespace, onlyBackends, targetLabels)
 		policies = append(policies, policiesByLabel...)
-		var sectionNamePoliciesByLabel []ir.PolicyWrapper
-		if sectionName != "" {
-			refIndexKeyByNamespace.SectionName = sectionName
-			sectionNamePoliciesByLabel = p.fetchByTargetRefLabels(kctx, refIndexKeyByNamespace, onlyBackends, targetLabels)
-		}
-		sectionNamePolicies = append(sectionNamePolicies, sectionNamePoliciesByLabel...)
 	}
 
 	for _, p := range policies {
@@ -562,19 +554,6 @@ func (p *PolicyIndex) getTargetingPoliciesMaybeForBackends(
 			Generation: p.Policy.GetGeneration(),
 			GroupKind:  p.GetGroupKind(),
 			PolicyIr:   p.PolicyIR,
-			PolicyRef: &ir.AttachedPolicyRef{
-				Group:     p.Group,
-				Kind:      p.Kind,
-				Name:      p.Name,
-				Namespace: p.Namespace,
-			},
-			Errors: p.Errors,
-		})
-	}
-	for _, p := range sectionNamePolicies {
-		ret = append(ret, ir.PolicyAtt{
-			GroupKind: p.GetGroupKind(),
-			PolicyIr:  p.PolicyIR,
 			PolicyRef: &ir.AttachedPolicyRef{
 				Group:       p.Group,
 				Kind:        p.Kind,
@@ -585,6 +564,7 @@ func (p *PolicyIndex) getTargetingPoliciesMaybeForBackends(
 			Errors: p.Errors,
 		})
 	}
+
 	slices.SortFunc(ret, func(a, b ir.PolicyAtt) int {
 		return a.PolicyIr.CreationTime().Compare(b.PolicyIr.CreationTime())
 	})

internal/kgateway/translator/irtranslator/route.go
@@ -13,7 +13,6 @@ import (
 	"google.golang.org/protobuf/types/known/anypb"
 	"google.golang.org/protobuf/types/known/wrapperspb"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
@@ -25,9 +24,10 @@ import (
 )
 
 type httpRouteConfigurationTranslator struct {
-	gw       ir.GatewayIR
-	listener ir.ListenerIR
-	fc       ir.FilterChainCommon
+	gw               ir.GatewayIR
+	listener         ir.ListenerIR
+	fc               ir.FilterChainCommon
+	attachedPolicies ir.AttachedPolicies
 
 	routeConfigName          string
 	reporter                 reportssdk.Reporter
@@ -37,17 +37,33 @@ type httpRouteConfigurationTranslator struct {
 }
 
 func (h *httpRouteConfigurationTranslator) ComputeRouteConfiguration(ctx context.Context, vhosts []*ir.VirtualHost) *envoy_config_route_v3.RouteConfiguration {
+	var attachedPolicies ir.AttachedPolicies
+	// the policies in order - first listener as they are more specific and thus higher priority.
+	// then gateway policies.
+	attachedPolicies.Append(h.attachedPolicies, h.gw.AttachedHttpPolicies)
 	cfg := &envoy_config_route_v3.RouteConfiguration{
 		Name: h.routeConfigName,
-		//		MaxDirectResponseBodySizeBytes: h.parentListener.GetRouteOptions().GetMaxDirectResponseBodySizeBytes(),
 	}
-	for _, pass := range h.PluginPass {
+	typedPerFilterConfigRoute := ir.TypedFilterConfigMap(map[string]proto.Message{})
+
+	for gk, pols := range attachedPolicies.Policies {
+		pass := h.PluginPass[gk]
 		if pass == nil {
+			// TODO: user error - they attached a non http policy
 			continue
 		}
-		pass.ApplyRouteConfigPlugin(ctx, &ir.RouteConfigContext{}, cfg)
+		for _, pol := range mergePolicies(pass, pols) {
+			reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
+			pass.ApplyRouteConfigPlugin(ctx, &ir.RouteConfigContext{
+				FilterChainName:   h.fc.FilterChainName,
+				TypedFilterConfig: typedPerFilterConfigRoute,
+				Policy:            pol.PolicyIr,
+			}, cfg)
+		}
 	}
+
 	cfg.VirtualHosts = h.computeVirtualHosts(ctx, vhosts)
+	cfg.TypedPerFilterConfig = toPerFilterConfigMap(typedPerFilterConfigRoute)
 
 	// Gateway API spec requires that port values in HTTP Host headers be ignored when performing a match
 	// See https://gateway-api.sigs.k8s.io/reference/spec/#gateway.networking.k8s.io/v1.HTTPRouteSpec - hostnames field
@@ -109,7 +125,7 @@ func (h *httpRouteConfigurationTranslator) computeVirtualHost(
 
 	typedPerFilterConfigRoute := ir.TypedFilterConfigMap(map[string]proto.Message{})
 	// run the http plugins that are attached to the listener or gateway on the virtual host
-	h.runVhostPlugins(ctx, out, typedPerFilterConfigRoute)
+	h.runVhostPlugins(ctx, virtualHost, out, typedPerFilterConfigRoute)
 	out.TypedPerFilterConfig = toPerFilterConfigMap(typedPerFilterConfigRoute)
 
 	return out
@@ -186,28 +202,23 @@ func toPerFilterConfigMap(typedPerFilterConfig ir.TypedFilterConfigMap) map[stri
 	return typedPerFilterConfigAny
 }
 
-func (h *httpRouteConfigurationTranslator) runVhostPlugins(ctx context.Context, out *envoy_config_route_v3.VirtualHost,
+func (h *httpRouteConfigurationTranslator) runVhostPlugins(ctx context.Context, virtualHost *ir.VirtualHost, out *envoy_config_route_v3.VirtualHost,
 	typedPerFilterConfig ir.TypedFilterConfigMap) {
-	attachedPoliciesSlice := []ir.AttachedPolicies{
-		h.gw.AttachedHttpPolicies,
-		h.listener.AttachedPolicies,
-	}
-	for _, attachedPolicies := range attachedPoliciesSlice {
-		for gk, pols := range attachedPolicies.Policies {
-			pass := h.PluginPass[gk]
-			if pass == nil {
-				// TODO: user error - they attached a non http policy
-				continue
+	for gk, pols := range virtualHost.AttachedPolicies.Policies {
+		pass := h.PluginPass[gk]
+		if pass == nil {
+			// TODO: user error - they attached a non http policy
+			continue
+		}
+		for _, pol := range mergePolicies(pass, pols) {
+			reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
+			pctx := &ir.VirtualHostContext{
+				Policy:            pol.PolicyIr,
+				TypedFilterConfig: typedPerFilterConfig,
+				FilterChainName:   h.fc.FilterChainName,
 			}
-			for _, pol := range pols {
-				reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
-				pctx := &ir.VirtualHostContext{
-					Policy:            pol.PolicyIr,
-					TypedFilterConfig: typedPerFilterConfig,
-				}
-				pass.ApplyVhostPlugin(ctx, pctx, out)
-				// TODO: check return value, if error returned, log error and report condition
-			}
+			pass.ApplyVhostPlugin(ctx, pctx, out)
+			// TODO: check return value, if error returned, log error and report condition
 		}
 	}
 }
@@ -224,9 +235,7 @@ func (h *httpRouteConfigurationTranslator) runRoutePlugins(
 	// NOTE: AttachedPolicies must have policies in the ordered by hierarchy from root to leaf in the delegation chain where
 	// each level has policies ordered by rule level policies before entire route level policies.
 
-	attachedPolicies := ir.AttachedPolicies{
-		Policies: map[schema.GroupKind][]ir.PolicyAtt{},
-	}
+	var attachedPolicies ir.AttachedPolicies
 	delegatingParent := in.DelegatingParent
 	var hierarchicalPriority int
 	for delegatingParent != nil {
@@ -252,7 +261,6 @@ func (h *httpRouteConfigurationTranslator) runRoutePlugins(
 			errs = append(errs, err)
 		}
 	}
-
 	for gk, pols := range attachedPolicies.Policies {
 		pass := h.PluginPass[gk]
 		if pass == nil {
@@ -265,10 +273,7 @@ func (h *httpRouteConfigurationTranslator) runRoutePlugins(
 			In:                in,
 			TypedFilterConfig: typedPerFilterConfig,
 		}
-		if pass.MergePolicies != nil {
-			pols = []ir.PolicyAtt{pass.MergePolicies(pols)}
-		}
-		for _, pol := range pols {
+		for _, pol := range mergePolicies(pass, pols) {
 			// TODO: should we append pol.Error to errs?
 			// i.e. errs = append(errs, pol.Error)
 			pctx.Policy = pol.PolicyIr
@@ -291,6 +296,15 @@ func (h *httpRouteConfigurationTranslator) runRoutePlugins(
 	return err
 }
 
+func mergePolicies(pass *TranslationPass, policies []ir.PolicyAtt) []ir.PolicyAtt {
+	if pass.MergePolicies != nil {
+		merged := [1]ir.PolicyAtt{pass.MergePolicies(policies)}
+		return merged[:]
+	}
+
+	return policies
+}
+
 func (h *httpRouteConfigurationTranslator) runBackendPolicies(ctx context.Context, in ir.HttpBackend, pCtx *ir.RouteBackendContext) error {
 	var errs []error
 	for gk, pols := range in.AttachedPolicies.Policies {
@@ -299,7 +313,7 @@ func (h *httpRouteConfigurationTranslator) runBackendPolicies(ctx context.Contex
 			// TODO: should never happen, log error and report condition
 			continue
 		}
-		for _, pol := range pols {
+		for _, pol := range mergePolicies(pass, pols) {
 			reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pol)
 			// Policy on extension ref
 			err := pass.ApplyForRouteBackend(ctx, pol.PolicyIr, pCtx)

internal/kgateway/extensions2/pluginutils/policy.go
@@ -3,6 +3,8 @@ package pluginutils
 import (
 	"maps"
 
+	"k8s.io/utils/ptr"
+
 	"github.com/kgateway-dev/kgateway/v2/api/v1alpha1"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
 )
@@ -14,9 +16,10 @@ func TargetRefsToPolicyRefs(
 	refs := make([]ir.PolicyRef, 0, len(targetRefs)+len(targetSelectors))
 	for _, targetRef := range targetRefs {
 		refs = append(refs, ir.PolicyRef{
-			Group: string(targetRef.Group),
-			Kind:  string(targetRef.Kind),
-			Name:  string(targetRef.Name),
+			Group:       string(targetRef.Group),
+			Kind:        string(targetRef.Kind),
+			Name:        string(targetRef.Name),
+			SectionName: string(ptr.Deref(targetRef.SectionName, "")),
 		})
 	}
 	for _, targetSelector := range targetSelectors {

commit 221550eb07a1f799be17a4c5aae8ffc75e5c0031

policy: propagate ExtensionRef errors to PolicyAtt and fix reporting
- Fixes a bug with ExtensionRef IR errors not being propagated
  to PolicyAtt.

- Fixes a regression introduced by 65aa25533 where the reporting
  must happen on indidual policies and not on the merged policy.

Signed-off-by: Shashank Ram <shashank.ram@solo.io>

internal/kgateway/krtcollections/policy.go
@@ -31,6 +31,7 @@ import (
 var (
 	ErrMissingReferenceGrant = errors.New("missing reference grant")
 	ErrUnknownBackendKind    = errors.New("unknown backend kind")
+	ErrPolicyNotFound        = errors.New("policy not found")
 )
 
 type NotFoundError struct {
@@ -1058,9 +1059,15 @@ func (h *RoutesIndex) getExtensionRefs(kctx krt.HandlerContext, ns string, r []g
 	}
 	for _, ext := range r {
 		// TODO: propagate error if we can't find the extension
-		gk, policy := h.resolveExtension(kctx, ns, ext)
+		gk, policy, errs := h.resolveExtension(kctx, ns, ext)
 		if policy != nil {
-			ret.Policies[gk] = append(ret.Policies[gk], ir.PolicyAtt{PolicyIr: policy /*direct attachment - no target ref*/})
+			ret.Policies[gk] = append(ret.Policies[gk], ir.PolicyAtt{
+				// direct attachment - no target ref
+				PolicyIr: policy,
+				Errors:   errs,
+			})
+		} else if len(errs) > 0 {
+			logger.Error("unresolved HTTPRouteFilter", "error", errors.Join(errs...))
 		}
 	}
 	return ret
@@ -1077,11 +1084,11 @@ func (h *RoutesIndex) getBuiltInRulePolicies(rule gwv1.HTTPRouteRule) ir.Attache
 	return ret
 }
 
-func (h *RoutesIndex) resolveExtension(kctx krt.HandlerContext, ns string, ext gwv1.HTTPRouteFilter) (schema.GroupKind, ir.PolicyIR) {
+func (h *RoutesIndex) resolveExtension(kctx krt.HandlerContext, ns string, ext gwv1.HTTPRouteFilter) (schema.GroupKind, ir.PolicyIR, []error) {
 	if ext.Type == gwv1.HTTPRouteFilterExtensionRef {
 		if ext.ExtensionRef == nil {
 			// TODO: report error!!
-			return schema.GroupKind{}, nil
+			return schema.GroupKind{}, nil, nil
 		}
 		ref := *ext.ExtensionRef
 		key := ir.ObjectSource{
@@ -1092,15 +1099,14 @@ func (h *RoutesIndex) resolveExtension(kctx krt.HandlerContext, ns string, ext g
 		}
 		policy := h.policies.fetchPolicy(kctx, key)
 		if policy == nil {
-			// TODO: report error!!
-			return schema.GroupKind{}, nil
+			return schema.GroupKind{}, nil, []error{ErrPolicyNotFound}
 		}
 
 		gk := schema.GroupKind{
 			Group: string(ref.Group),
 			Kind:  string(ref.Kind),
 		}
-		return gk, policy.PolicyIR
+		return gk, policy.PolicyIR, policy.Errors
 	}
 
 	fromGK := schema.GroupKind{
@@ -1108,7 +1114,8 @@ func (h *RoutesIndex) resolveExtension(kctx krt.HandlerContext, ns string, ext g
 		Kind:  "HTTPRoute",
 	}
 
-	return pluginsdkir.VirtualBuiltInGK, NewBuiltInIr(kctx, ext, fromGK, ns, h.refgrants, h.backends)
+	builtinIR := NewBuiltInIr(kctx, ext, fromGK, ns, h.refgrants, h.backends)
+	return pluginsdkir.VirtualBuiltInGK, builtinIR, nil
 }
 
 func toFromBackendRef(fromns string, ref gwv1.BackendObjectReference) ir.ObjectSource {

internal/kgateway/translator/irtranslator/route.go
@@ -53,8 +53,8 @@ func (h *httpRouteConfigurationTranslator) ComputeRouteConfiguration(ctx context
 			// TODO: user error - they attached a non http policy
 			continue
 		}
+		reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
 		for _, pol := range mergePolicies(pass, pols) {
-			reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
 			pass.ApplyRouteConfigPlugin(ctx, &ir.RouteConfigContext{
 				FilterChainName:   h.fc.FilterChainName,
 				TypedFilterConfig: typedPerFilterConfigRoute,
@@ -213,8 +213,8 @@ func (h *httpRouteConfigurationTranslator) runVhostPlugins(ctx context.Context, 
 			// TODO: user error - they attached a non http policy
 			continue
 		}
+		reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
 		for _, pol := range mergePolicies(pass, pols) {
-			reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
 			pctx := &ir.VirtualHostContext{
 				Policy:            pol.PolicyIr,
 				TypedFilterConfig: typedPerFilterConfig,
@@ -318,8 +318,8 @@ func (h *httpRouteConfigurationTranslator) runBackendPolicies(ctx context.Contex
 			// TODO: should never happen, log error and report condition
 			continue
 		}
+		reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pols...)
 		for _, pol := range mergePolicies(pass, pols) {
-			reportPolicyAcceptanceStatus(h.reporter, h.listener.PolicyAncestorRef, pol)
 			// Policy on extension ref
 			err := pass.ApplyForRouteBackend(ctx, pol.PolicyIr, pCtx)
 			if err != nil {