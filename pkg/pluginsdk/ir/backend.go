428775b2a3b55598e097d9c8f878ebcc79212bf6
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-05-30 12:33:35 -0400

    pr comments
    Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

--- pkg/pluginsdk/ir/backend.go
+++ pkg/pluginsdk/ir/backend.go
@@ -107,8 +107,8 @@ type BackendObjectIR struct {
 	// Not added to Equals() as it is derived from the inner ObjIr, which is already evaluated
 	Errors []error
 
-	// Name is the pre-calculated resource name
-	name string
+	// Name is the pre-calculated resource name. used as the krt resource name.
+	resourceName string
 }
 
 // NewBackendObjectIR creates a new BackendObjectIR with pre-calculated resource name
@@ -117,12 +117,12 @@ func NewBackendObjectIR(objSource ObjectSource, port int32, extraKey string) Bac
 		ObjectSource: objSource,
 		Port:         port,
 		ExtraKey:     extraKey,
-		name:         BackendResourceName(objSource, port, extraKey),
+		resourceName: BackendResourceName(objSource, port, extraKey),
 	}
 }
 
 func (c BackendObjectIR) ResourceName() string {
-	return c.name
+	return c.resourceName
 }
 
 func BackendResourceName(objSource ObjectSource, port int32, extraKey string) string {
@@ -141,7 +141,7 @@ func (c BackendObjectIR) Equals(in BackendObjectIR) bool {
 	objEq := c.ObjectSource.Equals(in.ObjectSource)
 	objVersionEq := versionEquals(c.Obj, in.Obj)
 	polEq := c.AttachedPolicies.Equals(in.AttachedPolicies)
-	nameEq := c.name == in.name
+	nameEq := c.resourceName == in.resourceName
 
 	// objIr may currently be nil in the case of k8s Services
 	// TODO: add an IR for Services to avoid the need for this
