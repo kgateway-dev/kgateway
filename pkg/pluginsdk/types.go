85115d9e9d18e17a773c03ad8f8d598083afb922
Steven Landow <steven.landow@solo.io>
2025-06-03 11:21:40 -0700

    fmt Signed-off-by: Steven Landow <steven.landow@solo.io>

--- pkg/pluginsdk/types.go
+++ pkg/pluginsdk/types.go
@@ -30,8 +30,8 @@ func (a AttachmentPoints) Has(p AttachmentPoints) bool {
 
 type (
 	EndpointsInputs = endpoints.EndpointsInputs
-	ProcessBackend func(ctx context.Context, pol ir.PolicyIR, in ir.BackendObjectIR, out *envoy_config_cluster_v3.Cluster)
-	EndpointPlugin func(
+	ProcessBackend  func(ctx context.Context, pol ir.PolicyIR, in ir.BackendObjectIR, out *envoy_config_cluster_v3.Cluster)
+	EndpointPlugin  func(
 		kctx krt.HandlerContext,
 		ctx context.Context,
 		ucc ir.UniqlyConnectedClient,
