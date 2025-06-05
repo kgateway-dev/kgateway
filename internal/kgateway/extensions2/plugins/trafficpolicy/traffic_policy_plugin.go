1714f8f15dfbc7f1480af8eb6495cd18340c029b
timflannagan <timflannagan@gmail.com>
2025-06-05 05:17:01 -0400

    trafficpolicy: Move util related functions to a dedicated utils.go file
    Signed-off-by: timflannagan <timflannagan@gmail.com>

--- internal/kgateway/extensions2/plugins/trafficpolicy/traffic_policy_plugin.go
+++ internal/kgateway/extensions2/plugins/trafficpolicy/traffic_policy_plugin.go
@@ -147,21 +147,6 @@ func (d *TrafficPolicy) Equals(in any) bool {
 	return true
 }
 
-type ProviderNeededMap struct {
-	// map filterhcain name -> providername -> provider
-	Providers map[string]map[string]*TrafficPolicyGatewayExtensionIR
-}
-
-func (p *ProviderNeededMap) Add(fcn, providerName string, provider *TrafficPolicyGatewayExtensionIR) {
-	if p.Providers == nil {
-		p.Providers = make(map[string]map[string]*TrafficPolicyGatewayExtensionIR)
-	}
-	if p.Providers[fcn] == nil {
-		p.Providers[fcn] = make(map[string]*TrafficPolicyGatewayExtensionIR)
-	}
-	p.Providers[fcn][providerName] = provider
-}
-
 type trafficPolicyPluginGwPass struct {
 	reporter reports.Reporter
 	ir.UnimplementedProxyTranslationPass
@@ -542,21 +527,6 @@ func (p *trafficPolicyPluginGwPass) handlePolicies(fcn string, typedFilterConfig
 
 	// Apply CORS configuration if present
 	p.handleCors(fcn, typedFilterConfig, spec.cors)
-}
-
-func AddDisableFilterIfNeeded(filters []plugins.StagedHttpFilter) []plugins.StagedHttpFilter {
-	for _, f := range filters {
-		if f.Filter.GetName() == extAuthGlobalDisableFilterName {
-			return filters
-		}
-	}
-
-	f := plugins.MustNewStagedFilter(extAuthGlobalDisableFilterName,
-		setMetadataConfig,
-		plugins.BeforeStage(plugins.FaultStage))
-	f.Filter.Disabled = true
-	filters = append(filters, f)
-	return filters
 }
 
 func (p *trafficPolicyPluginGwPass) SupportsPolicyMerge() bool {
