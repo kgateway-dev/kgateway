47d42af1c1981cfec4ba66c8201fded02ae7dc45
Shashank Ram <shashank.ram@solo.io>
2025-06-23 09:59:41 -0700

    http-route: implement weight based sorting
     Description
    - Enables weight based sorting of HTTPRoute rules
      such that routes with higher weights are evaluated
      before routes with lower weights. Routes with the
      same weight use the Gateway API precedence rules.
    
    - Adds a global controller setting to enable the feature,
      which results in HTTP routes having a weight associated
      with them if set using the `kgateway.dev/route-weight`
      annotation.
    
     Change Type
    
    ```
    /kind new_feature
    ```
    
     Changelog
    
    ```release-note
    Enables sorting of HTTPRoutes using weights assigned with the
    kgateway.dev/route-weight annotation when
    KGW_WEIGHTED_ROUTE_PRECEDENCE=true.
    ```
    
    Signed-off-by: Shashank Ram <shashank.ram@solo.io>

--- internal/kgateway/krtcollections/policy.go
+++ internal/kgateway/krtcollections/policy.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"slices"
+	"strconv"
 
 	"istio.io/istio/pkg/config/labels"
 	"istio.io/istio/pkg/kube/krt"
@@ -20,6 +21,7 @@ import (
 	apiannotations "github.com/kgateway-dev/kgateway/v2/api/annotations"
 	apilabels "github.com/kgateway-dev/kgateway/v2/api/labels"
 	extensionsplug "github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2/plugin"
+	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2/settings"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/ir"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/translator/backendref"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/translator/utils"
@@ -815,10 +817,11 @@ func (c RouteWrapper) Equals(in RouteWrapper) bool {
 // MARK: RoutesIndex
 
 type RoutesIndex struct {
-	routes         krt.Collection[RouteWrapper]
-	httpRoutes     krt.Collection[ir.HttpRouteIR]
-	httpBySelector krt.Index[HTTPRouteSelector, ir.HttpRouteIR]
-	byParentRef    krt.Index[targetRefIndexKey, RouteWrapper]
+	routes                  krt.Collection[RouteWrapper]
+	httpRoutes              krt.Collection[ir.HttpRouteIR]
+	httpBySelector          krt.Index[HTTPRouteSelector, ir.HttpRouteIR]
+	byParentRef             krt.Index[targetRefIndexKey, RouteWrapper]
+	weightedRoutePrecedence bool
 
 	policies  *PolicyIndex
 	refgrants *RefGrantIndex
@@ -845,8 +848,9 @@ func NewRoutesIndex(
 	policies *PolicyIndex,
 	backends *BackendIndex,
 	refgrants *RefGrantIndex,
+	globalSettings settings.Settings,
 ) *RoutesIndex {
-	h := &RoutesIndex{policies: policies, refgrants: refgrants, backends: backends}
+	h := &RoutesIndex{policies: policies, refgrants: refgrants, backends: backends, weightedRoutePrecedence: globalSettings.WeightedRoutePrecedence}
 	h.hasSyncedFuncs = append(h.hasSyncedFuncs, httproutes.HasSynced, grpcroutes.HasSynced, tcproutes.HasSynced, tlsroutes.HasSynced)
 	h.httpRoutes = krt.NewCollection(httproutes, h.transformHttpRoute, krtopts.ToOptions("http-routes-with-policy")...)
 	httpRouteCollection := krt.NewCollection(h.httpRoutes, func(kctx krt.HandlerContext, i ir.HttpRouteIR) *RouteWrapper {
@@ -1027,6 +1031,15 @@ func (h *RoutesIndex) transformHttpRoute(kctx krt.HandlerContext, i *gwv1.HTTPRo
 
 	delegationInheritedPolicyPriority := apiannotations.DelegationInheritedPolicyPriorityValue(i.Annotations[apiannotations.DelegationInheritedPolicyPriority])
 
+	var precedenceWeight int32
+	var err error
+	if h.weightedRoutePrecedence {
+		precedenceWeight, err = parseRoutePrecedenceWeight(i.Annotations)
+		if err != nil {
+			logger.Error("error parsing route weight; defaulting to 0", "resource_ref", src, "error", err)
+		}
+	}
+
 	return &ir.HttpRouteIR{
 		ObjectSource: src,
 		SourceObject: i,
@@ -1038,6 +1051,7 @@ func (h *RoutesIndex) transformHttpRoute(kctx krt.HandlerContext, i *gwv1.HTTPRo
 			h.policies.getTargetingPolicies(kctx, extensionsplug.RouteAttachmentPoint, src, "", i.GetLabels()),
 			ir.WithDelegationInheritedPolicyPriority(delegationInheritedPolicyPriority),
 		),
+		PrecedenceWeight: precedenceWeight,
 	}
 }
 
@@ -1321,4 +1335,16 @@ func (i *BackendIndex) normalizeInfPoolBackendPort(
 	correct := gwv1.PortNumber(resolvedPort)
 	ref.Port = &correct
 	return nil
+}
+
+func parseRoutePrecedenceWeight(annotations map[string]string) (int32, error) {
+	val, ok := annotations[apiannotations.RoutePrecedenceWeight]
+	if !ok {
+		return 0, nil
+	}
+	weight, err := strconv.ParseInt(val, 10, 32)
+	if err != nil {
+		return 0, fmt.Errorf("invalid value for annotation %s: %s; must be a valid integer", apiannotations.RoutePrecedenceWeight, val)
+	}
+	return int32(weight), nil
 }
