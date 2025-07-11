9aa169fd84d41dbd23d19565dcc48f480b2ab894
sheidkamp <seth.heidkamp@gmail.com>
2025-07-11 16:46:07 -0400

    Update gateway_parameters.go
    Signed-off-by: sheidkamp <seth.heidkamp@gmail.com>

--- internal/kgateway/deployer/gateway_parameters.go
+++ internal/kgateway/deployer/gateway_parameters.go
@@ -274,6 +274,8 @@ func (k *kGatewayParameters) getValues(gw *api.Gateway, gwParam *v1alpha1.Gatewa
 
 	// The security contexts may need to be updated if floating user ID is set or if privileged ports are used
 	// This may affect both the PodSecurityContext and the SecurityContexts for the containers defined in gwParam
+	// Note: this call may populate the PodSecurityContext and SecurityContext fields in the HelmConfig if they are null,
+	// so this needs to happen before those kubeProxyConfig fields are extracted to local variables.
 	deployer.UpdateSecurityContexts(gwParam.Spec.Kube, vals.Gateway.Ports)
 
 	// extract all the custom values from the GatewayParameters
