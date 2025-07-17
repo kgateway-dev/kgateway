b44cf4e3bd87c48ce2c2277488cb0a07b4254653
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-07-17 14:36:42 -0400

    failed redact
    Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

--- internal/kgateway/proxy_syncer/xdswrapper_test.go
+++ internal/kgateway/proxy_syncer/xdswrapper_test.go
@@ -9,6 +9,7 @@ import (
 	envoycachetypes "github.com/envoyproxy/go-control-plane/pkg/cache/types"
 	envoycache "github.com/envoyproxy/go-control-plane/pkg/cache/v3"
 	"github.com/onsi/gomega"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 
@@ -62,4 +63,55 @@ func TestRedacted(t *testing.T) {
 	s := string(data)
 	expectedJson := `{"Snap":{"Clusters":{"foo":{"transportSocket":{"name":"foo","typedConfig":{"@type":"type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.UpstreamTlsContext","commonTlsContext":{"tlsCertificates":[{"privateKey":{"inlineString":"[REDACTED]"}}]}}}}}},"ProxyKey":""}`
 	g.Expect(s).To(gomega.MatchJSON(expectedJson))
+}
+
+func TestNotCrashing(t *testing.T) {
+	UseDetailedUnmarshalling = true
+	g := gomega.NewWithT(t)
+
+	var a anypb.Any
+	var j protojson.UnmarshalOptions
+	j.Unmarshal([]byte(`{
+      "@type": "type.googleapis.com/envoy.config.cluster.v3.Cluster",
+      "name": "kube_gloo-gateway-system_rate-limiter-gloo-gateway-v2_8083",
+      "type": "EDS",
+      "eds_cluster_config": {
+       "eds_config": {
+        "ads": {},
+        "resource_api_version": "V3"
+       }
+      },
+      "connect_timeout": "5s",
+      "metadata": {},
+      "ignore_health_on_host_removal": true,
+      "typed_extension_protocol_options": {
+       "envoy.extensions.upstreams.http.v3.HttpProtocolOptions": {
+        "@type": "type.googleapis.com/envoy.extensions.upstreams.http.v3.HttpProtocolOptions",
+        "explicit_http_config": {
+         "http2_protocol_options": {}
+        }
+       }
+      }
+     }`), &a)
+	c, err := a.UnmarshalNew()
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c == nil {
+		t.Fatal("UnmarshalNew returned nil")
+	}
+	snap := &envoycache.Snapshot{}
+	snap.Resources[envoycachetypes.Cluster] = envoycache.Resources{
+		Version: "foo",
+		Items:   map[string]envoycachetypes.ResourceWithTTL{"foo": envoycachetypes.ResourceWithTTL{Resource: c}},
+	}
+
+	x := XdsSnapWrapper{}.WithSnapshot(snap)
+	data, err := x.MarshalJSON()
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	s := string(data)
+	g.Expect(s).ToNot(gomega.BeEmpty())
 }
