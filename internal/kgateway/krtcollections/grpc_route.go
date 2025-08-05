e52ca4476e9a7bd11a2c04c8c22765c03af2cbcf
Lawrence Gadban <lawrence.gadban@solo.io>
2025-08-05 16:08:02 -0500

    propagate err for missing extensionRef in route rule
    Signed-off-by: Lawrence Gadban <lawrence.gadban@solo.io>

--- internal/kgateway/krtcollections/grpc_route.go
+++ internal/kgateway/krtcollections/grpc_route.go
@@ -43,7 +43,7 @@ func (h *RoutesIndex) transformGRPCRulesToHttp(
 		httpMatches := h.convertGRPCMatchesToHTTP(r.Matches)
 		httpBackends := h.convertGRPCBackendsToHTTP(kctx, src, r.BackendRefs)
 
-		extensionRefs := h.getExtensionRefs(kctx, src.Namespace, convertFiltersToHTTP(r.Filters))
+		extensionRefs, _ := h.getExtensionRefs(kctx, src.Namespace, convertFiltersToHTTP(r.Filters))
 		var policies ir.AttachedPolicies
 		if r.Name != nil {
 			policies = toAttachedPolicies(h.policies.getTargetingPolicies(kctx, extensionsplug.RouteAttachmentPoint, src, string(*r.Name), srcLabels), opts...)
