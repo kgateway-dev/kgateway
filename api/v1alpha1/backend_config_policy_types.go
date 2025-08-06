c99314b90eee144a8c72fb2cdb36d3900979b9db
omar <omar.hammami@solo.io>
2025-08-06 12:10:29 -0500

    fix description of simpleTLS
    Signed-off-by: omar <omar.hammami@solo.io>

--- api/v1alpha1/backend_config_policy_types.go
+++ api/v1alpha1/backend_config_policy_types.go
@@ -222,7 +222,7 @@ type TLS struct {
 	AllowRenegotiation *bool `json:"allowRenegotiation,omitempty"`
 
 	// If the TLS config has the tls cert and key provided, kgateway uses it to perform mTLS by default.
-	// Set simpleTLS to true to disable mTLS in favor of client-only TLS (one-way TLS), even if kgateway has the client cert.
+	// Set simpleTLS to true to disable mTLS in favor of server-only TLS (one-way TLS), even if kgateway has the client cert.
 	// If unset, defaults to false.
 	// +optional
 	SimpleTLS *bool `json:"simpleTLS,omitempty"`
