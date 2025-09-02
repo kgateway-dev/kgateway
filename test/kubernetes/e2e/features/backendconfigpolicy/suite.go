75cd7b8919773995e972c7f2a0b1d8b433965953
omar <omar.hammami@solo.io>
2025-09-02 11:55:43 -0500

    refactor test
    Signed-off-by: omar <omar.hammami@solo.io>

--- test/kubernetes/e2e/features/backendconfigpolicy/suite.go
+++ test/kubernetes/e2e/features/backendconfigpolicy/suite.go
@@ -14,7 +14,6 @@ import (
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/envoyutils/admincli"
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/kubeutils"
@@ -26,6 +25,7 @@ import (
 
 var _ e2e.NewSuiteFunc = NewTestingSuite
 
+// testingSuite defines the test suite for BackendConfigPolicy e2e tests
 type testingSuite struct {
 	suite.Suite
 
@@ -34,6 +34,9 @@ type testingSuite struct {
 	// testInstallation contains all the metadata/utilities necessary to execute a series of tests
 	// against an installation of kgateway
 	testInstallation *e2e.TestInstallation
+
+	// manifests maps test name to a list of manifests to apply before the test
+	manifests map[string][]string
 }
 
 func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.TestingSuite {
@@ -43,40 +46,78 @@ func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.
 	}
 }
 
-func (s *testingSuite) TestBackendConfigPolicy() {
-	manifests := []string{
-		testdefaults.CurlPodManifest,
-		setupManifest,
+func (s *testingSuite) SetupSuite() {
+	// define which manifests are applied for each test
+	s.manifests = map[string][]string{
+		"TestBackendConfigPolicy": {
+			testdefaults.CurlPodManifest,
+			setupManifest,
+		},
+		"TestBackendConfigPolicyTLSInsecureSkipVerify": {
+			testdefaults.CurlPodManifest,
+			tlsInsecureManifest,
+			nginxManifest,
+		},
+		"TestBackendConfigPolicySimpleTLS": {
+			testdefaults.CurlPodManifest,
+			simpleTLSManifest,
+			nginxManifest,
+		},
+		"TestBackendConfigPolicyTLSSystemCA": {
+			testdefaults.CurlPodManifest,
+			nginxManifest,
+			systemCAManifest,
+		},
+		"TestBackendConfigPolicyOutlierDetection": {
+			testdefaults.CurlPodManifest,
+			setupManifest,
+			outlierDetectionManifest,
+		},
 	}
-	manifestObjects := []client.Object{
-		testdefaults.CurlPod, // curl
-		nginxPod, exampleSvc, // nginx
-		proxyService, proxyServiceAccount, proxyDeployment, // proxy
-	}
+}
 
-	s.T().Cleanup(func() {
-		for _, manifest := range manifests {
-			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
-			s.Require().NoError(err)
-		}
-		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
-	})
+func (s *testingSuite) TearDownSuite() {
+	// nothing specific; each test cleans up via AfterTest
+}
+
+func (s *testingSuite) BeforeTest(suiteName, testName string) {
+	manifests, ok := s.manifests[testName]
+	if !ok {
+		s.FailNow("no manifests found for %s, manifest map contents: %v", testName, s.manifests)
+	}
 
 	for _, manifest := range manifests {
 		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
-		s.Require().NoError(err)
+		s.Assert().NoError(err, "can apply manifest "+manifest)
 	}
-	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
 
-	// make sure pods are running
+	// wait for common resources for all tests
 	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
 		LabelSelector: testdefaults.CurlPodLabelSelector,
+	})
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, proxyObjectMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=gw",
 	})
 	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, nginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
 		LabelSelector: "app.kubernetes.io/name=nginx",
 	})
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, proxyObjectMeta.GetNamespace(), metav1.ListOptions{
-		LabelSelector: "app.kubernetes.io/name=gw",
+}
+
+func (s *testingSuite) AfterTest(suiteName, testName string) {
+	manifests, ok := s.manifests[testName]
+	if !ok {
+		s.FailNow("no manifests found for " + testName)
+	}
+	for _, manifest := range manifests {
+		output, err := s.testInstallation.Actions.Kubectl().DeleteFileWithOutput(s.ctx, manifest)
+		s.testInstallation.Assertions.ExpectObjectDeleted(manifest, err, output)
+	}
+}
+
+func (s *testingSuite) TestBackendConfigPolicy() {
+	// make sure pods are running
+	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, nginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
+		LabelSelector: "app.kubernetes.io/name=nginx",
 	})
 
 	// Should have a successful response
@@ -166,38 +207,6 @@ func (s *testingSuite) TestBackendConfigPolicy() {
 }
 
 func (s *testingSuite) TestBackendConfigPolicyTLSInsecureSkipVerify() {
-	manifests := []string{
-		testdefaults.CurlPodManifest,
-		tlsInsecureManifest,
-		nginxManifest,
-	}
-	manifestObjects := []client.Object{
-		testdefaults.CurlPod,                               // curl
-		proxyService, proxyServiceAccount, proxyDeployment, // proxy
-		nginxPod,
-	}
-
-	s.T().Cleanup(func() {
-		for _, manifest := range manifests {
-			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
-			s.Require().NoError(err)
-		}
-		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
-	})
-
-	for _, manifest := range manifests {
-		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
-		s.Require().NoError(err)
-	}
-	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
-
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
-		LabelSelector: testdefaults.CurlPodLabelSelector,
-	})
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, nginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
-		LabelSelector: "app.kubernetes.io/name=nginx",
-	})
-
 	s.testInstallation.Assertions.AssertEventualCurlResponse(
 		s.ctx,
 		testdefaults.CurlPodExecOpt,
@@ -214,38 +223,6 @@ func (s *testingSuite) TestBackendConfigPolicyTLSInsecureSkipVerify() {
 }
 
 func (s *testingSuite) TestBackendConfigPolicySimpleTLS() {
-	manifests := []string{
-		testdefaults.CurlPodManifest,
-		simpleTLSManifest,
-		nginxManifest,
-	}
-	manifestObjects := []client.Object{
-		testdefaults.CurlPod,                               // curl
-		proxyService, proxyServiceAccount, proxyDeployment, // proxy
-		nginxPod,
-	}
-
-	s.T().Cleanup(func() {
-		for _, manifest := range manifests {
-			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
-			s.Require().NoError(err)
-		}
-		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
-	})
-
-	for _, manifest := range manifests {
-		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
-		s.Require().NoError(err)
-	}
-	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
-
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
-		LabelSelector: testdefaults.CurlPodLabelSelector,
-	})
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, nginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
-		LabelSelector: "app.kubernetes.io/name=nginx",
-	})
-
 	s.testInstallation.Assertions.AssertEventualCurlResponse(
 		s.ctx,
 		testdefaults.CurlPodExecOpt,
@@ -262,36 +239,6 @@ func (s *testingSuite) TestBackendConfigPolicySimpleTLS() {
 }
 
 func (s *testingSuite) TestBackendConfigPolicyTLSSystemCA() {
-	manifests := []string{
-		testdefaults.CurlPodManifest,
-		nginxManifest,
-		systemCAManifest,
-	}
-	manifestObjects := []client.Object{
-		testdefaults.CurlPod,
-		proxyService, proxyServiceAccount, proxyDeployment,
-		nginxPod,
-	}
-
-	s.T().Cleanup(func() {
-		for _, m := range manifests {
-			_ = s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, m)
-		}
-		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
-	})
-
-	for _, m := range manifests {
-		s.Require().NoError(s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, m))
-	}
-	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
-
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
-		LabelSelector: testdefaults.CurlPodLabelSelector,
-	})
-	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, nginxPod.ObjectMeta.GetNamespace(), metav1.ListOptions{
-		LabelSelector: "app.kubernetes.io/name=nginx",
-	})
-
 	// self-signed upstream should fail when using system CA certificates
 	s.testInstallation.Assertions.AssertEventualCurlResponse(
 		s.ctx,
@@ -302,7 +249,7 @@ func (s *testingSuite) TestBackendConfigPolicyTLSSystemCA() {
 			curl.WithHeadersOnly(),
 		},
 		&testmatchers.HttpResponse{
-			StatusCode: http.StatusBadGateway, // 502 expected on TLS validation failure
+			StatusCode: http.StatusServiceUnavailable, // 503 expected on TLS validation failure
 		},
 	)
 }
@@ -313,30 +260,6 @@ func (s *testingSuite) TestBackendConfigPolicyOutlierDetection() {
 	// HTTP 503. OutlierDetection::MaxEjectionPercent will therefore govern how
 	// many backends are rejected. We use the 'stats' API in Envoy to verify
 	// that rejection functions as expected.
-	manifests := []string{
-		testdefaults.CurlPodManifest,
-		setupManifest,
-		outlierDetectionManifest,
-	}
-	manifestObjects := []client.Object{
-		testdefaults.CurlPod,                               // curl
-		proxyService, proxyServiceAccount, proxyDeployment, // proxy
-		nginxPod,
-	}
-
-	s.T().Cleanup(func() {
-		for _, manifest := range manifests {
-			err := s.testInstallation.Actions.Kubectl().DeleteFileSafe(s.ctx, manifest)
-			s.Require().NoError(err)
-		}
-		s.testInstallation.Assertions.EventuallyObjectsNotExist(s.ctx, manifestObjects...)
-	})
-
-	for _, manifest := range manifests {
-		err := s.testInstallation.Actions.Kubectl().ApplyFile(s.ctx, manifest)
-		s.Require().NoError(err)
-	}
-	s.testInstallation.Assertions.EventuallyObjectsExist(s.ctx, manifestObjects...)
 
 	s.testInstallation.Assertions.EventuallyPodsRunning(s.ctx, testdefaults.CurlPod.GetNamespace(), metav1.ListOptions{
 		LabelSelector: testdefaults.CurlPodLabelSelector,
