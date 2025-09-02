6d3153bffb5668a66d0a92ca1b980dc90e575d88
omar <omar.hammami@solo.io>
2025-09-02 10:08:44 -0500

    test name
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/translator/gateway/gateway_translator_test.go
+++ internal/kgateway/translator/gateway/gateway_translator_test.go
@@ -41,7 +41,7 @@ func TestBasic(t *testing.T) {
 		translatortest.TestTranslation(t, ctx, inputFiles, expectedProxyFile, in.gwNN, in.assertReports, settingOpts...)
 	}
 
-	t.Run("gateway with no routes", func(t *testing.T) {
+	t.Run("gateway with no routes should not add empty filter chain", func(t *testing.T) {
 		test(t, translatorTestCase{
 			inputFile:  "gateway-only/gateway.yaml",
 			outputFile: "gateway-only/proxy.yaml",
@@ -51,6 +51,7 @@ func TestBasic(t *testing.T) {
 			},
 		})
 	})
+
 	t.Run("http gateway with per connection buffer limit", func(t *testing.T) {
 		test(t, translatorTestCase{
 			inputFile:  "gateway-per-conn-buf-lim/gateway.yaml",
