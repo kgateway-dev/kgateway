cb00c8c6f33234ca8da896d477664707d179b511
npolshakova <nina.polshakova@solo.io>
2025-09-05 14:59:56 -0400

    initial tp status
    Signed-off-by: npolshakova <nina.polshakova@solo.io>

--- internal/kgateway/controller/start.go
+++ internal/kgateway/controller/start.go
@@ -202,6 +202,9 @@ func NewControllerBuilder(ctx context.Context, cfg StartConfig) (*ControllerBuil
 			return nil, err
 		}
 
+		policyStatusAsyncQueue := agentgatewaysyncer.NewPolicyStatusCollections()
+		agentGatewaySyncer.PolicyStatusQueue().SetQueue(policyStatusAsyncQueue)
+
 		agentGatewayStatusSyncer := agentgatewaysyncer.NewAgentGwStatusSyncer(
 			cfg.ControllerName,
 			cfg.AgentGatewayClassName,
@@ -210,6 +213,7 @@ func NewControllerBuilder(ctx context.Context, cfg StartConfig) (*ControllerBuil
 			agentGatewaySyncer.GatewayReportQueue(),
 			agentGatewaySyncer.ListenerSetReportQueue(),
 			agentGatewaySyncer.RouteReportQueue(),
+			policyStatusAsyncQueue.GetAsyncQueue(),
 			agentGatewaySyncer.CacheSyncs(),
 		)
 		if err := cfg.Manager.Add(agentGatewayStatusSyncer); err != nil {
