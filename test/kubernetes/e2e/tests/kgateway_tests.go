7b3265d650f3e6d6d2105c2f9c1786be958ccfea
Jenny Shu <jenny.shu@solo.io>
2025-09-17 20:08:46 -0400

    remove features/virtualhost_options
    Signed-off-by: Jenny Shu <jenny.shu@solo.io>

--- test/kubernetes/e2e/tests/kgateway_tests.go
+++ test/kubernetes/e2e/tests/kgateway_tests.go
@@ -36,12 +36,10 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/tracing"
 	"github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/transformation"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/admin_server"
-	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/crd_categories"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/headless_svc"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/listener_options"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/metrics"
 	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/port_routing"
-	// "github.com/kgateway-dev/kgateway/v2/test/kubernetes/e2e/features/virtualhost_options"
 )
 
 func KubeGatewaySuiteRunner() e2e.SuiteRunner {
@@ -83,11 +81,9 @@ func KubeGatewaySuiteRunner() e2e.SuiteRunner {
 	kubeGatewaySuiteRunner.Register("RBAC", rbac.NewTestingSuite)
 
 	// kubeGatewaySuiteRunner.Register("ListenerOptions", listener_options.NewTestingSuite)
-	// kubeGatewaySuiteRunner.Register("VirtualHostOptions", virtualhost_options.NewTestingSuite)
 	// kubeGatewaySuiteRunner.Register("HeadlessSvc", headless_svc.NewK8sGatewayHeadlessSvcSuite)
 	// kubeGatewaySuiteRunner.Register("PortRouting", port_routing.NewK8sGatewayTestingSuite)
 	// kubeGatewaySuiteRunner.Register("GlooAdminServer", admin_server.NewTestingSuite)
-	// kubeGatewaySuiteRunner.Register("DirectResponse", directresponse.NewTestingSuite)
 	// kubeGatewaySuiteRunner.Register("ProxyMetrics", proxy_metrics.NewTestingSuite)
 
 	return kubeGatewaySuiteRunner
