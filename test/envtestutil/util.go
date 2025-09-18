71c232f61309a4804256de233410e4d2e897ffd8
omar <omar.hammami@solo.io>
2025-09-18 15:57:05 -0500

    remove
    Signed-off-by: omar <omar.hammami@solo.io>

--- test/envtestutil/util.go
+++ test/envtestutil/util.go
@@ -56,12 +56,6 @@ func RunController(t *testing.T, logger *zap.Logger, globalSettings *settings.Se
 		}
 		globalSettings = st
 	}
-	// Always set once instead of each time to avoid races
-	logLevel := globalSettings.LogLevel
-	globalSettings.LogLevel = ""
-	setupLogging.Do(func() {
-		setup.SetupLogging(logLevel)
-	})
 
 	// Enable this if you want api server logs and audit logs.
 	if os.Getenv("DEBUG_APISERVER") == "true" {
