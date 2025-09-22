f90f836190c94bf9274e81813e2d8be4021baaf3
omar <omar.hammami@solo.io>
2025-09-22 10:07:30 -0500

    debug
    Signed-off-by: omar <omar.hammami@solo.io>

--- test/kubernetes/e2e/features/deployer/suite.go
+++ test/kubernetes/e2e/features/deployer/suite.go
@@ -69,10 +69,12 @@ func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.
 }
 
 func (s *testingSuite) TestProvisionDeploymentAndService() {
+	s.T().Skip("skipping test")
 	s.TestInstallation.Assertions.EventuallyReadyReplicas(s.Ctx, proxyObjectMeta, gomega.Equal(1))
 }
 
 func (s *testingSuite) TestConfigureProxiesFromGatewayParameters() {
+	s.T().Skip("skipping test")
 	s.TestInstallation.Assertions.EventuallyReadyReplicas(s.Ctx, proxyObjectMeta, gomega.Equal(1))
 
 	// check that the labels and annotations got passed through from GatewayParameters to the ServiceAccount
@@ -152,6 +154,7 @@ func (s *testingSuite) TestConfigureProxiesFromGatewayParameters() {
 }
 
 func (s *testingSuite) TestProvisionResourcesUpdatedWithValidParameters() {
+	s.T().Skip("skipping test")
 	s.TestInstallation.Assertions.EventuallyReadyReplicas(s.Ctx, proxyObjectMeta, gomega.Equal(1))
 
 	// modify the number of replicas in the GatewayParameters
@@ -169,6 +172,7 @@ func (s *testingSuite) TestProvisionResourcesUpdatedWithValidParameters() {
 // This is to make sure that the controller and status syncer are working properly
 // until this is fixed: https://github.com/kgateway-dev/kgateway/issues/12207
 func (s *testingSuite) TestMissingGatewayParameters() {
+	s.T().Skip("skipping test")
 	s.TestInstallation.Assertions.EventuallyReadyReplicas(s.Ctx, proxyObjectMeta, gomega.Equal(1))
 
 	// patch the Gateway to reference a missing GatewayParameters
@@ -217,6 +221,7 @@ func (s *testingSuite) TestMissingGatewayParameters() {
 }
 
 func (s *testingSuite) TestProvisionResourcesNotUpdatedWithInvalidParameters() {
+	s.T().Skip("skipping test")
 	s.TestInstallation.Assertions.EventuallyReadyReplicas(s.Ctx, proxyObjectMeta, gomega.Equal(1))
 
 	proxyDeployment := &appsv1.Deployment{}
