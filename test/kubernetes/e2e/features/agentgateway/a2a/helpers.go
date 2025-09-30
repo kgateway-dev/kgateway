530855988aa49e975aed05c7a66677c781f3b574
Petr McAllister <petr.mcallister@solo.io>
2025-09-30 15:02:37 -0700

    lint
    Signed-off-by: Petr McAllister <petr.mcallister@solo.io>

--- test/kubernetes/e2e/features/agentgateway/a2a/helpers.go
+++ test/kubernetes/e2e/features/agentgateway/a2a/helpers.go
@@ -42,7 +42,7 @@ func a2aHeaders() map[string]string {
 }
 
 func (s *testingSuite) execCurlA2A(port int, path string, headers map[string]string, body string, extraArgs ...string) (string, error) {
-	args := []string{"exec", "-n", "curl", "curl", "--", "curl", "-sS", "--http1.1"} 
+	args := []string{"exec", "-n", "curl", "curl", "--", "curl", "-sS", "--http1.1"}
 	for k, v := range headers {
 		args = append(args, "-H", fmt.Sprintf("%s: %s", k, v))
 	}
@@ -54,7 +54,7 @@ func (s *testingSuite) execCurlA2A(port int, path string, headers map[string]str
 
 	cmd := exec.Command("kubectl", args...)
 	out, err := cmd.CombinedOutput()
-	
+
 	s.T().Logf("kubectl %s", strings.Join(args, " "))
 	s.T().Logf("curl response: %s", string(out))
 
