5199655401dd6ebd62a41509df1fefc5a30b012c
zyfy29 <wasuremono127@gmail.com>
2025-10-07 17:18:07 +0900

    replace "k8s.io/utils/pointer" by "k8s.io/utils/ptr"
    Signed-off-by: zyfy29 <wasuremono127@gmail.com>

--- internal/kgateway/deployer/gateway_parameters.go
+++ internal/kgateway/deployer/gateway_parameters.go
@@ -9,7 +9,6 @@ import (
 
 	"helm.sh/helm/v3/pkg/chart"
 	"k8s.io/apimachinery/pkg/runtime/schema"
-	"k8s.io/utils/pointer"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	api "sigs.k8s.io/gateway-api/apis/v1"
@@ -400,7 +399,7 @@ func (k *kGatewayParameters) getValues(gw *api.Gateway, gwParam *v1alpha1.Gatewa
 	} else {
 		// Use the specified replica count
 		if deployConfig.GetReplicas() != nil {
-			gateway.ReplicaCount = pointer.Uint32(uint32(*deployConfig.GetReplicas())) // nolint:gosec // G115: kubebuilder validation ensures safe for uint32
+			gateway.ReplicaCount = ptr.To(uint32(*deployConfig.GetReplicas())) // nolint:gosec // G115: kubebuilder validation ensures safe for uint32
 		}
 	}
 	gateway.Strategy = deployConfig.GetStrategy()
