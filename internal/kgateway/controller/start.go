d0af53f761bf85afe01ec263330452ad752fd779
7h3-3mp7y-m4n <emailtorash@gmail.com>
2025-10-30 22:34:22 +0530

    Migrate InferencePool to agentgateway Pkg
    Signed-off-by: 7h3-3mp7y-m4n <emailtorash@gmail.com>

--- internal/kgateway/controller/start.go
+++ internal/kgateway/controller/start.go
@@ -21,8 +21,8 @@ import (
 
 	apisettings "github.com/kgateway-dev/kgateway/v2/api/settings"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/agentgatewaysyncer"
+	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/agentgatewaysyncer/backend/inferencepool"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2"
-	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2/plugins/inferenceextension/endpointpicker"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2/plugins/waypoint"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/extensions2/registry"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/krtcollections"
@@ -136,7 +136,7 @@ func NewControllerBuilder(ctx context.Context, cfg StartConfig) (*ControllerBuil
 			return nil, err
 		case exists:
 			setupLog.Info("adding the endpoint-picker inference extension")
-			gatedPlugins = append(gatedPlugins, endpointpicker.NewPlugin(ctx, cfg.CommonCollections))
+			gatedPlugins = append(gatedPlugins, inferencepool.NewPlugin(ctx, cfg.CommonCollections))
 		}
 	}
 	// Add the waypoint plugin if enabled
--- test/kubernetes/e2e/features/agentgateway/inference_extension/suite.go
+++ test/kubernetes/e2e/features/agentgateway/inference_extension/suite.go
@@ -1,6 +1,6 @@
 //go:build e2e
 
-package inferenceextension
+package inference_extension
 
 import (
 	"context"
