c71be621d1d6ad894c3cf1f1a4f340b158a44a4a
omar <omar.hammami@solo.io>
2025-11-18 11:19:58 -0600

    configure alpn using tls options
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/translator/irtranslator/fc.go
+++ internal/kgateway/translator/irtranslator/fc.go
@@ -17,6 +17,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 
+	"github.com/kgateway-dev/kgateway/v2/api/annotations"
 	"github.com/kgateway-dev/kgateway/v2/internal/kgateway/utils"
 	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/filters"
 	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/ir"
@@ -463,6 +464,8 @@ func (info *FilterChainInfo) toTransportSocket() *envoycorev3.TransportSocket {
 	alpnProtocols := ssl.AlpnProtocols
 	if len(alpnProtocols) == 0 {
 		alpnProtocols = defaultDownstreamAlpnProtocols
+	} else if len(alpnProtocols) == 1 && alpnProtocols[0] == string(annotations.AllowEmptyAlpnProtocols) {
+		alpnProtocols = []string{}
 	}
 
 	common := &envoytlsv3.CommonTlsContext{
