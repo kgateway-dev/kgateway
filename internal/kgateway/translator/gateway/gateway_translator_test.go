ea30c45af951fd585abfada93da6df7dc72baf94
omar <omar.hammami@solo.io>
2025-11-20 14:15:56 -0600

    support remote jwks
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/translator/gateway/gateway_translator_test.go
+++ internal/kgateway/translator/gateway/gateway_translator_test.go
@@ -1482,6 +1482,17 @@ func TestBasic(t *testing.T) {
 			},
 		})
 	})
+
+	t.Run("JWT Policy at route level using remote JWKS", func(t *testing.T) {
+		test(t, translatorTestCase{
+			inputFile:  "jwt/httproute-remote-jwks.yaml",
+			outputFile: "jwt/httproute-remote-jwks.yaml",
+			gwNN: types.NamespacedName{
+				Namespace: "default",
+				Name:      "example-gateway",
+			},
+		})
+	})
 }
 
 func TestValidation(t *testing.T) {
