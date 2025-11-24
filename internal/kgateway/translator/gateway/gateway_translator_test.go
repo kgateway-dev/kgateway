7a46ff6f2ec83aafde9b96e781a28a966ecd2452
omar <omar.hammami@solo.io>
2025-11-23 18:52:52 -0600

    add cross ns translator test
    Signed-off-by: omar <omar.hammami@solo.io>

--- internal/kgateway/translator/gateway/gateway_translator_test.go
+++ internal/kgateway/translator/gateway/gateway_translator_test.go
@@ -1483,6 +1483,17 @@ func TestBasic(t *testing.T) {
 		})
 	})
 
+	t.Run("JWT Policy with cross-namespace backends", func(t *testing.T) {
+		test(t, translatorTestCase{
+			inputFile:  "jwt/cross-namespace.yaml",
+			outputFile: "jwt/cross-namespace.yaml",
+			gwNN: types.NamespacedName{
+				Namespace: "default",
+				Name:      "example-gateway",
+			},
+		})
+	})
+
 	t.Run("JWT Policy at httproute level", func(t *testing.T) {
 		test(t, translatorTestCase{
 			inputFile:  "jwt/httproute.yaml",
