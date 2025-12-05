3eca7b4b226d36cc6e74fd5555a25b4bbc277b15
David Jumani <dj.davidjumani1994@gmail.com>
2025-12-05 11:21:16 -0500

    update func name
    Signed-off-by: David Jumani <dj.davidjumani1994@gmail.com>

--- pkg/kgateway/agentgatewaysyncer/option.go
+++ pkg/kgateway/agentgatewaysyncer/option.go
@@ -2,22 +2,22 @@ package agentgatewaysyncer
 
 import "github.com/kgateway-dev/kgateway/v2/pkg/agentgateway/translator"
 
-type agentGatewaySyncerConfig struct {
+type agentgatewaySyncerConfig struct {
 	GatewayTransformationFunc translator.GatewaysTransformationFunction
 }
 
-type AgentgatewaySyncerOption func(*agentGatewaySyncerConfig)
+type AgentgatewaySyncerOption func(*agentgatewaySyncerConfig)
 
-func processAgentgatewaySyncerOptions(opts ...AgentgatewaySyncerOption) *agentGatewaySyncerConfig {
-	cfg := &agentGatewaySyncerConfig{}
+func processAgentgatewaySyncerOptions(opts ...AgentgatewaySyncerOption) *agentgatewaySyncerConfig {
+	cfg := &agentgatewaySyncerConfig{}
 	for _, fn := range opts {
 		fn(cfg)
 	}
 	return cfg
 }
 
-func WithGatewayForDeployerTransformationFunc(f translator.GatewaysTransformationFunction) AgentgatewaySyncerOption {
-	return func(o *agentGatewaySyncerConfig) {
+func WithGatewayTransformationFunc(f translator.GatewaysTransformationFunction) AgentgatewaySyncerOption {
+	return func(o *agentgatewaySyncerConfig) {
 		o.GatewayTransformationFunc = f
 	}
 }
