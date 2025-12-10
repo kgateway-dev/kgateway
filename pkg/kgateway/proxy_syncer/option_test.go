920ecd779baf2796c719c3d02ca2bf97e572f366
David Jumani <dj.davidjumani1994@gmail.com>
2025-12-10 10:35:44 -0500

    Refactor and add tests for injecitons
    Signed-off-by: David Jumani <dj.davidjumani1994@gmail.com>

---
+++ pkg/kgateway/proxy_syncer/option_test.go
@@ -0,0 +1,18 @@
+package proxy_syncer
+
+import (
+	"context"
+	"testing"
+
+	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk"
+	"github.com/kgateway-dev/kgateway/v2/pkg/reports"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWithCustomStatusSync(t *testing.T) {
+	customStatusSync := func(ctx context.Context, rm reports.ReportMap) {}
+	statusSyncer := NewStatusSyncer(nil, pluginsdk.Plugin{}, "controller-name", "agw-controller-name", nil, nil, nil, nil, nil,
+		WithCustomStatusSync(customStatusSync))
+
+	assert.NotNil(t, statusSyncer.customStatusSync)
+}
