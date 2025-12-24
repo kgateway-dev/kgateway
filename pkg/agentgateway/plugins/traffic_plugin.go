3c6647e3535017583e4961e1baf7d3da87831e7d
Yuval Kohavi <yuval.kohavi@gmail.com>
2025-12-24 14:04:22 -0500

    add timeout to extauth in agentgw
    Signed-off-by: Yuval Kohavi <yuval.kohavi@gmail.com>

--- pkg/agentgateway/plugins/traffic_plugin.go
+++ pkg/agentgateway/plugins/traffic_plugin.go
@@ -866,6 +866,9 @@ func processExtAuthPolicy(
 			PackAsBytes: false,
 		}
 	}
+	if t := extAuth.Timeout; t != nil && t.Duration > 0 {
+		spec.Timeout = durationpb.New(t.Duration)
+	}
 
 	extauthPolicy := &api.Policy{
 		Key:    basePolicyName + extauthPolicySuffix + attachmentName(policyTarget),
