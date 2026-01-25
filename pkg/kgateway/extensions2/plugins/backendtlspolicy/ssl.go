d509fcd5d2def9884ec097dd3bc2cd6fe5a9536b
khushiiagrawal <khushisaritaagrawal@gmail.com>
2026-01-25 16:04:06 +0530

    refactor: extract shared TLS utilities to pluginutils package
    Signed-off-by: khushiiagrawal <khushisaritaagrawal@gmail.com>

--- pkg/kgateway/extensions2/plugins/backendtlspolicy/ssl.go
+++ pkg/kgateway/extensions2/plugins/backendtlspolicy/ssl.go
@@ -1,42 +1,16 @@
 package backendtlspolicy
 
 import (
-	envoycorev3 "github.com/envoyproxy/go-control-plane/envoy/config/core/v3"
 	envoytlsv3 "github.com/envoyproxy/go-control-plane/envoy/extensions/transport_sockets/tls/v3"
+
+	"github.com/kgateway-dev/kgateway/v2/pkg/kgateway/extensions2/pluginutils"
 )
 
 // handles conversion into envoy auth types
 // based on https://github.com/solo-io/gloo/blob/main/projects/gloo/pkg/utils/ssl.go#L76
 
-// ResolveUpstreamSslConfigFromCA creates an UpstreamTlsContext from a CA certificate string
+// ResolveUpstreamSslConfigFromCA creates an UpstreamTlsContext from a CA certificate string.
+// This delegates to the shared implementation in pluginutils.
 func ResolveUpstreamSslConfigFromCA(caCert string, validation *envoytlsv3.CertificateValidationContext, sni string) (*envoytlsv3.UpstreamTlsContext, error) {
-	common, err := ResolveCommonSslConfigFromCA(caCert, validation, false)
-	if err != nil {
-		return nil, err
-	}
-
-	return &envoytlsv3.UpstreamTlsContext{
-		CommonTlsContext: common,
-		Sni:              sni,
-	}, nil
-}
-
-func ResolveCommonSslConfigFromCA(caCert string, validation *envoytlsv3.CertificateValidationContext, mustHaveCert bool) (*envoytlsv3.CommonTlsContext, error) {
-	caCrtData := envoycorev3.DataSource{
-		Specifier: &envoycorev3.DataSource_InlineString{
-			InlineString: caCert,
-		},
-	}
-
-	tlsContext := &envoytlsv3.CommonTlsContext{
-		// default params
-		TlsParams: &envoytlsv3.TlsParameters{},
-	}
-	validation.TrustedCa = &caCrtData
-	validationCtx := &envoytlsv3.CommonTlsContext_ValidationContext{
-		ValidationContext: validation,
-	}
-
-	tlsContext.ValidationContextType = validationCtx
-	return tlsContext, nil
+	return pluginutils.ResolveUpstreamSslConfigFromCA(caCert, validation, sni)
 }
