35658e18026e1e062a357940e04966491fb15382
omar <omar.hammami@solo.io>
2026-01-28 07:22:59 -0600

    replace stopwatch with metrics pkg
    Signed-off-by: omar <omar.hammami@solo.io>

--- pkg/kgateway/translator/translator.go
+++ pkg/kgateway/translator/translator.go
@@ -14,13 +14,13 @@ import (
 	gwtranslator "github.com/kgateway-dev/kgateway/v2/pkg/kgateway/translator/gateway"
 	"github.com/kgateway-dev/kgateway/v2/pkg/kgateway/translator/irtranslator"
 	"github.com/kgateway-dev/kgateway/v2/pkg/kgateway/translator/listener"
+	"github.com/kgateway-dev/kgateway/v2/pkg/kgateway/translator/metrics"
 	"github.com/kgateway-dev/kgateway/v2/pkg/logging"
 	sdk "github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk"
 	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/collections"
 	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/ir"
 	"github.com/kgateway-dev/kgateway/v2/pkg/pluginsdk/reporter"
 	"github.com/kgateway-dev/kgateway/v2/pkg/reports"
-	"github.com/kgateway-dev/kgateway/v2/pkg/utils/stopwatch"
 	"github.com/kgateway-dev/kgateway/v2/pkg/validator"
 )
 
@@ -108,8 +108,16 @@ func (s *CombinedTranslator) HasSynced() bool {
 
 // buildProxy performs translation of a kube Gateway -> GatewayIR
 func (s *CombinedTranslator) buildProxy(kctx krt.HandlerContext, ctx context.Context, gw ir.Gateway, r reporter.Reporter) *ir.GatewayIR {
-	stopwatch := stopwatch.NewTranslatorStopWatch("CombinedTranslator")
-	stopwatch.Start()
+	var rErr error
+
+	finishMetrics := metrics.CollectTranslationMetrics(metrics.TranslatorMetricLabels{
+		Name:       gw.Name,
+		Namespace:  gw.Namespace,
+		Translator: "CombinedTranslator",
+	})
+	defer func() {
+		finishMetrics(rErr)
+	}()
 
 	var gatewayTranslator sdk.KGwTranslator = s.gwtranslator
 	if s.extensions.ContributesGwTranslator != nil {
@@ -120,11 +128,11 @@ func (s *CombinedTranslator) buildProxy(kctx krt.HandlerContext, ctx context.Con
 	}
 	proxy := gatewayTranslator.Translate(kctx, ctx, &gw, r)
 	if proxy == nil {
+		rErr = nil // proxy == nil is not necessarily an error, just means no proxy was created
 		return nil
 	}
 
-	duration := stopwatch.Stop(ctx)
-	logger.Debug("translated proxy", "namespace", gw.Namespace, "name", gw.Name, "duration", duration.String())
+	logger.Debug("translated proxy", "namespace", gw.Namespace, "name", gw.Name)
 
 	return proxy
 }
