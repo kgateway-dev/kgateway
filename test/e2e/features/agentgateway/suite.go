05dde0d72d5272d70decbd2857b20b1a57f067e2
DhruvArvindSingh <dhruvsinghxd@gmail.com>
2026-02-05 03:50:27 +0530

    fast-e2e-basicrouting
    Signed-off-by: DhruvArvindSingh <dhruvsinghxd@gmail.com>

--- test/e2e/features/agentgateway/suite.go
+++ test/e2e/features/agentgateway/suite.go
@@ -10,10 +10,9 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
 
-	"github.com/kgateway-dev/kgateway/v2/pkg/utils/kubeutils"
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/requestutils/curl"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e"
-	"github.com/kgateway-dev/kgateway/v2/test/e2e/defaults"
+	"github.com/kgateway-dev/kgateway/v2/test/e2e/common"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/tests/base"
 	"github.com/kgateway-dev/kgateway/v2/test/gomega/matchers"
 )
@@ -29,78 +28,95 @@ func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.
 	}
 }
 
+func (s *testingSuite) SetupSuite() {
+	s.BaseTestingSuite.SetupSuite()
+}
+
 func (s *testingSuite) TestAgentgatewayTCPRoute() {
 	s.TestInstallation.AssertionsT(s.T()).EventuallyGatewayCondition(
 		s.Ctx,
-		tcpGatewayObjectMeta.Name,
-		tcpGatewayObjectMeta.Namespace,
+		gateway.Name,
+		gateway.Namespace,
 		gwv1.GatewayConditionProgrammed,
 		metav1.ConditionTrue,
 	)
 	s.TestInstallation.AssertionsT(s.T()).EventuallyGatewayCondition(
 		s.Ctx,
-		tcpGatewayObjectMeta.Name,
-		tcpGatewayObjectMeta.Namespace,
+		gateway.Name,
+		gateway.Namespace,
 		gwv1.GatewayConditionAccepted,
 		metav1.ConditionTrue,
 	)
+	// This assertion verifies that the TCP listener on the gateway has exactly 1 route attached to it.
+	// It checks that the "tcp" listener in the "gateway" resource within the "agentgateway-base" namespace
+	// has successfully attached the expected number of TCPRoutes (in this case, 1).
 	s.TestInstallation.AssertionsT(s.T()).EventuallyGatewayListenerAttachedRoutes(
 		s.Ctx,
-		tcpGatewayObjectMeta.Name,
-		tcpGatewayObjectMeta.Namespace,
+		gateway.Name,
+		gateway.Namespace,
 		"tcp",
 		1,
 	)
 
-	s.TestInstallation.AssertionsT(s.T()).AssertEventualCurlResponse(
+	// Wait for agentgateway pods to be running before sending traffic
+	s.TestInstallation.AssertionsT(s.T()).EventuallyPodsRunning(
 		s.Ctx,
-		defaults.CurlPodExecOpt,
-		[]curl.Option{
-			curl.WithHost(kubeutils.ServiceFQDN(tcpGatewayObjectMeta)),
-			curl.VerboseOutput(),
-			curl.WithPort(8080),
+		gateway.Namespace,
+		metav1.ListOptions{
+			LabelSelector: "gateway.networking.k8s.io/gateway-name=" + gateway.Name,
 		},
+	)
+
+	// Send HTTP request to the TCP gateway using port 8080
+	common.BaseGateway.Send(
+		s.T(),
 		&matchers.HttpResponse{
 			StatusCode: http.StatusOK,
 		},
+		curl.WithPort(8080),
 	)
 }
 
 func (s *testingSuite) TestAgentgatewayHTTPRoute() {
+	// Verify the shared gateway has HTTP listener
 	s.TestInstallation.AssertionsT(s.T()).EventuallyGatewayCondition(
 		s.Ctx,
-		httpGatewayObjectMeta.Name,
-		httpGatewayObjectMeta.Namespace,
+		gateway.Name,
+		gateway.Namespace,
 		gwv1.GatewayConditionProgrammed,
 		metav1.ConditionTrue,
 	)
 	s.TestInstallation.AssertionsT(s.T()).EventuallyGatewayCondition(
 		s.Ctx,
-		httpGatewayObjectMeta.Name,
-		httpGatewayObjectMeta.Namespace,
+		gateway.Name,
+		gateway.Namespace,
 		gwv1.GatewayConditionAccepted,
 		metav1.ConditionTrue,
 	)
 	s.TestInstallation.AssertionsT(s.T()).EventuallyGatewayListenerAttachedRoutes(
 		s.Ctx,
-		httpGatewayObjectMeta.Name,
-		httpGatewayObjectMeta.Namespace,
+		gateway.Name,
+		gateway.Namespace,
 		"http",
 		1,
 	)
 
-	s.TestInstallation.AssertionsT(s.T()).AssertEventualCurlResponse(
+	// Wait for agentgateway pods to be running before sending traffic
+	s.TestInstallation.AssertionsT(s.T()).EventuallyPodsRunning(
 		s.Ctx,
-		defaults.CurlPodExecOpt,
-		[]curl.Option{
-			curl.WithHost(kubeutils.ServiceFQDN(httpGatewayObjectMeta)),
-			curl.VerboseOutput(),
-			curl.WithHostHeader("www.example.com"),
-			curl.WithPath("/status/200"),
-			curl.WithPort(8080),
+		gateway.Namespace,
+		metav1.ListOptions{
+			LabelSelector: "gateway.networking.k8s.io/gateway-name=" + gateway.Name,
 		},
+	)
+
+	// Send HTTP request to the shared backend service
+	common.BaseGateway.Send(
+		s.T(),
 		&matchers.HttpResponse{
 			StatusCode: http.StatusOK,
 		},
+		curl.WithHostHeader("www.example.com"),
+		curl.WithPort(80),
 	)
 }
--- test/e2e/features/agentgateway/types.go
+++ test/e2e/features/agentgateway/types.go
@@ -5,11 +5,14 @@ package agentgateway
 import (
 	"path/filepath"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/fsutils"
-	"github.com/kgateway-dev/kgateway/v2/test/e2e/defaults"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/tests/base"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
+)
+
+const (
+	namespace = "agentgateway-base"
 )
 
 var (
@@ -18,23 +21,22 @@ var (
 	// kgateway managed deployment for the agentgateway with basic TCPRoute
 	tcpRouteManifest = filepath.Join(fsutils.MustGetThisDir(), "testdata", "agw-tcp-route.yaml")
 
-	// Core infrastructure objects that we need to track
-	httpGatewayObjectMeta = metav1.ObjectMeta{
-		Name:      "http-gw-for-test",
-		Namespace: "default",
-	}
-	tcpGatewayObjectMeta = metav1.ObjectMeta{
-		Name:      "tcp-gw-for-test",
-		Namespace: "default",
-	}
-
 	testCases = map[string]*base.TestCase{
 		"TestAgentgatewayHTTPRoute": {
-			Manifests: []string{defaults.HttpbinManifest, defaults.CurlPodManifest, httpRouteManifest},
+			Manifests: []string{httpRouteManifest},
 		},
 		"TestAgentgatewayTCPRoute": {
-			Manifests:       []string{defaults.CurlPodManifest, tcpRouteManifest},
-			MinGwApiVersion: base.GwApiRequireTcpRoutes, // TCPRoutes are experimental only
+			Manifests: []string{tcpRouteManifest},
+			// TCPRoutes are experimental (v0.3.0+)
+			// Don't specify MinGwApiVersion here to avoid test framework skip logic
+			// The suite-level GwApiRequireRouteNames requirement ensures proper Gateway API version
+		},
+	}
+
+	gateway = &gwv1.Gateway{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "gateway",
+			Namespace: namespace,
 		},
 	}
 )
