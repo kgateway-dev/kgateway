ac8bd6c9e540b6b4b14a9befe98aea7ba1c6ecea
Daneyon Hansen <daneyon.hansen@solo.io>
2026-02-12 08:19:27 -0800

    backport: restore BackendConfigPolicy Hostname target validation
    Signed-off-by: Daneyon Hansen <daneyon.hansen@solo.io>

--- test/e2e/tests/api_validation_test.go
+++ test/e2e/tests/api_validation_test.go
@@ -176,7 +176,7 @@ spec:
     kind: Deployment
     name: test-deployment
 `,
-			wantErrors: []string{"TargetRefs must reference either a Kubernetes Service or a Backend API"},
+			wantErrors: []string{"TargetRefs must reference either a Kubernetes Service, a Backend, or an Istio Hostname"},
 		},
 		{
 			name: "BackendConfigPolicy: invalid target selector",
@@ -192,7 +192,7 @@ spec:
     matchLabels:
       app: myapp
 `,
-			wantErrors: []string{"TargetSelectors must reference either a Kubernetes Service or a Backend API"},
+			wantErrors: []string{"TargetSelectors must reference either a Kubernetes Service, a Backend, or an Istio Hostname"},
 		},
 		{
 			name: "BackendConfigPolicy: invalid aggression",
