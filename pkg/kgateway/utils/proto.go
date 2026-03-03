3b673ddbc856601a5abdc88fd05c94c80c5b588f
Marvin Rösch <marvinroesch99@gmail.com>
2026-03-03 10:00:58 +0100

    feat(directresponse): add body_format support
    Signed-off-by: Marvin Rösch <marvinroesch99@gmail.com>

--- pkg/kgateway/utils/proto.go
+++ pkg/kgateway/utils/proto.go
@@ -1,9 +1,13 @@
 package utils
 
 import (
+	"encoding/json"
+	"fmt"
 	"time"
 
 	"google.golang.org/protobuf/types/known/durationpb"
+	"google.golang.org/protobuf/types/known/structpb"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
 // DurationToProto converts a go Duration to a protobuf Duration.
@@ -12,4 +16,23 @@ func DurationToProto(d time.Duration) *durationpb.Duration {
 		Seconds: int64(d) / int64(time.Second),
 		Nanos:   int32(int64(d) % int64(time.Second)), //nolint:gosec // G115: nanoseconds modulo 1 second always fits in int32
 	}
+}
+
+// JSONToProtoStruct converts raw JSON data from a Kubernetes API resource to a protobuf Struct
+func JSONToProtoStruct(jsonExtension *runtime.RawExtension) (*structpb.Struct, error) {
+	if jsonExtension == nil {
+		return nil, nil
+	}
+
+	var formatMap map[string]any
+	if err := json.Unmarshal(jsonExtension.Raw, &formatMap); err != nil {
+		return nil, fmt.Errorf("invalid json object: %w", err)
+	}
+
+	structVal, err := structpb.NewStruct(formatMap)
+	if err != nil {
+		return nil, fmt.Errorf("invalid json object: %w", err)
+	}
+
+	return structVal, nil
 }
