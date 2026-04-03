1f91f3fd57365445951f6f9536ba1e04bf1f8ae6
Andy Fong <andy.fong@solo.io>
2026-04-03 14:09:24 -0400

    fixed tests
    Signed-off-by: Andy Fong <andy.fong@solo.io>

--- test/e2e/features/websocket/suite.go
+++ test/e2e/features/websocket/suite.go
@@ -5,6 +5,7 @@ package websocket
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/onsi/gomega"
@@ -13,7 +14,10 @@ import (
 	"github.com/kgateway-dev/kgateway/v2/pkg/utils/requestutils/websocket"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/common"
+	"github.com/kgateway-dev/kgateway/v2/test/e2e/defaults"
 	"github.com/kgateway-dev/kgateway/v2/test/e2e/tests/base"
+	envoyadmincli "github.com/kgateway-dev/kgateway/v2/test/envoyutils/admincli"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 var _ e2e.NewSuiteFunc = NewTestingSuite
@@ -26,6 +30,12 @@ func NewTestingSuite(ctx context.Context, testInst *e2e.TestInstallation) suite.
 	return &testingSuite{
 		BaseTestingSuite: base.NewBaseTestingSuite(ctx, testInst, setup, testCases),
 	}
+}
+
+func (s *testingSuite) SetupSuite() {
+	s.BaseTestingSuite.SetupSuite()
+
+	s.assertPodsRunning()
 }
 
 const (
@@ -54,6 +64,7 @@ func (s *testingSuite) dialWebSocket(g gomega.Gomega, host string) string {
 // route with no transformation policies.
 func (s *testingSuite) TestWebSocketHappyPath() {
 	g := gomega.NewWithT(s.T())
+	s.assertWebsocketUpgradeEnabled()
 	msg := s.dialWebSocket(g, "websocket.example.com")
 	g.Expect(msg).To(gomega.Equal("websocket-e2e-ping"),
 		"echo-server should echo back the test payload")
@@ -75,4 +86,53 @@ func (s *testingSuite) TestWebSocketWithDefaultTransformationBuffering() {
 	g.Expect(msg).To(gomega.Equal("websocket-e2e-ping"),
 		"echo-server should echo back the test payload; "+
 			"if this hangs/times out the Envoy is buffing the body")
+}
+
+func (s *testingSuite) assertWebsocketUpgradeEnabled() {
+	var proxyObjectMeta = metav1.ObjectMeta{
+		Name:      common.BaseGateway.Name,
+		Namespace: common.BaseGateway.Namespace,
+	}
+
+	s.TestInstallation.AssertionsT(s.T()).AssertEnvoyAdminApi(
+		s.Ctx,
+		proxyObjectMeta,
+		func(ctx context.Context, adminClient *envoyadmincli.Client) {
+			s.TestInstallation.AssertionsT(s.T()).Gomega.Eventually(func(g gomega.Gomega) {
+				listener, err := adminClient.GetSingleListenerFromDynamicListeners(ctx, "listener~80")
+				g.Expect(err).ToNot(gomega.HaveOccurred(), "failed to get listener")
+				if err != nil {
+					// when we get an error, the g.Expect() doesn't stop execution of this function
+					// but listener is nil and will cause a crash, Gomega will catch it and swallow it but
+					// will not retry even with Eventually()
+					return
+				}
+
+				websocketEnabled := strings.Contains(listener.String(), "upgrade_configs:{upgrade_type:\"websocket\"}")
+				g.Expect(websocketEnabled).To(gomega.BeTrue(), fmt.Sprintf("%v", listener.String()))
+			}).
+				WithContext(ctx).
+				WithTimeout(30*time.Second).
+				WithPolling(2*time.Second).
+				Should(gomega.Succeed(), "failed to get expected websocket enabled")
+		},
+	)
+}
+
+func (s *testingSuite) assertPodsRunning() {
+	// control-plane
+	s.TestInstallation.AssertionsT(s.T()).EventuallyPodsRunning(s.Ctx, s.TestInstallation.Metadata.InstallNamespace, metav1.ListOptions{
+		LabelSelector: defaults.ControllerLabelSelector,
+	})
+
+	// envoy proxy
+	s.TestInstallation.AssertionsT(s.T()).EventuallyPodsRunning(s.Ctx, common.BaseGateway.Namespace, metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=%s", defaults.WellKnownAppLabel, common.BaseGateway.Name),
+	})
+
+	// websocket server backend
+	s.TestInstallation.AssertionsT(s.T()).EventuallyPodsRunning(s.Ctx, "kgateway-base", metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("%s=websocket-backend", defaults.WellKnownAppLabel),
+	})
+
 }
