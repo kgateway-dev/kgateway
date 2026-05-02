660a53d729a85c6dcff43f3907710b93f29c6e08
devesh <chouhanlim2003@gmail.com>
2026-05-02 18:03:13 +0530

    test: add header-modifiers conformance suite and restructure for multi-feature composition
    Signed-off-by: devesh <chouhanlim2003@gmail.com>

---
+++ test/e2e/features/basicrouting_sigs/main_test.go
@@ -0,0 +1,29 @@
+package basicrouting_sigs
+
+import (
+	"os"
+	"testing"
+
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/e2e-framework/pkg/env"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	gwv1 "sigs.k8s.io/gateway-api/apis/v1"
+	gwv1beta1 "sigs.k8s.io/gateway-api/apis/v1beta1"
+)
+
+var testenv env.Environment
+
+func TestMain(m *testing.M) {
+	// Register Gateway API types with the global scheme
+	// The framework's client uses the global scheme by default
+	_ = gwv1.AddToScheme(scheme.Scheme)
+	_ = gwv1beta1.AddToScheme(scheme.Scheme)
+
+	cfg, err := envconf.NewFromFlags()
+	if err != nil {
+		panic(err)
+	}
+
+	testenv = env.NewWithConfig(cfg)
+	os.Exit(testenv.Run(m))
+}
---
+++ test/sigs_gateway_conformance/features/header_modifiers/header_modifiers.go
@@ -0,0 +1,99 @@
+//go:build e2e
+
+package header_modifiers
+
+import (
+	"embed"
+	"testing"
+
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/gateway-api/conformance/utils/http"
+	"sigs.k8s.io/gateway-api/conformance/utils/kubernetes"
+	confsuite "sigs.k8s.io/gateway-api/conformance/utils/suite"
+	"sigs.k8s.io/gateway-api/pkg/features"
+)
+
+//go:embed testdata
+var ManifestFS embed.FS
+
+const (
+	testNamespace = "kgateway-conformance-test"
+	gatewayName   = "conformance-gateway"
+	routeName     = "header-modifiers-route"
+	routeHostname = "header-modifiers.example.com"
+	echoBackend   = "echo-server"
+)
+
+// Tests exports all header-modifiers conformance scenarios.
+var Tests = []confsuite.ConformanceTest{
+	RequestHeaderModifier,
+}
+
+// RequestHeaderModifier exercises request header modification via HTTPRoute filters.
+// The test verifies that the RequestHeaderModifier filter adds headers to requests
+// sent to the backend.
+var RequestHeaderModifier = confsuite.ConformanceTest{
+	ShortName:   "RequestHeaderModifier",
+	Description: "An HTTPRoute with request header modifier filters adds headers to backend requests.",
+	Features: []features.FeatureName{
+		features.SupportGateway,
+		features.SupportHTTPRoute,
+	},
+	Manifests: []string{"testdata/header-modifiers-http-route.yaml"},
+	Test: func(t *testing.T, s *confsuite.ConformanceTestSuite) {
+		gwNN := types.NamespacedName{Name: gatewayName, Namespace: testNamespace}
+		routeNN := types.NamespacedName{Name: routeName, Namespace: testNamespace}
+
+		gwAddr := kubernetes.GatewayAndHTTPRoutesMustBeAccepted(
+			t, s.Client, s.TimeoutConfig, s.ControllerName,
+			kubernetes.NewGatewayRef(gwNN), routeNN,
+		)
+		kubernetes.HTTPRouteMustHaveResolvedRefsConditionsTrue(
+			t, s.Client, s.TimeoutConfig, routeNN, gwNN,
+		)
+
+		testCases := []http.ExpectedResponse{
+			{
+				Request: http.Request{
+					Host: routeHostname,
+					Path: "/set",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/set",
+						Headers: map[string]string{
+							"X-Test-Header": "test-value",
+						},
+					},
+				},
+				Backend:   echoBackend,
+				Namespace: testNamespace,
+			},
+			{
+				Request: http.Request{
+					Host: routeHostname,
+					Path: "/add",
+				},
+				ExpectedRequest: &http.ExpectedRequest{
+					Request: http.Request{
+						Path: "/add",
+						Headers: map[string]string{
+							"X-Added-Header": "added-value",
+						},
+					},
+				},
+				Backend:   echoBackend,
+				Namespace: testNamespace,
+			},
+		}
+
+		for i, tc := range testCases {
+			t.Run(tc.Request.Path, func(t *testing.T) {
+				http.MakeRequestAndExpectEventuallyConsistentResponse(
+					t, s.RoundTripper, s.TimeoutConfig,
+					gwAddr, testCases[i],
+				)
+			})
+		}
+	},
+}
