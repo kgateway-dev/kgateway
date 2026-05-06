f0126b48fbdb99054e54a70faaeaf22320df3e04
omar <omar.hammami@solo.io>
2026-05-06 14:07:10 -0500

    update validation for backendconfig policy
    Signed-off-by: omar <omar.hammami@solo.io>

--- api/v1alpha1/kgateway/backend_config_policy_types.go
+++ api/v1alpha1/kgateway/backend_config_policy_types.go
@@ -253,7 +253,7 @@ type Http2ProtocolOptions struct {
 
 	// The maximum number of concurrent streams that the connection can have.
 	// +optional
-	// +kubebuilder:validation:Minimum=0
+	// +kubebuilder:validation:Minimum=1
 	MaxConcurrentStreams *int32 `json:"maxConcurrentStreams,omitempty"`
 
 	// Allows invalid HTTP messaging and headers. When disabled (default), then
