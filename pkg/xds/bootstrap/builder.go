27962d128e8b0b3de8ed7eb380dadde5ce52c659
David Jumani <dj.davidjumani1994@gmail.com>
2026-05-07 16:46:30 -0400

    revert changes to ConfigBuilder
    Signed-off-by: David Jumani <dj.davidjumani1994@gmail.com>

--- pkg/xds/bootstrap/builder.go
+++ pkg/xds/bootstrap/builder.go
@@ -2,7 +2,6 @@ package bootstrap
 
 import (
 	"fmt"
-	"net/http"
 
 	envoybootstrapv3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
 	envoyclusterv3 "github.com/envoyproxy/go-control-plane/envoy/config/cluster/v3"
@@ -41,18 +40,7 @@ func (b *ConfigBuilder) AddFilterConfig(name string, config proto.Message) {
 
 // AddRoute adds a route to the builder.
 func (b *ConfigBuilder) AddRoute(route *envoyroutev3.Route) {
-	addedRoute := route
-	if route.GetAction() == nil {
-		// This is a delegating route. Envoy validation will fail if a route does not have an action, so add one now
-		clonedRoute := proto.Clone(route)
-		addedRoute = clonedRoute.(*envoyroutev3.Route)
-		addedRoute.Action = &envoyroutev3.Route_DirectResponse{
-			DirectResponse: &envoyroutev3.DirectResponseAction{
-				Status: http.StatusNotImplemented,
-			},
-		}
-	}
-	b.routes = append(b.routes, addedRoute)
+	b.routes = append(b.routes, route)
 }
 
 // AddCluster adds a cluster to the builder.
