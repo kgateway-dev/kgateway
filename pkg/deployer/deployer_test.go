aecb36470502b6695cbde22282c097da92834129
David Jumani <dj.davidjumani1994@gmail.com>
2026-06-11 19:26:02 -0400

    fix test
    Signed-off-by: David Jumani <dj.davidjumani1994@gmail.com>

--- pkg/deployer/deployer_test.go
+++ pkg/deployer/deployer_test.go
@@ -2604,7 +2604,7 @@ var _ = Describe("DeployObjs", func() {
 				Name:      name,
 				Namespace: ns,
 				Labels: map[string]string{
-					"app.kubernetes.io/managed-by":  "kgateway",
+					"app.kubernetes.io/managed-by":  wellknown.DefaultGatewayControllerName,
 					wellknown.GatewayClassNameLabel: wellknown.DefaultGatewayClassName,
 					wellknown.GatewayNameLabel:      name,
 				},
