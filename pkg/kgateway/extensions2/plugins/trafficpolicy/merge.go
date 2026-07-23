bbb3786b2abef3a5416ab268aba2e2c8d5a462fb
David Jumani <dj.davidjumani1994@gmail.com>
2026-07-23 16:08:46 -0400

    fix
    Signed-off-by: David Jumani <dj.davidjumani1994@gmail.com>

--- pkg/kgateway/extensions2/plugins/trafficpolicy/merge.go
+++ pkg/kgateway/extensions2/plugins/trafficpolicy/merge.go
@@ -42,9 +42,6 @@ func (o TrafficPolicyMergeOpts) Merge(other TrafficPolicyMergeOpts) TrafficPolic
 	if merged.Transformation == "" {
 		merged.Transformation = other.Transformation
 	}
-	if merged.ACL == "" {
-		merged.ACL = other.ACL
-	}
 
 	return merged
 }
